8358d82397afdfa7b2936ee3b8583eb3e4f7270a
Igor Zubkov <igor.zubkov@gmail.com>
2016-05-05 00:51:35 +0300

    Validate email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :email, presence: true
+
+  validates :email, uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 describe User do
   it { should have_secure_password }
+
+  it { should validate_presence_of(:email) }
+
+  it { should validate_uniqueness_of(:email).case_insensitive }
 end
