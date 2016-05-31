18b38ed2aa5b827b7d7ebc0039043e922a9dc7e2
Igor Zubkov <igor.zubkov@gmail.com>
2016-05-31 21:03:50 +0300

    Basic Api::BaseController

---
+++ app/controllers/api/base_controller.rb
@@ -0,0 +1,51 @@
+module Api
+  class BaseController < ActionController::Base
+    skip_before_action :verify_authenticity_token
+
+    before_action :authenticate
+
+    attr_reader :current_user
+
+    helper_method :parent, :collection, :resource, :current_user
+
+    def create
+      build_resource
+
+      resource.save!
+    end
+
+    def update
+      resource.update!(resource_params)
+    end
+
+    def destroy
+      resource.destroy!
+
+      head :ok
+    end
+
+    rescue_from ActionController::ParameterMissing do |exception|
+      @exception = exception
+
+      render :exception, status: :unprocessable_entity
+    end
+
+    rescue_from ActiveRecord::RecordInvalid, ActiveModel::StrictValidationFailed do
+      render :errors, status: :unprocessable_entity
+    end
+
+    rescue_from ActiveRecord::RecordNotFound do |exception|
+      @exception = exception
+
+      render :exception, status: :not_found
+    end
+
+    private
+
+    def authenticate
+      authenticate_or_request_with_http_token do |token, options|
+        @current_user = User.find_by(token: token)
+      end
+    end
+  end
+end
