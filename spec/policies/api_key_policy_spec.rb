c3427992052cc19c0f296eae4844f62a1955a36b
Igor Zubkov <igor.zubkov@gmail.com>
2016-07-29 23:56:45 +0300

    Cleanup

--- spec/policies/api_key_policy_spec.rb
+++ spec/policies/api_key_policy_spec.rb
@@ -1,42 +1,25 @@
 require 'rails_helper'
 
 describe ApiKeyPolicy do
-  context 'owner' do
-    let(:user) { stub_model User, id: 42 }
+  let(:user) { double }
 
-    let(:api_key) { stub_model ApiKey, user_id: 42 }
+  let(:record) { double }
 
-    before { allow(user).to receive(:api_keys).and_return([api_key]) }
+  subject { described_class.new(user, record) }
 
-    subject { ApiKeyPolicy.new(user, api_key) }
+  describe '#initialize' do
+    its(:user) { should eq(user) }
 
-    specify { should permit_action(:create) }
+    its(:record) { should eq(record) }
+  end
 
-    specify { should permit_action(:show) }
+  describe '#index?' do
+    specify { expect(subject.index?).to eq(false) }
+  end
 
-    specify { should permit_action(:destroy) }
-
-    specify { should forbid_action(:update) }
+  describe '#create?' do
+    specify { expect(subject.create?).to eq(true) }
   end
 
 
-
-  # [:index, :create, :show, :destroy]
-
-  # context 'not owner' do
-  #
-  # end
-  #
-  #
-  # let(:user) { stub_model User }
-  #
-  # let(:api_key) { stub_model ApiKey }
-  #
-  # subject { ApiKeyPolicy.new(user, api_key) }
-  #
-  # it { should permit_action(:show) }
-  #
-  # it { should permit_action(:show) }
-  #
-  # it { should permit_action(:destroy) }
 end
