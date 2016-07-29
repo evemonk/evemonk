fffedad9a2d4700d3484fbd612da5116d7619395
Igor Zubkov <igor.zubkov@gmail.com>
2016-07-29 23:56:27 +0300

    Update api key policy

--- app/policies/api_key_policy.rb
+++ app/policies/api_key_policy.rb
@@ -6,19 +6,48 @@ class ApiKeyPolicy
     @record = record
   end
 
+  def index?
+    false
+  end
+
+  def show?
+    scope.where(id: record.id).exists?
+  end
+
   def create?
     true
   end
 
-  def show?
-    user.api_keys.include?(record)
+  def new?
+    create?
   end
 
   def update?
-    false
+    show?
+  end
+
+  def edit?
+    show?
   end
 
   def destroy?
-    user.api_keys.include?(record)
+    show?
+  end
+
+  def scope
+    Pundit.policy_scope!(user, ApiKey)
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope.where(user: user)
+    end
   end
 end
