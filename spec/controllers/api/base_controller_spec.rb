38c6bc6efd55d78c2fa07635add431f05064e08c
Igor Zubkov <igor.zubkov@gmail.com>
2016-07-30 01:13:09 +0300

    More specs

--- spec/controllers/api/base_controller_spec.rb
+++ spec/controllers/api/base_controller_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 describe Api::BaseController do
   it { should be_a ActionController::Base }
 
+  it { should be_a Pundit }
+
   it { should_not use_before_action(:verify_authenticity_token) }
 
   it { should use_before_action(:authenticate) }
+
+  pending { should use_after_action(:verify_authorized).except(:index) }
+
+  pending { should use_after_action(:verify_policy_scoped).only(:index) }
 
   # TODO: spec all this
   #
@@ -13,11 +19,27 @@ describe Api::BaseController do
   #
   # helper_method :parent, :collection, :resource, :current_user
   #
+  # def show
+  #   authorize(resource)
+  # end
+  #
+  # def create
+  #   build_resource
+  #
+  #   authorize(resource)
+  #
+  #   resource.save!
+  # end
+  #
   # def update
+  #   authorize(resource)
+  #
   #   resource.update!(resource_params)
   # end
   #
   # def destroy
+  #   authorize(resource)
+  #
   #   resource.destroy!
   #
   #   head :ok
@@ -48,5 +70,21 @@ describe Api::BaseController do
     end
 
     its(:authenticate) { should eq(user) }
+  end
+
+  describe '#resource' do
+    specify { expect { subject.send(:resource) }.to raise_error(NotImplementedError) }
+  end
+
+  describe '#resource_params' do
+    specify { expect { subject.send(:resource_params) }.to raise_error(NotImplementedError) }
+  end
+
+  describe '#build_resource' do
+    specify { expect { subject.send(:build_resource) }.to raise_error(NotImplementedError) }
+  end
+
+  describe '#collection' do
+    specify { expect { subject.send(:collection) }.to raise_error(NotImplementedError) }
   end
 end
