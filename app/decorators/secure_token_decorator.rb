ae6037e2dfdf649e7d557ad5a36942e0536418e9
Igor Zubkov <igor.zubkov@gmail.com>
2016-12-07 01:19:14 +0200

    Add secure token decorator

---
+++ app/decorators/secure_token_decorator.rb
@@ -0,0 +1,22 @@
+class SecureTokenDecorator < Draper::Decorator
+  delegate_all
+
+  def as_json(*)
+    {
+      id: id,
+      token: token,
+      created_at: created_at,
+      updated_at: updated_at
+    }
+  end
+
+  private
+
+  def created_at
+    model.created_at.iso8601
+  end
+
+  def updated_at
+    model.updated_at.iso8601
+  end
+end
---
+++ spec/decorators/secure_token_decorator_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+describe SecureTokenDecorator do
+  describe '#as_json' do
+    let(:secure_token) do
+      stub_model SecureToken,
+                 id: 1234,
+                 token: 'token123'
+    end
+
+    let(:created_at) { double }
+
+    let(:updated_at) { double }
+
+    let(:decorated_object) { secure_token.decorate }
+
+    before { expect(decorated_object).to receive(:created_at).and_return(created_at) }
+
+    before { expect(decorated_object).to receive(:updated_at).and_return(updated_at) }
+
+    subject { decorated_object.as_json }
+
+    its([:id]) { should eq(1234) }
+
+    its([:token]) { should eq('token123') }
+
+    its([:created_at]) { should eq(created_at) }
+
+    its([:updated_at]) { should eq(updated_at) }
+  end
+
+  # private methods
+
+  describe '#created_at' do
+    let(:secure_token) { stub_model SecureToken }
+
+    subject { secure_token.decorate }
+
+    before do
+      #
+      # subject.model.created_at.iso8601
+      #
+      expect(subject).to receive(:model) do
+        double.tap do |a|
+          expect(a).to receive(:created_at) do
+            double.tap do |b|
+              expect(b).to receive(:iso8601)
+            end
+          end
+        end
+      end
+    end
+
+    specify { expect { subject.send(:created_at) }.not_to raise_error }
+  end
+
+  describe '#updated_at' do
+    let(:secure_token) { stub_model SecureToken }
+
+    subject { secure_token.decorate }
+
+    before do
+      #
+      # subject.model.updated_at.iso8601
+      #
+      expect(subject).to receive(:model) do
+        double.tap do |a|
+          expect(a).to receive(:updated_at) do
+            double.tap do |b|
+              expect(b).to receive(:iso8601)
+            end
+          end
+        end
+      end
+    end
+
+    specify { expect { subject.send(:updated_at) }.not_to raise_error }
+  end
+end
