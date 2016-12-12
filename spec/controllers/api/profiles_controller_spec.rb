e035903160d72f4c6696a9da3035d44f62b0ce16
Igor Zubkov <igor.zubkov@gmail.com>
2016-12-12 21:22:39 +0200

    More specs

--- spec/controllers/api/profiles_controller_spec.rb
+++ spec/controllers/api/profiles_controller_spec.rb
@@ -1,6 +1,32 @@
 require 'rails_helper'
 
 describe Api::ProfilesController do
+  describe '#show.json' do
+    context 'authorized' do
+      before { expect(subject).to receive(:verify_authorized).and_return(true) }
+
+      let(:user) { double }
+
+      before { expect(subject).to receive(:resource).and_return(user) }
+
+      before { expect(subject).to receive(:authorize).with(user) }
+
+      before { sign_in }
+
+      before { get :show, format: :json }
+
+      it { should render_template(:show) }
+
+      it { should respond_with(:ok) }
+    end
+
+    context 'not authorized' do
+      before { get :show, format: :json }
+
+      it { should respond_with(:unauthorized) }
+    end
+  end
+
   # private methods
 
   describe '#resource' do
