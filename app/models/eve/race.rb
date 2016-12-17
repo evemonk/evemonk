8fa2d0c30628660cbaecb04bebe4ea7910c01c01
Igor Zubkov <igor.zubkov@gmail.com>
2016-12-17 16:29:41 +0200

    Add spec

--- app/models/eve/race.rb
+++ app/models/eve/race.rb
@@ -4,7 +4,7 @@ module Eve
 
     validates :race_id, presence: true
 
-    validates :race_id, numericality: { only_integer: true, greater_than: 0 }
+    validates :race_id, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
     validates :race_name, presence: true
 
