55dc4a735654078996257b21dbef3726b0a59948
Igor Zubkov <igor.zubkov@gmail.com>
2017-01-13 23:53:26 +0200

    Remove broken migration

--- db/migrate/20170113211236_add_timestamps_to_agt_agent_types.rb
+++
@@ -1,6 +0,0 @@
-class AddTimestampsToAgtAgentTypes < ActiveRecord::Migration[5.0]
-  def change
-    add_column :agt_agent_types, :created_at, :datetime, null: false
-    add_column :agt_agent_types, :updated_at, :datetime, null: false
-  end
-end
