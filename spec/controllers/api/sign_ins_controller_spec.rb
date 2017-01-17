e1961fa4858d58f12890f2d5966a88a537e32f3c
Igor Zubkov <igor.zubkov@gmail.com>
2017-01-17 23:29:06 +0200

    More specs

--- spec/controllers/api/sign_ins_controller_spec.rb
+++ spec/controllers/api/sign_ins_controller_spec.rb
@@ -34,4 +34,49 @@ describe Api::SignInsController do
       it { should respond_with(:unprocessable_entity) }
     end
   end
+
+  # private methods
+
+  describe '#build_resource' do
+    let(:resource_params) { double }
+
+    let(:sign_in) { double }
+
+    before { expect(subject).to receive(:resource_params).and_return(resource_params) }
+
+    before { expect(Api::SignIn).to receive(:new).with(resource_params).and_return(sign_in) }
+
+    specify { expect { subject.send(:build_resource) }.not_to raise_error }
+
+    specify { expect { subject.send(:build_resource) }.to change { subject.instance_variable_get(:@sign_in) }.from(nil).to(sign_in) }
+  end
+
+  describe '#resource' do
+    let(:sign_in) { double }
+
+    before { subject.instance_variable_set(:@sign_in, sign_in) }
+
+    specify { expect(subject.send(:resource)).to eq(sign_in) }
+  end
+
+  describe '#resource_params' do
+    before do
+      #
+      # subject.params.require(:sign_ins)
+      #               .permit(:email, :password, :name, :device,
+      #                       :device_token)
+      #
+      expect(subject).to receive(:params) do
+        double.tap do |a|
+          expect(a).to receive(:require).with(:sign_ins) do
+            double.tap do |b|
+              expect(b).to receive(:permit).with(:email, :password, :name, :device, :device_token)
+            end
+          end
+        end
+      end
+    end
+
+    specify { expect { subject.send(:resource_params) }.not_to raise_error }
+  end
 end
