0c2d3281871751f9d867cdb85938d8c0c49f3f79
Igor Zubkov <igor.zubkov@gmail.com>
2017-02-07 21:37:44 +0200

    Use inline activejob adapter for test env and sidekiq for all other

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,5 @@ module Evemonk
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    config.active_job.queue_adapter = :sidekiq
   end
 end
