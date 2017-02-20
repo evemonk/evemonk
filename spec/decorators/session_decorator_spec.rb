3b89c02cdb7a394e9c0bc1705419bce37fc42b5d
Igor Zubkov <igor.zubkov@gmail.com>
2017-02-20 20:59:52 +0200

    Use session factory

--- spec/decorators/session_decorator_spec.rb
+++ spec/decorators/session_decorator_spec.rb
@@ -2,35 +2,18 @@ require 'rails_helper'
 
 describe SessionDecorator do # rubocop:disable Metrics/BlockLength
   describe '#as_json' do # rubocop:disable Metrics/BlockLength
-    let(:session) do
-      stub_model Session,
-                 id: 1234,
-                 token: 'token123',
-                 name: 'My Computer',
-                 device: 'ios',
-                 device_token: 'device-token123'
-    end
-
-    let(:created_at) { double }
-
-    let(:updated_at) { double }
-
-    before { expect(decorated_object).to receive(:created_at).and_return(created_at) }
-
-    before { expect(decorated_object).to receive(:updated_at).and_return(updated_at) }
-
-    subject { decorated_object.as_json }
+    let!(:session) { create(:session) }
 
     context 'with_token: false' do
-      let(:decorated_object) { session.decorate(context: { with_token: false }) }
+      subject { session.decorate(context: { with_token: false }).as_json }
 
-      its([:id]) { should eq(1234) }
+      its([:id]) { should eq(session.id) }
 
-      its([:name]) { should eq('My Computer') }
+      its([:name]) { should eq(session.name) }
 
-      its([:created_at]) { should eq(created_at) }
+      its([:created_at]) { should eq(session.created_at.iso8601) }
 
-      its([:updated_at]) { should eq(updated_at) }
+      its([:updated_at]) { should eq(session.updated_at.iso8601) }
 
       its([:token]) { should eq(nil) }
 
@@ -40,69 +23,21 @@ describe SessionDecorator do # rubocop:disable Metrics/BlockLength
     end
 
     context 'with_token: true' do
-      let(:decorated_object) { session.decorate(context: { with_token: true }) }
+      subject { session.decorate(context: { with_token: true }).as_json }
 
-      its([:id]) { should eq(1234) }
+      its([:id]) { should eq(session.id) }
 
-      its([:name]) { should eq('My Computer') }
+      its([:name]) { should eq(session.name) }
 
-      its([:created_at]) { should eq(created_at) }
+      its([:created_at]) { should eq(session.created_at.iso8601) }
 
-      its([:updated_at]) { should eq(updated_at) }
+      its([:updated_at]) { should eq(session.updated_at.iso8601) }
 
-      its([:token]) { should eq('token123') }
+      its([:token]) { should eq(session.token) }
 
-      its([:device]) { should eq('ios') }
+      its([:device]) { should eq(session.device) }
 
-      its([:device_token]) { should eq('device-token123') }
+      its([:device_token]) { should eq(session.device_token) }
     end
-  end
-
-  # private methods
-
-  describe '#created_at' do
-    let(:session) { stub_model Session }
-
-    subject { session.decorate }
-
-    before do
-      #
-      # subject.model.created_at.iso8601
-      #
-      expect(subject).to receive(:model) do
-        double.tap do |a|
-          expect(a).to receive(:created_at) do
-            double.tap do |b|
-              expect(b).to receive(:iso8601)
-            end
-          end
-        end
-      end
-    end
-
-    specify { expect { subject.send(:created_at) }.not_to raise_error }
-  end
-
-  describe '#updated_at' do
-    let(:session) { stub_model Session }
-
-    subject { session.decorate }
-
-    before do
-      #
-      # subject.model.updated_at.iso8601
-      #
-      expect(subject).to receive(:model) do
-        double.tap do |a|
-          expect(a).to receive(:updated_at) do
-            double.tap do |b|
-              expect(b).to receive(:iso8601)
-            end
-          end
-        end
-      end
-    end
-
-    specify { expect { subject.send(:updated_at) }.not_to raise_error }
   end
 end
