3c724daac5b516fa7de8dcfa0c74e2b3a5649b3b
Igor Zubkov <igor.zubkov@gmail.com>
2017-04-17 23:30:56 +0300

    Cleanup

--- spec/jobs/api_key_validate_job_spec.rb
+++ spec/jobs/api_key_validate_job_spec.rb
@@ -19,25 +19,4 @@ describe ApiKeyValidateJob do
 
     specify { expect { subject.perform(api_key.id) }.not_to raise_error }
   end
-
-  # def perform(api_key_id)
-  #   api_key = ApiKey.find(api_key_id)
-  #
-  #   Api::ApiKeyValidate.new(api_key).call
-  # end
-  
-  
-  # describe '#perform' do
-  #   let(:api_key_id) { double }
-  #
-  #   before do
-  #     #
-  #     # ValidateApiKey.call(api_key_id)
-  #     #
-  #     expect(ValidateApiKey).to receive(:call).with(api_key_id)
-  #   end
-  #
-  #   specify { expect { subject.perform(api_key_id) }.not_to raise_error }
-  # end
-
 end
