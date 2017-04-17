87c65765d7b9fe7c2a871781590a1e79916a3ede
Igor Zubkov <igor.zubkov@gmail.com>
2017-04-17 23:45:00 +0300

    Add db schema

---
+++ db/schema.rb
@@ -0,0 +1,186 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170113214508) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "agt_agent_types", force: :cascade do |t|
+    t.string   "agent_type"
+    t.integer  "agent_type_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "agt_agents", force: :cascade do |t|
+    t.integer  "agent_id"
+    t.integer  "agent_type_id"
+    t.integer  "corporation_id"
+    t.integer  "division_id"
+    t.boolean  "is_locator"
+    t.integer  "level"
+    t.integer  "location_id"
+    t.integer  "quality"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "agt_research_agents", force: :cascade do |t|
+    t.integer  "agent_id"
+    t.integer  "type_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "api_keys", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "key_id"
+    t.string   "v_code"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.datetime "expires"
+    t.string   "api_key_type"
+    t.bigint   "access_mask"
+    t.datetime "current_time"
+    t.datetime "cached_until"
+  end
+
+  create_table "chr_races", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "race_id"
+    t.string   "race_name"
+    t.string   "short_description"
+    t.integer  "icon_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
+
+  create_table "inv_flags", force: :cascade do |t|
+    t.integer  "flag_id"
+    t.string   "flag_name"
+    t.string   "flag_text"
+    t.integer  "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inv_items", force: :cascade do |t|
+    t.integer  "flag_id"
+    t.integer  "item_id"
+    t.integer  "location_id"
+    t.integer  "owner_id"
+    t.integer  "quantity"
+    t.integer  "type_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "inv_names", force: :cascade do |t|
+    t.integer  "item_id"
+    t.string   "item_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inv_positions", force: :cascade do |t|
+    t.integer  "item_id"
+    t.float    "pitch"
+    t.float    "roll"
+    t.float    "x"
+    t.float    "y"
+    t.float    "yaw"
+    t.float    "z"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rpush_apps", force: :cascade do |t|
+    t.string   "name",                                null: false
+    t.string   "environment"
+    t.text     "certificate"
+    t.string   "password"
+    t.integer  "connections",             default: 1, null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "type",                                null: false
+    t.string   "auth_key"
+    t.string   "client_id"
+    t.string   "client_secret"
+    t.string   "access_token"
+    t.datetime "access_token_expiration"
+  end
+
+  create_table "rpush_feedback", force: :cascade do |t|
+    t.string   "device_token", limit: 64, null: false
+    t.datetime "failed_at",               null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "app_id"
+    t.index ["device_token"], name: "index_rpush_feedback_on_device_token", using: :btree
+  end
+
+  create_table "rpush_notifications", force: :cascade do |t|
+    t.integer  "badge"
+    t.string   "device_token",      limit: 64
+    t.string   "sound",                        default: "default"
+    t.text     "alert"
+    t.text     "data"
+    t.integer  "expiry",                       default: 86400
+    t.boolean  "delivered",                    default: false,     null: false
+    t.datetime "delivered_at"
+    t.boolean  "failed",                       default: false,     null: false
+    t.datetime "failed_at"
+    t.integer  "error_code"
+    t.text     "error_description"
+    t.datetime "deliver_after"
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
+    t.boolean  "alert_is_json",                default: false
+    t.string   "type",                                             null: false
+    t.string   "collapse_key"
+    t.boolean  "delay_while_idle",             default: false,     null: false
+    t.text     "registration_ids"
+    t.integer  "app_id",                                           null: false
+    t.integer  "retries",                      default: 0
+    t.string   "uri"
+    t.datetime "fail_after"
+    t.boolean  "processing",                   default: false,     null: false
+    t.integer  "priority"
+    t.text     "url_args"
+    t.string   "category"
+    t.boolean  "content_available",            default: false
+    t.text     "notification"
+    t.index ["delivered", "failed"], name: "index_rpush_notifications_multi", where: "((NOT delivered) AND (NOT failed))", using: :btree
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "token"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "name"
+    t.string   "device_token"
+    t.integer  "device"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.integer  "notifications_count", default: 0
+  end
+
+  add_foreign_key "api_keys", "users"
+  add_foreign_key "sessions", "users"
+end
