a1f159265efd6efeaaf03fa838d1088f47e17aa9
Igor Zubkov <igor.zubkov@gmail.com>
2017-05-02 23:25:34 +0300

    Set default value for ApiKey#status

--- db/migrate/20170502201222_add_status_to_api_keys.rb
+++ db/migrate/20170502201222_add_status_to_api_keys.rb
@@ -1,5 +1,5 @@
 class AddStatusToApiKeys < ActiveRecord::Migration[5.1]
   def change
-    add_column :api_keys, :status, :integer
+    add_column :api_keys, :status, :integer, default: ApiKey.statuses[:fresh]
   end
 end
