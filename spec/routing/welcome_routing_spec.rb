354c91e0078dd2c89c2956e8ce4a83709316d43c
Igor Zubkov <igor.zubkov@gmail.com>
2017-08-20 01:35:12 +0300

    WelcomeController -> MainController

--- spec/routing/welcome_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe WelcomeController do
-  describe 'routing' do
-    it 'should route / to welcome#index' do
-      expect(get: '/').to route_to('welcome#index')
-    end
-  end
-end
