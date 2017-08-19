74f07689625eba2bf808687406417bc66c63f0b3
Igor Zubkov <igor.zubkov@gmail.com>
2017-08-20 00:34:07 +0300

    Configure

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,9 @@
 //= require underscore
 //= require backbone
 //= require handlebars.runtime
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree ./templates
 //= require_tree .
