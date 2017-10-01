1696de070e2c2c0b77cc9d0176d6f0641c356ca0
Igor Zubkov <igor.zubkov@gmail.com>
2017-10-01 15:36:33 +0300

    More specs

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,6 +1,13 @@
 FactoryGirl.define do
   factory :user do
+    kind :normal
     email { Faker::Internet.email }
     password { Faker::Internet.password }
+
+    trait :oauth do
+      kind :oauth
+      email nil
+      password nil
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,27 @@ require 'rails_helper'
 describe User do
   it { should be_a(ApplicationRecord) }
 
+  it { should define_enum_for(:kind).with([:normal, :oauth]) }
+
   it { should have_many(:sessions).dependent(:destroy) }
 
   it { should have_many(:characters).dependent(:destroy) }
 
-  it { should validate_presence_of(:email) }
+  context 'normal user' do
+    subject { build(:user) }
 
-  it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_presence_of(:email) }
+
+    it { should validate_uniqueness_of(:email).case_insensitive }
+  end
+
+  context 'oauth user' do
+    subject { build(:user, :oauth) }
+
+    it { should_not validate_presence_of(:email) }
+
+    it { should_not validate_uniqueness_of(:email).case_insensitive }
+  end
 
   it { should have_secure_password }
 end
