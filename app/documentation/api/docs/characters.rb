7eb4769aea851775c648f5680aec1e9c30ed5a3b
Igor Zubkov <igor.zubkov@gmail.com>
2017-10-02 18:19:44 +0300

    Update swagger

---
+++ app/documentation/api/docs/characters.rb
@@ -0,0 +1,70 @@
+module Api
+  module Docs
+    class Characters
+      # :nocov:
+      include Swagger::Blocks
+
+      swagger_path '/characters' do
+        operation :get do
+          key :summary, 'Get all user characters'
+          key :description, 'Get all user characters'
+          key :tags, ['characters']
+          parameter :authorization
+          parameter :page
+          response '200' do
+            key :description, 'Success'
+            schema do
+              key :'$ref', :OutputCharactersCollection
+            end
+          end
+          extend Api::Docs::Shared::Unauthorized
+          extend Api::Docs::Shared::NotAcceptable
+        end
+      end
+
+      swagger_path '/characters/{id}' do
+        operation :get do
+          key :summary, 'Get character by ID'
+          key :description, 'Get character by ID'
+          key :tags, ['characters']
+          parameter :authorization
+          parameter do
+            key :name, :id
+            key :in, :path
+            key :required, true
+            key :type, :integer
+            key :description, 'Session ID'
+          end
+          response '200' do
+            key :description, 'Success'
+            schema do
+              key :'$ref', :OutputCharacter
+            end
+          end
+          extend Api::Docs::Shared::Unauthorized
+          extend Api::Docs::Shared::NotAcceptable
+        end
+
+        operation :delete do
+          key :summary, 'Remove character by ID'
+          key :description, 'Remove character by ID'
+          key :tags, ['characters']
+          parameter :authorization
+          parameter do
+            key :name, :id
+            key :in, :path
+            key :required, true
+            key :type, :integer
+            key :description, 'Session ID'
+          end
+          response '200' do
+            key :description, 'Success'
+          end
+          extend Api::Docs::Shared::Unauthorized
+          extend Api::Docs::Shared::NotFound
+        end
+      end
+      # :nocov:
+    end
+  end
+end
