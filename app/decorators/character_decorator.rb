a683a600f9c89f2de551026fed7578a348f7e19a
Igor Zubkov <igor.zubkov@gmail.com>
2017-10-06 23:42:09 +0300

    Comment

--- app/decorators/character_decorator.rb
+++ app/decorators/character_decorator.rb
@@ -6,21 +6,22 @@ class CharacterDecorator < Draper::Decorator
       id: id,
       uid: uid,
       name: name,
-      corporation_id: corporation_id,
-      birthday: birthday,
-      gender: gender,
-      race_id: race_id,
-      bloodline_id: bloodline_id,
-      description: description,
-      alliance_id: alliance_id,
-      ancestry_id: ancestry_id,
-      security_status: security_status,
-      portrait: {
-        small: portrait_small,
-        medium: portrait_medium,
-        large: portrait_large,
-        huge: portrait_huge
-      },
+      # TODO: later
+      # corporation_id: corporation_id,
+      # birthday: birthday,
+      # gender: gender,
+      # race_id: race_id,
+      # bloodline_id: bloodline_id,
+      # description: description,
+      # alliance_id: alliance_id,
+      # ancestry_id: ancestry_id,
+      # security_status: security_status,
+      # portrait: {
+      #   small: portrait_small,
+      #   medium: portrait_medium,
+      #   large: portrait_large,
+      #   huge: portrait_huge
+      # },
       created_at: created_at.iso8601,
       updated_at: updated_at.iso8601
     }
