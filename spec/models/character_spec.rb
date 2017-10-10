420f9726ec56d3c96d80e31870829f0786431a78
Igor Zubkov <igor.zubkov@gmail.com>
2017-10-10 14:46:49 +0300

    Character#race. Make optional.

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 describe Character do
   it { should belong_to(:user) }
 
-  it { should belong_to(:race).class_name('Eve::Race').with_primary_key(:race_id) }
+  it { should belong_to(:race).class_name('Eve::Race').with_primary_key(:race_id) # .optional(true) }
 end
