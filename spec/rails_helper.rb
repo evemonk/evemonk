e69ba14ecc346cab1b065374322affa0ed0cef66
Igor Zubkov <igor.zubkov@gmail.com>
2017-10-19 17:05:16 +0300

    Enable --only-failures rspec feature

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,7 +22,7 @@ require 'rspec/rails'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-# Checks for pending migration and applies them before tests are run.
+# Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
