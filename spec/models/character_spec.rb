7b22e195c5c9431e4d95d06b58ca843d020608d1
Igor Zubkov <igor.zubkov@gmail.com>
2017-10-19 17:02:29 +0300

    One more spec

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe Character do
+  it { should be_a(ApplicationRecord) }
+
   it { should belong_to(:user) }
 
   it { should belong_to(:race).class_name('Eve::Race').with_primary_key(:race_id) } # .optional(true) }
