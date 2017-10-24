9873b6926c4337157b09bd581c87ee01289ec6d1
Igor Zubkov <igor.zubkov@gmail.com>
2017-10-24 21:37:36 +0300

    Add character attributes

---
+++ db/migrate/20171024183504_add_attributes_to_characters.rb
@@ -0,0 +1,12 @@
+class AddAttributesToCharacters < ActiveRecord::Migration[5.1]
+  def change
+    add_column :characters, :charisma, :integer
+    add_column :characters, :intelligence, :integer
+    add_column :characters, :memory, :integer
+    add_column :characters, :perception, :integer
+    add_column :characters, :willpower, :integer
+    add_column :characters, :bonus_remaps, :integer
+    add_column :characters, :last_remap_date, :datetime
+    add_column :characters, :accrued_remap_cooldown_date, :datetime
+  end
+end
