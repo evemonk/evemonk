f3d7de3fba088571ccd2366fa28ebae69d127dae
Igor Zubkov <igor.zubkov@gmail.com>
2017-12-03 17:33:45 +0200

    Update for rails 5.2

---
+++ config/initializers/bullet.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+if defined? Bullet
+  Bullet.enable = true
+  Bullet.bullet_logger = true
+  Bullet.rails_logger = true
+end
--- config/initializers/rpush.rb
+++ config/initializers/rpush.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rpush.configure do |config|
 
   # Supported clients are :active_record and :redis
