8bb5cb1fd347d46ce5f52513f736fa011a09b89b
Igor Zubkov <igor.zubkov@gmail.com>
2018-02-21 23:11:00 +0200

    Indent

--- spec/decorators/eve/race_decorator_spec.rb
+++ spec/decorators/eve/race_decorator_spec.rb
@@ -2,24 +2,26 @@
 
 require 'rails_helper'
 
-describe Eve::RaceDecorator do
-  describe '#as_json' do
-    let!(:race) do
-      create(:eve_race,
-             description: 'Once a thriving tribal civilization, the Minmatar...',
-             race_id: 2,
-             name: 'Minmatar',
-             alliance_id: 500_002)
+module Eve
+  describe RaceDecorator do
+    describe '#as_json' do
+      let!(:race) do
+        create(:eve_race,
+               description: 'Once a thriving tribal civilization, the Minmatar...',
+               race_id: 2,
+               name: 'Minmatar',
+               alliance_id: 500_002)
+      end
+
+      subject { race.decorate.as_json }
+
+      its([:description]) { should eq('Once a thriving tribal civilization, the Minmatar...') }
+
+      its([:race_id]) { should eq(2) }
+
+      its([:name]) { should eq('Minmatar') }
+
+      its([:alliance_id]) { should eq(500_002) }
     end
-
-    subject { race.decorate.as_json }
-
-    its([:description]) { should eq('Once a thriving tribal civilization, the Minmatar...') }
-
-    its([:race_id]) { should eq(2) }
-
-    its([:name]) { should eq('Minmatar') }
-
-    its([:alliance_id]) { should eq(500_002) }
   end
 end
