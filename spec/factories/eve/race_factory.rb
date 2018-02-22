d39ed4b01432e9d1a71df9056dfec38f5df0c7f0
Igor Zubkov <igor.zubkov@gmail.com>
2018-02-22 13:54:31 +0200

    Update factories

--- spec/factories/eve/race_factory.rb
+++ spec/factories/eve/race_factory.rb
@@ -2,11 +2,11 @@
 
 FactoryBot.define do
   factory :eve_race, class: Eve::Race do
-    description { Faker::Lorem.paragraph }
-
     sequence(:race_id)
 
     name { Faker::Lorem.word }
+
+    description { Faker::Lorem.paragraph }
 
     sequence(:alliance_id)
   end
