2b402499f0e7d6303e2229ba9742176d9e9a3b5e
Igor Zubkov <igor.zubkov@gmail.com>
2018-02-22 20:24:18 +0200

    Refactoring

--- app/services/api/sign_out.rb
+++ app/services/api/sign_out.rb
@@ -4,14 +4,12 @@ module Api
   class SignOut
     include ActionController::HttpAuthentication::Token
 
-    attr_reader :request
-
     def initialize(request)
       @request = request
     end
 
     def destroy!
-      token, = token_and_options(request)
+      token, = token_and_options(@request)
 
       session = Session.find_by!(token: token)
 
