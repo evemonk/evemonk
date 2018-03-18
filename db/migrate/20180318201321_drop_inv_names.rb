142d715cb7f909184c9a2c2235561f82bc630f81
Igor Zubkov <igor.zubkov@gmail.com>
2018-03-19 00:36:14 +0200

    Cleanup migrations

--- db/migrate/20180318201321_drop_inv_names.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class DropInvNames < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :inv_names
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_18_201321) do
+ActiveRecord::Schema.define(version: 2018_03_18_201126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
