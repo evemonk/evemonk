95f683e12a9a106685f59e8b4fbf0023dccd9463
Igor Zubkov <igor.zubkov@gmail.com>
2018-03-18 23:43:53 +0200

    Eve::BloodlineDecorator

---
+++ app/decorators/eve/bloodline_decorator.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Eve
+  class BloodlineDecorator < Draper::Decorator
+    delegate_all
+
+    def as_json(*)
+      {
+        bloodline_id: bloodline_id,
+        name: name,
+        description: description,
+        race_id: race_id,
+        ship_type_id: ship_type_id,
+        corporation_id: corporation_id,
+        perception: perception,
+        willpower: willpower,
+        charisma: charisma,
+        memory: memory,
+        intelligence: intelligence
+      }
+    end
+  end
+end
