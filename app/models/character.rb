ed917d27ad6ad42e70afd8d155b4321749e13b54
Igor Zubkov <igor.zubkov@gmail.com>
2018-03-18 22:55:06 +0200

    Character#bloodline

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,4 +4,6 @@ class Character < ApplicationRecord
   belongs_to :user
 
   belongs_to :race, class_name: 'Eve::Race', primary_key: :race_id, optional: true
+
+  belongs_to :bloodline, class_name: 'Eve::Bloodline', primary_key: :bloodline_id, optional: true
 end
--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -8,4 +8,6 @@ describe Character do
   it { should belong_to(:user) }
 
   it { should belong_to(:race).class_name('Eve::Race').with_primary_key(:race_id) } # .optional(true) }
+
+  it { should belong_to(:bloodline).class_name('Eve::Bloodline').with_primary_key(:bloodline_id) } # .optional(true) }
 end
