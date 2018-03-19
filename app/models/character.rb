ee14cd676f392b81c806115efe6b7b065451908c
Igor Zubkov <igor.zubkov@gmail.com>
2018-03-19 22:36:26 +0200

    Character#faction

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,4 +6,6 @@ class Character < ApplicationRecord
   belongs_to :race, class_name: 'Eve::Race', primary_key: :race_id, optional: true
 
   belongs_to :bloodline, class_name: 'Eve::Bloodline', primary_key: :bloodline_id, optional: true
+
+  belongs_to :faction, class_name: 'Eve::Faction', primary_key: :faction_id, optional: true
 end
