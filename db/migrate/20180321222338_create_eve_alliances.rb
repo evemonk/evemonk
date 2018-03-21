58117a4b5c38c22fb02bde3c37763efca632bfe3
Igor Zubkov <igor.zubkov@gmail.com>
2018-03-22 00:25:38 +0200

    Add alliances migration

---
+++ db/migrate/20180321222338_create_eve_alliances.rb
@@ -0,0 +1,16 @@
+class CreateEveAlliances < ActiveRecord::Migration[5.2]
+  def change
+    create_table :eve_alliances do |t|
+      t.integer :alliance_id
+      t.string :name
+      t.integer :creator_id
+      t.integer :creator_corporation_id
+      t.string :ticker
+      t.integer :executor_corporation_id
+      t.datetime :date_founded
+      t.integer :faction_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_20_230215) do
+ActiveRecord::Schema.define(version: 2018_03_21_222338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,19 @@ ActiveRecord::Schema.define(version: 2018_03_20_230215) do
     t.datetime "accrued_remap_cooldown_date"
     t.integer "faction_id"
     t.index ["user_id"], name: "index_characters_on_user_id"
+  end
+
+  create_table "eve_alliances", force: :cascade do |t|
+    t.integer "alliance_id"
+    t.string "name"
+    t.integer "creator_id"
+    t.integer "creator_corporation_id"
+    t.string "ticker"
+    t.integer "executor_corporation_id"
+    t.datetime "date_founded"
+    t.integer "faction_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "eve_ancestries", force: :cascade do |t|
