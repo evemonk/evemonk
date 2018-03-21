48cdf412b4d44c2c28e76ba07e51febe9a0b3fd1
Igor Zubkov <igor.zubkov@gmail.com>
2018-03-21 23:12:24 +0200

    More validations

--- app/models/eve/faction.rb
+++ app/models/eve/faction.rb
@@ -2,5 +2,28 @@
 
 module Eve
   class Faction < ApplicationRecord
+    validates :faction_id, presence: true
+
+    validates :faction_id, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+    validates :name, presence: true
+
+    validates :description, presence: true
+
+    validates :solar_system_id, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+    validates :corporation_id, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+    validates :militia_corporation_id, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+    validates :size_factor, presence: true
+
+    validates :station_count, presence: true
+
+    validates :station_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+    validates :station_system_count, presence: true
+
+    validates :station_system_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   end
 end
