0d2d3e75bdf49b75da18a776725e16ffd485f03c
Igor Zubkov <igor.zubkov@gmail.com>
2018-03-21 23:46:11 +0200

    More validations

--- spec/models/eve/ancestry_spec.rb
+++ spec/models/eve/ancestry_spec.rb
@@ -7,5 +7,19 @@ module Eve
     it { should be_a(ApplicationRecord) }
 
     it { expect(described_class.table_name).to eq('eve_ancestries') }
+
+    it { should validate_presence_of(:ancestry_id) }
+
+    it { should validate_numericality_of(:ancestry_id).only_integer.is_greater_than_or_equal_to(0) }
+
+    it { should validate_presence_of(:name) }
+
+    it { should validate_presence_of(:bloodline_id) }
+
+    it { should validate_numericality_of(:bloodline_id).only_integer.is_greater_than_or_equal_to(0) }
+
+    it { should validate_presence_of(:description) }
+
+    it { should validate_numericality_of(:icon_id).only_integer.is_greater_than_or_equal_to(0).allow_nil }
   end
 end
