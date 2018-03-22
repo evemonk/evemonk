9b9620c0d031e0d17e882171fc3c8cb3059fbde4
Igor Zubkov <igor.zubkov@gmail.com>
2018-03-22 22:13:52 +0200

    Add eve alliance factory

---
+++ spec/factories/eve/alliance_factory.rb
@@ -0,0 +1,23 @@
+FactoryBot.define do
+  factory :eve_alliance, class: Eve::Alliance do
+    sequence(:alliance_id)
+
+    name { Faker::Lorem.word }
+
+    sequence(:creator_id)
+
+    sequence(:creator_corporation_id)
+
+    ticker { Faker::Lorem.word.upcase }
+
+    sequence(:executor_corporation_id)
+
+    date_founded { Time.zone.now }
+
+    sequence(:faction_id)
+
+    icon_small { "https://imageserver.eveonline.com/Alliance/#{ alliance_id }_64.png" }
+
+    icon_medium { "https://imageserver.eveonline.com/Alliance/#{ alliance_id }_128.png" }
+  end
+end
