72970d22aa19162e4f46c55317177fb486188be7
Igor Zubkov <igor.zubkov@gmail.com>
2018-03-27 21:56:14 +0300

    Update FactionsImporter

--- app/importers/factions_importer.rb
+++ app/importers/factions_importer.rb
@@ -2,10 +2,12 @@
 
 class FactionsImporter
   def import
-    factions = EveOnline::ESI::Factions.new
+    factions = EveOnline::ESI::Factions.new.factions
 
-    factions.factions.each do |faction|
-      Eve::Faction.create!(faction.as_json)
+    factions.each do |faction|
+      eve_faction = Eve::Faction.find_or_initialize_by(faction_id: faction.faction_id)
+
+      eve_faction.update!(faction.as_json)
     end
   end
 end
--- spec/importers/factions_importer_spec.rb
+++ spec/importers/factions_importer_spec.rb
@@ -8,20 +8,28 @@ describe FactionsImporter do
 
     let(:faction) { instance_double(EveOnline::ESI::Models::Faction) }
 
+    let(:eve_faction) { instance_double(Eve::Faction) }
+
+    let(:faction_id) { double }
+
     let(:json) { double }
 
     before { expect(EveOnline::ESI::Factions).to receive(:new).and_return(factions) }
 
     before { expect(factions).to receive(:factions).and_return([faction]) }
 
-    before { expect(faction).to receive(:as_json).and_return(json) }
+    before { expect(faction).to receive(:faction_id).and_return(faction_id) }
 
     before do
       #
-      # Eve::Faction.create!(json)
+      # Eve::Faction.find_or_initialize_by(faction_id: faction.faction_id) => eve_faction
       #
-      expect(Eve::Faction).to receive(:create!).with(json)
+      expect(Eve::Faction).to receive(:find_or_initialize_by).with(faction_id: faction_id).and_return(eve_faction)
     end
+
+    before { expect(faction).to receive(:as_json).and_return(json) }
+
+    before { expect(eve_faction).to receive(:update!).with(json) }
 
     specify { expect { subject.import }.not_to raise_error }
   end
