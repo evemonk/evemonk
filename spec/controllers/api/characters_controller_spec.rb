6c9c85d3f2005ae63a586e1c043475ead6fd7b3b
Igor Zubkov <igor.zubkov@gmail.com>
2018-04-10 18:34:48 +0300

    New Api::CharactersController

--- spec/controllers/api/characters_controller_spec.rb
+++ spec/controllers/api/characters_controller_spec.rb
@@ -5,179 +5,88 @@ require 'rails_helper'
 describe Api::CharactersController do
   it { should be_a(Api::BaseController) }
 
-  it { should use_before_action(:authenticate!) }
-
   describe '#index' do
-    context 'authorized' do
-      let!(:session) { create(:session) }
+    context 'when user signed in' do
+      let(:characters) { double }
 
-      before { request.env['HTTP_AUTHORIZATION'] = "Bearer #{ session.token }" }
+      before { sign_in }
 
-      before { get :index, format: :json }
+      before do
+        #
+        # subject.policy_scope(Character).order(created_at: :asc)
+        #                                .page(params[:page])
+        #
+        expect(subject).to receive(:policy_scope).with(Character) do
+          double.tap do |a|
+            expect(a).to receive(:order).with(created_at: :asc) do
+              double.tap do |b|
+                expect(b).to receive(:page).with('1')
+                                           .and_return(characters)
+              end
+            end
+          end
+        end
+      end
 
-      it { should render_template(:index) }
+      before { expect(CharactersDecorator).to receive(:new).with(characters) }
+
+      before { get :index, params: { format: :json, page: '1' } }
 
       it { should respond_with(:ok) }
     end
 
-    context 'not authorized' do
-      before { get :index, format: :json }
+    context 'when user not signed in' do
+      before { get :index, params: { format: :json } }
 
       it { should respond_with(:unauthorized) }
-    end
-
-    context 'not supported accept:' do
-      let!(:session) { create(:session) }
-
-      before { request.env['HTTP_AUTHORIZATION'] = "Bearer #{ session.token }" }
-
-      before { get :index, format: :html }
-
-      it { should respond_with(:not_acceptable) }
     end
   end
 
   describe '#show' do
-    context 'authorized' do
-      context 'owner' do
-        let!(:user) { create(:user) }
+    context 'when user signed in' do
+      let(:character) { build_stubbed(:character, id: 1) }
 
-        let!(:session) { create(:session, user: user) }
+      before { sign_in }
 
-        let!(:character) { create(:character, user: user, id: 123) }
+      before { expect(Character).to receive(:find).with('1').and_return(character) }
 
-        before { request.env['HTTP_AUTHORIZATION'] = "Bearer #{ session.token }" }
+      before { expect(subject).to receive(:authorize).with(character) }
 
-        before { get :show, params: { id: '123', format: :json } }
+      before { expect(CharacterDecorator).to receive(:new).with(character) }
 
-        it { should render_template(:show) }
+      before { get :show, params: { id: '1', format: :json } }
 
-        it { should respond_with(:ok) }
-      end
-
-      context 'not owned' do
-        let!(:user) { create(:user) }
-
-        let!(:session) { create(:session, user: user) }
-
-        let!(:character) { create(:character, id: 123) }
-
-        before { request.env['HTTP_AUTHORIZATION'] = "Bearer #{ session.token }" }
-
-        before { get :show, params: { id: '123', format: :json } }
-
-        it { should respond_with(:forbidden) }
-      end
+      it { should respond_with(:ok) }
     end
 
-    context 'not authorized' do
-      let!(:character) { create(:character, id: 123) }
-
-      before { get :show, params: { id: '123', format: :json } }
+    context 'when user not signed in' do
+      before { get :show, params: { id: '1', format: :json } }
 
       it { should respond_with(:unauthorized) }
-    end
-
-    context 'not supported accept:' do
-      let!(:user) { create(:user) }
-
-      let!(:session) { create(:session, user: user) }
-
-      let!(:character) { create(:character, user: user, id: 123) }
-
-      before { request.env['HTTP_AUTHORIZATION'] = "Bearer #{ session.token }" }
-
-      before { get :show, params: { id: '123', format: :html } }
-
-      it { should respond_with(:not_acceptable) }
     end
   end
 
   describe '#destroy' do
-    context 'authorized' do
-      context 'owned' do
-        let!(:user) { create(:user) }
+    context 'when user signed in' do
+      let(:character) { build_stubbed(:character, id: 1) }
 
-        let!(:session) { create(:session, user: user) }
+      before { sign_in }
 
-        let!(:character) { create(:character, user: user, id: 123) }
+      before { expect(Character).to receive(:find).with('1').and_return(character) }
 
-        before { request.env['HTTP_AUTHORIZATION'] = "Bearer #{ session.token }" }
+      before { expect(subject).to receive(:authorize).with(character) }
 
-        before { delete :destroy, params: { id: '123', format: :json } }
+      before { expect(character).to receive(:destroy!) }
 
-        it { should respond_with(:no_content) }
-      end
+      before { delete :destroy, params: { id: '1', format: :json } }
 
-      context 'not owned' do
-        let!(:user) { create(:user) }
-
-        let!(:session) { create(:session, user: user) }
-
-        let!(:character) { create(:character, id: 123) }
-
-        before { request.env['HTTP_AUTHORIZATION'] = "Bearer #{ session.token }" }
-
-        before { delete :destroy, params: { id: '123', format: :json } }
-
-        it { should respond_with(:forbidden) }
-      end
+      it { should respond_with(:no_content) }
     end
 
-    context 'not authorized' do
-      let!(:character) { create(:character, id: 123) }
-
-      before { delete :destroy, params: { id: '123', format: :json } }
+    context 'when user not signed in' do
+      before { delete :destroy, params: { id: '1', format: :json } }
 
       it { should respond_with(:unauthorized) }
-    end
-  end
-
-  # private methods
-
-  describe '#resource' do
-    context '@character not set' do
-      let!(:character) { create(:character, id: 123) }
-
-      let(:params) { { id: '123' } }
-
-      before { expect(subject).to receive(:params).and_return(params) }
-
-      specify { expect(subject.send(:resource)).to eq(character) }
-
-      specify { expect { subject.send(:resource) }.to change { subject.instance_variable_get(:@character) }.from(nil).to(character) }
-    end
-
-    context '@character is set' do
-      let(:character) { double }
-
-      before { subject.instance_variable_set(:@character, character) }
-
-      specify { expect(subject.send(:resource)).to eq(character) }
-    end
-  end
-
-  describe '#collection' do
-    context '@characters not set' do
-      let!(:user) { create(:user) }
-
-      let!(:character1) { create(:character, user: user) }
-
-      let!(:character2) { create(:character, user: user) }
-
-      before { expect(subject).to receive(:current_user).and_return(user) }
-
-      specify { expect(subject.send(:collection)).to eq([character1, character2]) }
-
-      specify { expect { subject.send(:collection) }.to change { subject.instance_variable_get(:@characters) }.from(nil) }
-    end
-
-    context '@characters is set' do
-      let(:characters) { double }
-
-      before { subject.instance_variable_set(:@characters, characters) }
-
-      specify { expect(subject.send(:collection)).to eq(characters) }
     end
   end
 end
