c318aeaa3dcdff5ae7246a424b239974e564b99f
Igor Zubkov <igor.zubkov@gmail.com>
2018-04-10 18:45:28 +0300

    Namespace

--- spec/controllers/api/docs_controller_spec.rb
+++ spec/controllers/api/docs_controller_spec.rb
@@ -2,16 +2,18 @@
 
 require 'rails_helper'
 
-describe Api::DocsController do
-  it { should be_a(ActionController::Base) }
+module Api
+  describe DocsController do
+    it { should be_a(ActionController::Base) }
 
-  it { should be_a(Swagger::Blocks) }
+    it { should be_a(Swagger::Blocks) }
 
-  # protect_from_forgery with: :exception
+    # protect_from_forgery with: :exception
 
-  describe '#index' do
-    before { get :index, params: { format: :json } }
+    describe '#index' do
+      before { get :index, params: { format: :json } }
 
-    it { should respond_with(:ok) }
+      it { should respond_with(:ok) }
+    end
   end
 end
