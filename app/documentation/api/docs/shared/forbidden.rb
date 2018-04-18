fd3e5a23ae70eb0448940a1bb4af18dbdce50e94
Igor Zubkov <igor.zubkov@gmail.com>
2018-04-18 23:40:19 +0300

    Update swagger

---
+++ app/documentation/api/docs/shared/forbidden.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# :nocov:
+module Api
+  module Docs
+    module Shared
+      module Forbidden
+        def self.extended(base)
+          base.response '403' do
+            key :description, 'The server understood the request but refuses to authorize it.'
+          end
+        end
+      end
+    end
+  end
+end
+# :nocov:
