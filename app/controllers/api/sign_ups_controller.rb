4bb4524950cd414790a7c77b50f20c4d10b5b3cb
Igor Zubkov <igor.zubkov@gmail.com>
2018-04-19 21:54:53 +0300

    More

--- app/controllers/api/sign_ups_controller.rb
+++ app/controllers/api/sign_ups_controller.rb
@@ -4,14 +4,16 @@ module Api
   class SignUpsController < BaseController
     skip_before_action :authenticate!
 
+    respond_to :json
+
     def create
       sign_up = Api::SignUp.new(sign_ups_params)
 
       if sign_up.save
-        render json: SessionDecorator.new(sign_up.session,
-                                          context: { with_token: true })
+        respond_with(SessionDecorator.new(sign_up.session,
+                                          context: { with_token: true }))
       else
-        render json: { errors: sign_up.errors }, status: :unprocessable_entity
+        respond_with(sign_up, status: :unprocessable_entity)
       end
     end
 
