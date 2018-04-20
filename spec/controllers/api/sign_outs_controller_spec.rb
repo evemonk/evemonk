0077c9dc25c5670249ac0531a80e97f0ef05de03
Igor Zubkov <igor.zubkov@gmail.com>
2018-04-20 15:19:24 +0300

    Namespace

--- spec/controllers/api/sign_outs_controller_spec.rb
+++ spec/controllers/api/sign_outs_controller_spec.rb
@@ -2,39 +2,41 @@
 
 require 'rails_helper'
 
-describe Api::SignOutsController do
-  it { should be_a(Api::BaseController) }
+module Api
+  describe SignOutsController do
+    it { should be_a(Api::BaseController) }
 
-  it { should use_before_action(:authenticate!) }
+    it { should use_before_action(:authenticate!) }
 
-  describe '#destroy' do
-    context 'when authorized' do
-      before { sign_in }
+    describe '#destroy' do
+      context 'when authorized' do
+        before { sign_in }
 
-      let(:request1) { double.as_null_object }
+        let(:request1) { double.as_null_object }
 
-      before { expect(subject).to receive(:request).and_return(request1).at_least(:once) }
+        before { expect(subject).to receive(:request).and_return(request1).at_least(:once) }
 
-      before do
-        #
-        # Api::SignOut.new(request).destroy!
-        #
-        expect(Api::SignOut).to receive(:new).with(request1) do
-          double.tap do |a|
-            expect(a).to receive(:destroy!)
+        before do
+          #
+          # Api::SignOut.new(request).destroy!
+          #
+          expect(Api::SignOut).to receive(:new).with(request1) do
+            double.tap do |a|
+              expect(a).to receive(:destroy!)
+            end
           end
         end
+
+        before { delete :destroy, format: :json }
+
+        it { should respond_with(:no_content) }
       end
 
-      before { delete :destroy, format: :json }
+      context 'when not authorized' do
+        before { delete :destroy, format: :json }
 
-      it { should respond_with(:no_content) }
-    end
-
-    context 'when not authorized' do
-      before { delete :destroy, format: :json }
-
-      it { should respond_with(:unauthorized) }
+        it { should respond_with(:unauthorized) }
+      end
     end
   end
 end
