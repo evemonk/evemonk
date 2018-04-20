53d8c6c13e8ede3548122bdbffd750cac0791ca6
Igor Zubkov <igor.zubkov@gmail.com>
2018-04-20 15:18:29 +0300

    Namespace

--- spec/controllers/api/base_controller_spec.rb
+++ spec/controllers/api/base_controller_spec.rb
@@ -2,46 +2,48 @@
 
 require 'rails_helper'
 
-describe Api::BaseController do
-  it { should be_a(ApplicationController) }
+module Api
+  describe BaseController do
+    it { should be_a(ApplicationController) }
 
-  it { should be_a(Pundit) }
+    it { should be_a(Pundit) }
 
-  it { should use_before_action(:authenticate!) }
+    it { should use_before_action(:authenticate!) }
 
-  describe '#current_user' do
-    let!(:user) { instance_double(User) }
+    describe '#current_user' do
+      let!(:user) { instance_double(User) }
 
-    before { subject.instance_variable_set(:@current_user, user) }
+      before { subject.instance_variable_set(:@current_user, user) }
 
-    specify { expect(subject.current_user).to eq(user) }
-  end
+      specify { expect(subject.current_user).to eq(user) }
+    end
 
-  it { should rescue_from(ActiveRecord::RecordNotFound) }
+    it { should rescue_from(ActiveRecord::RecordNotFound) }
 
-  it { should rescue_from(ActionController::UnknownFormat) }
+    it { should rescue_from(ActionController::UnknownFormat) }
 
-  it { should rescue_from(Pundit::NotAuthorizedError) }
+    it { should rescue_from(Pundit::NotAuthorizedError) }
 
-  # private methods
+    # private methods
 
-  describe '#authenticate!' do
-    let!(:user) { create(:user) }
+    describe '#authenticate!' do
+      let!(:user) { create(:user) }
 
-    let!(:session) { create(:session, user: user) }
+      let!(:session) { create(:session, user: user) }
 
-    before { expect(subject).to receive(:authenticate_or_request_with_http_token).and_yield(session.token) }
+      before { expect(subject).to receive(:authenticate_or_request_with_http_token).and_yield(session.token) }
 
-    specify { expect(subject.send(:authenticate!)).to eq(user) }
-  end
+      specify { expect(subject.send(:authenticate!)).to eq(user) }
+    end
 
-  describe '#authenticate' do
-    let!(:user) { create(:user) }
+    describe '#authenticate' do
+      let!(:user) { create(:user) }
 
-    let!(:session) { create(:session, user: user) }
+      let!(:session) { create(:session, user: user) }
 
-    before { expect(subject).to receive(:authenticate_with_http_token).and_yield(session.token) }
+      before { expect(subject).to receive(:authenticate_with_http_token).and_yield(session.token) }
 
-    specify { expect(subject.send(:authenticate)).to eq(user) }
+      specify { expect(subject.send(:authenticate)).to eq(user) }
+    end
   end
 end
