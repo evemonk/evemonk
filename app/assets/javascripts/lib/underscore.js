aaf85e0510e34955a3db626330e266005129e26b
Awesome Code <team@xinminlabs.com>
2018-04-25 11:55:27 +0000

    Auto corrected by following quotes

--- app/assets/javascripts/lib/underscore.js
+++ app/assets/javascripts/lib/underscore.js
@@ -1466,7 +1466,7 @@
     '<': '&lt;',
     '>': '&gt;',
     '"': '&quot;',
-    "'": '&#x27;',
+    '\'': '&#x27;',
     '`': '&#x60;'
   };
   var unescapeMap = _.invert(escapeMap);
@@ -1532,7 +1532,7 @@
   // Certain characters need to be escaped so that they can be put into a
   // string literal.
   var escapes = {
-    "'": "'",
+    '\'': '\'',
     '\\': '\\',
     '\r': 'r',
     '\n': 'n',
@@ -1563,29 +1563,29 @@
 
     // Compile the template source, escaping string literals appropriately.
     var index = 0;
-    var source = "__p+='";
+    var source = '__p+=\'';
     text.replace(matcher, function(match, escape, interpolate, evaluate, offset) {
       source += text.slice(index, offset).replace(escapeRegExp, escapeChar);
       index = offset + match.length;
 
       if (escape) {
-        source += "'+\n((__t=(" + escape + "))==null?'':_.escape(__t))+\n'";
+        source += '\'+\n((__t=(' + escape + '))==null?\'\':_.escape(__t))+\n\'';
       } else if (interpolate) {
-        source += "'+\n((__t=(" + interpolate + "))==null?'':__t)+\n'";
+        source += '\'+\n((__t=(' + interpolate + '))==null?\'\':__t)+\n\'';
       } else if (evaluate) {
-        source += "';\n" + evaluate + "\n__p+='";
+        source += '\';\n' + evaluate + '\n__p+=\'';
       }
 
       // Adobe VMs need the match returned to produce the correct offset.
       return match;
     });
-    source += "';\n";
+    source += '\';\n';
 
     // If a variable is not specified, place data values in local scope.
     if (!settings.variable) source = 'with(obj||{}){\n' + source + '}\n';
 
-    source = "var __t,__p='',__j=Array.prototype.join," +
-      "print=function(){__p+=__j.call(arguments,'');};\n" +
+    source = 'var __t,__p=\'\',__j=Array.prototype.join,' +
+      'print=function(){__p+=__j.call(arguments,\'\');};\n' +
       source + 'return __p;\n';
 
     var render;
