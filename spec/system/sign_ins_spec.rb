864f78260eb3a39682b9ed2ae46c2fea2fa99062
Igor Zubkov <igor.zubkov@gmail.com>
2018-04-26 16:48:42 +0300

    More specs

--- spec/system/sign_ins_spec.rb
+++ spec/system/sign_ins_spec.rb
@@ -19,4 +19,53 @@ describe 'Sign in features' do
 
     expect(page).to have_content('Successful signed in!')
   end
+
+  it 'when user successfully sign ups with upper cased email' do
+    create(:user, email: 'me@example.com', password: 'eidii7EeooVe8ahk')
+
+    visit '/sign_in'
+
+    expect {
+      fill_in 'email', with: 'ME@EXAMPLE.COM'
+      fill_in 'password', with: 'eidii7EeooVe8ahk'
+      click_button 'Sign in'
+      wait_for_ajax
+    }.to change(Session, :count).by(1)
+
+    expect(current_path).to eq('/')
+
+    expect(page).to have_content('Successful signed in!')
+  end
+
+  it 'when user with given email not found' do
+    visit '/sign_in'
+
+    expect {
+      fill_in 'email', with: 'me@example.com'
+      fill_in 'password', with: 'eidii7EeooVe8ahk'
+      click_button 'Sign in'
+      wait_for_ajax
+    }.not_to change(Session, :count)
+
+    expect(current_path).to eq('/sign_in')
+
+    expect(page).to have_content('Email and/or password is invalid')
+  end
+
+  it 'when password wrong' do
+    create(:user, email: 'me@example.com', password: 'eidii7EeooVe8ahk')
+
+    visit '/sign_in'
+
+    expect {
+      fill_in 'email', with: 'me@example.com'
+      fill_in 'password', with: 'wrong-password'
+      click_button 'Sign in'
+      wait_for_ajax
+    }.not_to change(Session, :count)
+
+    expect(current_path).to eq('/sign_in')
+
+    expect(page).to have_content('Email and/or password is invalid')
+  end
 end
