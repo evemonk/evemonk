8c75c4d70cc1e2903f2e0ce8f7c8085cdfcce20c
Igor Zubkov <igor.zubkov@gmail.com>
2018-04-27 23:21:34 +0300

    Add missing index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_27_201919) do
+ActiveRecord::Schema.define(version: 2018_04_27_202045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2018_04_27_201919) do
     t.datetime "accrued_remap_cooldown_date"
     t.integer "faction_id"
     t.index ["alliance_id"], name: "index_characters_on_alliance_id"
+    t.index ["faction_id"], name: "index_characters_on_faction_id"
     t.index ["race_id"], name: "index_characters_on_race_id"
     t.index ["uid"], name: "index_characters_on_uid", unique: true
     t.index ["user_id"], name: "index_characters_on_user_id"
