7b264a221cef6226ff6f4353757e6a4f0266e1ed
Igor Zubkov <igor.zubkov@gmail.com>
2018-06-18 18:15:28 +0300

    Fix rubocop warnings

--- db/migrate/20180618151303_rpush_3_1_1_updates.rb
+++ db/migrate/20180618151303_rpush_3_1_1_updates.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Rpush311Updates < ActiveRecord::VERSION::MAJOR >= 5 ? ActiveRecord::Migration[5.0] : ActiveRecord::Migration
   def self.up
     change_table :rpush_notifications do |t|
