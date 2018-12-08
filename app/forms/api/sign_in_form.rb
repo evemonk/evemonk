37046f4aab8f0f518be3a762e1a9aa3d34663c95
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-08 21:22:52 +0200

    Api::SignInForm

---
+++ app/forms/api/sign_in_form.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module Api
+    class SignInForm
+      include ActiveModel::Model
+
+      attr_accessor :email, :password, :name, :device_type, :device_token
+
+      validates :email, presence: true
+
+      validates :password, presence: true
+
+      validate :user_presence
+
+      validate :user_password
+
+      delegate :id, :token, to: :session
+
+      def save
+        return false if !valid?
+
+        # TODO: add this
+        # PushNotifications::NewSignIn.new(device, device_token, user.notifications_count).execute!
+
+        session.save!
+      end
+
+      def user
+        @user ||= User.where('LOWER(email) = LOWER(?)', email).first
+      end
+
+      def user_presence
+        return if errors.any?
+
+        errors.add(:base, 'Email and/or password is invalid') if !user
+      end
+
+      def user_password
+        return if errors.any?
+
+        errors.add(:base, 'Email and/or password is invalid') if user && !user.authenticate(password)
+      end
+
+      def session
+        @session ||= user.sessions.build(name: name,
+                                         device_type: device_type,
+                                         device_token: device_token)
+      end
+    end
+  end
---
+++ spec/forms/api/sign_in_form_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Api::SignInForm, type: :model do
+  it { should be_a(ActiveModel::Model) }
+
+  it { should validate_presence_of(:email) }
+
+  it { should validate_presence_of(:password) }
+
+  describe '#save' do
+    context 'when user exist and password right' do
+      let!(:user) { create(:user, email: 'me@example.com', password: 'password') }
+
+      let(:params) do
+        {
+          email: 'me@example.com',
+          password: 'password',
+          name: 'My Computer',
+          device_type: 'ios',
+          device_token: 'token123'
+        }
+      end
+
+      subject { described_class.new(params) }
+
+      specify { expect(subject.save).to eq(true) }
+
+      specify { expect { subject.save }.to change { user.sessions.count }.by(1) }
+
+      specify { expect { subject.save }.to change { user.sessions.first&.token }.from(nil) }
+
+      specify { expect { subject.save }.to change { user.sessions.first&.name }.from(nil).to('My Computer') }
+
+      specify { expect { subject.save }.to change { user.sessions.first&.device_type }.from(nil).to('ios') }
+
+      specify { expect { subject.save }.to change { user.sessions.first&.device_token }.from(nil).to('token123') }
+    end
+
+    context 'when user exist but password wrong' do
+      let!(:user) { create(:user, email: 'me@example.com', password: 'password') }
+
+      let(:params) { { email: 'me@example.com', password: 'wrong password' } }
+
+      subject { described_class.new(params) }
+
+      specify { expect(subject.save).to eq(false) }
+
+      # specify { binding.pry }
+
+      specify { expect { subject.save }.to change { subject.errors.messages }.from({}).to(base: ['Email and/or password is invalid']) }
+    end
+  end
+end
