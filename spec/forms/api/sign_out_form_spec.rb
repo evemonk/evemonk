23c91c4f5166ec14c8cf0af40a922425b96871f8
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-08 23:00:47 +0200

    Api::SignOutForm

---
+++ spec/forms/api/sign_out_form_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+describe Api::SignOutForm do
+  let(:request) { double }
+
+  subject { described_class.new(request) }
+
+  it { should be_a(ActionController::HttpAuthentication::Token) }
+
+  describe '#destroy' do
+    let!(:user) { create(:user) }
+
+    let!(:session) { create(:session, user: user) }
+
+    before do
+      #
+      # subject.token_and_options(request) => [token]
+      #
+      expect(subject).to receive(:token_and_options).with(request)
+                                                    .and_return([session.token])
+    end
+
+    specify { expect { subject.destroy }.to change { user.sessions.count }.by(-1) }
+  end
+end
