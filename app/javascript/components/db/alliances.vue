f1b0dc84018e7901b978e16ba5379f9169f7e0e3
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-09 22:46:48 +0200

    More js

--- app/javascript/components/db/alliances.vue
+++ app/javascript/components/db/alliances.vue
@@ -1,29 +1,29 @@
 <template>
-    <div id="alliances">
-        <h1>Alliances</h1>
+  <div id="alliances">
+    <h1>Alliances</h1>
 
-        <h1>Current page {{ current_page }}</h1>
+    <h1>Current page {{ current_page }}</h1>
 
-        <b-pagination size="md" :total-rows="total_count" v-model="current_page" :per-page="25"></b-pagination>
+    <b-pagination size="md" :total-rows="total_count" v-model="current_page" :per-page="25"></b-pagination>
 
-        <table id="alliances-list">
-            <thead>
-                <th>ID</th>
-                <th>&nbsp;</th>
-                <th>Name</th>
-                <th>Corps count</th>
-                <th>Members count</th>
-            </thead>
-            <tr v-for="alliance in alliances">
-                <td>{{ alliance.id }}</td>
-                <td><img :src="alliance.icon" height="64" width="64"></td>
-                <td>{{ alliance.name }}</td>
-                <td>{{ alliance.corporations_count }}</td>
-                <td>{{ alliance.characters_count }}</td>
-            </tr>
-        </table>
+    <table id="alliances-list">
+      <thead>
+        <th>ID</th>
+        <th>&nbsp;</th>
+        <th>Name</th>
+        <th>Corps count</th>
+        <th>Members count</th>
+      </thead>
+      <tr v-for="alliance in alliances">
+          <td>{{ alliance.id }}</td>
+          <td><img :src="alliance.icon" height="64" width="64"></td>
+          <td>{{ alliance.name }}</td>
+          <td>{{ alliance.corporations_count }}</td>
+          <td>{{ alliance.characters_count }}</td>
+        </tr>
+      </table>
 
-        <b-pagination size="md" :total-rows="total_count" v-model="current_page" :per-page="25"></b-pagination>
+      <b-pagination size="md" :total-rows="total_count" v-model="current_page" :per-page="25"></b-pagination>
     </div>
 </template>
 
