64b03bde9189995f01b29e327d3687bb9c20471a
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-09 22:46:25 +0200

    Add bootstrap, popper.js and jquery

---
+++ app/javascript/components/db/alliance.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="alliance">
+        <h1>Hello</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        id: null,
+        name: null
+      }
+    }
+  }
+</script>
