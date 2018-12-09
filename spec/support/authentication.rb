7563b6125ad25cb5776a28ec779b2d52ae1eb07e
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-09 18:58:32 +0200

    Update

--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -2,7 +2,7 @@
 
 module Authentication
   def sign_in(user = User.new)
-    allow(controller).to receive(:authenticate!).and_return(user)
+    allow(controller).to receive(:authenticate).and_return(user)
 
     allow(controller).to receive(:current_user).and_return(user)
   end
