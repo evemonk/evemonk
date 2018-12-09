a4e653be3233e0a0f0cdf191191120cfcc4df48f
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-09 22:24:51 +0200

    More js

---
+++ app/javascript/packs/evemonk_vue.js
@@ -0,0 +1,25 @@
+/* eslint no-console: 0 */
+// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> (and
+// <%= stylesheet_pack_tag 'hello_vue' %> if you have styles in your component)
+// to the head of your layout file,
+// like app/views/layouts/application.html.erb.
+// All it does is render <div>Hello Vue</div> at the bottom of the page.
+
+import 'bootstrap';
+
+import Vue from 'vue';
+import App from '../app.vue';
+
+import router from '../router';
+import store from '../store';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    el: '#app',
+    router,
+    store,
+    render: h => h(App),
+  });
+
+  console.log(app);
+});
