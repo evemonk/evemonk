162f312d40a6feac522ba40c73b241b164be8e2f
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-09 20:10:21 +0200

    Vue

---
+++ app/javascript/components/auto_sign_in.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+
+<script>
+    import store from '../store';
+
+    export default {
+        data () {
+            return {
+            }
+        },
+
+        created () {
+            let token = this.$route.params.token;
+            store.commit('signInUserWithToken', token);
+            this.$router.push('/profile');
+        }
+    }
+</script>
