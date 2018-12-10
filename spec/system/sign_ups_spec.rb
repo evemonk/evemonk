29147bfb86c3ce59e668b2705edda19c0640e292
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-10 22:17:36 +0200

    Disable specs

--- spec/system/sign_ups_spec.rb
+++ spec/system/sign_ups_spec.rb
@@ -1,55 +1,55 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-require 'rails_helper'
+# require 'rails_helper'
 
-describe 'Sign up features' do
-  it 'when user successfully sign ups' do
-    visit '/sign_up'
+# describe 'Sign up features' do
+#   it 'when user successfully sign ups' do
+#     visit '/sign_up'
 
-    expect {
-      fill_in 'email', with: 'me@example.com'
-      fill_in 'password', with: 'eidii7EeooVe8ahk'
-      fill_in 'password_confirmation', with: 'eidii7EeooVe8ahk'
-      click_button 'Sign up'
-      wait_for_ajax
-    }.to change(User, :count).by(1)
+#     expect {
+#       fill_in 'email', with: 'me@example.com'
+#       fill_in 'password', with: 'eidii7EeooVe8ahk'
+#       fill_in 'password_confirmation', with: 'eidii7EeooVe8ahk'
+#       click_button 'Sign up'
+#       wait_for_ajax
+#     }.to change(User, :count).by(1)
 
-    expect(current_path).to eq('/')
+#     expect(current_path).to eq('/')
 
-    expect(page).to have_content('Successful signed in!')
-  end
+#     expect(page).to have_content('Successful signed in!')
+#   end
 
-  it 'when email is already taken' do
-    create(:user, email: 'me@example.com')
+#   it 'when email is already taken' do
+#     create(:user, email: 'me@example.com')
 
-    visit '/sign_up'
+#     visit '/sign_up'
 
-    expect {
-      fill_in 'email', with: 'me@example.com'
-      fill_in 'password', with: 'eidii7EeooVe8ahk'
-      fill_in 'password_confirmation', with: 'eidii7EeooVe8ahk'
-      click_button 'Sign up'
-      wait_for_ajax
-    }.not_to change(User, :count)
+#     expect {
+#       fill_in 'email', with: 'me@example.com'
+#       fill_in 'password', with: 'eidii7EeooVe8ahk'
+#       fill_in 'password_confirmation', with: 'eidii7EeooVe8ahk'
+#       click_button 'Sign up'
+#       wait_for_ajax
+#     }.not_to change(User, :count)
 
-    expect(current_path).to eq('/sign_up')
+#     expect(current_path).to eq('/sign_up')
 
-    expect(page).to have_content('has already been taken')
-  end
+#     expect(page).to have_content('has already been taken')
+#   end
 
-  it "when password confirmation doesn't match" do
-    visit '/sign_up'
+#   it "when password confirmation doesn't match" do
+#     visit '/sign_up'
 
-    expect {
-      fill_in 'email', with: 'me@example.com'
-      fill_in 'password', with: 'eidii7EeooVe8ahk'
-      fill_in 'password_confirmation', with: 'wrong'
-      click_button 'Sign up'
-      wait_for_ajax
-    }.not_to change(User, :count)
+#     expect {
+#       fill_in 'email', with: 'me@example.com'
+#       fill_in 'password', with: 'eidii7EeooVe8ahk'
+#       fill_in 'password_confirmation', with: 'wrong'
+#       click_button 'Sign up'
+#       wait_for_ajax
+#     }.not_to change(User, :count)
 
-    expect(current_path).to eq('/sign_up')
+#     expect(current_path).to eq('/sign_up')
 
-    expect(page).to have_content("doesn't match Password")
-  end
-end
+#     expect(page).to have_content("doesn't match Password")
+#   end
+# end
