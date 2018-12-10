38ddb84e98fb9f2e5986a08a8ea8e8f7e9186f00
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-11 00:29:38 +0200

    Update specs

--- spec/system/sign_ins_spec.rb
+++ spec/system/sign_ins_spec.rb
@@ -1,71 +1,68 @@
-# # frozen_string_literal: true
+# frozen_string_literal: true
 
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe 'Sign in features' do
-#   it 'when user successfully sign ins' do
-#     create(:user, email: 'me@example.com', password: 'eidii7EeooVe8ahk')
+describe 'Sign in features' do
+  it 'when user successfully sign ins' do
+    create(:user, email: 'me@example.com', password: 'eidii7EeooVe8ahk')
 
-#     visit '/sign_in'
+    visit '/sign_in'
 
-#     expect {
-#       fill_in 'email', with: 'me@example.com'
-#       fill_in 'password', with: 'eidii7EeooVe8ahk'
-#       click_button 'Sign in'
-#       wait_for_ajax
-#     }.to change(Session, :count).by(1)
+    expect {
+      fill_in 'email', with: 'me@example.com'
+      fill_in 'password', with: 'eidii7EeooVe8ahk'
+      click_button 'Sign in'
+    }.to change(Session, :count).by(1)
 
-#     expect(current_path).to eq('/')
+    expect(current_path).to eq('/')
 
-#     expect(page).to have_content('Successful signed in!')
-#   end
+    expect(page).to have_content('Successful signed in!')
+  end
 
-#   it 'when user successfully sign ins with upper cased email' do
-#     create(:user, email: 'me@example.com', password: 'eidii7EeooVe8ahk')
+  it 'when user successfully sign ins with upper cased email' do
+    create(:user, email: 'me@example.com', password: 'eidii7EeooVe8ahk')
 
-#     visit '/sign_in'
+    visit '/sign_in'
 
-#     expect {
-#       fill_in 'email', with: 'ME@EXAMPLE.COM'
-#       fill_in 'password', with: 'eidii7EeooVe8ahk'
-#       click_button 'Sign in'
-#       wait_for_ajax
-#     }.to change(Session, :count).by(1)
+    expect {
+      fill_in 'email', with: 'ME@EXAMPLE.COM'
+      fill_in 'password', with: 'eidii7EeooVe8ahk'
+      click_button 'Sign in'
+    }.to change(Session, :count).by(1)
 
-#     expect(current_path).to eq('/')
+    expect(current_path).to eq('/')
 
-#     expect(page).to have_content('Successful signed in!')
-#   end
+    expect(page).to have_content('Successful signed in!')
+  end
 
-#   it 'when user with given email not found' do
-#     visit '/sign_in'
+  it 'when user with given email not found' do
+    visit '/sign_in'
 
-#     expect {
-#       fill_in 'email', with: 'me@example.com'
-#       fill_in 'password', with: 'eidii7EeooVe8ahk'
-#       click_button 'Sign in'
-#       wait_for_ajax
-#     }.not_to change(Session, :count)
+    expect {
+      fill_in 'email', with: 'me@example.com'
+      fill_in 'password', with: 'eidii7EeooVe8ahk'
+      click_button 'Sign in'
+      wait_for_ajax
+    }.not_to change(Session, :count)
 
-#     expect(current_path).to eq('/sign_in')
+    expect(current_path).to eq('/sign_in')
 
-#     expect(page).to have_content('Email and/or password is invalid')
-#   end
+    expect(page).to have_content('Email and/or password is invalid')
+  end
 
-#   it 'when password wrong' do
-#     create(:user, email: 'me@example.com', password: 'eidii7EeooVe8ahk')
+  it 'when password wrong' do
+    create(:user, email: 'me@example.com', password: 'eidii7EeooVe8ahk')
 
-#     visit '/sign_in'
+    visit '/sign_in'
 
-#     expect {
-#       fill_in 'email', with: 'me@example.com'
-#       fill_in 'password', with: 'wrong-password'
-#       click_button 'Sign in'
-#       wait_for_ajax
-#     }.not_to change(Session, :count)
+    expect {
+      fill_in 'email', with: 'me@example.com'
+      fill_in 'password', with: 'wrong-password'
+      click_button 'Sign in'
+    }.not_to change(Session, :count)
 
-#     expect(current_path).to eq('/sign_in')
+    expect(current_path).to eq('/sign_in')
 
-#     expect(page).to have_content('Email and/or password is invalid')
-#   end
-# end
+    expect(page).to have_content('Email and/or password is invalid')
+  end
+end
