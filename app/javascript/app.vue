5de7bd42dadbb182f31ebbcf421623ef2c379f65
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-11 21:49:58 +0200

    Add flash

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <app-header />
+    <app-flash />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
   import Header from './components/header.vue';
+  import Flash from './components/flash.vue';
   import { mapGetters } from 'vuex';
 
   export default {
     name: 'app',
     components: {
-      'app-header': Header
+      'app-header': Header,
+      'app-flash': Flash
     },
 
     computed: {
