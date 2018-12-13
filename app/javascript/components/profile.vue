9e8126a83e86acb31a8c11fcb5582ecf189fab11
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-14 00:02:03 +0200

    Indent

--- app/javascript/components/profile.vue
+++ app/javascript/components/profile.vue
@@ -1,39 +1,38 @@
 <template>
-    <div>
-        <h1>Profile</h1>
+  <div>
+    <h1>Profile</h1>
 
-        <h2>Email: {{ email }}</h2>
-        <h1>Characters count: {{ characters_count }}</h1>
-    </div>
+    <h2>Email: {{ email }}</h2>
+    <h1>Characters count: {{ characters_count }}</h1>
+  </div>
 </template>
 
 <script>
-    import { mapActions } from 'vuex';
+  import { mapActions } from 'vuex';
 
-    export default {
-        data () {
-            return {
-                email: '',
-                characters_count: null
-            }
-        },
+  export default {
+    data () {
+      return {
+        email: '',
+        characters_count: null
+      }
+    },
 
-        created () {
-            this.fetchProfile().then(response => {
-                if (response.status === 200)
-                {
-                    this.email = response.data.email;
-                    this.characters_count = response.data.characters_count;
-                } else {
-                    this.$router.push('/sign_in');
-                }
-            });
-        },
-
-        methods: {
-            ...mapActions({
-                'fetchProfile': 'fetchProfile'
-            })
+    created() {
+      this.fetchProfile().then(response => {
+        if (response.status === 200) {
+          this.email = response.data.email;
+          this.characters_count = response.data.characters_count;
+        } else {
+          this.$router.push('/sign_in');
         }
+      });
+    },
+
+    methods: {
+      ...mapActions({
+        'fetchProfile': 'fetchProfile'
+      })
     }
+  }
 </script>
