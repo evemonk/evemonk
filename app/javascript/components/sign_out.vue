35a45eda2f57f017ced4553b5073332c089258be
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-17 21:45:03 +0200

    Sign out component

---
+++ app/javascript/components/sign_out.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+  import { mapGetters, mapActions } from 'vuex';
+
+  export default {
+    created() {
+      this.signOut();
+    },
+
+    methods: {
+      ...mapActions([
+        'signOut'
+      ])
+    }
+  }
+</script>
