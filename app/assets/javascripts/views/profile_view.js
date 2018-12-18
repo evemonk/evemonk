8df9a12c28f6f03f368add47d31b810b616d6373
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-18 22:00:18 +0200

    Cleanup

--- app/assets/javascripts/views/profile_view.js
+++
@@ -1,9 +0,0 @@
-EvemonkApp.Views.ProfileView = Backbone.View.extend({
-    template: JST['profile/show'],
-
-    render: function () {
-        this.$el.html(this.template(this.model.toJSON()));
-
-        return this;
-    }
-});
