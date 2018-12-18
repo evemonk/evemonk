cf1ae972595700eb3755554d5432798956113d16
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-18 23:18:31 +0200

    Update Eve::Corporation

--- app/models/eve/corporation.rb
+++ app/models/eve/corporation.rb
@@ -2,5 +2,16 @@
 
 module Eve
   class Corporation < ApplicationRecord
+    belongs_to :alliance, primary_key: :alliance_id, optional: true
+
+    belongs_to :ceo, primary_key: :character_id, class_name: 'Eve::Character', optional: true
+
+    belongs_to :creator, primary_key: :character_id, class_name: 'Eve::Character', optional: true
+
+    belongs_to :faction, primary_key: :faction_id, optional: true
+
+    # belongs_to :home_station_id
+
+    has_many :characters, primary_key: :corporation_id
   end
 end
