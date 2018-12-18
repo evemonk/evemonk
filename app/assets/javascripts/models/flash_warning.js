cd8eccc0f5b04fccdfd72d4bb9e3b5f3dbf8773c
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-18 20:08:39 +0200

    Cleanup

--- app/assets/javascripts/models/flash_warning.js
+++
@@ -1,7 +0,0 @@
-EvemonkApp.Models.FlashWarning = Backbone.Model.extend({
-    defaults: {
-        'classname': 'alert-warning',
-        'icon': 'fas fa-exclamation-circle',
-        'message': null
-    }
-});
--- app/assets/javascripts/views/welcome_view.js
+++
@@ -1,9 +0,0 @@
-EvemonkApp.Views.WelcomeView = Backbone.View.extend({
-    template: JST['welcome/index'],
-
-    render: function () {
-        this.$el.html(this.template());
-
-        return this;
-    }
-});
