90f00b0ee013be43b871ba9659c146a3718a6acc
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-18 23:44:19 +0200

    Update Eve::Bloodline

--- app/models/eve/bloodline.rb
+++ app/models/eve/bloodline.rb
@@ -2,5 +2,10 @@
 
 module Eve
   class Bloodline < ApplicationRecord
+    belongs_to :corporation, primary_key: :corporation_id, optional: true
+
+    belongs_to :race, primary_key: :race_id, optional: true
+
+    # belongs_to :eve_ship_type_id, optional: true
   end
 end
