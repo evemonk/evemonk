6287e27024604bebd1c37ead7868e0770cdc2ab9
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-18 17:14:10 +0200

    Fix specs

--- spec/system/sign_ins_spec.rb
+++ spec/system/sign_ins_spec.rb
@@ -4,49 +4,49 @@ require 'rails_helper'
 
 describe 'Sign in features' do
   it 'when user successfully sign in' do
-    create(:user, email: 'me@example.com', password: 'eidii7EeooVe8ahk')
+    user = create(:user, email: 'me@example.com', password: 'eidii7EeooVe8ahk')
 
     visit '/sign_in'
 
-    expect {
-      fill_in 'email', with: 'me@example.com'
-      fill_in 'password', with: 'eidii7EeooVe8ahk'
-      find('#sign_in_button').click # TODO: click on 'Sign In'
-    }.to change(Session, :count).by(1)
-
-    expect(current_path).to eq('/')
+    fill_in 'email', with: 'me@example.com'
+    fill_in 'password', with: 'eidii7EeooVe8ahk'
+    find('#sign_in_button').click # TODO: click on 'Sign In'
 
     expect(page).to have_content('Successful signed in!')
+
+    expect(current_path).to eq('/profile')
+
+    expect(user.sessions.count).to eq(1)
   end
 
   it 'when user successfully sign in with upper cased email' do
-    create(:user, email: 'me@example.com', password: 'eidii7EeooVe8ahk')
+    user = create(:user, email: 'me@example.com', password: 'eidii7EeooVe8ahk')
 
     visit '/sign_in'
 
-    expect {
-      fill_in 'email', with: 'ME@EXAMPLE.COM'
-      fill_in 'password', with: 'eidii7EeooVe8ahk'
-      find('#sign_in_button').click # TODO: click on 'Sign In'
-    }.to change(Session, :count).by(1)
-
-    expect(current_path).to eq('/')
+    fill_in 'email', with: 'ME@EXAMPLE.COM'
+    fill_in 'password', with: 'eidii7EeooVe8ahk'
+    find('#sign_in_button').click # TODO: click on 'Sign In'
 
     expect(page).to have_content('Successful signed in!')
+
+    expect(current_path).to eq('/profile')
+
+    expect(user.sessions.count).to eq(1)
   end
 
   it 'when user with given email not found' do
     visit '/sign_in'
 
-    expect {
-      fill_in 'email', with: 'me@example.com'
-      fill_in 'password', with: 'eidii7EeooVe8ahk'
-      find('#sign_in_button').click # TODO: click on 'Sign In'
-    }.not_to change(Session, :count)
+    fill_in 'email', with: 'me@example.com'
+    fill_in 'password', with: 'eidii7EeooVe8ahk'
+    find('#sign_in_button').click # TODO: click on 'Sign In'
+
+    expect(page).to have_content('Email and/or password is invalid')
 
     expect(current_path).to eq('/sign_in')
 
-    expect(page).to have_content('Email and/or password is invalid')
+    expect(Session.count).to eq(0)
   end
 
   it 'when password wrong' do
@@ -54,14 +54,14 @@ describe 'Sign in features' do
 
     visit '/sign_in'
 
-    expect {
-      fill_in 'email', with: 'me@example.com'
-      fill_in 'password', with: 'wrong-password'
-      find('#sign_in_button').click # TODO: click on 'Sign In'
-    }.not_to change(Session, :count)
+    fill_in 'email', with: 'me@example.com'
+    fill_in 'password', with: 'wrong-password'
+    find('#sign_in_button').click # TODO: click on 'Sign In'
+
+    expect(page).to have_content('Email and/or password is invalid')
 
     expect(current_path).to eq('/sign_in')
 
-    expect(page).to have_content('Email and/or password is invalid')
+    expect(Session.count).to eq(0)
   end
 end
