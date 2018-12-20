b1c391ea56c26d5385e9ad3c0d47e0b074790781
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-21 00:14:53 +0200

    Eve::Stargate

---
+++ app/models/eve/stargate.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Eve
+  class Stargate < ApplicationRecord
+    belongs_to :system, primary_key: :system_id, optional: true
+
+    # has_one :stargate_destination, primary_key: :stargate_id, foreign_key: :from_stargate_id, dependent: :destroy
+
+    # belongs_to :type, optional: true
+  end
+end
