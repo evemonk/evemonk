56ee1b2e002fc75a25354caaa6a404f16a384ffb
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-24 22:50:22 +0200

    Add Eve::Corporation#etag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_21_123710) do
+ActiveRecord::Schema.define(version: 2018_12_24_204752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -165,6 +165,7 @@ ActiveRecord::Schema.define(version: 2018_12_21_123710) do
     t.text "corporation_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "etag"
     t.index ["corporation_id"], name: "index_eve_corporations_on_corporation_id", unique: true
   end
 
