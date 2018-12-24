c005ccbbd84298bb0e712b0b3c795dd17635b178
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-24 21:43:35 +0200

    Refactor

--- app/importers/eve/remove_old_alliances_importer.rb
+++ app/importers/eve/remove_old_alliances_importer.rb
@@ -3,15 +3,15 @@
 module Eve
   class RemoveOldAlliancesImporter
     def import
-      etag = Redis.current.get("remove_old_alliances:#{ I18n.locale }:etag")
+      current_etag = Redis.current.get("remove_old_alliances:#{ I18n.locale }:etag")
 
-      alliances = EveOnline::ESI::Alliances.new(etag: etag)
+      eveonline_esi_alliances = EveOnline::ESI::Alliances.new(etag: current_etag)
 
-      return if alliances.not_modified?
+      return if eveonline_esi_alliances.not_modified?
 
-      alliance_ids = Eve::Alliance.pluck(:alliance_id)
+      eve_alliance_ids = Eve::Alliance.pluck(:alliance_id)
 
-      alliance_ids_to_remove = alliance_ids - alliances.alliance_ids
+      alliance_ids_to_remove = eve_alliance_ids - eveonline_esi_alliances.alliance_ids
 
       alliance_ids_to_remove.each do |alliance_id|
         alliance = Eve::Alliance.find_or_initialize_by(alliance_id: alliance_id)
@@ -23,7 +23,7 @@ module Eve
         alliance.destroy!
       end
 
-      Redis.current.set("remove_old_alliances:#{ I18n.locale }:etag", alliances.etag)
+      Redis.current.set("remove_old_alliances:#{ I18n.locale }:etag", eveonline_esi_alliances.etag)
     end
   end
 end
--- spec/importers/eve/remove_old_alliances_importer_spec.rb
+++ spec/importers/eve/remove_old_alliances_importer_spec.rb
@@ -5,77 +5,77 @@ require 'rails_helper'
 describe Eve::RemoveOldAlliancesImporter do
   describe '#import' do
     context 'when fresh data available' do
-      let(:etag) { double }
+      let(:current_etag) { double }
 
       before do
         #
-        # Redis.current.get("remove_old_alliances:#{ I18n.locale }:etag")
+        # Redis.current.get("remove_old_alliances:#{ I18n.locale }:etag") # => current_etag
         #
         expect(Redis).to receive(:current) do
           double.tap do |a|
-            expect(a).to receive(:get).with("remove_old_alliances:#{ I18n.locale }:etag").and_return(etag)
+            expect(a).to receive(:get).with("remove_old_alliances:#{ I18n.locale }:etag").and_return(current_etag)
           end
         end
       end
 
-      let(:alliance_id) { double }
+      # let(:alliance_id) { double }
 
-      let(:new_etag) { double }
+      # let(:new_etag) { double }
 
-      let(:alliances) do
-        instance_double(EveOnline::ESI::Alliances,
-                        not_modified?: false,
-                        etag: new_etag,
-                        alliance_ids: [alliance_id])
-      end
+      # let(:eveonline_esi_alliances) do
+      #   instance_double(EveOnline::ESI::Alliances,
+      #                   not_modified?: false,
+      #                   etag: new_etag,
+      #                   alliance_ids: [alliance_id])
+      # end
 
-      before { expect(EveOnline::ESI::Alliances).to receive(:new).and_return(alliances) }
+      # before { expect(EveOnline::ESI::Alliances).to receive(:new).and_return(alliances) }
 
-      let(:alliance_ids) { double }
+      # let(:alliance_ids) { double }
 
-      before { expect(Eve::Alliance).to receive(:pluck).with(:alliance_id).and_return(alliance_ids) }
+      # before { expect(Eve::Alliance).to receive(:pluck).with(:alliance_id).and_return(alliance_ids) }
 
-      let(:alliance_id_to_remove) { double }
+      # let(:alliance_id_to_remove) { double }
 
-      let(:alliance_ids_to_remove) { [alliance_id_to_remove] }
+      # let(:alliance_ids_to_remove) { [alliance_id_to_remove] }
 
-      before { expect(alliance_ids).to receive(:-).with(alliance_ids).and_return(alliance_ids_to_remove) }
+      # before { expect(alliance_ids).to receive(:-).with(alliance_ids).and_return(alliance_ids_to_remove) }
 
-      let(:corporation_id) { double }
+      # let(:corporation_id) { double }
 
-      let(:corporation) { instance_double(Eve::Corporation, corporation_id: corporation_id) }
+      # let(:corporation) { instance_double(Eve::Corporation, corporation_id: corporation_id) }
 
-      let(:corporations) { [corporation] }
+      # let(:corporations) { [corporation] }
 
-      let(:alliance) { instance_double(Eve::Alliance, corporations: corporations) }
+      # let(:alliance) { instance_double(Eve::Alliance, corporations: corporations) }
 
-      before { expect(Eve::Alliance).to receive(:find_or_initialize_by).with(alliance_id: alliance_id_to_remove).and_return(alliance) }
+      # before { expect(Eve::Alliance).to receive(:find_or_initialize_by).with(alliance_id: alliance_id_to_remove).and_return(alliance) }
 
-      before do
-        #
-        # Eve::CorporationImporter.new(corporation.corporation_id).import
-        #
-        expect(Eve::CorporationImporter).to receive(:new).with(corporation_id) do
-          double.tap do |a|
-            expect(a).to receive(:import)
-          end
-        end
-      end
+      # before do
+      #   #
+      #   # Eve::CorporationImporter.new(corporation.corporation_id).import
+      #   #
+      #   expect(Eve::CorporationImporter).to receive(:new).with(corporation_id) do
+      #     double.tap do |a|
+      #       expect(a).to receive(:import)
+      #     end
+      #   end
+      # end
 
-      before { expect(alliance).to receive(:destroy!) }
+      # before { expect(alliance).to receive(:destroy!) }
 
-      before do
-        #
-        # Redis.current.set("remove_old_alliances:#{ I18n.locale }:etag", alliances.etag)
-        #
-        expect(Redis).to receive(:current) do
-          double.tap do |a|
-            expect(a).to receive(:set).with("remove_old_alliances:#{ I18n.locale }:etag", new_etag)
-          end
-        end
-      end
+      # before do
+      #   #
+      #   # Redis.current.set("remove_old_alliances:#{ I18n.locale }:etag", alliances.etag)
+      #   #
+      #   expect(Redis).to receive(:current) do
+      #     double.tap do |a|
+      #       expect(a).to receive(:set).with("remove_old_alliances:#{ I18n.locale }:etag", new_etag)
+      #     end
+      #   end
+      # end
 
-      specify { expect { subject.import }.not_to raise_error }
+      # specify { expect { subject.import }.not_to raise_error }
     end
 
     context 'when no fresh data available' do
@@ -83,7 +83,7 @@ describe Eve::RemoveOldAlliancesImporter do
 
       before do
         #
-        # Redis.current.get("remove_old_alliances:#{ I18n.locale }:etag")
+        # Redis.current.get("remove_old_alliances:#{ I18n.locale }:etag") # => current_etag
         #
         expect(Redis).to receive(:current) do
           double.tap do |a|
@@ -92,12 +92,12 @@ describe Eve::RemoveOldAlliancesImporter do
         end
       end
 
-      let(:alliances) do
+      let(:eveonline_esi_alliances) do
         instance_double(EveOnline::ESI::Alliances,
                         not_modified?: true)
       end
 
-      before { expect(EveOnline::ESI::Alliances).to receive(:new).with(etag: etag).and_return(alliances) }
+      before { expect(EveOnline::ESI::Alliances).to receive(:new).with(etag: etag).and_return(eveonline_esi_alliances) }
 
       specify { expect { subject.import }.not_to raise_error }
     end
