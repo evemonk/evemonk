2b6a2bb7ce5cc3484432fe650b1a01a918bee5f6
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-24 16:27:23 +0200

    Fix

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,9 @@
 const { environment } = require('@rails/webpacker')
+const erb =  require('./loaders/erb')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue =  require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+environment.loaders.prepend('erb', erb)
 module.exports = environment
