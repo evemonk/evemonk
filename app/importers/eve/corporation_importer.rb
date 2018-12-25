a42a12e9a469108e5e3e07163424ec1dc648981a
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-25 03:07:05 +0200

    Update

--- app/importers/eve/corporation_importer.rb
+++ app/importers/eve/corporation_importer.rb
@@ -11,13 +11,16 @@ module Eve
     def import
       eve_corporation = Eve::Corporation.find_or_initialize_by(corporation_id: corporation_id)
 
-      corporation = EveOnline::ESI::Corporation.new(corporation_id: corporation_id)
+      eveonline_esi_corporation = EveOnline::ESI::Corporation.new(alliance_id: alliance_id,
+                                                                  etag: eve_corporation.etag)
 
-      eve_corporation.assign_attributes(corporation.as_json)
+      return if eveonline_esi_corporation.not_modified?
+
+      eve_corporation.assign_attributes(eveonline_esi_corporation.as_json)
 
       eve_corporation.save! if eve_corporation.changed?
     rescue EveOnline::Exceptions::ResourceNotFound
-      eve_corporation.destroy if eve_corporation.persisted?
+      eve_corporation.destroy! if eve_corporation.persisted?
     end
   end
 end
