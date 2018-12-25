173218e43a0cf60982d0540f93c2236a0aefcc66
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-25 03:18:11 +0200

    More specs

--- spec/importers/eve/corporation_importer_spec.rb
+++ spec/importers/eve/corporation_importer_spec.rb
@@ -51,32 +51,40 @@ describe Eve::CorporationImporter do
         end
       end
 
-      context 'when alliance not found' do
-        # let(:alliance_id) { double }
+      context 'when corporation not found' do
+        let(:corporation_id) { double }
 
-        # subject { described_class.new(alliance_id) }
+        subject { described_class.new(corporation_id) }
 
-        # let(:etag) { double }
+        let(:etag) { double }
 
-        # before { expect(Eve::Alliance).to receive(:find_or_initialize_by).with(alliance_id: alliance_id).and_return(eve_alliance) }
+        before { expect(Eve::Corporation).to receive(:find_or_initialize_by).with(corporation_id: corporation_id).and_return(eve_corporation) }
 
-        # before { expect(EveOnline::ESI::Alliance).to receive(:new).and_raise(EveOnline::Exceptions::ResourceNotFound) }
+        before { expect(EveOnline::ESI::Corporation).to receive(:new).and_raise(EveOnline::Exceptions::ResourceNotFound) }
 
-        # context 'when alliance persisted' do
-        #   let(:eve_alliance) { instance_double(Eve::Alliance, persisted?: true, etag: etag) }
+        context 'when corporation persisted' do
+          let(:eve_corporation) do
+            instance_double(Eve::Corporation,
+                            persisted?: true,
+                            etag: etag)
+          end
 
-        #   before { expect(eve_alliance).to receive(:destroy!) }
+          before { expect(eve_corporation).to receive(:destroy!) }
 
-        #   specify { expect { subject.import }.not_to raise_error }
-        # end
+          specify { expect { subject.import }.not_to raise_error }
+        end
 
-        # context 'when alliance not persisted' do
-        #   let(:eve_alliance) { instance_double(Eve::Alliance, persisted?: false, etag: etag) }
+        context 'when corporation not persisted' do
+          let(:eve_corporation) do
+            instance_double(Eve::Corporation,
+                            persisted?: false,
+                            etag: etag)
+          end
 
-        #   before { expect(eve_alliance).not_to receive(:destroy!) }
+          before { expect(eve_corporation).not_to receive(:destroy!) }
 
-        #   specify { expect { subject.import }.not_to raise_error }
-        # end
+          specify { expect { subject.import }.not_to raise_error }
+        end
       end
     end
 
@@ -101,64 +109,4 @@ describe Eve::CorporationImporter do
       # specify { expect { subject.import }.not_to raise_error }
     end
   end
-
-  # describe '#import' do
-  #   context 'when import ok' do
-  #     let(:corporation_id) { double }
-
-  #     subject { described_class.new(corporation_id) }
-
-  #     before { expect(Eve::Corporation).to receive(:find_or_initialize_by).with(corporation_id: corporation_id).and_return(eve_corporation) }
-
-  #     let(:json) { double }
-
-  #     let(:corporation) { instance_double(EveOnline::ESI::Corporation, as_json: json) }
-
-  #     before { expect(EveOnline::ESI::Corporation).to receive(:new).with(corporation_id: corporation_id).and_return(corporation) }
-
-  #     before { expect(eve_corporation).to receive(:assign_attributes).with(json) }
-
-  #     context 'when corporation changed' do
-  #       let(:eve_corporation) { instance_double(Eve::Corporation, changed?: true) }
-
-  #       before { expect(eve_corporation).to receive(:save!) }
-
-  #       specify { expect { subject.import }.not_to raise_error }
-  #     end
-
-  #     context 'when corporation not changed' do
-  #       let(:eve_corporation) { instance_double(Eve::Corporation, changed?: false) }
-
-  #       before { expect(eve_corporation).not_to receive(:save!) }
-
-  #       specify { expect { subject.import }.not_to raise_error }
-  #     end
-  #   end
-
-  #   context 'when corporation not found' do
-  #     let(:corporation_id) { double }
-
-  #     subject { described_class.new(corporation_id) }
-
-  #     before { expect(Eve::Corporation).to receive(:find_or_initialize_by).with(corporation_id: corporation_id).and_return(eve_corporation) }
-
-  #     before { expect(EveOnline::ESI::Corporation).to receive(:new).and_raise(EveOnline::Exceptions::ResourceNotFound) }
-
-  #     context 'when corporation persisted' do
-  #       let(:eve_corporation) { instance_double(Eve::Corporation, persisted?: true) }
-
-  #       before { expect(eve_corporation).to receive(:destroy) }
-
-  #       specify { expect { subject.import }.not_to raise_error }
-  #     end
-
-  #     context 'when corporation not persisted' do
-  #       let(:eve_corporation) { instance_double(Eve::Corporation, persisted?: false) }
-
-  #       before { expect(eve_corporation).not_to receive(:destroy) }
-
-  #       specify { expect { subject.import }.not_to raise_error }
-  #     end
-  #   end
-  # end
 end
