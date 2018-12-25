30aa95f81b590ffb743f9f8cc97baa1cc65d227a
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-25 17:30:17 +0200

    More specs

--- spec/importers/eve/factions_importer_spec.rb
+++ spec/importers/eve/factions_importer_spec.rb
@@ -67,35 +67,28 @@ describe Eve::FactionsImporter do
       end
     end
 
+    context 'when no fresh data available' do
+      let(:current_etag) { double }
 
-    # let(:faction_id) { double }
+      before do
+        #
+        # Redis.current.get("factions:#{ I18n.locale }:etag") # => current_etag
+        #
+        expect(Redis).to receive(:current) do
+          double.tap do |a|
+            expect(a).to receive(:get).with("factions:#{ I18n.locale }:etag").and_return(current_etag)
+          end
+        end
+      end
 
-    # let(:as_json) { double }
+      let(:eveonline_esi_factions) do
+        instance_double(EveOnline::ESI::UniverseFactions,
+                        not_modified?: true)
+      end
 
-    # let(:faction) { instance_double(EveOnline::ESI::Models::Faction, faction_id: faction_id, as_json: as_json) }
+      before { expect(EveOnline::ESI::UniverseFactions).to receive(:new).with(etag: current_etag).and_return(eveonline_esi_factions) }
 
-    # let(:eveonline_factions) { instance_double(EveOnline::ESI::Factions, factions: [faction]) }
-
-    # before { expect(EveOnline::ESI::Factions).to receive(:new).and_return(eveonline_factions) }
-
-    # before { expect(Eve::Faction).to receive(:find_or_initialize_by).with(faction_id: faction_id).and_return(eve_faction) }
-
-    # before { expect(eve_faction).to receive(:assign_attributes).with(as_json) }
-
-    # context 'when faction changed' do
-    #   let(:eve_faction) { instance_double(Eve::Faction, changed?: true) }
-
-    #   before { expect(eve_faction).to receive(:save!) }
-
-    #   specify { expect { subject.import }.not_to raise_error }
-    # end
-
-    # context 'when faction not changed' do
-    #   let(:eve_faction) { instance_double(Eve::Faction, changed?: false) }
-
-    #   before { expect(eve_faction).not_to receive(:save!) }
-
-    #   specify { expect { subject.import }.not_to raise_error }
-    # end
+      specify { expect { subject.import }.not_to raise_error }
+    end
   end
 end
