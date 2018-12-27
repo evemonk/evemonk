e735de52fcab91926c148775b7e2639caf3d4ce9
Igor Zubkov <igor.zubkov@gmail.com>
2018-12-27 18:38:27 +0200

    Add Etag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_27_162342) do
+ActiveRecord::Schema.define(version: 2018_12_27_163439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,13 @@ ActiveRecord::Schema.define(version: 2018_12_27_162342) do
     t.index ["race_id"], name: "index_characters_on_race_id"
     t.index ["uid"], name: "index_characters_on_uid", unique: true
     t.index ["user_id"], name: "index_characters_on_user_id"
+  end
+
+  create_table "etags", force: :cascade do |t|
+    t.text "url", null: false
+    t.text "etag", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "eve_alliance_corporations", force: :cascade do |t|
