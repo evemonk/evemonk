ad974f979880c02b850aea4a25e60bdfd527438c
Igor Zubkov <igor.zubkov@gmail.com>
2019-01-02 02:39:02 +0200

    Eve::CorporationImporterWorker

---
+++ app/workers/eve/corporation_importer_worker.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Eve
+  class CorporationImporterWorker
+    include Sidekiq::Worker
+
+    def perform(corporation_id)
+      Eve::CorporationImporter.new(corporation_id).import
+    end
+  end
+end
