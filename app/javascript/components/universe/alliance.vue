d31747c75bc03611f67fa440bedba8f72a0ff189
Igor Zubkov <igor.zubkov@gmail.com>
2019-01-05 02:07:39 +0200

    Show alliance info

--- app/javascript/components/universe/alliance.vue
+++ app/javascript/components/universe/alliance.vue
@@ -1,6 +1,39 @@
 <template>
   <div id="alliance">
     <h1>Hello</h1>
+
+    <v-card>
+      <v-container fluid grid-list-lg>
+        <v-layout row wrap>
+          <v-flex xs12>
+            <v-card dark>
+              <v-layout>
+                <v-flex xs5>
+                  <v-img :src="alliance.icon" height="128px" contain></v-img>
+                </v-flex>
+                <v-flex xs7>
+                  <v-card-title primary-title>
+                    <div>
+                      <div class="headline">{{ alliance.name }}</div>
+                      <div>({{ alliance.ticker }})</div>
+                      <div>Founded in {{ alliance.date_founded }}</div>
+                    </div>
+                  </v-card-title>
+                </v-flex>
+              </v-layout>
+              <v-divider light></v-divider>
+              <v-card-actions>
+                <v-btn color="info">Corporations ({{ alliance.corporations_count }})</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn color="info">Characters ({{ alliance.characters_count }})</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+
+        </v-layout>
+      </v-container>
+    </v-card>
+
   </div>
 </template>
 
