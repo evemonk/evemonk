196b7f40ee48fc4a320f830aad129e040a15d04f
Igor Zubkov <igor.zubkov@gmail.com>
2019-01-06 00:59:14 +0200

    Rework

--- spec/controllers/auth/eve_online_sso/callbacks_controller_spec.rb
+++ spec/controllers/auth/eve_online_sso/callbacks_controller_spec.rb
@@ -2,51 +2,47 @@
 
 require 'rails_helper'
 
-module Auth
-  module EveOnlineSso
-    describe CallbacksController do
-      it { should be_a(ApplicationController) }
+describe Auth::EveOnlineSso::CallbacksController do
+  it { should be_a(ApplicationController) }
 
-      describe '#show' do
-        let(:eve_online) { double }
+  describe '#show' do
+    let(:form) { instance_double(Api::EveOnlineForm) }
 
-        before do
-          #
-          # Api::EveOnline.new(request) => eve_online
-          #
-          expect(Api::EveOnline).to receive(:new).with(any_args).and_return(eve_online)
-        end
-
-        before { expect(eve_online).to receive(:save!) }
-
-        let(:token) { double }
-
-        before do
-          #
-          # eve_online.session.token => token
-          #
-          expect(eve_online).to receive(:session) do
-            double.tap do |a|
-              expect(a).to receive(:token).and_return(token)
-            end
-          end
-        end
-
-        before do
-          #
-          # subject.cookies['auth_token'] = eve_online.session.token
-          #
-          expect(subject).to receive(:cookies) do
-            double.tap do |a|
-              expect(a).to receive(:[]=).with('auth_token', token)
-            end
-          end
-        end
-
-        before { get :show }
-
-        it { should respond_with(:found) }
-      end
+    before do
+      #
+      # Api::EveOnlineForm.new(request) => form
+      #
+      expect(Api::EveOnlineForm).to receive(:new).with(any_args).and_return(form)
     end
+
+    before { expect(form).to receive(:save!) }
+
+    # let(:token) { double }
+
+    # before do
+    #   #
+    #   # eve_online.session.token => token
+    #   #
+    #   expect(eve_online).to receive(:session) do
+    #     double.tap do |a|
+    #       expect(a).to receive(:token).and_return(token)
+    #     end
+    #   end
+    # end
+
+    # before do
+    #   #
+    #   # subject.cookies['auth_token'] = eve_online.session.token
+    #   #
+    #   expect(subject).to receive(:cookies) do
+    #     double.tap do |a|
+    #       expect(a).to receive(:[]=).with('auth_token', token)
+    #     end
+    #   end
+    # end
+
+    before { get :show }
+
+    it { should respond_with(:found) }
   end
 end
