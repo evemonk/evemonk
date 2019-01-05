7e0c1672453741c409457a6bc3233d24622360c9
Igor Zubkov <igor.zubkov@gmail.com>
2019-01-06 01:00:30 +0200

    WIP

--- spec/forms/api/eve_online_form_spec.rb
+++ spec/forms/api/eve_online_form_spec.rb
@@ -3,11 +3,15 @@
 require 'rails_helper'
 
 describe Api::EveOnlineForm do
+  let(:request) { double }
+
+  subject { described_class.new(request) }
+
   describe '#initialize' do
-    let(:request) { double }
-
-    subject { described_class.new(request) }
-
     its(:request) { should eq(request) }
+  end
+
+  describe '#save!' do
+    specify { expect { subject.save! }.not_to raise_error }
   end
 end
