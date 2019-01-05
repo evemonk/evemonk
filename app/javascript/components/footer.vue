959a55e92f75498fc7cfe72bf60f51bd5cceb2b7
Igor Zubkov <igor.zubkov@gmail.com>
2019-01-05 02:09:30 +0200

    Bump year

--- app/javascript/components/footer.vue
+++ app/javascript/components/footer.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer app dark height="auto">
     <v-card-text class="white--text">
-      &copy; 2016 &mdash; 2018 <strong>EveMonk</strong>
+      &copy; 2016 &mdash; 2019 <strong>EveMonk</strong>
     </v-card-text>
   </v-footer>
 </template>
