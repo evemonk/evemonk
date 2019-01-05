329e8ca158bbe423699f50cfcf888108faf540db
Igor Zubkov <igor.zubkov@gmail.com>
2019-01-05 21:13:28 +0200

    More specs

--- app/views/api/eve/alliance_characters/index.json.jbuilder
+++ app/views/api/eve/alliance_characters/index.json.jbuilder
@@ -7,23 +7,32 @@ json.characters do
   json.array! @characters do |character|
     json.id character.character_id
     json.icon "https://imageserver.eveonline.com/Character/#{ character.character_id }_512.jpg"
-    json.alliance_id character.alliance_id
-    json.ancestry_id character.ancestry_id
-    json.birthday character.birthday&.iso8601
-    json.bloodline_id character.bloodline_id
-    json.corporation_id character.corporation_id
+    json.birthday character.birthday.iso8601
     json.description character.description
-    json.faction_id character.faction_id
-    if character.faction
-      json.faction do
-        json.id character.faction.faction_id
-        json.name character.faction.name
-        json.description character.faction.description
-      end
-    end
     json.gender character.gender
     json.name character.name
-    json.race_id character.race_id
     json.security_status character.security_status.to_s
+
+    # if character.ancestry
+    #   json.ancestry do
+    #     json.id character.ancestry.ancestry_id
+    #     json.bloodline_id character.ancestry.bloodline_id
+    #     json.description character.ancestry.description
+    #     json.icon_id character.ancestry.icon_id
+    #     json.name character.ancestry.name
+    #     json.short_description character.ancestry.short_description
+    #   end
+    # end
+
+    # json.bloodline_id character.bloodline_id
+    # json.corporation_id character.corporation_id
+    # if character.faction
+    #   json.faction do
+    #     json.id character.faction.faction_id
+    #     json.name character.faction.name
+    #     json.description character.faction.description
+    #   end
+    # end
+    # json.race_id character.race_id
   end
 end
--- spec/requests/api/eve/alliance_characters_controller_spec.rb
+++ spec/requests/api/eve/alliance_characters_controller_spec.rb
@@ -8,14 +8,42 @@ describe Api::Eve::AllianceCharactersController do
       create(:eve_alliance,
              alliance_id: 123)
 
+      create(:eve_corporation,
+             alliance_id: 123,
+             corporation_id: 456)
+
+      create(:eve_alliance_corporation,
+             alliance_id: 123,
+             corporation_id: 456)
+
+      create(:eve_character,
+             alliance_id: 123,
+             character_id: 123_123_123,
+             corporation_id: 456,
+             name: 'Character name',
+             description: 'Character description',
+             gender: 'male',
+             birthday: 'Sun, 03 May 2015 19:45:17 UTC +00:00',
+             security_status: 1.1)
+
       get '/api/eve/alliances/123/characters'
 
       expect(response).to have_http_status(:ok)
 
-      expect(JSON.parse(response.body)).to eq('total_count' => 0,
-                                              'total_pages' => 0,
+      puts JSON.parse(response.body)
+
+      expect(JSON.parse(response.body)).to eq('total_count' => 1,
+                                              'total_pages' => 1,
                                               'current_page' => 1,
-                                              'characters' => [])
+                                              'characters' => [{
+                                                'id' => 123_123_123,
+                                                'icon' => 'https://imageserver.eveonline.com/Character/123123123_512.jpg',
+                                                'birthday' => '2015-05-03T19:45:17Z',
+                                                'description' => 'Character description',
+                                                'gender' => 'male',
+                                                'name' => 'Character name',
+                                                'security_status' => '1.1'
+                                              }])
     end
   end
 end
