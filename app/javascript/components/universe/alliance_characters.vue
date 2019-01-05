878079192192c6ea00d36770bf229c16573431f7
Igor Zubkov <igor.zubkov@gmail.com>
2019-01-05 23:43:22 +0200

    More info

--- app/javascript/components/universe/alliance_characters.vue
+++ app/javascript/components/universe/alliance_characters.vue
@@ -6,7 +6,40 @@
 
     <v-pagination v-model="current_page" :length="total_pages"></v-pagination>
 
-    <div>{{ characters.length }}</div>
+    <v-card>
+      <v-container fluid grid-list-lg>
+        <v-layout row wrap>
+          <v-flex xs12 v-for="character in characters" :key="character.id">
+            <v-card dark>
+              <v-layout>
+                <v-flex xs5>
+                  <v-img :src="character.icon" height="128px" contain></v-img>
+                </v-flex>
+                <v-flex xs7>
+                  <v-card-title primary-title>
+                    <div>
+                      <div class="headline">{{ character.name }}</div>
+                      <div>{{ character.corporation.name }} / {{ character.alliance.name }}</div>
+                      <div>Birthday {{ character.birthday }}</div>
+                    </div>
+                  </v-card-title>
+                </v-flex>
+              </v-layout>
+              <v-divider light></v-divider>
+              <!-- <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="info">
+                  <router-link :to="{ name: 'character', params: { id: character.id }}">
+                    More
+                  </router-link>
+                </v-btn>
+              </v-card-actions> -->
+            </v-card>
+          </v-flex>
+
+        </v-layout>
+      </v-container>
+    </v-card>
 
     <v-pagination v-model="current_page" :length="total_pages"></v-pagination>
   </div>
