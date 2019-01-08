52b990aa64f8ce9056cb78d00c1381d590e89069
Igor Zubkov <igor.zubkov@gmail.com>
2019-01-09 00:12:36 +0200

    CharacterAttributesImporter

---
+++ app/importers/character_attributes_importer.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class CharacterAttributesImporter
+  attr_reader :character_id
+
+  def initialize(character_id)
+    @character_id = character_id
+  end
+
+  def import
+    character = Character.find_by!(character_id: character_id)
+
+    esi = EveOnline::ESI::CharacterAttributes.new(character_id: character_id,
+                                                  token: character.access_token)
+
+    character.update!(esi.as_json)
+  rescue EveOnline::Exceptions::ResourceNotFound
+    character.destroy!
+  rescue ActiveRecord::RecordNotFound
+    Rails.logger.info("Character with ID #{ character_id } not found")
+  end
+end
