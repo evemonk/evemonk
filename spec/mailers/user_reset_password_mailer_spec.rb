53a432c04ed6e183240625e9bf0145489d75434f
Igor Zubkov <igor.zubkov@gmail.com>
2019-02-01 01:30:50 +0200

    Add mailer spec

--- spec/mailers/user_reset_password_mailer_spec.rb
+++ spec/mailers/user_reset_password_mailer_spec.rb
@@ -1,5 +1,23 @@
-require "rails_helper"
+# frozen_string_literal: true
 
-RSpec.describe UserResetPasswordMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+require 'rails_helper'
+
+describe UserResetPasswordMailer do
+  describe '#email' do
+    let(:user) do
+      create(:user,
+             email: 'me@example.com',
+             reset_password_token: 'reset-token-123')
+    end
+
+    subject { described_class.with(user: user).email }
+
+    specify { expect(subject.subject).to eq('Reset password at evemonk.com') }
+
+    specify { expect(subject.to).to eq(['me@example.com']) }
+
+    specify { expect(subject.from).to eq(['from@example.com']) }
+
+    specify { expect(subject.body).to include('reset-token-123') }
+  end
 end
