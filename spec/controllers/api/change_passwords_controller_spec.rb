73edd9adc6b8f238a5dee1ddcdd52f331afaf0a9
Igor Zubkov <igor.zubkov@gmail.com>
2019-01-31 15:24:54 +0200

    More specs

--- spec/controllers/api/change_passwords_controller_spec.rb
+++ spec/controllers/api/change_passwords_controller_spec.rb
@@ -55,11 +55,53 @@ describe Api::ChangePasswordsController do
       it { should render_template(:create) }
     end
 
-    # context 'when user not successful change password' do
-    #   let(:form) { instance_double(Api::ChangePasswordForm, save: false) }
-    #
-    # end
-    #
+    context 'when user not successful change password' do
+      let(:current_user) { instance_double(User) }
+
+      before { sign_in(current_user) }
+
+      let(:form) { instance_double(Api::ChangePasswordForm, save: false) }
+
+      before do
+        #
+        # Api::ChangePasswordForm.new(old_password: 'old_password',
+        #                             password: 'new_password',
+        #                             password_confirmation: 'new_password',
+        #                             name: 'iOS session',
+        #                             device_type: 'ios',
+        #                             device_token: 'token123',
+        #                             user: current_user) # => form
+        #
+        expect(Api::ChangePasswordForm).to receive(:new)
+          .with(permitter(old_password: 'old_password',
+                          password: 'new_password',
+                          password_confirmation: 'new_password',
+                          name: 'iOS session',
+                          device_type: 'ios',
+                          device_token: 'token123',
+                          user: current_user))
+          .and_return(form)
+      end
+
+      before do
+        post :create, params: {
+          change_password: {
+            old_password: 'old_password',
+            password: 'new_password',
+            password_confirmation: 'new_password',
+            name: 'iOS session',
+            device_type: 'ios',
+            device_token: 'token123'
+          },
+          format: :json
+        }
+      end
+
+      it { should respond_with(:unprocessable_entity) }
+
+      it { should render_template(:errors) }
+    end
+
     # context 'when user not signed in' do
     #   before { post :create, params: { format: :json } }
     #
