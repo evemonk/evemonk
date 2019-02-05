7db5ec69f2c5fb6ee428b9337ce991f6a3748bd5
Igor Zubkov <igor.zubkov@gmail.com>
2019-02-06 01:30:07 +0200

    Api::EndAllUserSessions

---
+++ spec/services/api/end_all_user_sessions_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Api::EndAllUserSessions do
+  describe '#initialize' do
+    let(:user) { instance_double(User) }
+
+    subject { described_class.new(user) }
+
+    its(:user) { should eq(user) }
+  end
+
+  describe '#execute' do
+    let!(:user) { create(:user) }
+
+    let!(:session1) { create(:session, user: user) }
+
+    let!(:session2) { create(:session, user: user) }
+
+    let!(:session3) { create(:session, user: user) }
+
+    subject { described_class.new(user) }
+
+    specify { expect { subject.execute }.to change { user.sessions.count }.from(3).to(0) }
+  end
+end
