c420d11bf56dce6f515c192dd075c4113a0da837
Igor Zubkov <igor.zubkov@gmail.com>
2019-02-06 16:43:27 +0200

    Finish Api::ResetPasswordsController

--- app/controllers/api/reset_passwords_controller.rb
+++ app/controllers/api/reset_passwords_controller.rb
@@ -2,7 +2,25 @@
 
 module Api
   class ResetPasswordsController < BaseController
+    skip_before_action :authenticate
+
     def create
+      @form = ResetPasswordForm.new(resource_params)
+
+      skip_authorization
+
+      if @form.save
+        render :create
+      else
+        render :errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def resource_params
+      params.require(:reset_password)
+            .permit(:reset_password_token, :password, :password_confirmation)
     end
   end
 end
