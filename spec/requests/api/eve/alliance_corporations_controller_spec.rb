ee9aebf493499649de038bf4ca1b9c5e60e53921
Igor Zubkov <igor.zubkov@gmail.com>
2019-02-12 12:31:53 +0200

    Show Alliance corporations

--- spec/requests/api/eve/alliance_corporations_controller_spec.rb
+++ spec/requests/api/eve/alliance_corporations_controller_spec.rb
@@ -6,23 +6,31 @@ describe Api::Eve::AllianceCorporationsController do
   describe '#index' do
     it 'returns list of Eve Alliance Corporations' do
       create(:eve_alliance,
+             alliance_id: 123)
+
+      create(:eve_corporation,
              alliance_id: 123,
-             creator_corporation_id: 2222,
-             creator_id: 3333,
+             corporation_id: 456,
+             name: 'Character corporation name',
+             ticker: 'TICKER',
              date_founded: 'Sun, 03 May 2015 19:45:17 UTC +00:00',
-             executor_corporation_id: 4444,
-             faction_id: 5555,
-             name: 'Alliance name',
-             ticker: 'ALLIANCE_TICKER')
+             member_count: 2000)
 
       get '/api/eve/alliances/123/corporations'
 
       expect(response).to have_http_status(:ok)
 
-      expect(JSON.parse(response.body)).to eq('total_count' => 0,
-                                              'total_pages' => 0,
+      expect(JSON.parse(response.body)).to eq('total_count' => 1,
+                                              'total_pages' => 1,
                                               'current_page' => 1,
-                                              'corporations' => [])
+                                              'corporations' => [{
+                                                'date_founded' => '2015-05-03T19:45:17Z',
+                                                'icon' => 'https://imageserver.eveonline.com/Corporation/456_256.png',
+                                                'id' => 456,
+                                                'member_count' => 2000,
+                                                'name' => 'Character corporation name',
+                                                'ticker' => 'TICKER'
+                                              }])
     end
 
     it 'returns 404' do
