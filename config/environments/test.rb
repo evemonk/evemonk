22847e981d65f26196bbc44dd16557b95d9efbf5
Igor Zubkov <igor.zubkov@gmail.com>
2019-02-26 14:07:54 +0200

    Fix deprecation

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Prevent expensive template finalization at end of test suite runs.
-  config.action_view.finalize_compiled_template_methods = false
+  # config.action_view.finalize_compiled_template_methods = false
 
   config.active_job.queue_adapter = :inline
 end
