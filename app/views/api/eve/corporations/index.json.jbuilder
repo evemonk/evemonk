b6954202ed700e93846d7f821efcc393787b0690
Igor Zubkov <igor.zubkov@gmail.com>
2019-03-17 22:59:15 +0200

    Update view

--- app/views/api/eve/corporations/index.json.jbuilder
+++ app/views/api/eve/corporations/index.json.jbuilder
@@ -6,6 +6,10 @@ json.current_page @corporations.current_page
 json.corporations do
   json.array! @corporations do |corporation|
     json.id corporation.corporation_id
-    json.icon "http://imageserver.eveonline.com/Corporation/#{ corporation.corporation_id }_256.png"
+    json.icon "https://imageserver.eveonline.com/Corporation/#{ corporation.corporation_id }_256.png"
+    json.date_founded corporation.date_founded.iso8601
+    json.name corporation.name
+    json.member_count corporation.member_count
+    json.ticker corporation.ticker
   end
 end
--- spec/requests/api/eve/corporations_controller_spec.rb
+++ spec/requests/api/eve/corporations_controller_spec.rb
@@ -7,18 +7,14 @@ describe Api::Eve::CorporationsController do
     it 'returns list of Eve Corporations' do
       create(:eve_alliance,
              alliance_id: 123)
-             # creator_corporation_id: 123_123,
-             # creator_id: 123_123_123,
-             # date_founded: 'Sun, 03 May 2015 19:45:17 UTC +00:00',
-             # executor_corporation_id: 123_123_123_123,
-             # faction_id: 777,
-             # name: 'Alliance name',
-             # ticker: 'ABCDE')
 
       create(:eve_corporation,
              alliance_id: 123,
-             corporation_id: 456)
-             # member_count: 1234)
+             corporation_id: 456,
+             name: 'Character corporation name',
+             ticker: 'TICKER',
+             date_founded: 'Sun, 03 May 2015 19:45:17 UTC +00:00',
+             member_count: 2000)
 
       get '/api/eve/corporations'
 
@@ -28,9 +24,19 @@ describe Api::Eve::CorporationsController do
                                               'total_pages' => 1,
                                               'current_page' => 1,
                                               'corporations' => [{
+                                                'date_founded' => '2015-05-03T19:45:17Z',
+                                                'icon' => 'https://imageserver.eveonline.com/Corporation/456_256.png',
                                                 'id' => 456,
-                                                'icon' => 'http://imageserver.eveonline.com/Corporation/456_256.png'
+                                                'member_count' => 2000,
+                                                'name' => 'Character corporation name',
+                                                'ticker' => 'TICKER'
                                               }])
+    end
+
+    it 'returns 404' do
+      get '/api/eve/alliances/123'
+
+      expect(response).to have_http_status(:not_found)
     end
   end
 end
