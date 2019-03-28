dfd910848de331e410f6ef448a8bfc6622494998
Igor Zubkov <igor.zubkov@gmail.com>
2019-03-28 12:02:04 +0200

    Disable sentry if env SENTRY_DSN is empty

--- config/initializers/sentry-raven.rb
+++ config/initializers/sentry-raven.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-Raven.configure do |config|
-  config.dsn = ENV.fetch('SENTRY_DSN')
+if Rails.env.production? && ENV['SENTRY_DSN']
+  Raven.configure do |config|
+    config.dsn = ENV['SENTRY_DSN']
 
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  end
 end
