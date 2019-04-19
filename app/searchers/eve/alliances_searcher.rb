a66f251824c2529fc96ffeee8a2dafed80b08b45
Igor Zubkov <igor.zubkov@gmail.com>
2019-04-19 12:41:07 +0300

    Eve::AlliancesSearcher

---
+++ app/searchers/eve/alliances_searcher.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Eve
+  class AlliancesSearcher
+    attr_reader :q, :scope
+
+    def initialize(q, scope = Eve::Alliance.all)
+      @q = q
+      @scope = scope
+    end
+
+    def query
+      ids = Eve::Alliance.search(q, load: false).map(&:id) if q.present?
+
+      relation = scope.includes(:faction, :creator_corporation,
+                                     :creator, :executor_corporation)
+
+      relation = relation.where(id: ids) if ids.present?
+
+      relation
+    end
+  end
+end
