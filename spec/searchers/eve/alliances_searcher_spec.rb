ac4fdfca6f8aec69a143cf4d9b5fe94b97f4934e
Igor Zubkov <igor.zubkov@gmail.com>
2019-04-22 22:09:49 +0300

    More specs

--- spec/searchers/eve/alliances_searcher_spec.rb
+++ spec/searchers/eve/alliances_searcher_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 describe Eve::AlliancesSearcher do
   describe '#initialize' do
-    context 'when scope present' do
+    context 'with parameters' do
       let(:q) { double }
 
       let(:scope) { double }
@@ -16,16 +16,14 @@ describe Eve::AlliancesSearcher do
       its(:scope) { should eq(scope) }
     end
 
-    context 'when scope not present' do
-      let(:q) { double }
-
+    context 'without parameters' do
       let(:scope) { double }
 
       before { expect(Eve::Alliance).to receive(:all).and_return(scope) }
 
-      subject { described_class.new(q) }
+      subject { described_class.new }
 
-      its(:q) { should eq(q) }
+      its(:q) { should eq(nil) }
 
       its(:scope) { should eq(scope) }
     end
@@ -33,9 +31,45 @@ describe Eve::AlliancesSearcher do
 
   describe '#query' do
     context 'when q is empty' do
+      let!(:alliance1) { create(:eve_alliance) }
+
+      let!(:alliance2) { create(:eve_alliance) }
+
+      let!(:alliance3) { create(:eve_alliance) }
+
+      subject { described_class.new }
+
+      specify { expect(subject.query.count).to eq(3) }
+
+      specify { expect(subject.query.respond_to?(:all)).to eq(true) }
+
+      specify { expect(subject.query.to_a).to include(alliance1, alliance2, alliance3) }
     end
 
     context 'when q is present' do
+      context 'when name match' do
+        let!(:alliance) { create(:eve_alliance, name: 'Northern Coalition.', ticker: nil) }
+
+        let(:q) { 'Northern Coalition.' }
+
+        before { Eve::Alliance.reindex }
+
+        subject { described_class.new(q) }
+
+        specify { expect(subject.query.to_a).to eq([alliance]) }
+      end
+
+      context 'when ticker: match' do
+        let!(:alliance) { create(:eve_alliance, name: nil, ticker: 'NC') }
+
+        let(:q) { 'NC' }
+
+        before { Eve::Alliance.reindex }
+
+        subject { described_class.new(q) }
+
+        specify { expect(subject.query.to_a).to eq([alliance]) }
+      end
     end
   end
 end
