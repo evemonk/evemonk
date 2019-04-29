15c204b870fad851a65175f94f06bdd6a22146dc
Igor Zubkov <igor.zubkov@gmail.com>
2019-04-29 16:24:47 +0300

    Fix specs

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,4 +4,8 @@ VCR.configure do |c|
   c.cassette_library_dir = Rails.root.join('spec', 'cassettes')
   c.hook_into :webmock
   c.ignore_localhost = true
+  c.ignore_hosts('chromedriver.storage.googleapis.com',
+                 'github.com/mozilla/geckodriver/releases',
+                 'selenium-release.storage.googleapis.com',
+                 'developer.microsoft.com/en-us/microsoft-edge/tools/webdriver')
 end
