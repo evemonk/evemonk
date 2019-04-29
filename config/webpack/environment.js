8e08444f9e6877126003c7ceaf05819a1892eed4
Igor Zubkov <igor.zubkov@gmail.com>
2019-04-29 15:34:26 +0300

    Sync with new rails

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,7 @@
 const { environment } = require('@rails/webpacker')
-const erb =  require('./loaders/erb')
+const erb = require('./loaders/erb')
 const { VueLoaderPlugin } = require('vue-loader')
-const vue =  require('./loaders/vue')
+const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
