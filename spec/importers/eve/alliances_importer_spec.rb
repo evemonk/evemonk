e75332a308b78d26abe68bf5456c0bf406615ea0
Igor Zubkov <igor.zubkov@gmail.com>
2019-05-14 21:46:16 +0300

    Eve::AlliancesImporter

--- spec/importers/eve/alliances_importer_spec.rb
+++ spec/importers/eve/alliances_importer_spec.rb
@@ -3,57 +3,134 @@
 require 'rails_helper'
 
 describe Eve::AlliancesImporter do
-  describe '#import' do
-    context 'when fresh data available' do
+  describe '#initialize' do
+    let(:esi) { instance_double(EveOnline::ESI::Alliances) }
+
+    before { expect(EveOnline::ESI::Alliances).to receive(:new).and_return(esi) }
+
+    its(:esi) { should eq(esi) }
+  end
+
+  describe '#etag' do
+    context 'when @model set' do
+      let(:etag) { instance_double(Etag) }
+
+      before { subject.instance_variable_set(:@etag, etag) }
+
+      specify { expect(subject.etag).to eq(etag) }
+    end
+
+    context 'when @etag not set' do
       let(:url) { double }
 
-      let(:new_etag) { double }
+      let(:esi) { instance_double(EveOnline::ESI::Alliances, url: url) }
 
-      let(:alliance_id) { double }
-
-      let(:esi) do
-        instance_double(EveOnline::ESI::Alliances,
-                        not_modified?: false,
-                        url: url,
-                        etag: new_etag,
-                        alliance_ids: [alliance_id])
-      end
+      let(:etag) { instance_double(Etag) }
 
       before { expect(EveOnline::ESI::Alliances).to receive(:new).and_return(esi) }
 
-      let(:etag) { instance_double(Etag, etag: '97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a') }
-
       before { expect(Etag).to receive(:find_or_initialize_by).with(url: url).and_return(etag) }
 
-      before { expect(esi).to receive(:etag=).with('97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a') }
+      specify { expect { subject.etag }.not_to raise_error }
 
-      before { expect(Eve::AllianceImporterWorker).to receive(:perform_async).with(alliance_id) }
+      specify { expect { subject.etag }.to change { subject.instance_variable_get(:@etag) }.from(nil).to(etag) }
+    end
+  end
 
-      before { expect(etag).to receive(:update!).with(etag: new_etag) }
+  # def import
+  #   esi.etag = etag.etag
+  #
+  #   return if esi.not_modified?
+  #
+  #   import_new_alliances
+  #
+  #   remove_old_alliances
+  #
+  #   etag.update!(etag: esi.etag)
+  # end
 
-      specify { expect { subject.import }.not_to raise_error }
+
+  describe '#import' do
+    context 'when fresh data available' do
+
     end
 
     context 'when no fresh data available' do
-      let(:url) { double }
+      let(:etag) { instance_double(Etag, etag: '97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a') }
 
       let(:esi) do
         instance_double(EveOnline::ESI::Alliances,
-                        not_modified?: true,
-                        url: url)
+                        not_modified?: true)
       end
 
       before { expect(EveOnline::ESI::Alliances).to receive(:new).and_return(esi) }
 
-      let(:etag) { instance_double(Etag, etag: '97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a') }
-
-      before { expect(Etag).to receive(:find_or_initialize_by).with(url: url).and_return(etag) }
+      before { expect(subject).to receive(:etag).and_return(etag) }
 
       before { expect(esi).to receive(:etag=).with('97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a') }
 
-      before { expect(Eve::AllianceImporterWorker).not_to receive(:perform_async) }
+      before { expect(subject).not_to receive(:import_new_alliances) }
+
+      before { expect(subject).not_to receive(:remove_old_alliances) }
+
+      before { expect(etag).not_to receive(:update!) }
 
       specify { expect { subject.import }.not_to raise_error }
     end
   end
+
+
+  # describe '#import' do
+  #   context 'when fresh data available' do
+  #     let(:url) { double }
+  #
+  #     let(:new_etag) { double }
+  #
+  #     let(:alliance_id) { double }
+  #
+  #     let(:esi) do
+  #       instance_double(EveOnline::ESI::Alliances,
+  #                       not_modified?: false,
+  #                       url: url,
+  #                       etag: new_etag,
+  #                       alliance_ids: [alliance_id])
+  #     end
+  #
+  #     before { expect(EveOnline::ESI::Alliances).to receive(:new).and_return(esi) }
+  #
+  #     let(:etag) { instance_double(Etag, etag: '97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a') }
+  #
+  #     before { expect(Etag).to receive(:find_or_initialize_by).with(url: url).and_return(etag) }
+  #
+  #     before { expect(esi).to receive(:etag=).with('97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a') }
+  #
+  #     before { expect(Eve::AllianceImporterWorker).to receive(:perform_async).with(alliance_id) }
+  #
+  #     before { expect(etag).to receive(:update!).with(etag: new_etag) }
+  #
+  #     specify { expect { subject.import }.not_to raise_error }
+  #   end
+  #
+  #   context 'when no fresh data available' do
+  #     let(:url) { double }
+  #
+  #     let(:esi) do
+  #       instance_double(EveOnline::ESI::Alliances,
+  #                       not_modified?: true,
+  #                       url: url)
+  #     end
+  #
+  #     before { expect(EveOnline::ESI::Alliances).to receive(:new).and_return(esi) }
+  #
+  #     let(:etag) { instance_double(Etag, etag: '97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a') }
+  #
+  #     before { expect(Etag).to receive(:find_or_initialize_by).with(url: url).and_return(etag) }
+  #
+  #     before { expect(esi).to receive(:etag=).with('97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a') }
+  #
+  #     before { expect(Eve::AllianceImporterWorker).not_to receive(:perform_async) }
+  #
+  #     specify { expect { subject.import }.not_to raise_error }
+  #   end
+  # end
 end
