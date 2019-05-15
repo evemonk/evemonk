91b74823b8546e5267b710b99d4b1d730e31f87d
Igor Zubkov <igor.zubkov@gmail.com>
2019-05-15 21:40:10 +0300

    More specs

--- spec/importers/eve/alliance_corporations_importer_spec.rb
+++ spec/importers/eve/alliance_corporations_importer_spec.rb
@@ -119,4 +119,53 @@ describe Eve::AllianceCorporationsImporter do
       specify { expect { subject.send(:etag) }.to change { subject.instance_variable_get(:@etag) }.from(nil).to(etag) }
     end
   end
+
+  describe '#eve_alliance' do
+    context 'when @eve_alliance set' do
+      let(:alliance_id) { double }
+
+      subject { described_class.new(alliance_id) }
+
+      let(:eve_alliance) { instance_double(Eve::Alliance) }
+
+      before { subject.instance_variable_set(:@eve_alliance, eve_alliance) }
+
+      specify { expect(subject.send(:eve_alliance)).to eq(eve_alliance) }
+    end
+
+    context 'when @eve_alliance not set' do
+      let(:alliance_id) { double }
+
+      subject { described_class.new(alliance_id) }
+
+      let(:eve_alliance) { instance_double(Eve::Alliance) }
+
+      before { expect(Eve::Alliance).to receive(:find_by!).with(alliance_id: alliance_id).and_return(eve_alliance) }
+
+      specify { expect { subject.send(:eve_alliance) }.not_to raise_error }
+
+      specify { expect { subject.send(:eve_alliance) }.to change { subject.instance_variable_get(:@eve_alliance) }.from(nil).to(eve_alliance) }
+    end
+
+    context 'when alliance not found' do
+      let(:alliance_id) { double }
+
+      subject { described_class.new(alliance_id) }
+
+      before { expect(Eve::Alliance).to receive(:find_by!).with(alliance_id: alliance_id).and_raise(ActiveRecord::RecordNotFound) }
+
+      before do
+        #
+        # Rails.logger.info("Alliance with ID #{ alliance_id } not found")
+        #
+        expect(Rails).to receive(:logger) do
+          double.tap do |a|
+            expect(a).to receive(:info).with("Alliance with ID #{ alliance_id } not found")
+          end
+        end
+      end
+
+      specify { expect { subject.send(:eve_alliance) }.not_to raise_error }
+    end
+  end
 end
