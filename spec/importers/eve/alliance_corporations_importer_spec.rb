f601b0c51ea322ec6f87eaf8479a0a39dd56ecb1
Igor Zubkov <igor.zubkov@gmail.com>
2019-05-15 22:54:59 +0300

    More specs

--- spec/importers/eve/alliance_corporations_importer_spec.rb
+++ spec/importers/eve/alliance_corporations_importer_spec.rb
@@ -68,70 +68,6 @@ describe Eve::AllianceCorporationsImporter do
     end
   end
 
-  # describe '#import' do
-  #   let(:alliance_id) { double }
-  #
-  #   subject { described_class.new(alliance_id) }
-  #
-  #   context 'when import ok' do
-  #     let(:alliance_id) { double }
-  #
-  #     subject { described_class.new(alliance_id) }
-  #
-  #     let(:eve_alliance) { instance_double(Eve::Alliance) }
-  #
-  #     before { expect(Eve::Alliance).to receive(:find_by!).with(alliance_id: alliance_id).and_return(eve_alliance) }
-  #
-  #     let(:remote_corporation_id) { double }
-  #
-  #     let(:remote_corporation_ids) { [remote_corporation_id] }
-  #
-  #     let(:esi) { instance_double(EveOnline::ESI::AllianceCorporations, corporation_ids: remote_corporation_ids) }
-  #
-  #     before { expect(EveOnline::ESI::AllianceCorporations).to receive(:new).with(alliance_id: alliance_id).and_return(esi) }
-  #
-  #     let(:local_corporation_id) { double }
-  #
-  #     let(:local_corporation_ids) { [local_corporation_id] }
-  #
-  #     before do
-  #       expect(eve_alliance).to receive(:alliance_corporations) do
-  #         double.tap do |a|
-  #           expect(a).to receive(:pluck).with(:corporation_id).and_return(local_corporation_ids)
-  #         end
-  #       end
-  #     end
-  #
-  #     before do
-  #       #
-  #       # eve_alliance.alliance_corporations.create!(corporation_id: corporation_id)
-  #       #
-  #       expect(eve_alliance).to receive(:alliance_corporations) do
-  #         double.tap do |a|
-  #           expect(a).to receive(:create!).with(corporation_id: remote_corporation_id)
-  #         end
-  #       end
-  #     end
-  #
-  #     before do
-  #       #
-  #       # eve_alliance.alliance_corporations.where(corporation_id: corporation_id).destroy_all
-  #       #
-  #       expect(eve_alliance).to receive(:alliance_corporations) do
-  #         double.tap do |a|
-  #           expect(a).to receive(:where).with(corporation_id: local_corporation_id) do
-  #             double.tap do |b|
-  #               expect(b).to receive(:destroy_all)
-  #             end
-  #           end
-  #         end
-  #       end
-  #     end
-  #
-  #     specify { expect { subject.import }.not_to raise_error }
-  #   end
-  # end
-
   # private methods
 
   describe '#import_new_corporations' do
@@ -181,15 +117,55 @@ describe Eve::AllianceCorporationsImporter do
   end
 
 
-  # def remove_old_corporations
-  #   corporation_ids = eve_alliance.alliance_corporations.pluck(:corporation_id) - esi.corporation_ids
-  #
-  #   eve_alliance.alliance_corporations.where(corporation_id: corporation_ids).destroy_all
-  # end
+  describe '#remove_old_corporations' do
+    let(:remote_corporation_id) { double }
 
-  # describe '#remove_old_corporations' do
-  #   specify { expect { subject.send(:remove_old_corporations) }.not_to raise_error }
-  # end
+    let(:remote_corporation_ids) { [remote_corporation_id] }
+
+    let(:esi) do
+      instance_double(EveOnline::ESI::AllianceCorporations,
+                      not_modified?: false,
+                      corporation_ids: remote_corporation_ids)
+    end
+
+    before { expect(EveOnline::ESI::AllianceCorporations).to receive(:new).with(alliance_id: alliance_id).and_return(esi) }
+
+    let(:eve_alliance) { instance_double(Eve::Alliance) }
+
+    before { expect(subject).to receive(:eve_alliance).and_return(eve_alliance).twice }
+
+    let(:local_corporation_id) { double }
+
+    let(:local_corporation_ids) { [local_corporation_id] }
+
+    before do
+      #
+      # eve_alliance.alliance_corporations.pluck(:corporation_id)
+      #
+      expect(eve_alliance).to receive(:alliance_corporations) do
+        double.tap do |a|
+          expect(a).to receive(:pluck).with(:corporation_id).and_return(local_corporation_ids)
+        end
+      end
+    end
+
+    before do
+      #
+      # eve_alliance.alliance_corporations.where(corporation_id: corporation_ids).destroy_all
+      #
+      expect(eve_alliance).to receive(:alliance_corporations) do
+        double.tap do |a|
+          expect(a).to receive(:where).with(corporation_id: local_corporation_ids) do
+            double.tap do |b|
+              expect(b).to receive(:destroy_all)
+            end
+          end
+        end
+      end
+    end
+
+    specify { expect { subject.send(:remove_old_corporations) }.not_to raise_error }
+  end
 
   describe '#etag' do
     context 'when @etag set' do
