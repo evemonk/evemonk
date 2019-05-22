0667cf522c8a77eb501c86a74da26ab3f070fb6f
Igor Zubkov <igor.zubkov@gmail.com>
2019-05-22 21:04:09 +0300

    Cleanup

--- spec/importers/eve/graphics_importer_spec.rb
+++ spec/importers/eve/graphics_importer_spec.rb
@@ -135,18 +135,6 @@ describe Eve::GraphicsImporter do
     end
   end
 
-  # def remove_old_graphics
-  #   eve_graphic_ids = Eve::Graphic.pluck(:graphic_id)
-  #
-  #   graphic_ids_to_remove = eve_graphic_ids - esi.graphic_ids
-  #
-  #   graphic_ids_to_remove.each do |graphic_id|
-  #     eve_graphic = Eve::Graphic.find_or_initialize_by(graphic_id: graphic_id)
-  #
-  #     eve_graphic.destroy!
-  #   end
-  # end
-
   describe '#remove_old_graphics' do
     let(:graphic_id) { double }
 
@@ -177,59 +165,4 @@ describe Eve::GraphicsImporter do
 
     specify { expect { subject.send(:remove_old_graphics) }.not_to raise_error }
   end
-
-
-  # describe '#import' do
-  #   context 'when fresh data available' do
-  #     let(:url) { double }
-  #
-  #     let(:new_etag) { double }
-  #
-  #     let(:graphic_id) { double }
-  #
-  #     let(:esi) do
-  #       instance_double(EveOnline::ESI::UniverseGraphics,
-  #                       not_modified?: false,
-  #                       url: url,
-  #                       etag: new_etag,
-  #                       graphic_ids: [graphic_id])
-  #     end
-  #
-  #     before { expect(EveOnline::ESI::UniverseGraphics).to receive(:new).and_return(esi) }
-  #
-  #     let(:etag) { instance_double(Etag, etag: 'e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b') }
-  #
-  #     before { expect(Etag).to receive(:find_or_initialize_by).with(url: url).and_return(etag) }
-  #
-  #     before { expect(esi).to receive(:etag=).with('e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b') }
-  #
-  #     before { expect(Eve::GraphicImporterWorker).to receive(:perform_async).with(graphic_id) }
-  #
-  #     before { expect(etag).to receive(:update!).with(etag: new_etag) }
-  #
-  #     specify { expect { subject.import }.not_to raise_error }
-  #   end
-  #
-  #   context 'when no fresh data available' do
-  #     let(:url) { double }
-  #
-  #     let(:esi) do
-  #       instance_double(EveOnline::ESI::UniverseGraphics,
-  #                       not_modified?: true,
-  #                       url: url)
-  #     end
-  #
-  #     before { expect(EveOnline::ESI::UniverseGraphics).to receive(:new).and_return(esi) }
-  #
-  #     let(:etag) { instance_double(Etag, etag: 'e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b') }
-  #
-  #     before { expect(Etag).to receive(:find_or_initialize_by).with(url: url).and_return(etag) }
-  #
-  #     before { expect(esi).to receive(:etag=).with('e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b') }
-  #
-  #     before { expect(Eve::GraphicImporterWorker).not_to receive(:perform_async) }
-  #
-  #     specify { expect { subject.import }.not_to raise_error }
-  #   end
-  # end
 end
