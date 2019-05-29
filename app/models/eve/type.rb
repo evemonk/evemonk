2efe56b44bb3d23b3145c8d3a51ff0eced335439
Igor Zubkov <igor.zubkov@gmail.com>
2019-05-29 21:48:06 +0300

    Eve::Type#type_dogma_effects

--- app/models/eve/type.rb
+++ app/models/eve/type.rb
@@ -3,5 +3,7 @@
 module Eve
   class Type < ApplicationRecord
     has_many :type_dogma_attributes, primary_key: :type_id, foreign_key: :type_id, dependent: :destroy
+
+    has_many :type_dogma_effects, primary_key: :type_id, foreign_key: :type_id, dependent: :destroy
   end
 end
--- spec/models/eve/type_spec.rb
+++ spec/models/eve/type_spec.rb
@@ -8,4 +8,6 @@ describe Eve::Type do
   it { expect(described_class.table_name).to eq('eve_types') }
 
   it { should have_many(:type_dogma_attributes).with_primary_key(:type_id).with_foreign_key(:type_id).dependent(:destroy) }
+
+  it { should have_many(:type_dogma_effects).with_primary_key(:type_id).with_foreign_key(:type_id).dependent(:destroy) }
 end
