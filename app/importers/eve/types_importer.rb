8fb71777bbe8d78c1dc82eb760a1593524bda097
Igor Zubkov <igor.zubkov@gmail.com>
2019-05-30 14:39:25 +0300

    Add comment

--- app/importers/eve/types_importer.rb
+++ app/importers/eve/types_importer.rb
@@ -10,5 +10,21 @@ module Eve
 
     def import
     end
+
+    # def import
+    #   esi = EveOnline::ESI::UniverseTypes.new
+    #
+    #   etag = Etag.find_or_initialize_by(url: esi.url)
+    #
+    #   esi.etag = etag.etag
+    #
+    #   return if esi.not_modified?
+    #
+    #   esi.universe_type_ids.each do |type_id|
+    #     Eve::TypeImporterWorker.perform_async(type_id)
+    #   end
+    #
+    #   etag.update!(etag: esi.etag)
+    # end
   end
 end
