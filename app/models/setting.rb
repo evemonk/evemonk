890a4850378a2e885a62d307819f0d3475e6aef2
Igor Zubkov <igor.zubkov@gmail.com>
2019-06-08 21:50:52 +0300

    Configure eve_online_sso from Settings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -3,11 +3,7 @@
 class Setting < RailsSettings::Base
   # cache_prefix { 'v1' }
 
-  # Define your fields
-  # field :host, type: :string, default: "http://localhost:3000"
-  # field :default_locale, default: "en", type: :string
-  # field :confirmable_enable, default: "0", type: :boolean
-  # field :admin_emails, default: "admin@rubyonrails.org", type: :array
-  # field :omniauth_google_client_id, default: (ENV["OMNIAUTH_GOOGLE_CLIENT_ID"] || ""), type: :string, readonly: true
-  # field :omniauth_google_client_secret, default: (ENV["OMNIAUTH_GOOGLE_CLIENT_SECRET"] || ""), type: :string, readonly: true
+  field :eve_online_sso_client_id, default: ENV['EVE_ONLINE_SSO_CLIENT_ID'], readonly: true
+
+  field :eve_online_sso_secret_key, default: ENV['EVE_ONLINE_SSO_SECRET_KEY'], readonly: true
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :eve_online_sso, ENV['EVE_ONLINE_SSO_CLIENT_ID'], ENV['EVE_ONLINE_SSO_SECRET_KEY'], scope: 'esi-wallet.read_character_wallet.v1 esi-characters.read_loyalty.v1 esi-skills.read_skillqueue.v1 esi-skills.read_skills.v1 esi-assets.read_assets.v1'
+  provider :eve_online_sso,
+           Setting.eve_online_sso_client_id,
+           Setting.eve_online_sso_secret_key,
+           scope: 'esi-wallet.read_character_wallet.v1 esi-characters.read_loyalty.v1 esi-skills.read_skillqueue.v1 esi-skills.read_skills.v1 esi-assets.read_assets.v1'
 end
