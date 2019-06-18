2430ec4b4d95f8f30557320e40d69543fa6d25f8
Igor Zubkov <igor.zubkov@gmail.com>
2019-06-18 20:30:23 +0300

    Fix docker stuff

--- bin/backend.sh
+++ bin/backend.sh
@@ -4,16 +4,12 @@ rm -rf /shared/*
 
 cp -R /app/public/ /shared/
 
-# wait-for-it "elasticsearch:9200" -- echo "Elastic up and running"
-
 wait-for-it "redis:6379" -- echo "Redis up and running"
 
 wait-for-it "memcached:11211" -- echo "Memcached up and running"
 
 wait-for-it "postgresql:5432" -- echo "PostgreSQL up and running"
 
-# bundle exec rails db:create db:migrate
-
-# bundle exec rails searchkick:reindex:all
+wait-for-it "elasticsearch:9200" -- echo "Elastic up and running"
 
 bundle exec puma -C config/puma.rb
