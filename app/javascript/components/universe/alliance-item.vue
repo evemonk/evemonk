5f48b72d39355b822222b34396e8e147f2822b28
Igor Zubkov <igor.zubkov@gmail.com>
2019-06-23 23:27:32 +0300

    Extract vue component alliance-item

---
+++ app/javascript/components/universe/alliance-item.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-flex xs12>
+    <v-card dark>
+      <v-layout>
+        <v-flex xs5>
+          <v-img :src="icon" height="128px" contain></v-img>
+        </v-flex>
+        <v-flex xs7>
+          <v-card-title primary-title>
+            <div>
+              <div class="headline">
+                <router-link :to="{ name: 'universe_alliance', params: { id: id }}">
+                  {{ name }}
+                </router-link>
+              </div>
+              <div>({{ ticker }})</div>
+              <div>Founded in {{ date_founded }}</div>
+            </div>
+          </v-card-title>
+        </v-flex>
+      </v-layout>
+      <v-divider light></v-divider>
+      <v-card-actions>
+        <v-btn color="info">
+          <router-link :to="{ name: 'universe_alliance_corporations', params: { id: id }}">
+            Corporations ({{ corporations_count }})
+          </router-link>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="info">
+          <router-link :to="{ name: 'universe_alliance_characters', params: { id: id }}">
+            Characters ({{ characters_count }})
+          </router-link>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+  export default {
+    props: ['id',
+            'name',
+            'ticker',
+            'date_founded',
+            'corporations_count',
+            'characters_count',
+            'icon'],
+  }
+</script>
