c92315b561100d32f0a44fcbbe2e239172560ea0
Igor Zubkov <igor.zubkov@gmail.com>
2019-06-23 23:57:54 +0300

    Extract component

---
+++ app/javascript/components/universe/character-item.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-flex xs12>
+    <v-card dark>
+      <v-layout>
+        <v-flex xs5>
+          <v-img :src="icon" height="128px" contain></v-img>
+        </v-flex>
+        <v-flex xs7>
+          <v-card-title primary-title>
+            <div>
+              <div class="headline">{{ name }}</div>
+              <div>{{ corporation.name }} / {{ alliance.name }}</div>
+              <div>Birthday {{ birthday }}</div>
+            </div>
+          </v-card-title>
+        </v-flex>
+      </v-layout>
+      <v-divider light></v-divider>
+      <!-- <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="info">
+          <router-link :to="{ name: 'universe_character', params: { id: id }}">
+            More
+          </router-link>
+        </v-btn>
+      </v-card-actions> -->
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+  export default {
+    props: ['id',
+            'icon',
+            'name',
+            'corporation',
+            'alliance',
+            'birthday']
+  }
+</script>
