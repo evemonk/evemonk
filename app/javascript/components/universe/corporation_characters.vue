f018bc3e26738fc9058426ba891460733d25f490
Igor Zubkov <igor.zubkov@gmail.com>
2019-06-24 01:06:06 +0300

    Add CorporationCharacters vue component

--- app/javascript/components/universe/corporation_characters.vue
+++ app/javascript/components/universe/corporation_characters.vue
@@ -1,7 +1,42 @@
 <template>
-  <div id="corporation_characters">
+  <div id="characters">
   </div>
 </template>
 
 <script>
+  import { mapActions } from 'vuex';
+
+  import CharacterItem from './character-item.vue';
+
+  export default {
+    data () {
+      return {
+        title: 'EveMonk: EveOnline management suite',
+        loaded: false,
+        characters: [],
+        current_page: 1,
+        total_count: null,
+        total_pages: null,
+        alliance_id: null,
+        breadcrumbs: [
+          {
+            text: 'Home',
+            to: { name: 'welcome' },
+            exact: true
+          },
+        ]
+      }
+    },
+
+    components: {
+      'character-item': CharacterItem,
+    },
+
+    methods: {
+      ...mapActions([
+        'fetchUniverseCorporation',
+        'fetchUniverseCorporationCharacters'
+      ])
+    }
+  }
 </script>
