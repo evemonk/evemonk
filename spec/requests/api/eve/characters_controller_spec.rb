17228c939d8c225ea73fad80b72e54c0f2b3f44f
Igor Zubkov <igor.zubkov@gmail.com>
2019-06-25 23:19:40 +0300

    Add basic view

--- spec/requests/api/eve/characters_controller_spec.rb
+++ spec/requests/api/eve/characters_controller_spec.rb
@@ -7,5 +7,36 @@ describe Api::Eve::CharactersController do
   end
 
   describe '#show' do
+    it 'returns Eve Character' do
+      create(:eve_character,
+             character_id: 123,
+             birthday: 'Sun, 03 May 2015 19:45:17 UTC +00:00',
+             description: 'character description',
+             gender: 'male',
+             name: 'character name',
+             security_status: 1.1,
+             title: 'character title')
+
+      get '/api/eve/characters/123'
+
+      expect(response).to have_http_status(:ok)
+
+      expect(JSON.parse(response.body)).to eq('character' => {
+                                                'icon' => 'https://imageserver.eveonline.com/Character/123_512.jpg',
+                                                'id' => 123,
+                                                'birthday' => '2015-05-03T19:45:17Z',
+                                                'description' => 'character description',
+                                                'gender' => 'male',
+                                                'name' => 'character name',
+                                                'security_status' => '1.1',
+                                                'title' => 'character title'
+                                             })
+    end
+
+    it 'returns 404' do
+      get '/api/eve/characters/456'
+
+      expect(response).to have_http_status(:not_found)
+    end
   end
 end
