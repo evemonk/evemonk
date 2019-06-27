a25959ca03cccd61d0ecaeb49975cccf20439773
Igor Zubkov <igor.zubkov@gmail.com>
2019-06-27 13:06:05 +0300

    Add index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_24_085408) do
+ActiveRecord::Schema.define(version: 2019_06_27_100341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -282,6 +282,7 @@ ActiveRecord::Schema.define(version: 2019_06_24_085408) do
     t.float "volume"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["type_id"], name: "index_eve_types_on_type_id", unique: true
   end
 
   create_table "loyalty_points", force: :cascade do |t|
