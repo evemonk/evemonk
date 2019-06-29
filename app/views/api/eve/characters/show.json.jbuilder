c972fca495470239516059d16838ab6f7e080d00
Igor Zubkov <igor.zubkov@gmail.com>
2019-06-29 19:45:43 +0300

    Add more info to api eve characters

--- app/views/api/eve/characters/show.json.jbuilder
+++ app/views/api/eve/characters/show.json.jbuilder
@@ -4,92 +4,92 @@ json.character do
   json.id @character.character_id
   json.icon "https://imageserver.eveonline.com/Character/#{ @character.character_id }_512.jpg"
   json.birthday @character.birthday.iso8601
-  json.description @character.description
+  json.description Rails::Html::FullSanitizer.new.sanitize(@character.description)
   json.gender @character.gender
   json.name @character.name
   json.security_status @character.security_status.to_s
   json.title @character.title
-end
 
-# if character.alliance
-#   json.alliance do
-#     json.id character.alliance.alliance_id
-#     json.creator_corporation_id character.alliance.creator_corporation_id
-#     json.creator_id character.alliance.creator_id
-#     json.date_founded character.alliance.date_founded.to_s
-#     json.executor_corporation_id character.alliance.executor_corporation_id
-#     json.faction_id character.alliance.faction_id
-#     json.name character.alliance.name
-#     json.ticker character.alliance.ticker
-#   end
-# end
-#
-# if character.ancestry
-#   json.ancestry do
-#     json.id character.ancestry.ancestry_id
-#     json.bloodline_id character.ancestry.bloodline_id
-#     json.name character.ancestry.name
-#     json.short_description character.ancestry.short_description
-#     json.description character.ancestry.description
-#     json.icon_id character.ancestry.icon_id
-#   end
-# end
-#
-# if character.bloodline
-#   json.bloodline do
-#     json.id character.bloodline.bloodline_id
-#     json.corporation_id character.bloodline.corporation_id
-#     json.race_id character.bloodline.race_id
-#     json.name character.bloodline.name
-#     json.description character.bloodline.description
-#     json.charisma character.bloodline.charisma
-#     json.intelligence character.bloodline.intelligence
-#     json.memory character.bloodline.memory
-#     json.perception character.bloodline.perception
-#     json.willpower character.bloodline.willpower
-#     json.ship_type_id character.bloodline.ship_type_id
-#   end
-# end
-#
-# if character.faction
-#   json.faction do
-#     json.id character.faction.faction_id
-#     json.corporation_id character.faction.corporation_id
-#     json.name character.faction.name
-#     json.description character.faction.description
-#     json.militia_corporation_id character.faction.militia_corporation_id
-#     json.solar_system_id character.faction.solar_system_id
-#     json.station_count character.faction.station_count
-#     json.station_system_count character.faction.station_system_count
-#     json.size_factor character.faction.size_factor.to_s
-#     json.is_unique character.faction.is_unique
-#   end
-# end
-#
-# if character.race
-#   json.race do
-#     json.id character.race.race_id
-#     json.alliance_id character.race.alliance_id
-#     json.name character.race.name
-#     json.description character.race.description
-#   end
-# end
-#
-# if character.corporation
-#   json.corporation do
-#     json.id character.corporation.corporation_id
-#     json.name character.corporation.name
-#     json.description character.corporation.description
-#     json.ticker character.corporation.ticker
-#     json.date_founded character.corporation.date_founded.iso8601
-#     json.url character.corporation.corporation_url
-#     json.member_count character.corporation.member_count
-#     json.shares character.corporation.shares
-#     json.tax_rate character.corporation.tax_rate.to_s
-#     json.alliance_id character.corporation.alliance_id
-#     json.ceo_id character.corporation.ceo_id
-#     json.creator_id character.corporation.creator_id
-#     json.faction_id character.corporation.faction_id
-#     json.home_station_id character.corporation.home_station_id
-#   end
-# end
+  if @character.alliance
+    json.alliance do
+      json.id @character.alliance.alliance_id
+      json.creator_corporation_id @character.alliance.creator_corporation_id
+      json.creator_id @character.alliance.creator_id
+      json.date_founded @character.alliance.date_founded.to_s
+      json.executor_corporation_id @character.alliance.executor_corporation_id
+      json.faction_id @character.alliance.faction_id
+      json.name @character.alliance.name
+      json.ticker @character.alliance.ticker
+    end
+  end
+
+  if @character.ancestry
+    json.ancestry do
+      json.id @character.ancestry.ancestry_id
+      json.bloodline_id @character.ancestry.bloodline_id
+      json.name @character.ancestry.name
+      json.short_description @character.ancestry.short_description
+      json.description @character.ancestry.description
+      json.icon_id @character.ancestry.icon_id
+    end
+  end
+
+  if @character.bloodline
+    json.bloodline do
+      json.id @character.bloodline.bloodline_id
+      json.corporation_id @character.bloodline.corporation_id
+      json.race_id @character.bloodline.race_id
+      json.name @character.bloodline.name
+      json.description @character.bloodline.description
+      json.charisma @character.bloodline.charisma
+      json.intelligence @character.bloodline.intelligence
+      json.memory @character.bloodline.memory
+      json.perception @character.bloodline.perception
+      json.willpower @character.bloodline.willpower
+      json.ship_type_id @character.bloodline.ship_type_id
+    end
+  end
+
+  if @character.corporation
+    json.corporation do
+      json.id @character.corporation.corporation_id
+      json.name @character.corporation.name
+      json.description @character.corporation.description
+      json.ticker @character.corporation.ticker
+      json.date_founded @character.corporation.date_founded.iso8601
+      json.url @character.corporation.corporation_url
+      json.member_count @character.corporation.member_count
+      json.shares @character.corporation.shares
+      json.tax_rate @character.corporation.tax_rate.to_s
+      json.alliance_id @character.corporation.alliance_id
+      json.ceo_id @character.corporation.ceo_id
+      json.creator_id @character.corporation.creator_id
+      json.faction_id @character.corporation.faction_id
+      json.home_station_id @character.corporation.home_station_id
+    end
+  end
+
+  if @character.faction
+    json.faction do
+      json.id @character.faction.faction_id
+      json.corporation_id @character.faction.corporation_id
+      json.name @character.faction.name
+      json.description @character.faction.description
+      json.militia_corporation_id @character.faction.militia_corporation_id
+      json.solar_system_id @character.faction.solar_system_id
+      json.station_count @character.faction.station_count
+      json.station_system_count @character.faction.station_system_count
+      json.size_factor @character.faction.size_factor.to_s
+      json.is_unique @character.faction.is_unique
+    end
+  end
+
+  if @character.race
+    json.race do
+      json.id @character.race.race_id
+      json.alliance_id @character.race.alliance_id
+      json.name @character.race.name
+      json.description @character.race.description
+    end
+  end
+end
--- spec/requests/api/eve/characters_controller_spec.rb
+++ spec/requests/api/eve/characters_controller_spec.rb
@@ -5,14 +5,89 @@ require 'rails_helper'
 describe Api::Eve::CharactersController do
   describe '#index' do
     it 'returns list of Eve Characters' do
+      create(:eve_alliance,
+             alliance_id: 123,
+             creator_corporation_id: 2222,
+             creator_id: 3333,
+             date_founded: 'Sun, 03 May 2015 19:45:17 UTC +00:00',
+             executor_corporation_id: 4444,
+             faction_id: 5555,
+             name: 'Alliance name',
+             ticker: 'ALLIANCE_TICKER')
+
+      create(:eve_corporation,
+             alliance_id: 123,
+             corporation_id: 456,
+             name: 'Character corporation name',
+             description: 'Character corporation description',
+             ticker: 'TICKER',
+             date_founded: 'Sun, 03 May 2015 19:45:17 UTC +00:00',
+             corporation_url: 'https://evemonk.com/',
+             member_count: 2000,
+             shares: 1000,
+             tax_rate: 0.99,
+             ceo_id: 1234,
+             creator_id: 4321,
+             faction_id: 5555,
+             home_station_id: 999)
+
+      create(:eve_alliance_corporation,
+             alliance_id: 123,
+             corporation_id: 456)
+
       create(:eve_character,
-             character_id: 123,
-             birthday: 'Sun, 03 May 2015 19:45:17 UTC +00:00',
-             description: 'character description',
+             alliance_id: 123,
+             character_id: 123_123_123,
+             corporation_id: 456,
+             ancestry_id: 10,
+             bloodline_id: 12,
+             faction_id: 5555,
+             race_id: 123,
+             name: 'Character name',
+             description: 'Character description',
              gender: 'male',
-             name: 'character name',
+             birthday: 'Sun, 03 May 2015 19:45:17 UTC +00:00',
              security_status: 1.1,
-             title: 'character title')
+             title: 'Character title')
+
+      create(:eve_bloodline,
+             bloodline_id: 12,
+             name: 'Bloodline name',
+             description: 'Bloodline description',
+             race_id: 123,
+             corporation_id: 456,
+             ship_type_id: 30,
+             perception: 21,
+             willpower: 22,
+             charisma: 23,
+             memory: 24,
+             intelligence: 25)
+
+      create(:eve_ancestry,
+             ancestry_id: 10,
+             name: 'Ancestry name',
+             bloodline_id: 12,
+             description: 'Ancestry description',
+             short_description: 'Ancestry short description',
+             icon_id: 14)
+
+      create(:eve_faction,
+             faction_id: 5555,
+             name: 'Faction name',
+             description: 'Faction description',
+             solar_system_id: 10,
+             corporation_id: 456,
+             militia_corporation_id: 20,
+             size_factor: 2.0,
+             station_count: 10,
+             station_system_count: 100,
+             is_unique: true)
+
+      create(:eve_race,
+             race_id: 123,
+             name: 'Race name',
+             description: 'Race description',
+             alliance_id: 123)
 
       get '/api/eve/characters'
 
@@ -22,42 +97,247 @@ describe Api::Eve::CharactersController do
                                               'total_pages' => 1,
                                               'current_page' => 1,
                                               'characters' => [{
-                                                'icon' => 'https://imageserver.eveonline.com/Character/123_512.jpg',
-                                                'id' => 123,
+                                                'icon' => 'https://imageserver.eveonline.com/Character/123123123_512.jpg',
+                                                'id' => 123123123,
                                                 'birthday' => '2015-05-03T19:45:17Z',
-                                                'description' => 'character description',
+                                                'description' => 'Character description',
                                                 'gender' => 'male',
-                                                'name' => 'character name',
+                                                'name' => 'Character name',
                                                 'security_status' => '1.1',
-                                                'title' => 'character title'
+                                                'title' => 'Character title',
+                                                'alliance' => {
+                                                  'id' => 123,
+                                                  'creator_corporation_id' => 2222,
+                                                  'creator_id' => 3333,
+                                                  'date_founded' => '2015-05-03 19:45:17 UTC',
+                                                  'executor_corporation_id' => 4444,
+                                                  'faction_id' => 5555,
+                                                  'name' => 'Alliance name',
+                                                  'ticker' => 'ALLIANCE_TICKER'
+                                                },
+                                                'ancestry' => {
+                                                  'id' => 10,
+                                                  'name' => 'Ancestry name',
+                                                  'bloodline_id' => 12,
+                                                  'description' => 'Ancestry description',
+                                                  'short_description' => 'Ancestry short description',
+                                                  'icon_id' => 14
+                                                },
+                                                'bloodline' => {
+                                                  'id' => 12,
+                                                  'name' => 'Bloodline name',
+                                                  'description' => 'Bloodline description',
+                                                  'race_id' => 123,
+                                                  'corporation_id' => 456,
+                                                  'ship_type_id' => 30,
+                                                  'perception' => 21,
+                                                  'willpower' => 22,
+                                                  'charisma' => 23,
+                                                  'memory' => 24,
+                                                  'intelligence' => 25
+                                                },
+                                                'faction' => {
+                                                  'id' => 5555,
+                                                  'name' => 'Faction name',
+                                                  'description' => 'Faction description',
+                                                  'solar_system_id' => 10,
+                                                  'corporation_id' => 456,
+                                                  'militia_corporation_id' => 20,
+                                                  'size_factor' => '2.0',
+                                                  'station_count' => 10,
+                                                  'station_system_count' => 100,
+                                                  'is_unique' => true
+                                                },
+                                                'race' => {
+                                                  'id' => 123,
+                                                  'name' => 'Race name',
+                                                  'description' => 'Race description',
+                                                  'alliance_id' => 123
+                                                },
+                                                'corporation' => {
+                                                  'id' => 456,
+                                                  'name' => 'Character corporation name',
+                                                  'description' => 'Character corporation description',
+                                                  'ticker' => 'TICKER',
+                                                  'date_founded' => '2015-05-03T19:45:17Z',
+                                                  'url' => 'https://evemonk.com/',
+                                                  'member_count' => 2000,
+                                                  'shares' => 1000,
+                                                  'tax_rate' => '0.99',
+                                                  'alliance_id' => 123,
+                                                  'ceo_id' => 1234,
+                                                  'creator_id' => 4321,
+                                                  'faction_id' => 5555,
+                                                  'home_station_id' => 999
+                                                }
                                              }])
     end
   end
 
   describe '#show' do
     it 'returns Eve Character' do
-      create(:eve_character,
-             character_id: 123,
-             birthday: 'Sun, 03 May 2015 19:45:17 UTC +00:00',
-             description: 'character description',
-             gender: 'male',
-             name: 'character name',
-             security_status: 1.1,
-             title: 'character title')
+      create(:eve_alliance,
+             alliance_id: 123,
+             creator_corporation_id: 2222,
+             creator_id: 3333,
+             date_founded: 'Sun, 03 May 2015 19:45:17 UTC +00:00',
+             executor_corporation_id: 4444,
+             faction_id: 5555,
+             name: 'Alliance name',
+             ticker: 'ALLIANCE_TICKER')
 
-      get '/api/eve/characters/123'
+      create(:eve_corporation,
+             alliance_id: 123,
+             corporation_id: 456,
+             name: 'Character corporation name',
+             description: 'Character corporation description',
+             ticker: 'TICKER',
+             date_founded: 'Sun, 03 May 2015 19:45:17 UTC +00:00',
+             corporation_url: 'https://evemonk.com/',
+             member_count: 2000,
+             shares: 1000,
+             tax_rate: 0.99,
+             ceo_id: 1234,
+             creator_id: 4321,
+             faction_id: 5555,
+             home_station_id: 999)
+
+      create(:eve_alliance_corporation,
+             alliance_id: 123,
+             corporation_id: 456)
+
+      create(:eve_character,
+             alliance_id: 123,
+             character_id: 123_123_123,
+             corporation_id: 456,
+             ancestry_id: 10,
+             bloodline_id: 12,
+             faction_id: 5555,
+             race_id: 123,
+             name: 'Character name',
+             description: 'Character description',
+             gender: 'male',
+             birthday: 'Sun, 03 May 2015 19:45:17 UTC +00:00',
+             security_status: 1.1,
+             title: 'Character title')
+
+      create(:eve_bloodline,
+             bloodline_id: 12,
+             name: 'Bloodline name',
+             description: 'Bloodline description',
+             race_id: 123,
+             corporation_id: 456,
+             ship_type_id: 30,
+             perception: 21,
+             willpower: 22,
+             charisma: 23,
+             memory: 24,
+             intelligence: 25)
+
+      create(:eve_ancestry,
+             ancestry_id: 10,
+             name: 'Ancestry name',
+             bloodline_id: 12,
+             description: 'Ancestry description',
+             short_description: 'Ancestry short description',
+             icon_id: 14)
+
+      create(:eve_faction,
+             faction_id: 5555,
+             name: 'Faction name',
+             description: 'Faction description',
+             solar_system_id: 10,
+             corporation_id: 456,
+             militia_corporation_id: 20,
+             size_factor: 2.0,
+             station_count: 10,
+             station_system_count: 100,
+             is_unique: true)
+
+      create(:eve_race,
+             race_id: 123,
+             name: 'Race name',
+             description: 'Race description',
+             alliance_id: 123)
+
+      get '/api/eve/characters/123123123'
 
       expect(response).to have_http_status(:ok)
 
       expect(JSON.parse(response.body)).to eq('character' => {
-                                                'icon' => 'https://imageserver.eveonline.com/Character/123_512.jpg',
-                                                'id' => 123,
+                                                'icon' => 'https://imageserver.eveonline.com/Character/123123123_512.jpg',
+                                                'id' => 123123123,
                                                 'birthday' => '2015-05-03T19:45:17Z',
-                                                'description' => 'character description',
+                                                'description' => 'Character description',
                                                 'gender' => 'male',
-                                                'name' => 'character name',
+                                                'name' => 'Character name',
                                                 'security_status' => '1.1',
-                                                'title' => 'character title'
+                                                'title' => 'Character title',
+                                                'alliance' => {
+                                                  'id' => 123,
+                                                  'creator_corporation_id' => 2222,
+                                                  'creator_id' => 3333,
+                                                  'date_founded' => '2015-05-03 19:45:17 UTC',
+                                                  'executor_corporation_id' => 4444,
+                                                  'faction_id' => 5555,
+                                                  'name' => 'Alliance name',
+                                                  'ticker' => 'ALLIANCE_TICKER'
+                                                },
+                                                'ancestry' => {
+                                                  'id' => 10,
+                                                  'name' => 'Ancestry name',
+                                                  'bloodline_id' => 12,
+                                                  'description' => 'Ancestry description',
+                                                  'short_description' => 'Ancestry short description',
+                                                  'icon_id' => 14
+                                                },
+                                                'bloodline' => {
+                                                  'id' => 12,
+                                                  'name' => 'Bloodline name',
+                                                  'description' => 'Bloodline description',
+                                                  'race_id' => 123,
+                                                  'corporation_id' => 456,
+                                                  'ship_type_id' => 30,
+                                                  'perception' => 21,
+                                                  'willpower' => 22,
+                                                  'charisma' => 23,
+                                                  'memory' => 24,
+                                                  'intelligence' => 25
+                                                },
+                                                'faction' => {
+                                                  'id' => 5555,
+                                                  'name' => 'Faction name',
+                                                  'description' => 'Faction description',
+                                                  'solar_system_id' => 10,
+                                                  'corporation_id' => 456,
+                                                  'militia_corporation_id' => 20,
+                                                  'size_factor' => '2.0',
+                                                  'station_count' => 10,
+                                                  'station_system_count' => 100,
+                                                  'is_unique' => true
+                                                },
+                                                'race' => {
+                                                  'id' => 123,
+                                                  'name' => 'Race name',
+                                                  'description' => 'Race description',
+                                                  'alliance_id' => 123
+                                                },
+                                                'corporation' => {
+                                                  'id' => 456,
+                                                  'name' => 'Character corporation name',
+                                                  'description' => 'Character corporation description',
+                                                  'ticker' => 'TICKER',
+                                                  'date_founded' => '2015-05-03T19:45:17Z',
+                                                  'url' => 'https://evemonk.com/',
+                                                  'member_count' => 2000,
+                                                  'shares' => 1000,
+                                                  'tax_rate' => '0.99',
+                                                  'alliance_id' => 123,
+                                                  'ceo_id' => 1234,
+                                                  'creator_id' => 4321,
+                                                  'faction_id' => 5555,
+                                                  'home_station_id' => 999
+                                                }
                                              })
     end
 
