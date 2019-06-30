5704c2005e6cf139d8645d0f28c26bed6763d572
Igor Zubkov <igor.zubkov@gmail.com>
2019-06-30 21:24:19 +0300

    More specs

--- spec/importers/eve/character_corporation_history_importer_spec.rb
+++ spec/importers/eve/character_corporation_history_importer_spec.rb
@@ -6,6 +6,63 @@ describe Eve::CharacterCorporationHistoryImporter do
   describe '#import' do
     context 'when fresh data available' do
       context 'when character found' do
+        let(:character_id) { double }
+
+        subject { described_class.new(character_id) }
+
+        let(:eve_character) { instance_double(Eve::Character) }
+
+        before { expect(Eve::Character).to receive(:find_by!).with(character_id: character_id).and_return(eve_character) }
+
+        let(:corporation_id) { double }
+
+        let(:json) { double }
+
+        let(:url) { double }
+
+        let(:new_etag) { double }
+
+        let(:entry) { instance_double(EveOnline::ESI::Models::CharacterCorporationHistory, corporation_id: corporation_id, as_json: json) }
+
+        let(:entries) { [entry] }
+
+        let(:esi) do
+          instance_double(EveOnline::ESI::CharacterCorporationHistory,
+                          url: url,
+                          not_modified?: false,
+                          etag: new_etag,
+                          entries: entries)
+        end
+
+        before { expect(EveOnline::ESI::CharacterCorporationHistory).to receive(:new).with(character_id: character_id).and_return(esi) }
+
+        let(:etag) { instance_double(Etag, etag: '22c39689783a86032b8d43fa0b2e8f4809c4f38a585e39471035aa8b') }
+
+        before { expect(Etag).to receive(:find_or_initialize_by).with(url: url).and_return(etag) }
+
+        before { expect(esi).to receive(:etag=).with('22c39689783a86032b8d43fa0b2e8f4809c4f38a585e39471035aa8b') }
+
+        let(:character_corporation_history) { instance_double(Eve::CharacterCorporationHistory) }
+
+        before do
+          #
+          # eve_character.character_corporation_histories
+          #              .find_or_initialize_by(corporation_id: entry.corporation_id) # => character_corporation_history
+          #
+          expect(eve_character).to receive(:character_corporation_histories) do
+            double.tap do |a|
+              expect(a).to receive(:find_or_initialize_by).with(corporation_id: corporation_id)
+                                                          .and_return(character_corporation_history)
+            end
+          end
+        end
+
+        before { expect(character_corporation_history).to receive(:update!).with(json) }
+
+        before { expect(etag).to receive(:update!).with(etag: new_etag) }
+
+        specify { expect { subject.import }.not_to raise_error }
+
       end
 
       context 'when character not found (404)' do
