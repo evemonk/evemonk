3f20c8dfd16911c7e6e9804bd9be1bc4ce77319d
Igor Zubkov <igor.zubkov@gmail.com>
2019-06-30 20:27:25 +0300

    Eve::CharacterCorporationHistory

---
+++ app/models/eve/character_corporation_history.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Eve
+  class CharacterCorporationHistory < ApplicationRecord
+    belongs_to :character,
+               primary_key: :character_id,
+               class_name: 'Eve::Character'
+
+    belongs_to :corporation,
+               primary_key: :corporation_id,
+               class_name: 'Eve::Corporation',
+               optional: true
+  end
+end
---
+++ spec/models/eve/character_corporation_history_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Eve::CharacterCorporationHistory do
+  it { should be_an(ApplicationRecord) }
+
+  it { expect(described_class.table_name).to eq('eve_character_corporation_histories') }
+
+  it { should belong_to(:character).with_primary_key(:character_id).class_name('Eve::Character') }
+
+  it { should belong_to(:corporation).with_primary_key(:corporation_id).class_name('Eve::Corporation').optional }
+end
