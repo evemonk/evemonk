5d575bbe5a8f0c0641b58aaf5d6780a984772745
Igor Zubkov <igor.zubkov@gmail.com>
2019-06-30 21:11:12 +0300

    More specs

--- spec/importers/eve/character_corporation_history_importer_spec.rb
+++ spec/importers/eve/character_corporation_history_importer_spec.rb
@@ -47,6 +47,33 @@ describe Eve::CharacterCorporationHistoryImporter do
     end
 
     context 'when no fresh data available' do
+      let(:character_id) { double }
+
+      subject { described_class.new(character_id) }
+
+      let(:eve_character) { instance_double(Eve::Character) }
+
+      before { expect(Eve::Character).to receive(:find_by!).with(character_id: character_id).and_return(eve_character) }
+
+      let(:url) { double }
+
+      let(:esi) do
+        instance_double(EveOnline::ESI::CharacterCorporationHistory,
+                        url: url,
+                        not_modified?: true)
+      end
+
+      before { expect(EveOnline::ESI::CharacterCorporationHistory).to receive(:new).with(character_id: character_id).and_return(esi) }
+
+      let(:etag) { instance_double(Etag, etag: '22c39689783a86032b8d43fa0b2e8f4809c4f38a585e39471035aa8b') }
+
+      before { expect(Etag).to receive(:find_or_initialize_by).with(url: url).and_return(etag) }
+
+      before { expect(esi).to receive(:etag=).with('22c39689783a86032b8d43fa0b2e8f4809c4f38a585e39471035aa8b') }
+
+      before { expect(etag).not_to receive(:update!) }
+
+      specify { expect { subject.import }.not_to raise_error }
     end
   end
 end
