42498fa62640cda4e8bb6df304f4b3bd817be84d
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-01 14:36:45 +0300

    Eve::CorporationDecorator#date_founded

--- app/decorators/eve/corporation_decorator.rb
+++ app/decorators/eve/corporation_decorator.rb
@@ -2,5 +2,8 @@
 
 module Eve
   class CorporationDecorator < ApplicationDecorator
+    def date_founded
+      object.date_founded.iso8601 if object.date_founded
+    end
   end
 end
--- spec/decorators/eve/corporation_decorator_spec.rb
+++ spec/decorators/eve/corporation_decorator_spec.rb
@@ -6,4 +6,28 @@ describe Eve::CorporationDecorator do
   subject { described_class.new(double) }
 
   it { should be_a(ApplicationDecorator) }
+
+  describe '#date_founded' do
+    context 'when date_founded is empty' do
+      let(:eve_corporation) do
+        create(:eve_corporation,
+               date_founded: nil)
+      end
+
+      subject { eve_corporation.decorate }
+
+      specify { expect(subject.date_founded).to eq(nil) }
+    end
+
+    context 'when date_founded is present' do
+      let(:eve_corporation) do
+        create(:eve_corporation,
+               date_founded: 'Sun, 03 May 2015 19:45:17 UTC +00:00')
+      end
+
+      subject { eve_corporation.decorate }
+
+      specify { expect(subject.date_founded).to eq('2015-05-03T19:45:17Z') }
+    end
+  end
 end
