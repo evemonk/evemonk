6fa7dd7122e49c62c6d73c5eadd7ead9de222bd9
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-01 20:18:27 +0300

    More decorators

--- app/decorators/eve/alliance_decorator.rb
+++ app/decorators/eve/alliance_decorator.rb
@@ -2,5 +2,16 @@
 
 module Eve
   class AllianceDecorator < ApplicationDecorator
+    decorates_associations :creator_corporation, :creator,
+                           :executor_corporation, :faction,
+                           :corporations, :characters
+
+    def date_founded
+      object.date_founded.iso8601
+    end
+
+    def icon
+      "https://imageserver.eveonline.com/Alliance/#{ alliance_id }_128.png"
+    end
   end
 end
