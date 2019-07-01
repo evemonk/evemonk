aa17f1a6d0e02ed0dd34742d3e82c7e1f4c5cc70
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-01 19:07:52 +0300

    More decorator stuff

--- app/decorators/eve/character_decorator.rb
+++ app/decorators/eve/character_decorator.rb
@@ -2,5 +2,16 @@
 
 module Eve
   class CharacterDecorator < ApplicationDecorator
+    def birthday
+      object.birthday.iso8601
+    end
+
+    def icon
+      "https://imageserver.eveonline.com/Character/#{ character_id }_512.jpg"
+    end
+
+    def description
+      Rails::Html::FullSanitizer.new.sanitize(object.description)
+    end
   end
 end
