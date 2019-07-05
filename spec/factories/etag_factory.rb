b124ea7dcdf4bd39f1ebda1c4a5220123ca45a5a
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-05 15:31:01 +0300

    Add etag factory

---
+++ spec/factories/etag_factory.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :etag do
+    sequence :url do |n|
+      "https://esi.evetech.net/v3/alliances/#{ n }/?datasource=tranquility"
+    end
+
+    etag { SecureRandom.hex }
+  end
+end
