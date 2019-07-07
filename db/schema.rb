772e5c1dcbbed1f992e3a004b2af162a0558b071
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-08 00:27:46 +0300

    Add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_05_122435) do
+ActiveRecord::Schema.define(version: 2019_07_07_212031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,21 @@ ActiveRecord::Schema.define(version: 2019_07_05_122435) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "character_assets", force: :cascade do |t|
+    t.bigint "character_id"
+    t.boolean "is_blueprint_copy"
+    t.boolean "is_singleton"
+    t.bigint "item_id"
+    t.string "location_flag"
+    t.bigint "location_id"
+    t.string "location_type"
+    t.bigint "quantity"
+    t.bigint "type_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["character_id"], name: "index_character_assets_on_character_id"
   end
 
   create_table "characters", force: :cascade do |t|
@@ -425,6 +440,7 @@ ActiveRecord::Schema.define(version: 2019_07_05_122435) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "character_assets", "characters"
   add_foreign_key "characters", "users"
   add_foreign_key "sessions", "users"
 end
