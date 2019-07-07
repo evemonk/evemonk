3d583709394cb2b6da0c8ea70cf36a00d2069176
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-07 23:48:14 +0300

    Update request spec

--- spec/requests/api/characters_controller_spec.rb
+++ spec/requests/api/characters_controller_spec.rb
@@ -9,7 +9,42 @@ describe Api::CharactersController do
 
       session = create(:session, user: user)
 
-      create(:character, user: user, character_id: '123', name: 'Character name')
+      create(:eve_alliance,
+             alliance_id: 123,
+             creator_corporation_id: 2222,
+             creator_id: 3333,
+             date_founded: 'Sun, 03 May 2015 19:45:17 UTC +00:00',
+             executor_corporation_id: 4444,
+             faction_id: 5555,
+             name: 'Alliance name',
+             ticker: 'ALLIANCE_TICKER')
+
+      create(:eve_corporation,
+             alliance_id: 123,
+             corporation_id: 456,
+             name: 'Character corporation name',
+             description: '<b>Character corporation description</b>',
+             ticker: 'TICKER',
+             date_founded: 'Sun, 03 May 2015 19:45:17 UTC +00:00',
+             corporation_url: 'https://evemonk.com/',
+             member_count: 2000,
+             shares: 1000,
+             tax_rate: 0.99,
+             ceo_id: 1234,
+             creator_id: 4321,
+             faction_id: 5555,
+             home_station_id: 999)
+
+      create(:eve_alliance_corporation,
+             alliance_id: 123,
+             corporation_id: 456)
+
+      create(:character,
+             user: user,
+             alliance_id: 123,
+             character_id: 123_123_123,
+             corporation_id: 456,
+             name: 'Character name')
 
       get '/api/characters', headers: { 'Authorization': "Bearer #{ session.token }" }
 
@@ -25,13 +60,81 @@ describe Api::CharactersController do
 
       expect(JSON.parse(response.body)['characters'].size).to eq(1)
 
-      expect(JSON.parse(response.body)['characters'].first.keys.sort).to eq(['icon', 'id', 'name'])
+      expect(JSON.parse(response.body)['characters'].first.keys.sort).to eq(['alliance', 'corporation', 'icon', 'id', 'name'])
 
-      expect(JSON.parse(response.body)['characters'].first['id']).to eq(123)
+      expect(JSON.parse(response.body)['characters'].first['id']).to eq(123_123_123)
 
-      expect(JSON.parse(response.body)['characters'].first['icon']).to eq('https://imageserver.eveonline.com/Character/123_512.jpg')
+      expect(JSON.parse(response.body)['characters'].first['icon']).to eq('https://imageserver.eveonline.com/Character/123123123_512.jpg')
 
       expect(JSON.parse(response.body)['characters'].first['name']).to eq('Character name')
+
+      expect(JSON.parse(response.body)['characters'].first['alliance'].keys.sort).to eq(['creator_corporation_id',
+                                                                                         'creator_id',
+                                                                                         'date_founded',
+                                                                                         'executor_corporation_id',
+                                                                                         'faction_id',
+                                                                                         'id',
+                                                                                         'name',
+                                                                                         'ticker'])
+
+      expect(JSON.parse(response.body)['characters'].first['alliance']['creator_corporation_id']).to eq(2222)
+
+      expect(JSON.parse(response.body)['characters'].first['alliance']['creator_id']).to eq(3333)
+
+      expect(JSON.parse(response.body)['characters'].first['alliance']['date_founded']).to eq('2015-05-03T19:45:17Z')
+
+      expect(JSON.parse(response.body)['characters'].first['alliance']['executor_corporation_id']).to eq(4444)
+
+      expect(JSON.parse(response.body)['characters'].first['alliance']['faction_id']).to eq(5555)
+
+      expect(JSON.parse(response.body)['characters'].first['alliance']['id']).to eq(123)
+
+      expect(JSON.parse(response.body)['characters'].first['alliance']['name']).to eq('Alliance name')
+
+      expect(JSON.parse(response.body)['characters'].first['alliance']['ticker']).to eq('ALLIANCE_TICKER')
+
+      expect(JSON.parse(response.body)['characters'].first['corporation'].keys.sort).to eq(['alliance_id',
+                                                                                            'ceo_id',
+                                                                                            'creator_id',
+                                                                                            'date_founded',
+                                                                                            'description',
+                                                                                            'faction_id',
+                                                                                            'home_station_id',
+                                                                                            'id',
+                                                                                            'member_count',
+                                                                                            'name',
+                                                                                            'shares',
+                                                                                            'tax_rate',
+                                                                                            'ticker',
+                                                                                            'url'])
+
+      expect(JSON.parse(response.body)['characters'].first['corporation']['alliance_id']).to eq(123)
+
+      expect(JSON.parse(response.body)['characters'].first['corporation']['ceo_id']).to eq(1234)
+
+      expect(JSON.parse(response.body)['characters'].first['corporation']['creator_id']).to eq(4321)
+
+      expect(JSON.parse(response.body)['characters'].first['corporation']['date_founded']).to eq('2015-05-03T19:45:17Z')
+
+      expect(JSON.parse(response.body)['characters'].first['corporation']['description']).to eq('Character corporation description')
+
+      expect(JSON.parse(response.body)['characters'].first['corporation']['faction_id']).to eq(5555)
+
+      expect(JSON.parse(response.body)['characters'].first['corporation']['home_station_id']).to eq(999)
+
+      expect(JSON.parse(response.body)['characters'].first['corporation']['id']).to eq(456)
+
+      expect(JSON.parse(response.body)['characters'].first['corporation']['member_count']).to eq(2000)
+
+      expect(JSON.parse(response.body)['characters'].first['corporation']['name']).to eq('Character corporation name')
+
+      expect(JSON.parse(response.body)['characters'].first['corporation']['shares']).to eq(1000)
+
+      expect(JSON.parse(response.body)['characters'].first['corporation']['tax_rate']).to eq('0.99')
+
+      expect(JSON.parse(response.body)['characters'].first['corporation']['ticker']).to eq('TICKER')
+
+      expect(JSON.parse(response.body)['characters'].first['corporation']['url']).to eq('https://evemonk.com/')
     end
   end
 
