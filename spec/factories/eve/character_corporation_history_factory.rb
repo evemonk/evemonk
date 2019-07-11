e97fd5834d5bfcf73893a9ca240e5c7ce7f062a1
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-12 00:05:27 +0300

    Add Eve::CharacterCorporationHistory

---
+++ spec/factories/eve/character_corporation_history_factory.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :eve_character_corporation_history, class: 'Eve::CharacterCorporationHistory' do
+    sequence(:character_id)
+
+    sequence(:corporation_id)
+
+    is_deleted { [true, false].sample }
+
+    sequence(:record_id)
+
+    start_date { Time.zone.now }
+  end
+end
