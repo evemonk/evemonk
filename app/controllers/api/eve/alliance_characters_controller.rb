7b5e400745defb9f0a78df2c71e736022418c648
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-11 20:15:25 +0300

    Cleanup

--- app/controllers/api/eve/alliance_characters_controller.rb
+++ app/controllers/api/eve/alliance_characters_controller.rb
@@ -6,10 +6,10 @@ module Api
       skip_before_action :authenticate
 
       def index
-        @alliance = policy_scope(::Eve::Alliance).find_by!(alliance_id: params[:alliance_id])
-                                                 .decorate
+        alliance = policy_scope(::Eve::Alliance).find_by!(alliance_id: params[:alliance_id])
+                                                .decorate
 
-        @characters = policy_scope(::Eve::Character).where(alliance: @alliance)
+        @characters = policy_scope(::Eve::Character).where(alliance: alliance)
                                                     .includes(:alliance,
                                                               :ancestry,
                                                               :bloodline,
