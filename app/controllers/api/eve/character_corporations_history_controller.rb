4c4a80fba2d53af49ad7b1eef859a13347ad6498
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-12 00:14:57 +0300

    Add Api::Eve::CharacterCorporationsHistoryController

---
+++ app/controllers/api/eve/character_corporations_history_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Api
+  module Eve
+    class CharacterCorporationsHistoryController < BaseController
+      skip_before_action :authenticate
+
+      def index
+        @history = policy_scope(::Eve::CharacterCorporationHistory).includes(:corporation)
+                                                                   .where(character_id: params[:character_id])
+                                                                   .order(record_id: :desc)
+                                                                   .page(params[:page])
+                                                                   .decorate
+      end
+    end
+  end
+end
