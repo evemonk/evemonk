61383a0934f2b6ecd26144e61d29a6bd5b7c6d51
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-13 16:01:40 +0300

    Show character corporation history

--- app/javascript/components/universe/character_corporation_history.vue
+++ app/javascript/components/universe/character_corporation_history.vue
@@ -1,34 +1,39 @@
 <template>
   <div id="character_corporation_history">
-    <v-card-title>
-      Corporations History
-      <v-spacer></v-spacer>
-      <v-text-field v-model="search"
-                    append-icon="search"
-                    label="Search"
-                    single-line
-                    hide-details>
-      </v-text-field>
-    </v-card-title>
+    <template v-if="loaded">
+      <v-card-title>
+        Corporations History ({{ total_count }})
+        <v-spacer></v-spacer>
+        <v-text-field v-model="search"
+                      append-icon="search"
+                      label="Search"
+                      single-line
+                      hide-details>
+        </v-text-field>
+      </v-card-title>
 
-    <v-data-table :headers="headers"
-                  :items="history"
-                  :search="search"
-                  hide-actions
-                  dark>
-      <template v-slot:items="props">
-        <td>
-          <router-link :to="{ name: 'universe_corporation', params: { id: props.item.id }}">{{ props.item.name }}</router-link>
-        </td>
-        <td class="text-xs-right">{{ props.item.start_date }}</td>
-      </template>
+      <v-data-table :headers="headers"
+                    :items="history"
+                    :search="search"
+                    hide-actions
+                    dark>
+        <template v-slot:items="props">
+          <td>
+            <router-link :to="{ name: 'universe_corporation',
+                                params: { id: props.item.corporation.id }}">
+              {{ props.item.corporation.name }}
+            </router-link>
+          </td>
+          <td class="text-xs-right">{{ props.item.start_date }}</td>
+        </template>
 
-      <template v-slot:no-results>
-        <v-alert :value="true" color="error" icon="warning">
-          Your search for "{{ search }}" found no results.
-        </v-alert>
-      </template>
-    </v-data-table>
+        <template v-slot:no-results>
+          <v-alert :value="true" color="error" icon="warning">
+            Your search for "{{ search }}" found no results.
+          </v-alert>
+        </template>
+      </v-data-table>
+    </template>
   </div>
 </template>
 
@@ -38,11 +43,38 @@
   export default {
     props: ['id'],
 
-    created () {
-      this.fetchUniverseCharacterCorporationsHistory({ id: id, page: '1' }).then(response => {
-        
-      });
+    data () {
+      return {
+        loading: true,
+        loaded: false,
+        error: false,
+        current_page: 1,
+        total_count: null,
+        total_pages: null,
+        history: [],
+        headers: [
+          { text: 'Corporation', value: 'corporation.name', sortable: false },
+          { text: 'Joined at', value: 'start_date', sortable: false }
+        ],
+        search: '',
+      }
+    },
 
+    created () {
+      this.fetchUniverseCharacterCorporationsHistory({ id: this.id, page: '1' }).then(response => {
+        if (response.status === 200) {
+          this.history.push(...response.data.history);
+          this.total_count = response.data.total_count;
+          this.total_pages = response.data.total_pages;
+
+          this.loaded = true;
+          this.loading = false;
+        } else {
+          this.loading = false;
+          this.loaded = false;
+          this.error = true;
+        }
+      });
     },
 
     methods: {
