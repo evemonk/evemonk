e50ec233b81489394ca07a0a9979ddb734dbfae5
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-15 00:04:22 +0300

    Import corporation alliance history on corporation import

--- spec/workers/eve/corporation_importer_worker_spec.rb
+++ spec/workers/eve/corporation_importer_worker_spec.rb
@@ -19,6 +19,17 @@ describe Eve::CorporationImporterWorker do
       end
     end
 
+    before do
+      #
+      # Eve::CorporationAllianceHistoryImporter.new(corporation_id).import
+      #
+      expect(Eve::CorporationAllianceHistoryImporter).to receive(:new).with(corporation_id) do
+        double.tap do |a|
+          expect(a).to receive(:import)
+        end
+      end
+    end
+
     specify { expect { subject.perform(corporation_id) }.not_to raise_error }
   end
 end
