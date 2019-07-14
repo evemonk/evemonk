a1284dba14b47feed6cc371734a535815dc126dd
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-14 22:28:01 +0300

    Add migration

---
+++ db/migrate/20190714192614_create_eve_corporation_alliance_histories.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateEveCorporationAllianceHistories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :eve_corporation_alliance_histories do |t|
+      t.bigint :alliance_id
+      t.boolean :is_deleted
+      t.bigint :record_id
+      t.datetime :start_date
+
+      t.timestamps
+    end
+  end
+end
