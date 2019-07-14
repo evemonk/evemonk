f1d10d3bb5d2349496eb7e07a33a3afe69977f8d
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-15 00:12:03 +0300

    Eve::CorporationAllianceHistoryImporter

---
+++ app/importers/eve/corporation_alliance_history_importer.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Eve
+  class CorporationAllianceHistoryImporter
+    attr_reader :corporation_id
+
+    def initialize(corporation_id)
+      @corporation_id = corporation_id
+    end
+
+    def import
+      eve_corporation = Eve::Corporation.find_by!(corporation_id: corporation_id)
+
+      esi = EveOnline::ESI::CorporationAllianceHistory.new(corporation_id: corporation_id)
+
+      etag = Eve::Etag.find_or_initialize_by(url: esi.url)
+
+      esi.etag = etag.etag
+
+      return if esi.not_modified?
+
+      esi.entries.each do |entry|
+        history = eve_corporation.corporation_alliance_histories
+                                 .find_or_initialize_by(record_id: entry.record_id)
+
+        history.update!(entry.as_json)
+      end
+
+      etag.update!(etag: esi.etag)
+    rescue EveOnline::Exceptions::ResourceNotFound
+      eve_character.destroy!
+    rescue ActiveRecord::RecordNotFound
+      Rails.logger.info("Character with ID #{ character_id } not found")
+    end
+  end
+end
---
+++ spec/importers/eve/corporation_alliance_history_importer_spec.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Eve::CorporationAllianceHistoryImporter do
+  describe '#import' do
+    context 'when fresh data available' do
+      context 'when character found' do
+        let(:corporation_id) { double }
+
+        subject { described_class.new(corporation_id) }
+
+        let(:eve_corporation) { instance_double(Eve::Corporation) }
+
+        before { expect(Eve::Corporation).to receive(:find_by!).with(corporation_id: corporation_id).and_return(eve_corporation) }
+
+        let(:record_id) { double }
+
+        let(:json) { double }
+
+        let(:url) { double }
+
+        let(:new_etag) { double }
+
+        let(:entry) { instance_double(EveOnline::ESI::Models::CorporationAllianceHistory, record_id: record_id, as_json: json) }
+
+        let(:entries) { [entry] }
+
+        let(:esi) do
+          instance_double(EveOnline::ESI::CorporationAllianceHistory,
+                          url: url,
+                          not_modified?: false,
+                          etag: new_etag,
+                          entries: entries)
+        end
+
+        before { expect(EveOnline::ESI::CorporationAllianceHistory).to receive(:new).with(corporation_id: corporation_id).and_return(esi) }
+
+        let(:etag) { instance_double(Eve::Etag, etag: '22c39689783a86032b8d43fa0b2e8f4809c4f38a585e39471035aa8b') }
+
+        before { expect(Eve::Etag).to receive(:find_or_initialize_by).with(url: url).and_return(etag) }
+
+        before { expect(esi).to receive(:etag=).with('22c39689783a86032b8d43fa0b2e8f4809c4f38a585e39471035aa8b') }
+
+        let(:corporation_alliance_history) { instance_double(Eve::CorporationAllianceHistory) }
+
+        before do
+          #
+          # eve_corporation.corporation_alliance_histories
+          #                .find_or_initialize_by(record_id: entry.record_id) # => corporation_alliance_history
+          #
+          expect(eve_corporation).to receive(:corporation_alliance_histories) do
+            double.tap do |a|
+              expect(a).to receive(:find_or_initialize_by).with(record_id: record_id)
+                                                          .and_return(corporation_alliance_history)
+            end
+          end
+        end
+
+        before { expect(corporation_alliance_history).to receive(:update!).with(json) }
+
+        before { expect(etag).to receive(:update!).with(etag: new_etag) }
+
+        specify { expect { subject.import }.not_to raise_error }
+      end
+    end
+  end
+end
