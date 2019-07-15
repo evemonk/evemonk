cca858ca8bfd4955b4ac7f315d97b942af5a5a42
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-15 20:01:19 +0300

    Wrap in transaction

--- app/importers/eve/alliances_importer.rb
+++ app/importers/eve/alliances_importer.rb
@@ -9,15 +9,17 @@ module Eve
     end
 
     def import
-      esi.etag = etag.etag
+      ActiveRecord::Base.transaction do
+        esi.etag = etag.etag
 
-      return if esi.not_modified?
+        return if esi.not_modified?
 
-      import_new_alliances
+        import_new_alliances
 
-      remove_old_alliances
+        remove_old_alliances
 
-      etag.update!(etag: esi.etag)
+        etag.update!(etag: esi.etag)
+      end
     end
 
     private
