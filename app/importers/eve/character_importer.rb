f98bc2749db061531c99cba6b010f4da084ca160
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-16 19:49:11 +0300

    Wrap in transaction

--- app/importers/eve/character_importer.rb
+++ app/importers/eve/character_importer.rb
@@ -9,22 +9,23 @@ module Eve
     end
 
     def import
-      eve_character = Eve::Character.find_or_initialize_by(character_id: character_id)
+      ActiveRecord::Base.transaction do
+        eve_character = Eve::Character.find_or_initialize_by(character_id: character_id)
 
-      esi = EveOnline::ESI::Character.new(character_id: character_id)
+        esi = EveOnline::ESI::Character.new(character_id: character_id)
 
-      etag = Eve::Etag.find_or_initialize_by(url: esi.url)
+        etag = Eve::Etag.find_or_initialize_by(url: esi.url)
 
-      esi.etag = etag.etag
+        esi.etag = etag.etag
 
-      return if esi.not_modified?
+        return if esi.not_modified?
 
-      # FIXME: character security status always updates
-      eve_character.update!(esi.as_json)
+        eve_character.update!(esi.as_json)
 
-      etag.update!(etag: esi.etag)
-    rescue EveOnline::Exceptions::ResourceNotFound
-      eve_character.destroy!
+        etag.update!(etag: esi.etag)
+      rescue EveOnline::Exceptions::ResourceNotFound
+        eve_character.destroy!
+      end
     end
   end
 end
