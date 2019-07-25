a5ba3b90b37c63012f54778788a3952ecce4d497
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-25 14:16:16 +0300

    Update for vuetify 2

--- app/javascript/components/footer.vue
+++ app/javascript/components/footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer app dark height="auto">
+  <v-footer app height="auto">
     <v-card-text class="white--text">
       &copy; 2016 &mdash; 2019 <strong>EveMonk</strong>
     </v-card-text>
