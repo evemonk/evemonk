22a572f41856705c8e0ed080cc897658a4673adc
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-25 14:08:45 +0300

    Add index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_110626) do
+ActiveRecord::Schema.define(version: 2019_07_25_110754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -305,6 +305,7 @@ ActiveRecord::Schema.define(version: 2019_07_25_110626) do
     t.float "value"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["type_id"], name: "index_eve_type_dogma_attributes_on_type_id"
   end
 
   create_table "eve_type_dogma_effects", force: :cascade do |t|
