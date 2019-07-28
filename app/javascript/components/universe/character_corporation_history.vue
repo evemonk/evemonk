f1fdc11f762ac163d3208bece6bca9e809872d70
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-28 22:16:49 +0300

    Fix icon

--- app/javascript/components/universe/character_corporation_history.vue
+++ app/javascript/components/universe/character_corporation_history.vue
@@ -32,7 +32,7 @@
         </template>
 
         <template v-slot:no-results>
-          <v-alert :value="true" color="error" icon="warning">
+          <v-alert :value="true" color="error" icon="mdi-alert">
             Your search for "{{ search }}" found no results.
           </v-alert>
         </template>
