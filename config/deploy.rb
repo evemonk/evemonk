a372ae874bae15585931e58d110fe62620152692
Igor Zubkov <igor.zubkov@gmail.com>
2019-07-30 22:54:19 +0300

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-lock '3.11.0'
+lock "3.11.0"
 
-set :application, 'evemonk'
-set :repo_url, 'git@github.com:evemonk/evemonk.git'
+set :application, "evemonk"
+set :repo_url, "git@github.com:evemonk/evemonk.git"
 
 set :rbenv_type, :user
-set :rbenv_ruby, File.read('.ruby-version').strip
+set :rbenv_ruby, File.read(".ruby-version").strip
 
-set :linked_files, ['config/newrelic.yml', '.env']
-set :linked_dirs, ['log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/assets', 'public/packs']
+set :linked_files, ["config/newrelic.yml", ".env"]
+set :linked_dirs, ["log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/assets", "public/packs"]
 
-set :deploy_to, '/home/evemonkapp/www'
+set :deploy_to, "/home/evemonkapp/www"
 
 set :keep_releases, 2
 
@@ -25,5 +25,5 @@ namespace :deploy do
     end
   end
 
-  after :finishing, 'systemd:restart'
+  after :finishing, "systemd:restart"
 end
