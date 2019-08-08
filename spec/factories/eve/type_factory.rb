4bb1661827015a812eda4d407d14bbf1d7eacaba
Igor Zubkov <igor.zubkov@gmail.com>
2019-08-08 23:00:32 +0300

    Update eve type factory

--- spec/factories/eve/type_factory.rb
+++ spec/factories/eve/type_factory.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :eve_type, class: "Eve::Type" do
+  factory :eve_type, class: Eve::Type do
     capacity { 1.5 }
-    description { "MyText" }
-    graphic_id { "" }
-    group_id { "" }
-    icon_id { "" }
-    market_group_id { "" }
+    description { Faker::Lorem.paragraph }
+    sequence(:graphic_id)
+    sequence(:group_id)
+    sequence(:icon_id)
+    sequence(:market_group_id)
     mass { 1.5 }
-    name { "MyString" }
+    name { Faker::Lorem.word }
     packaged_volume { 1.5 }
     portion_size { 1 }
     published { [false, true].sample }
     radius { 1.5 }
-    type_id { "" }
+    sequence(:type_id)
     volume { 1.5 }
   end
 end
