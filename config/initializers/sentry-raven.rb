34dc9376a14eb6a4ab67bc227a53c6b807fd37f9
Igor Zubkov <igor.zubkov@gmail.com>
2019-08-08 23:30:07 +0300

    Exclude eve_online gem exceptions

--- config/initializers/sentry-raven.rb
+++ config/initializers/sentry-raven.rb
@@ -5,5 +5,10 @@ if Rails.env.production? && ENV["SENTRY_DSN"]
     config.dsn = ENV["SENTRY_DSN"]
 
     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+
+    config.excluded_exceptions += ['EveOnline::Exceptions::InternalServerError',
+                                   'EveOnline::Exceptions::BadGateway',
+                                   'EveOnline::Exceptions::ServiceUnavailable',
+                                   'EveOnline::Exceptions::Timeout']
   end
 end
