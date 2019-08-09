e0bcb557b482f9204d893d24b5fe620c2bd053e0
Igor Zubkov <igor.zubkov@gmail.com>
2019-08-09 21:47:04 +0300

    More icons

--- app/decorators/eve/alliance_decorator.rb
+++ app/decorators/eve/alliance_decorator.rb
@@ -10,8 +10,16 @@ module Eve
       object.date_founded.iso8601
     end
 
-    def icon
-      "https://imageserver.eveonline.com/Alliance/#{alliance_id}_128.png"
+    def icon_tiny
+      "#{imageproxy_url}https://imageserver.eveonline.com/Alliance/#{alliance_id}_32.png"
+    end
+
+    def icon_small
+      "#{imageproxy_url}https://imageserver.eveonline.com/Alliance/#{alliance_id}_64.png"
+    end
+
+    def icon_medium
+      "#{imageproxy_url}https://imageserver.eveonline.com/Alliance/#{alliance_id}_128.png"
     end
   end
 end
