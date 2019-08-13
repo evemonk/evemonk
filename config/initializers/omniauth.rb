e83f10235ca50e1da20b65da91a5f7de56fd86f1
Igor Zubkov <igor.zubkov@gmail.com>
2019-08-13 23:55:02 +0300

    Add full host name for OmniAuth. Add more scopes.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+OmniAuth.config.full_host = 'https://evemonk.com'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :eve_online_sso,
            Setting.eve_online_sso_client_id,
            Setting.eve_online_sso_secret_key,
-           scope: "esi-wallet.read_character_wallet.v1 esi-characters.read_loyalty.v1 esi-skills.read_skillqueue.v1 esi-skills.read_skills.v1 esi-assets.read_assets.v1"
+           scope: "esi-wallet.read_character_wallet.v1 esi-characters.read_loyalty.v1 esi-skills.read_skillqueue.v1 esi-skills.read_skills.v1 esi-assets.read_assets.v1 esi-location.read_location.v1 esi-location.read_online.v1 esi-location.read_ship_type.v1"
 end
