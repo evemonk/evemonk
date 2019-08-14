1e507077d88bb73713ec73afb765655524e13336
Igor Zubkov <igor.zubkov@gmail.com>
2019-08-15 01:22:13 +0300

    Fix spec

--- spec/requests/api/characters_controller_spec.rb
+++ spec/requests/api/characters_controller_spec.rb
@@ -3,6 +3,8 @@
 require "rails_helper"
 
 describe Api::CharactersController do
+  before { ENV["IMAGEPROXY_ENABLED"] = "yes, please" }
+
   describe "#index" do
     it "returns list of user characters" do
       user = create(:user)
@@ -115,151 +117,106 @@ describe Api::CharactersController do
 
       session = create(:session, user: user)
 
-      alliance = create(:eve_alliance,
-                        alliance_id: 1234,
-                        name: "Alliance name")
+      create(:eve_alliance,
+             alliance_id: 123,
+             creator_corporation_id: 2222,
+             creator_id: 3333,
+             date_founded: "Sun, 03 May 2015 19:45:17 UTC +00:00",
+             executor_corporation_id: 4444,
+             faction_id: 5555,
+             name: "Alliance name",
+             ticker: "ALLIANCE_TICKER")
 
-      corporation = create(:eve_corporation,
-                           alliance_id: alliance.alliance_id,
-                           corporation_id: 12_345,
-                           name: "Corporation name")
+      create(:eve_corporation,
+             alliance_id: 123,
+             corporation_id: 456,
+             name: "Character corporation name",
+             description: "Character corporation description",
+             ticker: "TICKER",
+             date_founded: "Sun, 03 May 2015 19:45:17 UTC +00:00",
+             corporation_url: "https://evemonk.com/",
+             member_count: 2000,
+             shares: 1000,
+             tax_rate: 0.99,
+             ceo_id: 1234,
+             creator_id: 4321,
+             faction_id: 5555,
+             home_station_id: 999,
+             war_eligible: false)
 
-      race = create(:eve_race,
-                    race_id: 124,
-                    name: "Race name")
+      create(:eve_alliance_corporation,
+             alliance_id: 123,
+             corporation_id: 456)
 
-      bloodline = create(:eve_bloodline,
-                         bloodline_id: 125,
-                         name: "Bloodline name")
+      create(:character,
+             user: user,
+             alliance_id: 123,
+             character_id: 123_123_123,
+             corporation_id: 456,
+             ancestry_id: 10,
+             bloodline_id: 12,
+             faction_id: 5555,
+             race_id: 123,
+             name: "Character name",
+             description: "Character description",
+             gender: "male",
+             birthday: "Sun, 03 May 2015 19:45:17 UTC +00:00",
+             security_status: 1.1,
+             title: "Character title",
+             charisma: 20,
+             intelligence: 21,
+             memory: 22,
+             perception: 23,
+             willpower: 24,
+             wallet: 252.49,
+             bonus_remaps: 2,
+             last_remap_date: "2011-05-07T12:58:06Z",
+             accrued_remap_cooldown_date: "2012-05-06T12:58:06Z")
 
-      ancestry = create(:eve_ancestry,
-                        ancestry_id: 126,
-                        name: "Ancestry name")
+      create(:eve_bloodline,
+             bloodline_id: 12,
+             name: "Bloodline name",
+             description: "Bloodline description",
+             race_id: 123,
+             corporation_id: 456,
+             ship_type_id: 30,
+             perception: 21,
+             willpower: 22,
+             charisma: 23,
+             memory: 24,
+             intelligence: 25)
 
-      faction = create(:eve_faction,
-                       faction_id: 127,
-                       name: "Faction name")
+      create(:eve_ancestry,
+             ancestry_id: 10,
+             name: "Ancestry name",
+             bloodline_id: 12,
+             description: "Ancestry description",
+             short_description: "Ancestry short description",
+             icon_id: 14)
 
-      character = create(:character,
-                         user: user,
-                         alliance_id: alliance.alliance_id,
-                         corporation_id: corporation.corporation_id,
-                         race_id: race.race_id,
-                         bloodline_id: bloodline.bloodline_id,
-                         ancestry_id: ancestry.ancestry_id,
-                         faction_id: faction.faction_id,
-                         character_id: "123",
-                         name: "Character name",
-                         description: "Character description",
-                         birthday: "2010-01-15T15:26:00Z",
-                         gender: "male",
-                         charisma: 20,
-                         intelligence: 21,
-                         memory: 22,
-                         perception: 23,
-                         willpower: 24,
-                         security_status: 1.8,
-                         wallet: 252.49,
-                         bonus_remaps: 2,
-                         last_remap_date: "2011-05-07T12:58:06Z",
-                         accrued_remap_cooldown_date: "2012-05-06T12:58:06Z")
+      create(:eve_faction,
+             faction_id: 5555,
+             name: "Faction name",
+             description: "Faction description",
+             solar_system_id: 10,
+             corporation_id: 456,
+             militia_corporation_id: 20,
+             size_factor: 2.0,
+             station_count: 10,
+             station_system_count: 100,
+             is_unique: true)
 
-      get "/api/characters/#{character.character_id}", headers: {'Authorization': "Bearer #{session.token}"}
+      create(:eve_race,
+             race_id: 123,
+             name: "Race name",
+             description: "Race description",
+             alliance_id: 123)
+
+      get "/api/characters/123123123", headers: {'Authorization': "Bearer #{session.token}"}
 
       expect(response).to have_http_status(:ok)
 
-      expect(JSON.parse(response.body).keys.sort).to eq(["character"])
-
-      expect(JSON.parse(response.body)["character"].keys.sort).to eq(["accrued_remap_cooldown_date",
-                                                                      "alliance",
-                                                                      "ancestry",
-                                                                      "birthday",
-                                                                      "bloodline",
-                                                                      "bonus_remaps",
-                                                                      "charisma",
-                                                                      "corporation",
-                                                                      "description",
-                                                                      "faction",
-                                                                      "gender",
-                                                                      "icon",
-                                                                      "id",
-                                                                      "intelligence",
-                                                                      "last_remap_date",
-                                                                      "memory",
-                                                                      "name",
-                                                                      "perception",
-                                                                      "race",
-                                                                      "security_status",
-                                                                      "wallet",
-                                                                      "willpower",])
-
-      expect(JSON.parse(response.body)["character"]["id"]).to eq(123)
-
-      expect(JSON.parse(response.body)["character"]["icon"]).to eq("https://imageserver.eveonline.com/Character/123_512.jpg")
-
-      expect(JSON.parse(response.body)["character"]["name"]).to eq("Character name")
-
-      expect(JSON.parse(response.body)["character"]["description"]).to eq("Character description")
-
-      expect(JSON.parse(response.body)["character"]["birthday"]).to eq("2010-01-15T15:26:00Z")
-
-      expect(JSON.parse(response.body)["character"]["gender"]).to eq("male")
-
-      expect(JSON.parse(response.body)["character"]["charisma"]).to eq(20)
-
-      expect(JSON.parse(response.body)["character"]["intelligence"]).to eq(21)
-
-      expect(JSON.parse(response.body)["character"]["memory"]).to eq(22)
-
-      expect(JSON.parse(response.body)["character"]["perception"]).to eq(23)
-
-      expect(JSON.parse(response.body)["character"]["willpower"]).to eq(24)
-
-      expect(JSON.parse(response.body)["character"]["security_status"]).to eq("1.8")
-
-      expect(JSON.parse(response.body)["character"]["wallet"]).to eq("252.49 ISK")
-
-      expect(JSON.parse(response.body)["character"]["bonus_remaps"]).to eq(2)
-
-      expect(JSON.parse(response.body)["character"]["last_remap_date"]).to eq("2011-05-07T12:58:06Z")
-
-      expect(JSON.parse(response.body)["character"]["accrued_remap_cooldown_date"]).to eq("2012-05-06T12:58:06Z")
-
-      expect(JSON.parse(response.body)["character"]["race"].keys.sort).to eq(["id", "name"])
-
-      expect(JSON.parse(response.body)["character"]["race"]["id"]).to eq(124)
-
-      expect(JSON.parse(response.body)["character"]["race"]["name"]).to eq("Race name")
-
-      expect(JSON.parse(response.body)["character"]["bloodline"].keys.sort).to eq(["id", "name"])
-
-      expect(JSON.parse(response.body)["character"]["bloodline"]["id"]).to eq(125)
-
-      expect(JSON.parse(response.body)["character"]["bloodline"]["name"]).to eq("Bloodline name")
-
-      expect(JSON.parse(response.body)["character"]["ancestry"].keys.sort).to eq(["id", "name"])
-
-      expect(JSON.parse(response.body)["character"]["ancestry"]["id"]).to eq(126)
-
-      expect(JSON.parse(response.body)["character"]["ancestry"]["name"]).to eq("Ancestry name")
-
-      expect(JSON.parse(response.body)["character"]["faction"].keys.sort).to eq(["id", "name"])
-
-      expect(JSON.parse(response.body)["character"]["faction"]["id"]).to eq(127)
-
-      expect(JSON.parse(response.body)["character"]["faction"]["name"]).to eq("Faction name")
-
-      expect(JSON.parse(response.body)["character"]["alliance"].keys.sort).to eq(["id", "name"])
-
-      expect(JSON.parse(response.body)["character"]["alliance"]["id"]).to eq(1234)
-
-      expect(JSON.parse(response.body)["character"]["alliance"]["name"]).to eq("Alliance name")
-
-      expect(JSON.parse(response.body)["character"]["corporation"].keys.sort).to eq(["id", "name"])
-
-      expect(JSON.parse(response.body)["character"]["corporation"]["id"]).to eq(12_345)
-
-      expect(JSON.parse(response.body)["character"]["corporation"]["name"]).to eq("Corporation name")
+      expect(JSON.parse(response.body)).to eq("character"=>{"id"=>123123123, "icon"=>{"tiny"=>"https://imageproxy.evemonk.com/https://imageserver.eveonline.com/Character/123123123_32.png", "small"=>"https://imageproxy.evemonk.com/https://imageserver.eveonline.com/Character/123123123_64.png", "medium"=>"https://imageproxy.evemonk.com/https://imageserver.eveonline.com/Character/123123123_128.png", "large"=>"https://imageproxy.evemonk.com/https://imageserver.eveonline.com/Character/123123123_256.png", "huge"=>"https://imageproxy.evemonk.com/https://imageserver.eveonline.com/Character/123123123_512.png", "gigantic"=>"https://imageproxy.evemonk.com/https://imageserver.eveonline.com/Character/123123123_1024.png"}, "name"=>"Character name", "description"=>"Character description", "birthday"=>"2015-05-03T19:45:17Z", "gender"=>"male", "security_status"=>"1.1", "perception"=>23, "memory"=>22, "willpower"=>24, "intelligence"=>21, "charisma"=>20, "wallet"=>"252.49 ISK", "bonus_remaps"=>2, "last_remap_date"=>"2011-05-07T12:58:06Z", "accrued_remap_cooldown_date"=>"2012-05-06T12:58:06Z", "alliance"=>{"id"=>123, "icon"=>{"tiny"=>"https://imageproxy.evemonk.com/https://imageserver.eveonline.com/Alliance/123_32.png", "small"=>"https://imageproxy.evemonk.com/https://imageserver.eveonline.com/Alliance/123_64.png", "medium"=>"https://imageproxy.evemonk.com/https://imageserver.eveonline.com/Alliance/123_128.png"}, "creator_corporation_id"=>2222, "creator_id"=>3333, "date_founded"=>"2015-05-03T19:45:17Z", "executor_corporation_id"=>4444, "faction_id"=>5555, "name"=>"Alliance name", "ticker"=>"ALLIANCE_TICKER", "corporations_count"=>1, "characters_count"=>2000}, "ancestry"=>{"id"=>10, "bloodline_id"=>12, "name"=>"Ancestry name", "short_description"=>"Ancestry short description", "description"=>"Ancestry description", "icon_id"=>14}, "bloodline"=>{"id"=>12, "corporation_id"=>456, "race_id"=>123, "name"=>"Bloodline name", "description"=>"Bloodline description", "charisma"=>23, "intelligence"=>25, "memory"=>24, "perception"=>21, "willpower"=>22, "ship_type_id"=>30}, "corporation"=>{"id"=>456, "icon"=>{"tiny"=>"https://imageproxy.evemonk.com/https://imageserver.eveonline.com/Corporation/456_32.png", "small"=>"https://imageproxy.evemonk.com/https://imageserver.eveonline.com/Corporation/456_64.png", "medium"=>"https://imageproxy.evemonk.com/https://imageserver.eveonline.com/Corporation/456_128.png", "large"=>"https://imageproxy.evemonk.com/https://imageserver.eveonline.com/Corporation/456_256.png"}, "name"=>"Character corporation name", "description"=>"Character corporation description", "ticker"=>"TICKER", "date_founded"=>"2015-05-03T19:45:17Z", "url"=>"https://evemonk.com/", "member_count"=>2000, "shares"=>1000, "tax_rate"=>"0.99", "alliance_id"=>123, "ceo_id"=>1234, "creator_id"=>4321, "faction_id"=>5555, "home_station_id"=>999, "war_eligible"=>false}, "faction"=>{"id"=>5555, "corporation_id"=>456, "name"=>"Faction name", "description"=>"Faction description", "militia_corporation_id"=>20, "solar_system_id"=>10, "station_count"=>10, "station_system_count"=>100, "size_factor"=>"2.0", "is_unique"=>true}, "race"=>{"id"=>123, "alliance_id"=>123, "name"=>"Race name", "description"=>"Race description"}})
     end
   end
 end
