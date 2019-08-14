01f129d3132aeed66ebf52777b531c5c1c7a21da
Igor Zubkov <igor.zubkov@gmail.com>
2019-08-15 01:33:22 +0300

    Cleanup

--- spec/requests/api/profiles_controller_spec.rb
+++ spec/requests/api/profiles_controller_spec.rb
@@ -12,21 +12,16 @@ describe Api::ProfilesController do
 
       session = create(:session, user: user)
 
-      create(:character, user: user, character_id: "123", name: "Character name")
+      create(:character, user: user)
 
       get "/api/profile", headers: {'Authorization': "Bearer #{session.token}"}
 
       expect(response).to have_http_status(:ok)
 
-      expect(JSON.parse(response.body).keys.sort).to eq(["characters_count", "created_at", "email", "updated_at"])
-
-      expect(JSON.parse(response.body)["characters_count"]).to eq(1)
-
-      expect(JSON.parse(response.body)["created_at"]).to eq("2010-01-15T15:26:00Z")
-
-      expect(JSON.parse(response.body)["email"]).to eq("me@example.com")
-
-      expect(JSON.parse(response.body)["updated_at"]).to eq("2010-01-16T15:26:00Z")
+      expect(JSON.parse(response.body)).to eq("characters_count" => 1,
+                                              "created_at" => "2010-01-15T15:26:00Z",
+                                              "email" => "me@example.com",
+                                              "updated_at" => "2010-01-16T15:26:00Z")
     end
   end
 end
