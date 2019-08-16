2685ff23bda890a1cd710aa871b2a0feeb4f0849
Igor Zubkov <igor.zubkov@gmail.com>
2019-08-16 16:32:06 +0300

    Update factory

--- spec/factories/eve/server_status_factory.rb
+++ spec/factories/eve/server_status_factory.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :eve_server_status, class: 'Eve::ServerStatus' do
-    players { "" }
-    server_version { "MyString" }
-    start_time { "2019-08-16 16:20:00" }
-    vip { false }
+  factory :eve_server_status, class: Eve::ServerStatus do
+    sequence(:players)
+    server_version { Faker::App.version }
+    start_time { Time.zone.now }
+    vip { [true, false].sample }
   end
 end
