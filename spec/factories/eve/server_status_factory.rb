6366b5d022d9433613763b5b5a68d767523d3638
Igor Zubkov <igor.zubkov@gmail.com>
2019-08-16 16:34:49 +0300

    Cleanup

--- spec/factories/eve/server_status_factory.rb
+++ spec/factories/eve/server_status_factory.rb
@@ -3,8 +3,11 @@
 FactoryBot.define do
   factory :eve_server_status, class: Eve::ServerStatus do
     sequence(:players)
+
     server_version { Faker::App.version }
+
     start_time { Time.zone.now }
+
     vip { [true, false].sample }
   end
 end
