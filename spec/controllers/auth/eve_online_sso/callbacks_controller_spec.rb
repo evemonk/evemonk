a38a09ff26ec3911242af55729b1dba129a58e08
Igor Zubkov <igor.zubkov@gmail.com>
2019-08-16 20:00:16 +0300

    More exceptions

--- spec/controllers/auth/eve_online_sso/callbacks_controller_spec.rb
+++ spec/controllers/auth/eve_online_sso/callbacks_controller_spec.rb
@@ -7,6 +7,12 @@ describe Auth::EveOnlineSso::CallbacksController do
 
   it { should rescue_from(EveOnline::Exceptions::ServiceUnavailable).with(:handle_service_unavailable) }
 
+  it { should rescue_from(EveOnline::Exceptions::InternalServerError).with(:handle_internal_server_error) }
+
+  it { should rescue_from(EveOnline::Exceptions::BadGateway).with(:handle_bad_gateway) }
+
+  it { should rescue_from(EveOnline::Exceptions::Timeout).with(:handle_timeout) }
+
   describe "#show" do
     let(:session) { instance_double(Session, token: "token123") }
 
@@ -31,8 +37,26 @@ describe Auth::EveOnlineSso::CallbacksController do
   # private methods
 
   describe "#handle_service_unavailable" do
-    before { expect(subject).to receive(:render).with(inline: "Net::HTTPServiceUnavailable (503). Try again later.") }
+    before { expect(subject).to receive(:render).with(inline: "Service Unavailable (503). Try again later.") }
 
     specify { expect { subject.send(:handle_service_unavailable) }.not_to raise_error }
+  end
+
+  describe "#handle_internal_server_error" do
+    before { expect(subject).to receive(:render).with(inline: "Internal Server Error (500). Try again later.") }
+
+    specify { expect { subject.send(:handle_internal_server_error) }.not_to raise_error }
+  end
+
+  describe "#handle_bad_gateway" do
+    before { expect(subject).to receive(:render).with(inline: "Bad Gateway (502). Try again later.") }
+
+    specify { expect { subject.send(:handle_bad_gateway) }.not_to raise_error }
+  end
+
+  describe "#handle_timeout" do
+    before { expect(subject).to receive(:render).with(inline: "Timeout Error. Try again later.") }
+
+    specify { expect { subject.send(:handle_timeout) }.not_to raise_error }
   end
 end
