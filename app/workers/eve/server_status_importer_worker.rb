ae1d282c8f599ecbfcbaaa8b26b3d57cb114b465
Igor Zubkov <igor.zubkov@gmail.com>
2019-08-16 16:51:45 +0300

    Don't retry for ServerStatusImporterWorker

--- app/workers/eve/server_status_importer_worker.rb
+++ app/workers/eve/server_status_importer_worker.rb
@@ -4,6 +4,8 @@ module Eve
   class ServerStatusImporterWorker
     include Sidekiq::Worker
 
+    sidekiq_options retry: false
+
     def perform(*)
       Eve::ServerStatusImporter.new.import
     end
--- spec/workers/eve/server_status_importer_worker_spec.rb
+++ spec/workers/eve/server_status_importer_worker_spec.rb
@@ -5,6 +5,10 @@ require "rails_helper"
 describe Eve::ServerStatusImporterWorker do
   it { should be_a(Sidekiq::Worker) }
 
+  describe ".sidekiq_options" do
+    specify { expect(described_class.sidekiq_options["retry"]).to eq(false) }
+  end
+
   describe "#perform" do
     before do
       #
