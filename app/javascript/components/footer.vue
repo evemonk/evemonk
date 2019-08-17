03d5cabd001928554b2ba40d00eee57c6b29944e
Igor Zubkov <igor.zubkov@gmail.com>
2019-08-17 16:55:09 +0300

    Fix footer

--- app/javascript/components/footer.vue
+++ app/javascript/components/footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer app height="auto">
+  <v-footer app max-height="54" height="auto">
     <v-card-text>
       &copy; 2016 &mdash; 2019 <strong>EveMonk</strong>
     </v-card-text>
