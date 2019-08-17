b6718bbbb4e5242b80aaec7e843d376351929849
Igor Zubkov <igor.zubkov@gmail.com>
2019-08-17 22:44:11 +0300

    Add spec

---
+++ spec/requests/api/eve/server_status_lasts_controller_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Api::Eve::ServerStatusLastsController do
+  describe "#show" do
+    it "returns last eve server status" do
+      create(:eve_server_status,
+             players: 28_384,
+             server_version: "1550977",
+             start_time: "2019-08-17T11:01:55Z",
+             vip: false)
+
+      get "/api/eve/server_status_last"
+
+      expect(response).to have_http_status(:ok)
+
+      expect(JSON.parse(response.body)).to eq("server_status" => {
+                                                "players" => 28_384,
+                                                "server_version" => "1550977",
+                                                "start_time" => "2019-08-17T11:01:55Z",
+                                                "vip" => false,
+                                              })
+    end
+  end
+end
