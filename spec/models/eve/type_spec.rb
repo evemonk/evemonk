838a451a576b8b1744dd7efefbe1b3128eabb56d
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-02 00:27:03 +0300

    Add translations and paper trail to eve type

--- spec/models/eve/type_spec.rb
+++ spec/models/eve/type_spec.rb
@@ -7,6 +7,12 @@ describe Eve::Type do
 
   it { expect(described_class).to respond_to(:search) }
 
+  it { should respond_to(:versions) }
+
+  it { expect(described_class).to respond_to(:translates) }
+
+  it { expect(described_class.translated_attribute_names).to eq(["description"]) }
+
   it { expect(described_class.table_name).to eq("eve_types") }
 
   it { should have_many(:type_dogma_attributes).with_primary_key(:type_id).with_foreign_key(:type_id).dependent(:destroy) }
