44fdc07cd67c1c19d747a2e3929cfcff69eff9db
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-01 20:43:19 +0300

    Translate Eve::Race

--- app/models/eve/race.rb
+++ app/models/eve/race.rb
@@ -2,6 +2,10 @@
 
 module Eve
   class Race < ApplicationRecord
+    extend Mobility
+
+    translates :name, :description
+
     belongs_to :alliance, primary_key: :alliance_id, optional: true
   end
 end
--- spec/models/eve/race_spec.rb
+++ spec/models/eve/race_spec.rb
@@ -5,6 +5,10 @@ require "rails_helper"
 describe Eve::Race do
   it { should be_a(ApplicationRecord) }
 
+  it { expect(described_class).to respond_to(:translates) }
+
+  it { expect(described_class.translated_attribute_names).to eq(["name", "description"]) }
+
   it { expect(described_class.table_name).to eq("eve_races") }
 
   it { should belong_to(:alliance).with_primary_key(:alliance_id).optional }
