6119b461dd6cbe9a8f4735dbfb7cca1f446a20e8
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-01 21:51:46 +0300

    Enable paper trail

--- app/models/eve/race.rb
+++ app/models/eve/race.rb
@@ -4,6 +4,8 @@ module Eve
   class Race < ApplicationRecord
     extend Mobility
 
+    has_paper_trail
+
     translates :name, :description
 
     belongs_to :alliance, primary_key: :alliance_id, optional: true
--- spec/models/eve/race_spec.rb
+++ spec/models/eve/race_spec.rb
@@ -5,6 +5,8 @@ require "rails_helper"
 describe Eve::Race do
   it { should be_a(ApplicationRecord) }
 
+  it { should respond_to(:versions) }
+
   it { expect(described_class).to respond_to(:translates) }
 
   it { expect(described_class.translated_attribute_names).to eq(["name", "description"]) }
