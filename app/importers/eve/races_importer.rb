55730c10968bf045821f2285d5736f0c74a0fc1f
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-01 20:48:40 +0300

    Update Eve::Race's with locale :en

--- app/importers/eve/races_importer.rb
+++ app/importers/eve/races_importer.rb
@@ -3,22 +3,24 @@
 module Eve
   class RacesImporter
     def import
-      ActiveRecord::Base.transaction do
-        esi = EveOnline::ESI::UniverseRaces.new
+      Mobility.with_locale(:en) do
+        ActiveRecord::Base.transaction do
+          esi = EveOnline::ESI::UniverseRaces.new
 
-        etag = Eve::Etag.find_or_initialize_by(url: esi.url)
+          etag = Eve::Etag.find_or_initialize_by(url: esi.url)
 
-        esi.etag = etag.etag
+          esi.etag = etag.etag
 
-        return if esi.not_modified?
+          return if esi.not_modified?
 
-        esi.races.each do |race|
-          eve_race = Eve::Race.find_or_initialize_by(race_id: race.race_id)
+          esi.races.each do |race|
+            eve_race = Eve::Race.find_or_initialize_by(race_id: race.race_id)
 
-          eve_race.update!(race.as_json)
+            eve_race.update!(race.as_json)
+          end
+
+          etag.update!(etag: esi.etag)
         end
-
-        etag.update!(etag: esi.etag)
       end
     end
   end
