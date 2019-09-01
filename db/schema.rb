caff3fd03cf99e2b5078e4fe72d3312e10bf89b8
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-01 21:45:12 +0300

    Add migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_01_165045) do
+ActiveRecord::Schema.define(version: 2019_09_01_184354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -481,6 +481,17 @@ ActiveRecord::Schema.define(version: 2019_09_01_165045) do
     t.string "reset_password_token"
     t.index "lower((email)::text)", name: "index_users_on_LOWER_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.bigint "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object"
+    t.datetime "created_at"
+    t.text "object_changes"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "character_assets", "characters"
