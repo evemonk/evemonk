583201a09a3810ff6d486b03bd896d736066b0ba
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-02 22:36:21 +0300

    Eve::RacesTranslationsImporterWorker

---
+++ spec/workers/eve/races_translations_importer_worker_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::RacesTranslationsImporterWorker do
+  it { should be_a(Sidekiq::Worker) }
+
+  describe "#perform" do
+    before do
+      #
+      # Eve::RacesTranslationsImporter.new.import
+      #
+      expect(Eve::RacesTranslationsImporter).to receive(:new) do
+        double.tap do |a|
+          expect(a).to receive(:import)
+        end
+      end
+    end
+
+    specify { expect { subject.perform }.not_to raise_error }
+  end
+end
