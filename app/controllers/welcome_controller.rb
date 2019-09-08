105821a2b61839921d48e19b62ff54a6019925ae
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-08 21:10:57 +0300

    More

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
+  def index
+    @alliances = Eve::Alliance.limit(20).decorate
+    @corporations = Eve::Corporation.limit(20).decorate
+  end
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const erb = require('./loaders/erb')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-environment.loaders.prepend('erb', erb)
 module.exports = environment
