3062625b7de7e1f46d67db3390dec216c7d5cb99
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-10 23:27:53 +0300

    More work

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@
 
 class WelcomeController < ApplicationController
   def index
-    @alliances = Eve::Alliance.limit(20).decorate
-    @corporations = Eve::Corporation.limit(20).decorate
+    @alliances = policy_scope(::Eve::Alliance).limit(20).decorate
+    @corporations = policy_scope(::Eve::Corporation).limit(20).decorate
   end
 end
