a5d0c68d50d929b7d238938c98da6da32c9994ae
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-12 22:29:34 +0300

    Update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("@rails/ujs").start()
+require("turbolinks").start()
 require("channels")
+require("jquery")
+require("popper")
+require("bootstrap")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
