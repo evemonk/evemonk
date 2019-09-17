7c317e6b82b03ee7dfe13e632cad483284788bad
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-18 02:10:44 +0300

    Fix migration

--- db/migrate/20161206202414_remove_token_from_users.rb
+++ db/migrate/20161206202414_remove_token_from_users.rb
@@ -2,6 +2,6 @@
 
 class RemoveTokenFromUsers < ActiveRecord::Migration[5.0]
   def change
-    remove_column :users, :token, :string
+    safety_assured { remove_column :users, :token, :string }
   end
 end
