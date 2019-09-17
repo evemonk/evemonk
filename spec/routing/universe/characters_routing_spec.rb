36d09def2f013cf91352ab36fae50b07f45766b4
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-17 19:15:27 +0300

    More specs

---
+++ spec/routing/universe/characters_routing_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Universe::CharactersController do
+  describe "routing" do
+    it "should route GET /universe/characters to universe/characters#index" do
+      expect(get: "/universe/characters").to route_to(
+        controller: "universe/characters",
+        action: "index",
+      )
+    end
+
+    it "should route GET /universe/characters/90729314 to universe/characters#show" do
+      expect(get: "/universe/characters/90729314").to route_to(
+        controller: "universe/characters",
+        action: "show",
+        id: "90729314",
+      )
+    end
+  end
+end
---
+++ spec/routing/universe/corporations_routing_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Universe::CorporationsController do
+  describe "routing" do
+    it "should route GET /universe/corporations to universe/corporations#index" do
+      expect(get: "/universe/corporations").to route_to(
+        controller: "universe/corporations",
+        action: "index",
+      )
+    end
+
+    it "should route GET /universe/corporations/98005120 to universe/corporations#show" do
+      expect(get: "/universe/corporations/98005120").to route_to(
+        controller: "universe/corporations",
+        action: "show",
+        id: "98005120",
+      )
+    end
+  end
+end
