48f406c354cfac2d419fa49bf52d0a7c06e5043b
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-18 03:02:03 +0300

    Cleanup

--- db/migrate/20161206202414_remove_token_from_users.rb
+++ db/migrate/20161206202414_remove_token_from_users.rb
@@ -2,6 +2,6 @@
 
 class RemoveTokenFromUsers < ActiveRecord::Migration[5.0]
   def change
-    safety_assured { remove_column :users, :token, :string }
+    remove_column :users, :token, :string
   end
 end
--- db/migrate/20161206221740_add_foreign_key_secure_tokens_users.rb
+++ db/migrate/20161206221740_add_foreign_key_secure_tokens_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddForeignKeySecureTokensUsers < ActiveRecord::Migration[6.0]
+class AddForeignKeySecureTokensUsers < ActiveRecord::Migration[5.0]
   def change
     add_foreign_key :secure_tokens, :users
   end
