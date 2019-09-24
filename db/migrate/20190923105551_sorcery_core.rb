638b568edeca5458a84f16171d780509af2886c1
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-24 22:13:46 +0300

    Cleanup

--- db/migrate/20190923105551_sorcery_core.rb
+++ db/migrate/20190923105551_sorcery_core.rb
@@ -4,15 +4,5 @@ class SorceryCore < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :crypted_password, :string
     add_column :users, :salt, :string
-
-    # create_table :users do |t|
-    #   t.string :email,            null: false
-    #   t.string :crypted_password
-    #   t.string :salt
-    #
-    #   t.timestamps                null: false
-    # end
-    #
-    # add_index :users, :email, unique: true
   end
 end
