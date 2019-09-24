0c1bc7d652c86631650bc63256feaf91e6eb4565
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-24 21:58:36 +0300

    More specs

--- spec/controllers/universe/alliances_controller_spec.rb
+++ spec/controllers/universe/alliances_controller_spec.rb
@@ -5,6 +5,8 @@ require "rails_helper"
 describe Universe::AlliancesController do
   it { should be_a(ApplicationController) }
 
+  it { should_not use_before_action(:require_login) }
+
   describe "#index" do
     let(:eve_alliance_policy) { Eve::Alliance }
 
