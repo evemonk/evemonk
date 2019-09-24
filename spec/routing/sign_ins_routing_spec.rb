639259ccca5c87238df993ca6a8a81743b2ec7ad
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-25 00:06:50 +0300

    Add specs

---
+++ spec/routing/sign_ins_routing_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe SignInsController do
+  describe "routing" do
+    it "should route GET sign_in to sign_ins#show" do
+      expect(get: "/sign_in").to route_to(
+        controller: "sign_ins",
+        action: "show"
+      )
+    end
+
+    it "should route POST /sign_in to sign_ins#create" do
+      expect(post: "/sign_in").to route_to(
+        controller: "sign_ins",
+        action: "create"
+      )
+    end
+  end
+end
