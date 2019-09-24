bd005f8faa473df043efc69d946ade97bd846e50
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-24 22:15:29 +0300

    Remove users#password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_104505) do
+ActiveRecord::Schema.define(version: 2019_09_24_191403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -518,7 +518,6 @@ ActiveRecord::Schema.define(version: 2019_09_24_104505) do
 
   create_table "users", id: :serial, force: :cascade do |t|
     t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "notifications_count", default: 0
