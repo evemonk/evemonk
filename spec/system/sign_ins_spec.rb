ed9fbfe9c3b074301da551672b7a84079ef8e259
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-26 22:49:03 +0300

    Cleanup

--- spec/system/sign_ins_spec.rb
+++ spec/system/sign_ins_spec.rb
@@ -4,45 +4,33 @@ require "rails_helper"
 
 describe "Sign in features" do
   it "when user successfully sign in" do
-    user = create(:user, email: "me@example.com", password: "eidii7EeooVe8ahk")
-
-    # create(:eve_server_status)
+    create(:user, email: "me@example.com", password: "eidii7EeooVe8ahk")
 
     visit "/sign_in"
 
     fill_in "sign_in[email]", with: "me@example.com"
     fill_in "sign_in[password]", with: "eidii7EeooVe8ahk"
 
-    # find("Sign in").click # TODO: click on 'Sign In'
-
     click_button "Sign in"
 
     # expect(page).to have_content("Successful signed in!")
 
     expect(current_path).to eq("/characters")
-
-    # expect(user.sessions.count).to eq(1)
   end
 
   it "when user successfully sign in with upper cased email" do
-    user = create(:user, email: "me@example.com", password: "eidii7EeooVe8ahk")
-
-    # create(:eve_server_status)
+    create(:user, email: "me@example.com", password: "eidii7EeooVe8ahk")
 
     visit "/sign_in"
 
     fill_in "sign_in[email]", with: "ME@EXAMPLE.COM"
     fill_in "sign_in[password]", with: "eidii7EeooVe8ahk"
 
-    # find("#sign_in_button").click # TODO: click on 'Sign In'
-
     click_button "Sign in"
 
     # expect(page).to have_content("Successful signed in!")
 
     expect(current_path).to eq("/characters")
-
-    # expect(user.sessions.count).to eq(1)
   end
 
   # it "when user with given email not found" do
