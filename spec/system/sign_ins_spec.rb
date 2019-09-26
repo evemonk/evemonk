67b995f65dc2489623b30f014c2240e0b9bb8612
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-26 22:45:16 +0300

    More specs

--- spec/system/sign_ins_spec.rb
+++ spec/system/sign_ins_spec.rb
@@ -24,23 +24,26 @@ describe "Sign in features" do
     # expect(user.sessions.count).to eq(1)
   end
 
-  # it "when user successfully sign in with upper cased email" do
-  #   user = create(:user, email: "me@example.com", password: "eidii7EeooVe8ahk")
-  #
-  #   create(:eve_server_status)
-  #
-  #   visit "/sign_in"
-  #
-  #   fill_in "email", with: "ME@EXAMPLE.COM"
-  #   fill_in "password", with: "eidii7EeooVe8ahk"
-  #   find("#sign_in_button").click # TODO: click on 'Sign In'
-  #
-  #   expect(page).to have_content("Successful signed in!")
-  #
-  #   expect(current_path).to eq("/profile")
-  #
-  #   expect(user.sessions.count).to eq(1)
-  # end
+  it "when user successfully sign in with upper cased email" do
+    user = create(:user, email: "me@example.com", password: "eidii7EeooVe8ahk")
+
+    # create(:eve_server_status)
+
+    visit "/sign_in"
+
+    fill_in "sign_in[email]", with: "ME@EXAMPLE.COM"
+    fill_in "sign_in[password]", with: "eidii7EeooVe8ahk"
+
+    # find("#sign_in_button").click # TODO: click on 'Sign In'
+
+    click_button "Sign in"
+
+    # expect(page).to have_content("Successful signed in!")
+
+    expect(current_path).to eq("/characters")
+
+    # expect(user.sessions.count).to eq(1)
+  end
 
   # it "when user with given email not found" do
   #   create(:eve_server_status)
