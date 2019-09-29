9bf9f76ade217b6f5b669c5e7f15f2e94fe85a0e
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-29 23:18:12 +0300

    Fix bootstrap javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("channels")
-require("jquery")
-require("popper")
-require("bootstrap")
-
 // https://docs.sentry.io/platforms/javascript/
 
 // When using npm, import Sentry
@@ -17,9 +10,21 @@ import * as Sentry from '@sentry/browser';
 
 Sentry.init({ dsn: 'https://185a236f4b994411a9f33c3c714cb34e@sentry.io/1424888' });
 
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("channels")
+
+import "bootstrap"
+
+import "../stylesheets/application"
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip();
+});
