d68ecf7e5710a3fafae5de6b1808374e0443a55e
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-30 20:38:15 +0300

    CharacterAssetDecorator

---
+++ spec/decorators/character_asset_decorator_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe CharacterAssetDecorator do
+  subject { described_class.new(double) }
+
+  it { should be_a(ApplicationDecorator) }
+end
