4bc841b21a52e12cb61d7681262137cb58c6af86
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-30 20:00:50 +0300

    CharacterAsset#type

--- app/models/character_asset.rb
+++ app/models/character_asset.rb
@@ -2,4 +2,10 @@
 
 class CharacterAsset < ApplicationRecord
   belongs_to :character
+
+  belongs_to :type,
+    class_name: "Eve::Type",
+    primary_key: :type_id,
+    foreign_key: :type_id,
+    optional: true
 end
--- spec/models/character_asset_spec.rb
+++ spec/models/character_asset_spec.rb
@@ -6,4 +6,6 @@ describe CharacterAsset do
   it { should be_a(ApplicationRecord) }
 
   it { should belong_to(:character) }
+
+  it { should belong_to(:type).class_name("Eve::Type").with_primary_key(:type_id).with_foreign_key(:type_id).optional(true) }
 end
