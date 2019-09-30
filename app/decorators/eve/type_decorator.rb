81808485945e7620b042966b82393a34de1719f0
Igor Zubkov <igor.zubkov@gmail.com>
2019-09-30 20:40:34 +0300

    Add icons

--- app/decorators/eve/type_decorator.rb
+++ app/decorators/eve/type_decorator.rb
@@ -3,5 +3,13 @@
 module Eve
   class TypeDecorator < ApplicationDecorator
     decorates_associations :type_dogma_attributes, :type_dogma_effects
+
+    def icon_tiny
+      "#{imageproxy_url}https://imageserver.eveonline.com/Type/#{type_id}_32.png"
+    end
+
+    def icon_small
+      "#{imageproxy_url}https://imageserver.eveonline.com/Type/#{type_id}_64.png"
+    end
   end
 end
--- spec/decorators/eve/type_decorator_spec.rb
+++ spec/decorators/eve/type_decorator_spec.rb
@@ -6,4 +6,46 @@ describe Eve::TypeDecorator do
   subject { described_class.new(double) }
 
   it { should be_a(ApplicationDecorator) }
+
+  describe "#icon_tiny" do
+    let(:eve_type) do
+      build(:eve_type,
+            type_id: 123)
+    end
+
+    subject { eve_type.decorate }
+
+    context "when IMAGEPROXY_ENABLED is set" do
+      before { ENV["IMAGEPROXY_ENABLED"] = "yes, please" }
+
+      specify { expect(subject.icon_tiny).to eq("https://imageproxy.evemonk.com/https://imageserver.eveonline.com/Type/123_32.png") }
+    end
+
+    context "when IMAGEPROXY_ENABLED is not set" do
+      before { ENV["IMAGEPROXY_ENABLED"] = nil }
+
+      specify { expect(subject.icon_tiny).to eq("https://imageserver.eveonline.com/Type/123_32.png") }
+    end
+  end
+
+  describe "#icon_small" do
+    let(:eve_type) do
+      build(:eve_type,
+            type_id: 123)
+    end
+
+    subject { eve_type.decorate }
+
+    context "when IMAGEPROXY_ENABLED is set" do
+      before { ENV["IMAGEPROXY_ENABLED"] = "yes, please" }
+
+      specify { expect(subject.icon_small).to eq("https://imageproxy.evemonk.com/https://imageserver.eveonline.com/Type/123_64.png") }
+    end
+
+    context "when IMAGEPROXY_ENABLED is not set" do
+      before { ENV["IMAGEPROXY_ENABLED"] = nil }
+
+      specify { expect(subject.icon_small).to eq("https://imageserver.eveonline.com/Type/123_64.png") }
+    end
+  end
 end
