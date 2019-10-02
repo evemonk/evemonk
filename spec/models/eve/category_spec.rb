f56da0e7265e4f7f6fcac3049704a542f8c67a5b
Igor Zubkov <igor.zubkov@gmail.com>
2019-10-03 01:30:09 +0300

    More specs

--- spec/models/eve/category_spec.rb
+++ spec/models/eve/category_spec.rb
@@ -1,5 +1,15 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-RSpec.describe Eve::Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+require "rails_helper"
+
+describe Eve::Category do
+  it { should be_an(ApplicationRecord) }
+
+  it { should respond_to(:versions) }
+
+  it { expect(described_class).to respond_to(:translates) }
+
+  it { expect(described_class.translated_attribute_names).to eq(["name"]) }
+
+  it { expect(described_class.table_name).to eq("eve_categories") }
 end
