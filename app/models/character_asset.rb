bb30f631c9ccc87a8bbf465c5af217d9a660b976
Igor Zubkov <igor.zubkov@gmail.com>
2019-10-03 02:01:23 +0300

    Cleanup

--- app/models/character_asset.rb
+++ app/models/character_asset.rb
@@ -5,7 +5,7 @@ class CharacterAsset < ApplicationRecord
 
   belongs_to :type,
     class_name: "Eve::Type",
-    primary_key: :type_id,
-    foreign_key: :type_id,
+    primary_key: "type_id",
+    foreign_key: "type_id",
     optional: true
 end
