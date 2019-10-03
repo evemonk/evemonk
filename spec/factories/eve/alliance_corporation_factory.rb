e65d5955ca1984104c44d7ec43089665a9a7a5f9
Igor Zubkov <igor.zubkov@gmail.com>
2019-10-03 23:22:00 +0300

    Cleanup

--- spec/factories/eve/alliance_corporation_factory.rb
+++ spec/factories/eve/alliance_corporation_factory.rb
@@ -3,6 +3,7 @@
 FactoryBot.define do
   factory :eve_alliance_corporation, class: Eve::AllianceCorporation do
     sequence(:alliance_id)
+
     sequence(:corporation_id)
   end
 end
--- spec/factories/eve/type_dogma_effect_factory.rb
+++ spec/factories/eve/type_dogma_effect_factory.rb
@@ -2,8 +2,10 @@
 
 FactoryBot.define do
   factory :eve_type_dogma_effect, class: Eve::TypeDogmaEffect do
-    sequence(:type_id) # TODO: replace type_id with factory "type"
+    sequence(:type_id)
+
     sequence(:effect_id)
+
     is_default { [true, false].sample }
   end
 end
