6ba8bc69bde33df087785b5a723b6a85ccd4be0b
Igor Zubkov <igor.zubkov@gmail.com>
2019-10-04 13:23:47 +0300

    Add setting to enable/disable google analytics (with configure). Disable google anaylitcs by default

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -6,4 +6,8 @@ class Setting < RailsSettings::Base
   field :eve_online_sso_client_id, default: ENV["EVE_ONLINE_SSO_CLIENT_ID"], readonly: true
 
   field :eve_online_sso_secret_key, default: ENV["EVE_ONLINE_SSO_SECRET_KEY"], readonly: true
+
+  field :use_google_analytics, type: :boolean, default: false
+
+  field :google_analytics_id, default: ENV["GOOGLE_ANALYTICS_ID"], readonly: true
 end
