3125b3aade057bebd4d32cabbbbb4911e3b99bce
Igor Zubkov <igor.zubkov@gmail.com>
2019-10-14 19:40:15 +0300

    More specs

---
+++ spec/decorators/loyalty_point_decorator_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe LoyaltyPointDecorator do
+  subject { described_class.new(double) }
+
+  it { should be_a(ApplicationDecorator) }
+end
