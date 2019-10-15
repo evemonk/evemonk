12b52c210c33635b72507dc794174355eb537c10
Igor Zubkov <igor.zubkov@gmail.com>
2019-10-15 22:40:00 +0300

    Add character attributes

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,17 +3,35 @@
 class Character < ApplicationRecord
   belongs_to :user
 
-  belongs_to :race, class_name: "Eve::Race", primary_key: "race_id", optional: true
+  belongs_to :race,
+    class_name: "Eve::Race",
+    primary_key: "race_id",
+    optional: true
 
-  belongs_to :bloodline, class_name: "Eve::Bloodline", primary_key: "bloodline_id", optional: true
+  belongs_to :bloodline,
+    class_name: "Eve::Bloodline",
+    primary_key: "bloodline_id",
+    optional: true
 
-  belongs_to :faction, class_name: "Eve::Faction", primary_key: "faction_id", optional: true
+  belongs_to :faction,
+    class_name: "Eve::Faction",
+    primary_key: "faction_id",
+    optional: true
 
-  belongs_to :ancestry, class_name: "Eve::Ancestry", primary_key: "ancestry_id", optional: true
+  belongs_to :ancestry,
+    class_name: "Eve::Ancestry",
+    primary_key: "ancestry_id",
+    optional: true
 
-  belongs_to :alliance, class_name: "Eve::Alliance", primary_key: "alliance_id", optional: true
+  belongs_to :alliance,
+    class_name: "Eve::Alliance",
+    primary_key: "alliance_id",
+    optional: true
 
-  belongs_to :corporation, class_name: "Eve::Corporation", primary_key: "corporation_id", optional: true
+  belongs_to :corporation,
+    class_name: "Eve::Corporation",
+    primary_key: "corporation_id",
+    optional: true
 
   has_many :loyalty_points, dependent: :destroy
 
@@ -26,6 +44,26 @@ class Character < ApplicationRecord
   has_many :skillqueues, dependent: :destroy
 
   has_many :skills, dependent: :destroy
+
+  def charisma_attribute
+    @charisma_attribute ||= Eve::CharacterAttribute.find_by(attribute_name: "Charisma").decorate
+  end
+
+  def intelligence_attribute
+    @intelligence_attribute ||= Eve::CharacterAttribute.find_by(attribute_name: "Intelligence").decorate
+  end
+
+  def memory_attribute
+    @memory_attribute ||= Eve::CharacterAttribute.find_by(attribute_name: "Memory").decorate
+  end
+
+  def perception_attribute
+    @perception_attribute ||= Eve::CharacterAttribute.find_by(attribute_name: "Perception").decorate
+  end
+
+  def willpower_attribute
+    @willpower_attribute ||= Eve::CharacterAttribute.find_by(attribute_name: "Willpower").decorate
+  end
 
   def token_expired?
     token_expires_at <= Time.zone.now
