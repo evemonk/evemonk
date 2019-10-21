cf2230e662f9e8e06276cc9a00db42651f22b708
Igor Zubkov <igor.zubkov@gmail.com>
2019-10-21 23:14:19 +0300

    Add spec

--- spec/models/eve/character_attribute_spec.rb
+++ spec/models/eve/character_attribute_spec.rb
@@ -6,4 +6,6 @@ describe Eve::CharacterAttribute do
   it { should be_an(ApplicationRecord) }
 
   it { expect(described_class.table_name).to eq("eve_character_attributes") }
+
+  it { should belong_to(:icon).with_primary_key("icon_id").class_name("Eve::Icon").optional(true) }
 end
