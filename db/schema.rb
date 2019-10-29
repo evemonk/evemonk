f299f70c49f285ebc91a246c5e1774eaca21eb17
Igor Zubkov <igor.zubkov@gmail.com>
2019-10-29 22:30:58 +0200

    Remember me migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_202814) do
+ActiveRecord::Schema.define(version: 2019_10_29_202923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -607,7 +607,10 @@ ActiveRecord::Schema.define(version: 2019_10_29_202814) do
     t.string "reset_password_token"
     t.string "crypted_password"
     t.string "salt"
+    t.string "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
     t.index "lower((email)::text)", name: "index_users_on_LOWER_email", unique: true
+    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
