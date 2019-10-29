5ab607c65d46d0e0e728d4339813c850a35f94b5
Igor Zubkov <igor.zubkov@gmail.com>
2019-10-29 23:23:59 +0200

    Redirect to sign_in_path with 'Please login first' if user not logged in and try to access private pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,10 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: :index
 
   after_action :verify_policy_scoped, only: :index
+
+  private
+
+  def not_authenticated
+    redirect_to sign_in_path, alert: "Please login first"
+  end
 end
