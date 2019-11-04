84d62062aa381b2c1b1bce545011cc598eeb81b5
Igor Zubkov <igor.zubkov@gmail.com>
2019-11-05 01:40:15 +0200

    Add factory

---
+++ spec/factories/eve/corporation_alliance_history_factory.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :corporation_alliance_history, class: Eve::CorporationAllianceHistory do
+    sequence(:corporation_id)
+
+    sequence(:alliance_id)
+
+    is_deleted { [true, false].sample }
+
+    sequence(:record_id)
+
+    start_date { Time.zone.now }
+  end
+end
