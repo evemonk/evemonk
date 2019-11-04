47dedfb456249ada0ec4c126448c97c5fef6be81
Igor Zubkov <igor.zubkov@gmail.com>
2019-11-04 20:22:41 +0200

    Add body to eve etags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_181024) do
+ActiveRecord::Schema.define(version: 2019_11_04_182145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -297,6 +297,7 @@ ActiveRecord::Schema.define(version: 2019_11_04_181024) do
     t.text "etag", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "body"
     t.index ["url"], name: "index_eve_etags_on_url", unique: true
   end
 
