a6de7a65a07b442ed5d6ce0cf628caaad51e3aee
Igor Zubkov <igor.zubkov@gmail.com>
2019-11-04 20:11:30 +0200

    Add body to etags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_202923) do
+ActiveRecord::Schema.define(version: 2019_11_04_181024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 2019_10_29_202923) do
     t.text "etag", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "body"
   end
 
   create_table "eve_alliance_corporations", force: :cascade do |t|
