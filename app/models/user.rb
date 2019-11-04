1e13b8d4b4dbc6e34a08c5c4ca334639f8290cb1
Igor Zubkov <igor.zubkov@gmail.com>
2019-11-04 23:18:31 +0200

    Add TODO

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  # TODO: drop oauth kind and then drop kind from users
   enum kind: [:normal, :oauth]
 
   has_many :sessions, dependent: :destroy
