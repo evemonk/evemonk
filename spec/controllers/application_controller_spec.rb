3dfbd21bd03150053cb9f40516d7d396812a31e8
Igor Zubkov <igor.zubkov@gmail.com>
2019-11-19 12:28:10 +0200

    One more spec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,4 +4,12 @@ require "rails_helper"
 
 describe ApplicationController do
   it { should use_before_action(:require_login) }
+
+  # private methods
+
+  describe "#not_authenticated" do
+    before { expect(subject).to receive(:redirect_to).with(sign_in_path, alert: "Please login first") }
+
+    specify { expect { subject.send(:not_authenticated) }.not_to raise_error }
+  end
 end
