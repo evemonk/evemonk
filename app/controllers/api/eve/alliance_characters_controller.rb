fa69dee02549409a3c259bc2ff4d64999929ec1b
Igor Zubkov <igor.zubkov@gmail.com>
2019-11-23 01:09:12 +0200

    Cleanup

--- app/controllers/api/eve/alliance_characters_controller.rb
+++ app/controllers/api/eve/alliance_characters_controller.rb
@@ -8,8 +8,7 @@ module Api
       def index
         @characters = ::Eve::Character
           .where(alliance_id: params[:alliance_id])
-          .includes(:alliance, :ancestry, :bloodline, :corporation, :faction,
-            :race)
+          .includes(:alliance, :ancestry, :bloodline, :corporation, :faction, :race)
           .page(params[:page])
           .decorate
       end
