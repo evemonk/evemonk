f60a0d891ae42df96524a8af0748c94794043bf2
Igor Zubkov <igor.zubkov@gmail.com>
2019-11-23 04:14:16 +0200

    Eve::LargestAlliances

---
+++ app/queries/eve/largest_alliances.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Eve
+  class LargestAlliances
+    # def collection
+    #   @collection ||= ::Eve::Alliance.order(characters_count: :desc)
+    #                                  .limit(20)
+    #                                  .decorate
+    # end
+  end
+end
