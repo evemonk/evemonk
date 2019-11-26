070011d1cf289fd49506aa81afead638450ec1b1
Igor Zubkov <igor.zubkov@gmail.com>
2019-11-27 01:10:10 +0200

    Eve::UpdateAllianceJob

---
+++ app/jobs/eve/update_alliance_job.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Eve
+  class UpdateAllianceJob < ActiveJob::Base
+    queue_as :default
+
+    retry_on EveOnline::Exceptions::Timeout,
+             EveOnline::Exceptions::ServiceUnavailable,
+             EveOnline::Exceptions::BadGateway,
+             EveOnline::Exceptions::InternalServerError
+
+    def perform(alliance_id)
+      Eve::AllianceImporter.new(alliance_id).import
+    end
+  end
+end
---
+++ spec/jobs/eve/update_alliance_job_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::UpdateAllianceJob do
+  it { expect(described_class.queue_name).to eq("default") }
+
+  describe "#perform" do
+    let(:alliance_id) { 1_354_830_081 }
+
+    before do
+      #
+      # Eve::AllianceImporter.new(alliance_id).import
+      #
+      expect(Eve::AllianceImporter).to receive(:new).with(alliance_id) do
+        double.tap do |a|
+          expect(a).to receive(:import)
+        end
+      end
+    end
+
+    subject { described_class.new }
+
+    specify { expect { subject.perform(alliance_id) }.not_to raise_error }
+  end
+end
