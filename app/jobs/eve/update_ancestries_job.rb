26410c8abc667bce0ed9c37ea376711a2703dbdc
Igor Zubkov <igor.zubkov@gmail.com>
2019-11-26 21:29:45 +0200

    Eve::UpdateAncestriesJob

---
+++ app/jobs/eve/update_ancestries_job.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Eve
+  class UpdateAncestriesJob < ActiveJob::Base
+    queue_as :default
+
+    retry_on EveOnline::Exceptions::Timeout,
+             EveOnline::Exceptions::ServiceUnavailable,
+             EveOnline::Exceptions::BadGateway,
+             EveOnline::Exceptions::InternalServerError
+
+    def perform
+      LanguageMapper::LANGUAGES.each_key do |locale|
+        Eve::AncestriesImporter.new(locale).import
+      end
+    end
+  end
+end
