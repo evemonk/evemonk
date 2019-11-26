794ca3aafe2a6753161537470d31b7cbf4e429f6
Igor Zubkov <igor.zubkov@gmail.com>
2019-11-26 21:40:28 +0200

    Cleanup

--- app/models/language_mapper.rb
+++ app/models/language_mapper.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class LanguageMapper
-  LANGUAGES = { :en => "en-us",
-                :de => "de",
-                :fr => "fr",
-                :ja => "ja",
-                :ru => "ru",
-                :zh => "zh",
-                :ko => "ko" }
+  LANGUAGES = { en: "en-us",
+                de: "de",
+                fr: "fr",
+                ja: "ja",
+                ru: "ru",
+                zh: "zh",
+                ko: "ko" }
 end
