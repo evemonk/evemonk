2533260a6b60259ca94b1aad45596700488922e9
Igor Zubkov <igor.zubkov@gmail.com>
2019-11-26 20:43:48 +0200

    Eve::UpdateFactionsJob

---
+++ app/jobs/eve/update_factions_job.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Eve
+  class UpdateFactionsJob < ActiveJob::Base
+    queue_as :default
+
+    retry_on EveOnline::Exceptions::Timeout,
+             EveOnline::Exceptions::ServiceUnavailable,
+             EveOnline::Exceptions::BadGateway,
+             EveOnline::Exceptions::InternalServerError
+
+    def perform
+      LanguageMapper::LANGUAGES.each_key do |locale|
+        Eve::FactionsImporter.new(locale).import
+      end
+    end
+  end
+end
