128c2650f80d92d6668f312beea31e69b82bb68b
Igor Zubkov <igor.zubkov@gmail.com>
2019-11-26 20:53:26 +0200

    Eve::UpdateBloodlinesJob

---
+++ app/jobs/eve/update_bloodlines_job.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Eve
+  class UpdateBloodlinesJob < ActiveJob::Base
+    queue_as :default
+
+    retry_on EveOnline::Exceptions::Timeout,
+             EveOnline::Exceptions::ServiceUnavailable,
+             EveOnline::Exceptions::BadGateway,
+             EveOnline::Exceptions::InternalServerError
+
+    def perform
+      LanguageMapper::LANGUAGES.each_key do |locale|
+        Eve::BloodlinesImporter.new(locale).import
+      end
+    end
+  end
+end
