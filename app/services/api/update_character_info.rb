f59b9ecb4828726e70dda3b85144e0722955c94e
Igor Zubkov <igor.zubkov@gmail.com>
2019-11-27 23:10:01 +0200

    DRY

--- app/services/api/update_character_info.rb
+++ app/services/api/update_character_info.rb
@@ -9,36 +9,10 @@ module Api
     end
 
     def update!
-      character_info
-
-      character_wallet
-
-      character_attributes
-
       character_loyalty_points
     end
 
     private
-
-    def character_info
-      esi = EveOnline::ESI::Character.new(character_id: character.character_id)
-
-      character.update!(esi.as_json)
-    end
-
-    def character_wallet
-      esi = EveOnline::ESI::CharacterWallet.new(character_id: character.character_id,
-                                                token: character.access_token)
-
-      character.update!(esi.as_json)
-    end
-
-    def character_attributes
-      esi = EveOnline::ESI::CharacterAttributes.new(character_id: character.character_id,
-                                                    token: character.access_token)
-
-      character.update!(esi.as_json)
-    end
 
     def character_loyalty_points
       esi = EveOnline::ESI::CharacterLoyaltyPoints.new(character_id: character.character_id,
--- spec/services/api/update_character_info_spec.rb
+++ spec/services/api/update_character_info_spec.rb
@@ -16,76 +16,12 @@ describe Api::UpdateCharacterInfo do
 
     subject(:service) { described_class.new(character) }
 
-    before { expect(service).to receive(:character_info) }
-
-    before { expect(service).to receive(:character_wallet) }
-
-    before { expect(service).to receive(:character_attributes) }
-
     before { expect(service).to receive(:character_loyalty_points) }
 
     specify { expect { service.update! }.not_to raise_error }
   end
 
   # private methods
-
-  describe "#character_info" do
-    let(:character_id) { double }
-
-    let(:character) { instance_double(Character, character_id: character_id) }
-
-    subject(:service) { described_class.new(character) }
-
-    let(:as_json) { double }
-
-    let(:esi) { instance_double(EveOnline::ESI::Character, as_json: as_json) }
-
-    before { expect(EveOnline::ESI::Character).to receive(:new).with(character_id: character_id).and_return(esi) }
-
-    before { expect(character).to receive(:update!).with(as_json) }
-
-    specify { expect { service.send(:character_info) }.not_to raise_error }
-  end
-
-  describe "#character_wallet" do
-    let(:character_id) { double }
-
-    let(:access_token) { double }
-
-    let(:character) { instance_double(Character, character_id: character_id, access_token: access_token) }
-
-    subject(:service) { described_class.new(character) }
-
-    let(:as_json) { double }
-
-    let(:esi) { instance_double(EveOnline::ESI::CharacterWallet, as_json: as_json) }
-
-    before { expect(EveOnline::ESI::CharacterWallet).to receive(:new).with(character_id: character_id, token: access_token).and_return(esi) }
-
-    before { expect(character).to receive(:update!).with(as_json) }
-
-    specify { expect { service.send(:character_wallet) }.not_to raise_error }
-  end
-
-  describe "#character_attributes" do
-    let(:character_id) { double }
-
-    let(:access_token) { double }
-
-    let(:character) { instance_double(Character, character_id: character_id, access_token: access_token) }
-
-    subject(:service) { described_class.new(character) }
-
-    let(:as_json) { double }
-
-    let(:esi) { instance_double(EveOnline::ESI::CharacterAttributes, as_json: as_json) }
-
-    before { expect(EveOnline::ESI::CharacterAttributes).to receive(:new).with(character_id: character_id, token: access_token).and_return(esi) }
-
-    before { expect(character).to receive(:update!).with(as_json) }
-
-    specify { expect { service.send(:character_attributes) }.not_to raise_error }
-  end
 
   describe "#character_loyalty_points" do
     let(:character_id) { double }
