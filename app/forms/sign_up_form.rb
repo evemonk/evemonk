e700d548083555c159d1b357bda42e464029507e
Igor Zubkov <igor.zubkov@gmail.com>
2019-11-27 19:58:19 +0200

    SignUpForm

--- app/forms/sign_up_form.rb
+++ app/forms/sign_up_form.rb
@@ -3,7 +3,7 @@
 class SignUpForm
   include ActiveModel::Model
 
-  attr_accessor :email, :password, :password_confirmation, :controller
+  attr_accessor :email, :password, :password_confirmation, :remember_me, :controller
 
   validates :email, presence: true
 
@@ -13,7 +13,27 @@ class SignUpForm
 
   validates :password, confirmation: true
 
+  validates :remember_me, inclusion: {in: ["0", "1"]}
+
+  delegate :login, to: :controller
+
   def save
     return false unless valid?
+
+    create_user!
+
+    login(email, password, remember_me == "1")
+
+    true
+  rescue ActiveRecord::RecordNotUnique
+    errors.add(:email, I18n.t("errors.messages.taken"))
+
+    false
+  end
+
+  private
+
+  def create_user!
+    User.create!(email: email, password: password)
   end
 end
