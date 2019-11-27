28efc5dfdb74906f33d6b03309be96825577b2da
Igor Zubkov <igor.zubkov@gmail.com>
2019-11-27 20:58:35 +0200

    More specs

--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -7,7 +7,43 @@ describe CharactersController do
 
   it { should use_before_action(:require_login) }
 
-  describe "#index"
+  describe "#index" do
+    let(:current_user) { instance_double(User) }
+
+    before { sign_in(current_user) }
+
+    before do
+      #
+      # current_user.characters
+      #             .includes(:alliance, :corporation)
+      #             .order(created_at: :asc)
+      #             .page(params[:page])
+      #             .decorate
+      expect(current_user).to receive(:characters) do
+        double.tap do |a|
+          expect(a).to receive(:includes).with(:alliance, :corporation) do
+            double.tap do |b|
+              expect(b).to receive(:order).with(created_at: :asc) do
+                double.tap do |c|
+                  expect(c).to receive(:page).with("1") do
+                    double.tap do |d|
+                      expect(d).to receive(:decorate)
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+
+    before { get :index, params: { page: "1" } }
+
+    it { should respond_with(:ok) }
+
+    it { should render_template(:index) }
+  end
 
   describe "#show"
 
