fcb519063072d6f5f475ec8ba456c9f5733257d7
Igor Zubkov <igor.zubkov@gmail.com>
2019-11-28 14:13:35 +0200

    More specs

--- spec/controllers/employment_histories_controller_spec.rb
+++ spec/controllers/employment_histories_controller_spec.rb
@@ -7,5 +7,37 @@ describe EmploymentHistoriesController do
 
   it { should use_before_action(:require_login) }
 
-  describe "#index"
+  describe "#index" do
+    let(:current_user) { instance_double(User) }
+
+    before { sign_in(current_user) }
+
+    before do
+      #
+      # current_user.characters
+      #             .includes(character_corporation_histories: :corporation)
+      #             .find_by!(character_id: params[:character_id])
+      #             .decorate
+      #
+      expect(current_user).to receive(:characters) do
+        double.tap do |a|
+          expect(a).to receive(:includes).with(character_corporation_histories: :corporation) do
+            double.tap do |b|
+              expect(b).to receive(:find_by!).with(character_id: "1") do
+                double.tap do |c|
+                  expect(c).to receive(:decorate)
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+
+    before { get :index, params: {character_id: "1"} }
+
+    it { should respond_with(:ok) }
+
+    it { should render_template(:index) }
+  end
 end
