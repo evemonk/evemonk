037701270018a9ef7b17b7110f4bbda3acb885bc
Igor Zubkov <igor.zubkov@gmail.com>
2019-11-28 02:54:55 +0200

    Add spec

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -3,7 +3,7 @@
 class AssetsController < ApplicationController
   def index
     @character = current_user.characters
-      .eager_load(:race, :bloodline, :ancestry, :faction, :alliance, :corporation)
+      .includes(:race, :bloodline, :ancestry, :faction, :alliance, :corporation)
       .find_by!(character_id: params[:character_id])
       .decorate
 
