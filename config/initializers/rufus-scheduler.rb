d0da7e3caac7b0f20b5c1625ed5f958bb7a27c68
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-01 17:23:05 +0200

    Update characters info each hour

--- config/initializers/rufus-scheduler.rb
+++ config/initializers/rufus-scheduler.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-return if defined?(Rails::Console) || Rails.env.test? || File.split($0).last == 'rake'
+return if defined?(Rails::Console) || Rails.env.test? || File.split($0).last == "rake"
 
 s = Rufus::Scheduler.singleton
 
-s.every '1m' do
-  Rails.logger.info "hello, it's #{Time.now}"
+s.every "1h" do
+  Rails.logger.info "Update characters"
   Rails.logger.flush
+  UpdateCharactersJob.perform_later
 end
