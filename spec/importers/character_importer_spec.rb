82038a525e4263d764ce2eb52cd44249fe31dbaa
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-03 20:16:08 +0200

    Update spec

--- spec/importers/character_importer_spec.rb
+++ spec/importers/character_importer_spec.rb
@@ -9,22 +9,25 @@ describe CharacterImporter do
 
   it { should be_a(CharacterBaseImporter) }
 
-  # context "when character found" do
-  #   let(:character) { instance_double(Character) }
-  #
-  #   before { expect(Character).to receive(:find_by!).with(character_id: character_id).and_return(character) }
-  #
-  #   let(:json) { double }
-  #
-  #   let(:esi) do
-  #     instance_double(EveOnline::ESI::Character,
-  #       as_json: json)
-  #   end
-  #
-  #   before { expect(EveOnline::ESI::Character).to receive(:new).with(character_id: character_id).and_return(esi) }
-  #
-  #   before { expect(character).to receive(:update!).with(json) }
-  #
-  #   specify { expect { subject.import }.not_to raise_error }
-  # end
+  describe "#update!" do
+    let(:character) do
+      instance_double(Character,
+        character_id: character_id)
+    end
+
+    before { expect(subject).to receive(:character).and_return(character) }
+
+    let(:json) { double }
+
+    let(:esi) do
+      instance_double(EveOnline::ESI::Character,
+        as_json: json)
+    end
+
+    before { expect(EveOnline::ESI::Character).to receive(:new).with(character_id: character_id).and_return(esi) }
+
+    before { expect(character).to receive(:update!).with(json) }
+
+    specify { expect { subject.update! }.not_to raise_error }
+  end
 end
