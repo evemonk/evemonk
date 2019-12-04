f96dc7c57ceff63a640e2bf22069e356a50aa71e
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-04 13:01:39 +0200

    More specs

--- spec/importers/character_loyalty_points_importer_spec.rb
+++ spec/importers/character_loyalty_points_importer_spec.rb
@@ -75,7 +75,29 @@ describe CharacterLoyaltyPointsImporter do
     end
 
     context "when scope not present" do
+      before { expect(subject).to receive(:refresh_character_access_token) }
 
+      let(:access_token) { double }
+
+      let(:character) do
+        instance_double(Character,
+          character_id: character_id,
+          access_token: access_token,
+          scopes: "")
+      end
+
+      before { expect(subject).to receive(:character).and_return(character).exactly(3).times }
+
+      let(:esi) do
+        instance_double(EveOnline::ESI::CharacterLoyaltyPoints,
+          scope: "esi-characters.read_loyalty.v1")
+      end
+
+      before { expect(EveOnline::ESI::CharacterLoyaltyPoints).to receive(:new).with(character_id: character_id, token: access_token).and_return(esi) }
+
+      before { expect(character).not_to receive(:loyalty_points) }
+
+      specify { expect { subject.update! }.not_to raise_error }
     end
   end
 end
