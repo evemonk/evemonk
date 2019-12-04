408ff96ee23f1f5f4bbcb06ef58f00f7c2256303
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-04 13:11:07 +0200

    Update spec

--- spec/importers/character_skillqueue_importer_spec.rb
+++ spec/importers/character_skillqueue_importer_spec.rb
@@ -9,48 +9,82 @@ describe CharacterSkillqueueImporter do
 
   it { should be_a(CharacterBaseImporter) }
 
-  # context "when character found" do
-  #   let(:access_token) { double }
-  #
-  #   let(:character) { instance_double(Character, access_token: access_token) }
-  #
-  #   before { expect(Character).to receive(:find_by!).with(character_id: character_id).and_return(character) }
-  #
-  #   before { expect(subject).to receive(:refresh_character_access_token) }
-  #
-  #   let(:json) { double }
-  #
-  #   let(:skill) { instance_double(EveOnline::ESI::Models::SkillQueueEntry, as_json: json) }
-  #
-  #   let(:esi) do
-  #     instance_double(EveOnline::ESI::CharacterSkillQueue,
-  #       skills: [skill])
-  #   end
-  #
-  #   before { expect(EveOnline::ESI::CharacterSkillQueue).to receive(:new).with(character_id: character_id, token: access_token).and_return(esi) }
-  #
-  #   before do
-  #     #
-  #     # character.skillqueues.destroy_all
-  #     #
-  #     expect(character).to receive(:skillqueues) do
-  #       double.tap do |a|
-  #         expect(a).to receive(:destroy_all)
-  #       end
-  #     end
-  #   end
-  #
-  #   before do
-  #     #
-  #     # character.skillqueues.create!(skill.as_json)
-  #     #
-  #     expect(character).to receive(:skillqueues) do
-  #       double.tap do |a|
-  #         expect(a).to receive(:create!).with(json)
-  #       end
-  #     end
-  #   end
-  #
-  #   specify { expect { subject.import }.not_to raise_error }
-  # end
+  describe "#update!" do
+    context "when scope present" do
+      before { expect(subject).to receive(:refresh_character_access_token) }
+
+      let(:access_token) { double }
+
+      let(:character) do
+        instance_double(Character,
+          character_id: character_id,
+          access_token: access_token,
+          scopes: "esi-skills.read_skillqueue.v1")
+      end
+
+      before { expect(subject).to receive(:character).and_return(character).exactly(5).times }
+
+      let(:json) { double }
+
+      let(:skill) { instance_double(EveOnline::ESI::Models::SkillQueueEntry, as_json: json) }
+
+      let(:esi) do
+        instance_double(EveOnline::ESI::CharacterSkillQueue,
+          skills: [skill],
+          scope: "esi-skills.read_skillqueue.v1")
+      end
+
+      before { expect(EveOnline::ESI::CharacterSkillQueue).to receive(:new).with(character_id: character_id, token: access_token).and_return(esi) }
+
+      before do
+        #
+        # character.skillqueues.destroy_all
+        #
+        expect(character).to receive(:skillqueues) do
+          double.tap do |a|
+            expect(a).to receive(:destroy_all)
+          end
+        end
+      end
+
+      before do
+        #
+        # character.skillqueues.create!(skill.as_json)
+        #
+        expect(character).to receive(:skillqueues) do
+          double.tap do |a|
+            expect(a).to receive(:create!).with(json)
+          end
+        end
+      end
+
+      specify { expect { subject.update! }.not_to raise_error }
+    end
+
+    context "when scope not present" do
+      before { expect(subject).to receive(:refresh_character_access_token) }
+
+      let(:access_token) { double }
+
+      let(:character) do
+        instance_double(Character,
+          character_id: character_id,
+          access_token: access_token,
+          scopes: "")
+      end
+
+      before { expect(subject).to receive(:character).and_return(character).exactly(3).times }
+
+      let(:esi) do
+        instance_double(EveOnline::ESI::CharacterSkillQueue,
+          scope: "esi-skills.read_skillqueue.v1")
+      end
+
+      before { expect(EveOnline::ESI::CharacterSkillQueue).to receive(:new).with(character_id: character_id, token: access_token).and_return(esi) }
+
+      before { expect(character).not_to receive(:skillqueues) }
+
+      specify { expect { subject.update! }.not_to raise_error }
+    end
+  end
 end
