92dd5aa60675ca4ee5c24e987cb07e65397d9508
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-04 13:17:39 +0200

    Update spec

--- spec/importers/character_skills_importer_spec.rb
+++ spec/importers/character_skills_importer_spec.rb
@@ -70,7 +70,31 @@ describe CharacterSkillsImporter do
     end
 
     context "when scope not present" do
-      
+      before { expect(subject).to receive(:refresh_character_access_token) }
+
+      let(:access_token) { double }
+
+      let(:character) do
+        instance_double(Character,
+          character_id: character_id,
+          access_token: access_token,
+          scopes: "")
+      end
+
+      before { expect(subject).to receive(:character).and_return(character).exactly(3).times }
+
+      let(:esi) do
+        instance_double(EveOnline::ESI::CharacterSkills,
+          scope: "esi-skills.read_skills.v1")
+      end
+
+      before { expect(EveOnline::ESI::CharacterSkills).to receive(:new).with(character_id: character_id, token: access_token).and_return(esi) }
+
+      before { expect(character).not_to receive(:update!) }
+
+      before { expect(character).not_to receive(:character_skills) }
+
+      specify { expect { subject.update! }.not_to raise_error }
     end
   end
 end
