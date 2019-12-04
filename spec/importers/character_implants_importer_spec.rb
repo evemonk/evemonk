7c48c915d06e85adbfd51fec8484db6e1c806adf
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-04 12:55:07 +0200

    Update spec

--- spec/importers/character_implants_importer_spec.rb
+++ spec/importers/character_implants_importer_spec.rb
@@ -9,46 +9,80 @@ describe CharacterImplantsImporter do
 
   it { should be_a(CharacterBaseImporter) }
 
-  # context "when character found" do
-  #   let(:access_token) { double }
-  #
-  #   let(:character) { instance_double(Character, access_token: access_token) }
-  #
-  #   before { expect(Character).to receive(:find_by!).with(character_id: character_id).and_return(character) }
-  #
-  #   before { expect(subject).to receive(:refresh_character_access_token) }
-  #
-  #   before do
-  #     #
-  #     # character.character_implants.destroy_all
-  #     #
-  #     expect(character).to receive(:character_implants) do
-  #       double.tap do |a|
-  #         expect(a).to receive(:destroy_all)
-  #       end
-  #     end
-  #   end
-  #
-  #   let(:implant_id) { double }
-  #
-  #   let(:esi) do
-  #     instance_double(EveOnline::ESI::CharacterImplants,
-  #       implant_ids: [implant_id])
-  #   end
-  #
-  #   before { expect(EveOnline::ESI::CharacterImplants).to receive(:new).with(character_id: character_id, token: access_token).and_return(esi) }
-  #
-  #   before do
-  #     #
-  #     # character.character_implants.create!(type_id: implant_id)
-  #     #
-  #     expect(character).to receive(:character_implants) do
-  #       double.tap do |a|
-  #         expect(a).to receive(:create!).with(type_id: implant_id)
-  #       end
-  #     end
-  #   end
-  #
-  #   specify { expect { subject.import }.not_to raise_error }
-  # end
+  describe "#update!" do
+    context "when scope present" do
+      before { expect(subject).to receive(:refresh_character_access_token) }
+
+      let(:access_token) { double }
+
+      let(:character) do
+        instance_double(Character,
+          character_id: character_id,
+          access_token: access_token,
+          scopes: "esi-clones.read_implants.v1")
+      end
+
+      before { expect(subject).to receive(:character).and_return(character).exactly(5).times }
+
+      let(:implant_id) { double }
+
+      let(:esi) do
+        instance_double(EveOnline::ESI::CharacterImplants,
+          implant_ids: [implant_id],
+          scope: "esi-clones.read_implants.v1")
+      end
+
+      before { expect(EveOnline::ESI::CharacterImplants).to receive(:new).with(character_id: character_id, token: access_token).and_return(esi) }
+
+      before do
+        #
+        # character.character_implants.destroy_all
+        #
+        expect(character).to receive(:character_implants) do
+          double.tap do |a|
+            expect(a).to receive(:destroy_all)
+          end
+        end
+      end
+
+      before do
+        #
+        # character.character_implants.create!(type_id: implant_id)
+        #
+        expect(character).to receive(:character_implants) do
+          double.tap do |a|
+            expect(a).to receive(:create!).with(type_id: implant_id)
+          end
+        end
+      end
+
+      specify { expect { subject.update! }.not_to raise_error }
+    end
+
+    context "when scope not present" do
+      before { expect(subject).to receive(:refresh_character_access_token) }
+
+      let(:access_token) { double }
+
+      let(:character) do
+        instance_double(Character,
+          character_id: character_id,
+          access_token: access_token,
+          scopes: "")
+      end
+
+      before { expect(subject).to receive(:character).and_return(character).exactly(3).times }
+
+      let(:esi) do
+        instance_double(EveOnline::ESI::CharacterImplants,
+          scope: "esi-clones.read_implants.v1")
+      end
+
+      before { expect(EveOnline::ESI::CharacterImplants).to receive(:new).with(character_id: character_id, token: access_token).and_return(esi) }
+
+      before { expect(character).not_to receive(:character_implants) }
+
+      specify { expect { subject.update! }.not_to raise_error }
+    end
+  end
 end
