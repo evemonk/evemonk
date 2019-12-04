2c75bfcb81732705d1b250cd4476191cb8d47845
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-04 20:27:45 +0200

    Add jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,7 @@ import "../stylesheets/application";
 
 import ApexCharts from "apexcharts";
 window.ApexCharts = ApexCharts;
+window.$ = $;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
