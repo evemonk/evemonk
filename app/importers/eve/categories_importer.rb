a8ee2ae8c3ba08898b302e6f5c76fbd4040ff158
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-05 20:31:38 +0200

    Eve::CategoriesImporter

---
+++ app/importers/eve/categories_importer.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module Eve
+  class CategoriesImporter
+    def import
+      ActiveRecord::Base.transaction do
+        esi = EveOnline::ESI::UniverseCategories.new
+
+        etag = Eve::Etag.find_or_initialize_by(url: esi.url)
+
+        esi.etag = etag.etag
+
+        return if esi.not_modified?
+
+        import_new_categories
+
+        remove_old_categories
+
+        etag.update!(etag: esi.etag, body: esi.response)
+      end
+    end
+
+    def import_new_categories
+      esi.category_ids.each do |category_id|
+        unless Eve::Category.exists?(category_id: category_id)
+          Eve::UpdateCategoryJob.perform_later(category_id)
+        end
+      end
+    end
+
+    def remove_old_categories
+      eve_categories_ids = Eve::Category.pluck(:category_id)
+
+      categories_ids_to_remove = eve_categories_ids - esi.category_ids
+
+      categories_ids_to_remove.each do |category_id|
+        eve_category = Eve::Category.find_or_initialize_by(category_id: category_id)
+
+        # eve_alliance.corporations.each do |corporation|
+        #   Eve::CorporationImporterWorker.perform_async(corporation.corporation_id)
+        # end
+
+        eve_category.destroy!
+      end
+    end
+  end
+end
