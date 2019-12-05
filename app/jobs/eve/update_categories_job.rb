3d791badb55f0329b8406fd3fbfe07e7fb2b4d9d
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-05 19:24:52 +0200

    Stub jobs

---
+++ app/jobs/eve/update_categories_job.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Eve
+  class UpdateCategoriesJob < ActiveJob::Base
+    queue_as :default
+
+    retry_on EveOnline::Exceptions::Timeout,
+      EveOnline::Exceptions::ServiceUnavailable,
+      EveOnline::Exceptions::BadGateway,
+      EveOnline::Exceptions::InternalServerError
+
+    def perform
+      # LanguageMapper::LANGUAGES.each_key do |locale|
+      #   Eve::CategoriesImporter.new(locale).import
+      # end
+    end
+  end
+end
---
+++ app/jobs/eve/update_category_job.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Eve
+  class UpdateCategoryJob < ActiveJob::Base
+    queue_as :default
+
+    retry_on EveOnline::Exceptions::Timeout,
+      EveOnline::Exceptions::ServiceUnavailable,
+      EveOnline::Exceptions::BadGateway,
+      EveOnline::Exceptions::InternalServerError
+
+    def perform(category_id)
+      # LanguageMapper::LANGUAGES.each_key do |locale|
+      #   Eve::CategoryImporter.new(category_id, locale).import
+      # end
+    end
+  end
+end
