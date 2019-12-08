419a028b10b54b534b062c4cb0b058c82fcf95ea
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-08 19:13:42 +0200

    Stub

---
+++ app/importers/eve/dogma_attribute_importer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Eve
+  class DogmaAttributeImporter
+    def import
+    end
+  end
+end
---
+++ app/importers/eve/dogma_attributes_importer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Eve
+  class DogmaAttributesImporter
+    def import
+    end
+  end
+end
