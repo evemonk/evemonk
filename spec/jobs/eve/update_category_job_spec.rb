6da1d7c085605c1f4a0e08a48c2278ea8fca3bf1
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-09 16:06:33 +0200

    Add spec

---
+++ spec/jobs/eve/update_category_job_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::UpdateCategoryJob do
+  it { expect(described_class.queue_name).to eq("default") }
+
+  describe "#perform" do
+    let(:category_id) { double }
+
+    before do
+      #
+      # LanguageMapper::LANGUAGES.each_key do |locale|
+      #   Eve::CategoryImporter.new(category_id, locale).import
+      # end
+      #
+      LanguageMapper::LANGUAGES.each_key do |locale|
+        expect(Eve::CategoryImporter).to receive(:new).with(category_id, locale) do
+          double.tap do |a|
+            expect(a).to receive(:import)
+          end
+        end
+      end
+    end
+
+    subject { described_class.new }
+
+    specify { expect { subject.perform(category_id) }.not_to raise_error }
+  end
+end
