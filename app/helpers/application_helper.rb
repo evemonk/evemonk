a8dfbc525fe3e0bef7cc488af1f6c0cdc57ba0be
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-09 21:16:38 +0200

    Extract to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def sign_in_via_eve_online
+    link_to(image_tag("#{ Setting.image_proxy_url if Setting.use_image_proxy }https://web.ccpgamescdn.com/eveonlineassets/developers/eve-sso-login-black-large.png",
+                      alt: "Sign in via EVE Online"),
+            "/auth/eve_online_sso")
+  end
 end
