440d142760f27d3e8b82be861a89708b31e4de70
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-10 21:59:53 +0200

    Fix indent

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@
 
 module ApplicationHelper
   def sign_in_via_eve_online
-    link_to(image_tag("#{ Setting.image_proxy_url if Setting.use_image_proxy }https://web.ccpgamescdn.com/eveonlineassets/developers/eve-sso-login-black-large.png",
-                      alt: "Sign in via EVE Online"),
-            "/auth/eve_online_sso")
+    link_to(image_tag("#{Setting.image_proxy_url if Setting.use_image_proxy}https://web.ccpgamescdn.com/eveonlineassets/developers/eve-sso-login-black-large.png",
+      alt: "Sign in via EVE Online"),
+      "/auth/eve_online_sso")
   end
 end
