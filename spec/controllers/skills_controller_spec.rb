d7411416253e59fea0f08cc299d414a58c9bede0
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-10 21:34:40 +0200

    SkillsController

---
+++ spec/controllers/skills_controller_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe SkillsController do
+  it { should be_a(ApplicationController) }
+
+  it { should use_before_action(:require_login) }
+
+  describe "#index" do
+    let(:current_user) { instance_double(User) }
+
+    before { sign_in(current_user) }
+
+    let(:character) { instance_double(Character) }
+
+    before do
+      #
+      # current_user.characters
+      #             .includes(character_skills: :skill)
+      #             .find_by!(character_id: params[:character_id])
+      #             .decorate
+      #
+      expect(current_user).to receive(:characters) do
+        double.tap do |a|
+          expect(a).to receive(:includes).with(character_skills: :skill) do
+            double.tap do |b|
+              expect(b).to receive(:find_by!).with(character_id: "1") do
+                double.tap do |c|
+                  expect(c).to receive(:decorate)
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+
+    before { get :index, params: {character_id: "1"} }
+
+    it { should respond_with(:ok) }
+
+    it { should render_template(:index) }
+  end
+end
