b57cf80cffce6779b68f23b29f253787d3db31f3
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-11 18:57:20 +0200

    Fix

--- app/importers/eve/local_graphics_importer.rb
+++ app/importers/eve/local_graphics_importer.rb
@@ -4,7 +4,7 @@ module Eve
   class LocalGraphicsImporter
     def import
       Eve::Graphic.pluck(:graphic_id).each do |graphic_id|
-        Eve::GraphicImporterWorker.perform_async(graphic_id)
+        Eve::UpdateGraphicJob.perform_later(graphic_id)
       end
     end
   end
