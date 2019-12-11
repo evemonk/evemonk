c2cae64e2bc79c9d2f95eef4f2ead662e0ed56f6
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-11 19:03:00 +0200

    Eve::UpdateGraphicsJob

---
+++ app/jobs/eve/update_graphics_job.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Eve
+  class UpdateGraphicsJob < ActiveJob::Base
+    queue_as :default
+
+    retry_on EveOnline::Exceptions::Timeout,
+      EveOnline::Exceptions::ServiceUnavailable,
+      EveOnline::Exceptions::BadGateway,
+      EveOnline::Exceptions::InternalServerError
+
+    def perform
+      Eve::GraphicsImporter.new.import
+    end
+  end
+end
