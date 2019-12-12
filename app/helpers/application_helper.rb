23840dabf4f4d0bbbd1d4f773b50e370550d3be5
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-12 15:06:26 +0200

    Make it more readable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,9 @@
 
 module ApplicationHelper
   def sign_in_via_eve_online
-    link_to(image_tag("#{Setting.image_proxy_url if Setting.use_image_proxy}https://web.ccpgamescdn.com/eveonlineassets/developers/eve-sso-login-black-large.png",
+    image_uri = "https://web.ccpgamescdn.com/eveonlineassets/developers/eve-sso-login-black-large.png"
+
+    link_to(image_tag("#{Setting.image_proxy_url if Setting.use_image_proxy}#{image_uri}",
       alt: "Sign in via EVE Online"),
       "/auth/eve_online_sso")
   end
