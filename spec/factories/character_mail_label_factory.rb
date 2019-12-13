fd735bd815e9361c786b1decf4e978b3fc07d7c5
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-13 11:37:16 +0200

    Add factory

---
+++ spec/factories/character_mail_label_factory.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :character_mail_label do
+    character
+
+    color { ["#666666", "#999999", "#ffffff"].sample }
+
+    sequence(:label_id)
+
+    name { ["Inbox", "Sent", "[Corp]", "[Alliance]"].sample  }
+
+    unread_count { 0 }
+  end
+end
