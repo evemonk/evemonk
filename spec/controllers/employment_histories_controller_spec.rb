384e188be387deaf030067ac5e80fc1f9522427b
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-15 19:11:38 +0200

    More specs

--- spec/controllers/employment_histories_controller_spec.rb
+++ spec/controllers/employment_histories_controller_spec.rb
@@ -8,38 +8,54 @@ describe EmploymentHistoriesController do
   it { should use_before_action(:require_login) }
 
   describe "#index" do
-    let(:current_user) { instance_double(User) }
+    context "when user signed in" do
+      let(:current_user) { instance_double(User) }
 
-    before { sign_in(current_user) }
+      before { sign_in(current_user) }
 
-    before do
-      #
-      # current_user.characters
-      #             .includes(character_corporation_histories: :corporation)
-      #             .find_by!(character_id: params[:character_id])
-      #             .decorate
-      #
-      expect(current_user).to receive(:characters) do
-        double.tap do |a|
-          expect(a).to receive(:includes).with(character_corporation_histories: :corporation) do
-            double.tap do |b|
-              expect(b).to receive(:find_by!).with(character_id: "1") do
-                double.tap do |c|
-                  expect(c).to receive(:decorate)
+      before do
+        #
+        # current_user.characters
+        #             .includes(character_corporation_histories: :corporation)
+        #             .find_by!(character_id: params[:character_id])
+        #             .decorate
+        #
+        expect(current_user).to receive(:characters) do
+          double.tap do |a|
+            expect(a).to receive(:includes).with(character_corporation_histories: :corporation) do
+              double.tap do |b|
+                expect(b).to receive(:find_by!).with(character_id: "1") do
+                  double.tap do |c|
+                    expect(c).to receive(:decorate)
+                  end
                 end
               end
             end
           end
         end
       end
+
+      before { expect(current_user).to receive(:set_last_activity_at).with(any_args) }
+
+      before { get :index, params: {character_id: "1"} }
+
+      it { should respond_with(:ok) }
+
+      it { should render_template(:index) }
     end
 
-    before { expect(current_user).to receive(:set_last_activity_at).with(any_args) }
+    context "when user not signed in" do
+      context "when format js" do
+        before { get :index, params: {character_id: "1", format: "js"} }
 
-    before { get :index, params: {character_id: "1"} }
+        it { should redirect_to(sign_in_path) }
+      end
 
-    it { should respond_with(:ok) }
+      context "when format html" do
+        before { get :index, params: {character_id: "1", format: "html"} }
 
-    it { should render_template(:index) }
+        it { should redirect_to(sign_in_path) }
+      end
+    end
   end
 end
