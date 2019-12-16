ce1072bb985df6ba37bace35d6e2c7aaae8c4664
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-17 00:10:11 +0200

    Add devise fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_16_202036) do
+ActiveRecord::Schema.define(version: 2019_12_16_220428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -680,6 +680,19 @@ ActiveRecord::Schema.define(version: 2019_12_16_202036) do
     t.integer "kind", default: 0
     t.string "crypted_password"
     t.string "salt"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index "lower((email)::text)", name: "index_users_on_LOWER_email", unique: true
   end
 
