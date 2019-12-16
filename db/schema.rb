64deb7e5bbf341e8d6019f8c56bbbfba95cddac5
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-17 00:26:13 +0200

    Migrate user schema to devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_16_220428) do
+ActiveRecord::Schema.define(version: 2019_12_16_221034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -693,7 +693,9 @@ ActiveRecord::Schema.define(version: 2019_12_16_220428) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.index "lower((email)::text)", name: "index_users_on_LOWER_email", unique: true
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "versions", force: :cascade do |t|
