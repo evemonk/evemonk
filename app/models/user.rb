8ae3796d62073a29aa9eb79dc5244a08cc0a00c2
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-17 00:41:18 +0200

    Enable devise for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  authenticates_with_sorcery!
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable,
+         :rememberable, :validatable, :confirmable, :trackable
 
   # TODO: drop oauth kind and then drop kind from users
   enum kind: [:normal, :oauth]
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,14 @@ require "rails_helper"
 describe User do
   it { should be_a(ApplicationRecord) }
 
+  it { expect(described_class.devise_modules).to eq([:database_authenticatable,
+                                                     :rememberable,
+                                                     :recoverable,
+                                                     :registerable,
+                                                     :validatable,
+                                                     :confirmable,
+                                                     :trackable]) }
+
   it { should define_enum_for(:kind).with_values([:normal, :oauth]) }
 
   it { should have_many(:sessions).dependent(:destroy) }
