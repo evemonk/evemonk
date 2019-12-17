e4acdcdf951a091183b967c7df073738f43564b1
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-17 14:05:49 +0200

    Cleanup

--- app/controllers/change_passwords_controller.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class ChangePasswordsController < ApplicationController
-  def show
-  end
-end
--- app/controllers/sign_outs_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class SignOutsController < ApplicationController
-  def destroy
-    logout
-
-    redirect_to root_path, notice: "Logged out!"
-  end
-end
