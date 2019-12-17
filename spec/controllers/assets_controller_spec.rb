7d541f334eaddc82228cf2f9b92b518bdbcaa7ad
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-17 22:31:02 +0200

    Update spec

--- spec/controllers/assets_controller_spec.rb
+++ spec/controllers/assets_controller_spec.rb
@@ -5,30 +5,35 @@ require "rails_helper"
 describe AssetsController do
   it { should be_a(ApplicationController) }
 
-  it { should use_before_action(:require_login) }
+  it { should use_before_action(:authenticate_user!) }
 
   describe "#index" do
     context "when user signed in" do
-      let(:current_user) { instance_double(User) }
+      let(:user) { create(:user) }
 
-      before { sign_in(current_user) }
+      before { sign_in(user) }
 
       let(:character) { instance_double(Character) }
 
       before do
         #
-        # current_user.characters
-        #             .includes(:race, :bloodline, :ancestry, :faction, :alliance, :corporation)
-        #             .find_by!(character_id: params[:character_id])
-        #             .decorate
+        # subject.current_user
+        #        .characters
+        #        .includes(:race, :bloodline, :ancestry, :faction, :alliance, :corporation)
+        #        .find_by!(character_id: params[:character_id])
+        #        .decorate
         #
-        expect(current_user).to receive(:characters) do
+        expect(subject).to receive(:current_user) do
           double.tap do |a|
-            expect(a).to receive(:includes).with(:race, :bloodline, :ancestry, :faction, :alliance, :corporation) do
+            expect(a).to receive(:characters) do
               double.tap do |b|
-                expect(b).to receive(:find_by!).with(character_id: "1") do
+                expect(b).to receive(:includes).with(:race, :bloodline, :ancestry, :faction, :alliance, :corporation) do
                   double.tap do |c|
-                    expect(c).to receive(:decorate).and_return(character)
+                    expect(c).to receive(:find_by!).with(character_id: "1") do
+                      double.tap do |d|
+                        expect(d).to receive(:decorate).and_return(character)
+                      end
+                    end
                   end
                 end
               end
@@ -49,8 +54,6 @@ describe AssetsController do
         end
       end
 
-      before { expect(current_user).to receive(:set_last_activity_at).with(any_args) }
-
       before { get :index, params: {character_id: "1"} }
 
       it { should respond_with(:ok) }
@@ -59,17 +62,9 @@ describe AssetsController do
     end
 
     context "when user not signed in" do
-      context "when format js" do
-        before { get :index, params: {character_id: "1", format: "js"} }
+      before { get :index, params: {character_id: "1"} }
 
-        it { should redirect_to(sign_in_path) }
-      end
-
-      context "when format html" do
-        before { get :index, params: {character_id: "1", format: "html"} }
-
-        it { should redirect_to(sign_in_path) }
-      end
+      it { should redirect_to(new_user_session_path) }
     end
   end
 end
