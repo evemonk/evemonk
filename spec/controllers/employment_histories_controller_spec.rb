d0327288aeae427d3a0ef92c22135ca5f7d875df
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-17 22:17:38 +0200

    Update spec

--- spec/controllers/employment_histories_controller_spec.rb
+++ spec/controllers/employment_histories_controller_spec.rb
@@ -5,28 +5,33 @@ require "rails_helper"
 describe EmploymentHistoriesController do
   it { should be_a(ApplicationController) }
 
-  it { should use_before_action(:require_login) }
+  it { should use_before_action(:authenticate_user!) }
 
   describe "#index" do
     context "when user signed in" do
-      let(:current_user) { instance_double(User) }
+      let(:user) { create(:user) }
 
-      before { sign_in(current_user) }
+      before { sign_in(user) }
 
       before do
         #
-        # current_user.characters
-        #             .includes(character_corporation_histories: :corporation)
-        #             .find_by!(character_id: params[:character_id])
-        #             .decorate
+        # subject.current_user
+        #        .characters
+        #        .includes(character_corporation_histories: :corporation)
+        #        .find_by!(character_id: params[:character_id])
+        #        .decorate
         #
-        expect(current_user).to receive(:characters) do
+        expect(subject).to receive(:current_user) do
           double.tap do |a|
-            expect(a).to receive(:includes).with(character_corporation_histories: :corporation) do
+            expect(a).to receive(:characters) do
               double.tap do |b|
-                expect(b).to receive(:find_by!).with(character_id: "1") do
+                expect(b).to receive(:includes).with(character_corporation_histories: :corporation) do
                   double.tap do |c|
-                    expect(c).to receive(:decorate)
+                    expect(c).to receive(:find_by!).with(character_id: "1") do
+                      double.tap do |d|
+                        expect(d).to receive(:decorate)
+                      end
+                    end
                   end
                 end
               end
@@ -34,8 +39,6 @@ describe EmploymentHistoriesController do
           end
         end
       end
-
-      before { expect(current_user).to receive(:set_last_activity_at).with(any_args) }
 
       before { get :index, params: {character_id: "1"} }
 
@@ -45,17 +48,9 @@ describe EmploymentHistoriesController do
     end
 
     context "when user not signed in" do
-      context "when format js" do
-        before { get :index, params: {character_id: "1", format: "js"} }
+      before { get :index, params: {character_id: "1"} }
 
-        it { should redirect_to(sign_in_path) }
-      end
-
-      context "when format html" do
-        before { get :index, params: {character_id: "1", format: "html"} }
-
-        it { should redirect_to(sign_in_path) }
-      end
+      it { should redirect_to(new_user_session_path) }
     end
   end
 end
