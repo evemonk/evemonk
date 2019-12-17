370623f4adbb4e74b8e7c609b495f3130d254edf
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-17 23:16:10 +0200

    Configure devise

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,4 +46,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.active_job.queue_adapter = :inline
+
+  # Devise
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 end
