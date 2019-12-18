f9213217c2f52cdd0164de4f5de125586649dfbe
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-18 14:41:35 +0200

    Add spec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,4 +4,12 @@ require "rails_helper"
 
 describe ApplicationController do
   it { should use_before_action(:authenticate_user!) }
+
+  # private methods
+
+  describe "#after_sign_in_path_for" do
+    let(:resource) { double }
+
+    specify { expect(subject.send(:after_sign_in_path_for, resource)).to eq("/characters") }
+  end
 end
