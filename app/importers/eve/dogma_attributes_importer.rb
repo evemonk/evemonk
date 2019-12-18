f702f11ac835423a2fd2c00db8751ee1365e0ae0
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-18 14:38:37 +0200

    Cleanup

--- app/importers/eve/dogma_attributes_importer.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Eve
-  class DogmaAttributesImporter
-    def import
-    end
-  end
-end
--- app/importers/eve/market_groups_importer.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Eve
-  class MarketGroupsImporter
-    def import
-    end
-  end
-end
