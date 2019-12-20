a2711d4c10a37721398388c8e7307e45d7c47fee
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-20 20:18:44 +0200

    Update

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,5 +2,10 @@
 
 class EmailsController < ApplicationController
   def index
+    @character = current_user.characters
+      .includes(:race, :bloodline, :ancestry, :faction, :alliance, :corporation)
+      .find_by!(character_id: params[:character_id])
+      .decorate
+
   end
 end
