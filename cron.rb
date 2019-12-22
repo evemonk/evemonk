b94d3dbbbcf6fa3ebe949db3c5f3f86a30144571
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-22 19:54:46 +0200

    Update cron

--- cron.rb
+++ cron.rb
@@ -9,22 +9,22 @@ scheduler.every "1h" do
   UpdateCharactersJob.perform_later
 end
 
-scheduler.every "7d" do
+scheduler.every "1d" do
   Rails.logger.info "Update eve races"
   Eve::UpdateRacesJob.perform_later
 end
 
-scheduler.every "7d" do
+scheduler.every "1d" do
   Rails.logger.info "Update eve bloodlines"
   Eve::UpdateBloodlinesJob.perform_later
 end
 
-scheduler.every "7d" do
+scheduler.every "1d" do
   Rails.logger.info "Update eve ancestries"
   Eve::UpdateAncestriesJob.perform_later
 end
 
-scheduler.every "7d" do
+scheduler.every "1d" do
   Rails.logger.info "Update eve factions"
   Eve::UpdateFactionsJob.perform_later
 end
