15bfc620e9b53f85ca20a2f860c05eaebc2ef1f1
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-22 16:22:32 +0200

    Eve::LocalCharactersJob

---
+++ spec/jobs/eve/local_characters_job_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::LocalCharactersJob do
+  it { expect(described_class.queue_name).to eq("default") }
+
+  describe "#perform" do
+    before do
+      #
+      # Eve::LocalCharactersImporter.new.import
+      #
+      expect(Eve::LocalCharactersImporter).to receive(:new) do
+        double.tap do |a|
+          expect(a).to receive(:import)
+        end
+      end
+    end
+
+    specify { expect { subject.perform }.not_to raise_error }
+  end
+end
