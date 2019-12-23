ef96861e2286079e2119e99876fafafa9d86eb31
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-23 20:17:34 +0200

    Eve::LocalCategoriesJob

---
+++ spec/jobs/eve/local_categories_job_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::LocalCategoriesJob do
+  it { expect(described_class.queue_name).to eq("default") }
+
+  describe "#perform" do
+    before do
+      #
+      # Eve::LocalCategoriesImporter.new.import
+      #
+      expect(Eve::LocalCategoriesImporter).to receive(:new) do
+        double.tap do |a|
+          expect(a).to receive(:import)
+        end
+      end
+    end
+
+    specify { expect { subject.perform }.not_to raise_error }
+  end
+end
