a8cbf9fe63168fdcc98d072afc03f55fba8073f0
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-26 00:20:48 +0200

    Eve::LocalDogmaAttributesImporter

---
+++ spec/importers/eve/local_dogma_attributes_importer_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::LocalDogmaAttributesImporter do
+  describe "#import" do
+    let(:attribute_id) { double }
+
+    before do
+      #
+      # Eve::DogmaAttribute.pluck(:attribute_id) # => [attribute_id]
+      #
+      expect(Eve::DogmaAttribute).to receive(:pluck).with(:attribute_id).and_return([attribute_id])
+    end
+
+    before { expect(Eve::UpdateDogmaAttributeJob).to receive(:perform_later).with(attribute_id) }
+
+    specify { expect { subject.import }.not_to raise_error }
+  end
+end
