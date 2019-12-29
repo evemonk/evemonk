2fe66103048cc84b0734fbb09b976172347672a2
Igor Zubkov <igor.zubkov@gmail.com>
2019-12-29 17:01:00 +0200

    Configure mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "robot@evemonk.com"
+  default from: "EveMonk <robot@evemonk.com>"
 
   layout "mailer"
 end
