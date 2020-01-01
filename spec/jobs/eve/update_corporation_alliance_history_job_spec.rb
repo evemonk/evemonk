6f13962646c092ff7dabb482ea57acd07c54e18f
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-01 20:17:58 +0200

    Eve::UpdateCorporationAllianceHistoryJob

---
+++ spec/jobs/eve/update_corporation_alliance_history_job_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::UpdateCorporationAllianceHistoryJob do
+  it { expect(described_class.queue_name).to eq("default") }
+
+  describe "#perform" do
+    let(:corporation_id) { double }
+
+    before do
+      #
+      # Eve::CorporationAllianceHistoryImporter.new(corporation_id).import
+      #
+      expect(Eve::CorporationAllianceHistoryImporter).to receive(:new).with(corporation_id) do
+        double.tap do |a|
+          expect(a).to receive(:import)
+        end
+      end
+    end
+
+    specify { expect { subject.perform(corporation_id) }.not_to raise_error }
+  end
+end
