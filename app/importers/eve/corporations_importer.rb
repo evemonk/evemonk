243badd9769b3f5a23315f963771743a147a0efa
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-01 21:40:57 +0200

    Update

--- app/importers/eve/corporations_importer.rb
+++ app/importers/eve/corporations_importer.rb
@@ -3,18 +3,34 @@
 module Eve
   class CorporationsImporter
     def import
+      imported_corporation_ids = Eve::Corporation.pluck(:corporation_id)
+
       corporation_ids1 = Eve::AllianceCorporation.pluck(:corporation_id).uniq
 
       corporation_ids2 = Eve::Character.pluck(:corporation_id).uniq
 
-      corporation_ids3 = Eve::Corporation.pluck(:corporation_id).uniq
+      corporation_ids3 = Eve::CharacterCorporationHistory.pluck(:corporation_id).uniq
 
-      corporation_ids4 = Eve::CharacterCorporationHistory.pluck(:corporation_id).uniq
+      corporation_ids4 = Eve::Bloodline.pluck(:corporation_id).uniq
 
-      corporation_ids = (corporation_ids1 + corporation_ids2 + corporation_ids3 + corporation_ids4).uniq
+      corporation_ids5 = Eve::Alliance.pluck(:creator_corporation_id).uniq
+
+      corporation_ids6 = Eve::Alliance.pluck(:executor_corporation_id).uniq
+
+      corporation_ids7 = Eve::CorporationAllianceHistory.pluck(:corporation_id).uniq
+
+      corporation_ids8 = Eve::Faction.where.not(corporation_id: nil).pluck(:corporation_id).uniq
+
+      corporation_ids9 = Eve::Faction.where.not(militia_corporation_id: nil).pluck(:militia_corporation_id).uniq
+
+      new_corporation_ids = (corporation_ids1 + corporation_ids2 + corporation_ids3 +
+        corporation_ids4 + corporation_ids5 + corporation_ids6 + corporation_ids7 +
+        corporation_ids8 + corporation_ids9).uniq
+
+      corporation_ids = new_corporation_ids - imported_corporation_ids
 
       corporation_ids.each do |corporation_id|
-        Eve::CorporationImporterWorker.perform_async(corporation_id)
+        Eve::UpdateCorporationJob.perform_later(corporation_id)
       end
     end
   end
--- spec/importers/eve/corporations_importer_spec.rb
+++ spec/importers/eve/corporations_importer_spec.rb
@@ -4,6 +4,17 @@ require "rails_helper"
 
 describe Eve::CorporationsImporter do
   describe "#import" do
+    let(:imported_corporation_id) { double }
+
+    let(:imported_corporation_ids) { [imported_corporation_id] }
+
+    before do
+      #
+      # Eve::Corporation.pluck(:corporation_id) # => imported_corporation_ids
+      #
+      expect(Eve::Corporation).to receive(:pluck).with(:corporation_id).and_return(imported_corporation_ids)
+    end
+
     let(:corporation_id1) { double }
 
     let(:corporation_ids1) { [corporation_id1] }
@@ -40,9 +51,9 @@ describe Eve::CorporationsImporter do
 
     before do
       #
-      # Eve::Corporation.pluck(:corporation_id).uniq # => corporation_ids3
+      # Eve::CharacterCorporationHistory.pluck(:corporation_id).uniq # => corporation_ids3
       #
-      expect(Eve::Corporation).to receive(:pluck).with(:corporation_id) do
+      expect(Eve::CharacterCorporationHistory).to receive(:pluck).with(:corporation_id) do
         double.tap do |a|
           expect(a).to receive(:uniq).and_return(corporation_ids3)
         end
@@ -55,22 +66,78 @@ describe Eve::CorporationsImporter do
 
     before do
       #
-      # Eve::CharacterCorporationHistory.pluck(:corporation_id).uniq # => corporation_ids4
+      # Eve::Bloodline.pluck(:corporation_id).uniq # => corporation_ids4
       #
-      expect(Eve::CharacterCorporationHistory).to receive(:pluck).with(:corporation_id) do
+      expect(Eve::Bloodline).to receive(:pluck).with(:corporation_id) do
         double.tap do |a|
           expect(a).to receive(:uniq).and_return(corporation_ids4)
         end
       end
     end
 
-    before { expect(Eve::CorporationImporterWorker).to receive(:perform_async).with(corporation_id1) }
+    let(:corporation_id5) { double }
 
-    before { expect(Eve::CorporationImporterWorker).to receive(:perform_async).with(corporation_id2) }
+    let(:corporation_ids5) { [corporation_id5] }
 
-    before { expect(Eve::CorporationImporterWorker).to receive(:perform_async).with(corporation_id3) }
+    before do
+      #
+      # Eve::Alliance.pluck(:creator_corporation_id).uniq # => corporation_ids5
+      #
+      expect(Eve::Alliance).to receive(:pluck).with(:creator_corporation_id) do
+        double.tap do |a|
+          expect(a).to receive(:uniq).and_return(corporation_ids5)
+        end
+      end
+    end
 
-    before { expect(Eve::CorporationImporterWorker).to receive(:perform_async).with(corporation_id4) }
+    let(:corporation_id6) { double }
+
+    let(:corporation_ids6) { [corporation_id6] }
+
+    before do
+      #
+      # Eve::Alliance.pluck(:executor_corporation_id).uniq # => corporation_ids6
+      #
+      expect(Eve::Alliance).to receive(:pluck).with(:executor_corporation_id) do
+        double.tap do |a|
+          expect(a).to receive(:uniq).and_return(corporation_ids6)
+        end
+      end
+    end
+
+    let(:corporation_id7) { double }
+
+    let(:corporation_ids7) { [corporation_id7] }
+
+    before do
+      #
+      # Eve::CorporationAllianceHistory.pluck(:corporation_id).uniq # => corporation_ids7
+      #
+      expect(Eve::CorporationAllianceHistory).to receive(:pluck).with(:corporation_id) do
+        double.tap do |a|
+          expect(a).to receive(:uniq).and_return(corporation_ids7)
+        end
+      end
+    end
+
+    # TODO: addd this
+    # corporation_ids8 = Eve::Faction.where.not(corporation_id: nil).pluck(:corporation_id).uniq
+    #
+    # corporation_ids9 = Eve::Faction.where.not(militia_corporation_id: nil).pluck(:militia_corporation_id).uniq
+
+    before { expect(Eve::UpdateCorporationJob).to receive(:perform_later).with(corporation_id1) }
+
+    before { expect(Eve::UpdateCorporationJob).to receive(:perform_later).with(corporation_id2) }
+
+    before { expect(Eve::UpdateCorporationJob).to receive(:perform_later).with(corporation_id3) }
+
+    before { expect(Eve::UpdateCorporationJob).to receive(:perform_later).with(corporation_id4) }
+
+    before { expect(Eve::UpdateCorporationJob).to receive(:perform_later).with(corporation_id5) }
+
+    before { expect(Eve::UpdateCorporationJob).to receive(:perform_later).with(corporation_id6) }
+
+    before { expect(Eve::UpdateCorporationJob).to receive(:perform_later).with(corporation_id7) }
 
     specify { expect { subject.import }.not_to raise_error }
   end
