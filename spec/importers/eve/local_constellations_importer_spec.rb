70a5b845b8d5b3e0dab9aff3bfbd92fe1d1fb602
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-03 23:06:39 +0200

    Eve::LocalConstellationsImporter

---
+++ spec/importers/eve/local_constellations_importer_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::LocalConstellationsImporter do
+  describe "#import" do
+    let(:constellation_id) { double }
+
+    before do
+      #
+      # Eve::Constellation.pluck(:constellation_id) => [constellation_id]
+      #
+      expect(Eve::Constellation).to receive(:pluck).with(:constellation_id).and_return([constellation_id])
+    end
+
+    before { expect(Eve::UpdateConstellationJob).to receive(:perform_later).with(constellation_id) }
+
+    specify { expect { subject.import }.not_to raise_error }
+  end
+end
