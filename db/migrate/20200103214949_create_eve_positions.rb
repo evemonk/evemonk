42902344aecac0304b469ab8085e8115f1ea687c
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-03 23:55:20 +0200

    Add migration

---
+++ db/migrate/20200103214949_create_eve_positions.rb
@@ -0,0 +1,13 @@
+class CreateEvePositions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :eve_positions do |t|
+      t.bigint :x
+      t.bigint :y
+      t.bigint :z
+      t.bigint :positionable_id
+      t.string :positionable_type
+
+      t.timestamps
+    end
+  end
+end
