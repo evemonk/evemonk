04251f9b29ce2d2699f5b3f747bb659160886ef8
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-07 01:16:06 +0200

    Eve::UpdateConstellationsJob

---
+++ app/jobs/eve/update_constellations_job.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Eve
+  class UpdateConstellationsJob < ActiveJob::Base
+    queue_as :default
+
+    retry_on EveOnline::Exceptions::Timeout,
+      EveOnline::Exceptions::ServiceUnavailable,
+      EveOnline::Exceptions::BadGateway,
+      EveOnline::Exceptions::InternalServerError,
+      OpenSSL::SSL::SSLError,
+      Faraday::Error::TimeoutError
+
+    def perform
+      Eve::ConstellationsImporter.new.import
+    end
+  end
+end
