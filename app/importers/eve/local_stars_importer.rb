4a62752f20bd260a74a97a6f56377a329380e49e
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-07 16:40:29 +0200

    Eve::LocalStarsImporter

---
+++ app/importers/eve/local_stars_importer.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Eve
+  class LocalStarsImporter
+    def import
+      Eve::Star.pluck(:star_id).each do |star_id|
+        Eve::UpdateStarJob.perform_later(star_id)
+      end
+    end
+  end
+end
