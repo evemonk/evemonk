7af7c84e3590234bb2fbc33af2c2ee035e43ac17
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-07 04:34:44 +0200

    Eve::LocalRegionsImporter

---
+++ spec/importers/eve/local_regions_importer_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::LocalRegionsImporter do
+  describe "#import" do
+    let(:region_id) { double }
+
+    before do
+      #
+      # Eve::Region.pluck(:region_id) => [region_id]
+      #
+      expect(Eve::Region).to receive(:pluck).with(:region_id).and_return([region_id])
+    end
+
+    before { expect(Eve::UpdateRegionJob).to receive(:perform_later).with(region_id) }
+
+    specify { expect { subject.import }.not_to raise_error }
+  end
+end
