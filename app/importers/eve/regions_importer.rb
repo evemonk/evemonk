f45583624667747d332bda46d9f89d3f48fcdc86
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-07 03:21:46 +0200

    Eve::RegionsImporter

---
+++ app/importers/eve/regions_importer.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+module Eve
+  class RegionsImporter
+    attr_reader :esi
+
+    def initialize
+      @esi = EveOnline::ESI::UniverseRegions.new
+    end
+
+    def import
+      ActiveRecord::Base.transaction do
+        etag = Eve::Etag.find_or_initialize_by(url: esi.url)
+
+        esi.etag = etag.etag
+
+        return if esi.not_modified?
+
+        import_new_regions
+
+        remove_old_regions
+
+        etag.update!(etag: esi.etag, body: esi.response)
+      end
+    end
+
+    private
+
+    def import_new_regions
+      eve_region_ids = Eve::Region.pluck(:region_id)
+
+      eve_region_ids_to_create = esi.universe_region_ids - eve_region_ids
+
+      eve_region_ids_to_create.each do |region_id|
+        Eve::UpdateRegionJob.perform_later(region_id)
+      end
+    end
+
+    def remove_old_regions
+      eve_region_ids = Eve::Region.pluck(:region_id)
+
+      eve_region_ids_to_remove = eve_region_ids - esi.universe_region_ids
+
+      eve_region_ids_to_remove.each do |region_id|
+        eve_region = Eve::Region.find_or_initialize_by(region_id: region_id)
+
+        eve_region.destroy!
+      end
+    end
+  end
+end
