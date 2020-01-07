16cd88f15bf1b32966f26440f7bff6f7be1da5de
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-07 16:50:23 +0200

    Eve::Star#type

--- app/models/eve/star.rb
+++ app/models/eve/star.rb
@@ -3,5 +3,12 @@
 module Eve
   class Star < ApplicationRecord
     has_paper_trail
+
+    # belongs_to :solar_system_id
+
+    belongs_to :type,
+      primary_key: "type_id",
+      foreign_key: "type_id",
+      optional: true
   end
 end
--- spec/models/eve/star_spec.rb
+++ spec/models/eve/star_spec.rb
@@ -8,4 +8,6 @@ describe Eve::Star do
   it { should respond_to(:versions) }
 
   it { expect(described_class.table_name).to eq("eve_stars") }
+
+  it { should belong_to(:type).with_primary_key("type_id").with_foreign_key("type_id").optional(true) }
 end
