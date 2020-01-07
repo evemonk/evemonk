7e24216175744b342fa7787cc8b9c496b0b97ed5
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-07 17:03:54 +0200

    Eve::UpdateSystemJob

---
+++ spec/jobs/eve/update_system_job_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::UpdateSystemJob do
+  it { expect(described_class.queue_name).to eq("default") }
+
+  describe "#perform" do
+    let(:system_id) { double }
+
+    before do
+      #
+      # LanguageMapper::LANGUAGES.each_key do |locale|
+      #   Eve::SystemImporter.new(system_id, locale).import
+      # end
+      #
+      LanguageMapper::LANGUAGES.each_key do |locale|
+        expect(Eve::SystemImporter).to receive(:new).with(system_id, locale) do
+          double.tap do |a|
+            expect(a).to receive(:import)
+          end
+        end
+      end
+    end
+
+    specify { expect { subject.perform(system_id) }.not_to raise_error }
+  end
+end
