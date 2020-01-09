ec5e5b01696bf92abfe387b8bd0a7d030e39ac98
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-09 23:21:28 +0200

    Eve::AsteroidBelt#position

--- app/models/eve/asteroid_belt.rb
+++ app/models/eve/asteroid_belt.rb
@@ -3,5 +3,7 @@
 module Eve
   class AsteroidBelt < ApplicationRecord
     has_paper_trail
+
+    has_one :position, as: :positionable, dependent: :destroy
   end
 end
--- spec/models/eve/asteroid_belt_spec.rb
+++ spec/models/eve/asteroid_belt_spec.rb
@@ -6,4 +6,6 @@ describe Eve::AsteroidBelt do
   it { should be_a(ApplicationRecord) }
 
   it { should respond_to(:versions) }
+
+  it { should have_one(:position).dependent(:destroy) }
 end
