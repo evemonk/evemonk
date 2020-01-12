d6ca5befc4863917bb31f29d68804a7a0b8f170d
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-12 18:28:31 +0200

    Add specs

--- spec/importers/eve/systems_importer_spec.rb
+++ spec/importers/eve/systems_importer_spec.rb
@@ -3,4 +3,82 @@
 require "rails_helper"
 
 describe Eve::SystemsImporter do
+  describe "#initialize" do
+    let(:esi) { instance_double(EveOnline::ESI::UniverseSystems) }
+
+    before do
+      expect(EveOnline::ESI::UniverseSystems).to receive(:new)
+        .and_return(esi)
+    end
+
+    its(:esi) { should eq(esi) }
+  end
+
+  describe "#import" do
+    context "when fresh data available" do
+      let(:etag) { instance_double(Eve::Etag, etag: "97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a") }
+
+      let(:url) { double }
+
+      let(:new_etag) { double }
+
+      let(:response) { double }
+
+      let(:esi) do
+        instance_double(EveOnline::ESI::UniverseSystems,
+          not_modified?: false,
+          url: url,
+          etag: new_etag,
+          response: response)
+      end
+
+      before do
+        expect(EveOnline::ESI::UniverseSystems).to receive(:new)
+          .and_return(esi)
+      end
+
+      before { expect(Eve::Etag).to receive(:find_or_initialize_by).with(url: url).and_return(etag) }
+
+      before do
+        expect(esi).to receive(:etag=)
+          .with("97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a")
+      end
+
+      before { expect(subject).to receive(:import_new_systems) }
+
+      before { expect(subject).to receive(:remove_old_systems) }
+
+      before { expect(etag).to receive(:update!).with(etag: new_etag, body: response) }
+
+      specify { expect { subject.import }.not_to raise_error }
+    end
+
+    context "when no fresh data available" do
+      let(:etag) { instance_double(Eve::Etag, etag: "97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a") }
+
+      let(:url) { double }
+
+      let(:esi) do
+        instance_double(EveOnline::ESI::UniverseSystems,
+          not_modified?: true,
+          url: url)
+      end
+
+      before { expect(EveOnline::ESI::UniverseSystems).to receive(:new).and_return(esi) }
+
+      before { expect(Eve::Etag).to receive(:find_or_initialize_by).with(url: url).and_return(etag) }
+
+      before { expect(esi).to receive(:etag=).with("97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a") }
+
+      before { expect(subject).not_to receive(:import_new_systems) }
+
+      before { expect(subject).not_to receive(:remove_old_systems) }
+
+      before { expect(etag).not_to receive(:update!) }
+
+      specify { expect { subject.import }.not_to raise_error }
+    end
+  end
+
+  # private methods
 end
