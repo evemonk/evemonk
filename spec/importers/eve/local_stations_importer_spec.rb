8534e7ae928063c05c5caa92345dd139bfe0b6a5
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-19 13:44:20 +0200

    Fix

--- spec/importers/eve/local_stations_importer_spec.rb
+++ spec/importers/eve/local_stations_importer_spec.rb
@@ -10,7 +10,7 @@ describe Eve::LocalStationsImporter do
       #
       # Eve::Station.pluck(:station_id) => [station_id]
       #
-      expect(Eve::Station).to receive(:pluck).with(:star_id).and_return([station_id])
+      expect(Eve::Station).to receive(:pluck).with(:station_id).and_return([station_id])
     end
 
     before { expect(Eve::UpdateStationJob).to receive(:perform_later).with(station_id) }
