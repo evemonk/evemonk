4f51c3f7ef962fb33b31c476b0a25f5ee3b9fa80
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-19 02:35:51 +0200

    Eve::LocalStargatesImporter

---
+++ spec/importers/eve/local_stargates_importer_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::LocalStargatesImporter do
+  describe "#import" do
+    let(:stargate_id) { double }
+
+    before do
+      #
+      # Eve::Stargate.pluck(:stargate_id) => [stargate_id]
+      #
+      expect(Eve::Stargate).to receive(:pluck).with(:stargate_id).and_return([stargate_id])
+    end
+
+    before { expect(Eve::UpdateStargateJob).to receive(:perform_later).with(stargate_id) }
+
+    specify { expect { subject.import }.not_to raise_error }
+  end
+end
