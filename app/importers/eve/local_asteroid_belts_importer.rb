8acb29a9ba9187288b4b40637750061c6e30e208
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-19 13:26:20 +0200

    Eve::LocalAsteroidBeltsImporter

---
+++ app/importers/eve/local_asteroid_belts_importer.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Eve
+  class LocalAsteroidBeltsImporter
+    def import
+      Eve::AsteroidBelt.pluck(:planet_id, :asteroid_belt_id).each do |planet_id, asteroid_belt_id|
+        Eve::UpdateAsteroidBeltJob.perform_later(planet_id, asteroid_belt_id)
+      end
+    end
+  end
+end
