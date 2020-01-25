6b70a9b6f7f92fb4b7ba17de7375ac74ffd6a17c
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-25 23:05:19 +0200

    More data

--- app/services/skills_tree.rb
+++ app/services/skills_tree.rb
@@ -10,6 +10,7 @@ class SkillsTree
   def groups
     @groups ||= Eve::Category.find_by!(category_id: SKILLS_CATEGORY_ID)
                              .groups
+                             .includes(:types)
                              .where(published: true)
                              .order(:name_en)
                              .decorate
@@ -22,6 +23,26 @@ class SkillsTree
   def levels_trained_in_group(group)
     skill_ids = group.types.pluck(:type_id)
 
-    character.character_skills.where(skill_id: skill_ids).pluck(:trained_skill_level).sum
+    character.character_skills
+             .where(skill_id: skill_ids)
+             .pluck(:trained_skill_level)
+             .sum
+  end
+
+  def skill_points_in_group(group)
+    skill_ids = group.types.pluck(:type_id)
+
+    character.character_skills
+             .where(skill_id: skill_ids)
+             .pluck(:skillpoints_in_skill)
+             .sum
+  end
+
+  def skills_in_group(group)
+    group.types.order(:name_en)
+  end
+
+  def skill(skill)
+    character.character_skills.where(skill_id: skill.type_id).first
   end
 end
