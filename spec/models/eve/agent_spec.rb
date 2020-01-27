018e5d43b2cf88ef39600f1d77eab220f170fe50
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-27 13:34:36 +0200

    Eve::Agent

---
+++ spec/models/eve/agent_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::Agent do
+  it { should be_an(ApplicationRecord) }
+
+  it { expect(described_class.table_name).to eq("eve_agents") }
+
+  it { should respond_to(:versions) }
+end
