bdc208e5c592319891459c9726afd3cf8de551e1
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-27 13:22:08 +0200

    Update migration

--- db/migrate/20200126212421_create_eve_agents.rb
+++ db/migrate/20200126212421_create_eve_agents.rb
@@ -1,8 +1,19 @@
 class CreateEveAgents < ActiveRecord::Migration[6.0]
   def change
     create_table :eve_agents do |t|
+      t.bigint :agent_id
+      t.bigint :agent_type_id
+      t.bigint :corporation_id
+      t.bigint :division_id
+      t.boolean :is_locator
+      t.integer :level
+      t.bigint :location_id
+      t.integer :quality
 
       t.timestamps
     end
+
+    add_index :eve_agents, :agent_id, unique: true
+    add_index :eve_agents, :corporation_id
   end
 end
