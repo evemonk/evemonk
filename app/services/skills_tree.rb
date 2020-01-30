2a6249a15b4b98f2f17b11a146918a6b2afb89c7
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-31 01:10:53 +0200

    Add spec

--- app/services/skills_tree.rb
+++ app/services/skills_tree.rb
@@ -18,33 +18,33 @@ class SkillsTree
       .decorate
   end
 
-  def total_levels_in_group(group)
-    group.types.where(published: true).count * 5
-  end
-
-  def levels_trained_in_group(group)
-    skill_ids = group.types.pluck(:type_id)
-
-    character.character_skills
-      .where(skill_id: skill_ids)
-      .pluck(:trained_skill_level)
-      .sum
-  end
-
-  def skill_points_in_group(group)
-    skill_ids = group.types.pluck(:type_id)
-
-    character.character_skills
-      .where(skill_id: skill_ids)
-      .pluck(:skillpoints_in_skill)
-      .sum
-  end
-
-  def skills_in_group(group)
-    group.types.order(:name_en)
-  end
-
-  def skill(skill)
-    character.character_skills.where(skill_id: skill.type_id).first
-  end
+  # def total_levels_in_group(group)
+  #   group.types.where(published: true).count * 5
+  # end
+  #
+  # def levels_trained_in_group(group)
+  #   skill_ids = group.types.pluck(:type_id)
+  #
+  #   character.character_skills
+  #     .where(skill_id: skill_ids)
+  #     .pluck(:trained_skill_level)
+  #     .sum
+  # end
+  #
+  # def skill_points_in_group(group)
+  #   skill_ids = group.types.pluck(:type_id)
+  #
+  #   character.character_skills
+  #     .where(skill_id: skill_ids)
+  #     .pluck(:skillpoints_in_skill)
+  #     .sum
+  # end
+  #
+  # def skills_in_group(group)
+  #   group.types.order(:name_en)
+  # end
+  #
+  # def skill(skill)
+  #   character.character_skills.where(skill_id: skill.type_id).first
+  # end
 end
