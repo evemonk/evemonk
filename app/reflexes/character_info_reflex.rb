f72af80a2fa0d514f20720d86c95877773152d13
Igor Zubkov <igor.zubkov@gmail.com>
2020-01-30 21:52:11 +0200

    Update

--- app/reflexes/character_info_reflex.rb
+++ app/reflexes/character_info_reflex.rb
@@ -2,4 +2,13 @@
 
 class CharacterInfoReflex < ApplicationReflex
   delegate :current_user, to: :connection
+
+  def update
+    @character = current_user.characters
+      .includes(:race, :bloodline, :ancestry, :faction, :alliance, :corporation, :current_ship_type)
+      .find_by!(character_id: element.dataset[:id])
+      .decorate
+
+    CharacterWalletImporter.new(@character.character_id).import
+  end
 end
