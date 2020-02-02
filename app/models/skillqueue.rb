dbf20d8a8d756e751bc5f8208d97e89d3f5ebf5f
Igor Zubkov <igor.zubkov@gmail.com>
2020-02-03 00:11:06 +0200

    Skillqueue#skill

--- app/models/skillqueue.rb
+++ app/models/skillqueue.rb
@@ -2,4 +2,10 @@
 
 class Skillqueue < ApplicationRecord
   belongs_to :character
+
+  belongs_to :skill,
+    class_name: "Eve::Type",
+    primary_key: "type_id",
+    foreign_key: "skill_id",
+    optional: true
 end
