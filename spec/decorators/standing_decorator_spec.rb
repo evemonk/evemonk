dd4953b39ba664fbe319b81075c62de7fbeb4bd4
Igor Zubkov <igor.zubkov@gmail.com>
2020-02-03 16:10:46 +0200

    StandingDecorator#standing

--- spec/decorators/standing_decorator_spec.rb
+++ spec/decorators/standing_decorator_spec.rb
@@ -6,4 +6,12 @@ describe StandingDecorator do
   subject { described_class.new(double) }
 
   it { should be_a(ApplicationDecorator) }
+
+  describe "#standing" do
+    let(:standing) { build(:standing, standing: 1.56789) }
+
+    subject { standing.decorate }
+
+    specify { expect(subject.standing).to eq(1.6) }
+  end
 end
