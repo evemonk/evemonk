fd1268c3526f262543013de22383219d99475140
Igor Zubkov <igor.zubkov@gmail.com>
2020-02-03 17:32:00 +0200

    Fix factory

--- spec/factories/standing_factory.rb
+++ spec/factories/standing_factory.rb
@@ -9,5 +9,19 @@ FactoryBot.define do
     from_type { ["agent", "npc_corp", "faction"].sample }
 
     standing { 1.56789 }
+
+    for_agent
+
+    trait :for_agent do
+      association :standingable, factory: :eve_agent
+    end
+
+    trait :for_corporation do
+      association :standingable, factory: :eve_corporation
+    end
+
+    trait :for_faction do
+      association :standingable, factory: :eve_faction
+    end
   end
 end
