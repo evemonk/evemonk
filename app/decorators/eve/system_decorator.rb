88148f03afcfd123cd72925e9d4e307b7a4c1703
Igor Zubkov <igor.zubkov@gmail.com>
2020-02-09 14:37:40 +0200

    Eve::SystemDecorator#security_status

--- app/decorators/eve/system_decorator.rb
+++ app/decorators/eve/system_decorator.rb
@@ -2,6 +2,10 @@
 
 module Eve
   class SystemDecorator < ApplicationDecorator
-    decorates_associations :stargates
+    decorates_associations :constellation, :star, :stargates, :position
+
+    def security_status
+      object.security_status.round(1)
+    end
   end
 end
--- spec/decorators/eve/system_decorator_spec.rb
+++ spec/decorators/eve/system_decorator_spec.rb
@@ -6,4 +6,22 @@ describe Eve::SystemDecorator do
   subject { described_class.new(double) }
 
   it { should be_a(ApplicationDecorator) }
+
+  describe "#security_status" do
+    context "when security status is 0.14" do
+      let(:eve_system) { build(:eve_system, security_status: 0.14) }
+
+      subject { eve_system.decorate }
+
+      specify { expect(subject.security_status).to eq(0.1) }
+    end
+
+    context "when security status is 0.15" do
+      let(:eve_system) { build(:eve_system, security_status: 0.15) }
+
+      subject { eve_system.decorate }
+
+      specify { expect(subject.security_status).to eq(0.2) }
+    end
+  end
 end
