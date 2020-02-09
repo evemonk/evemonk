b5086aec183ab48e8bb95b84692e81f817229ec2
Igor Zubkov <igor.zubkov@gmail.com>
2020-02-09 14:39:50 +0200

    Update

--- spec/models/eve/system_spec.rb
+++ spec/models/eve/system_spec.rb
@@ -13,6 +13,10 @@ describe Eve::System do
 
   it { expect(described_class.table_name).to eq("eve_systems") }
 
+  it { should belong_to(:constellation).with_primary_key("constellation_id").optional(true) }
+
+  it { should belong_to(:star).with_primary_key("star_id").optional(true) }
+
   it { should have_many(:stargates).with_primary_key("system_id") }
 
   it { should have_one(:position).dependent(:destroy) }
