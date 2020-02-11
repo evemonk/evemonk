6cd5187e301bbc6d0d5b93294d8cec6b0af5f1b7
Igor Zubkov <igor.zubkov@gmail.com>
2020-02-11 23:13:12 +0200

    Add eve agents icons

--- app/decorators/eve/agent_decorator.rb
+++ app/decorators/eve/agent_decorator.rb
@@ -3,5 +3,29 @@
 module Eve
   class AgentDecorator < ApplicationDecorator
     decorates_associations :standings
+
+    def icon_tiny
+      "#{imageproxy_url}https://images.evetech.net/characters/#{agent_id}/portrait?size=32&tenant=tranquility"
+    end
+
+    def icon_small
+      "#{imageproxy_url}https://images.evetech.net/characters/#{agent_id}/portrait?size=64&tenant=tranquility"
+    end
+
+    def icon_medium
+      "#{imageproxy_url}https://images.evetech.net/characters/#{agent_id}/portrait?size=128&tenant=tranquility"
+    end
+
+    def icon_large
+      "#{imageproxy_url}https://images.evetech.net/characters/#{agent_id}/portrait?size=256&tenant=tranquility"
+    end
+
+    def icon_huge
+      "#{imageproxy_url}https://images.evetech.net/characters/#{agent_id}/portrait?size=512&tenant=tranquility"
+    end
+
+    def icon_gigantic
+      "#{imageproxy_url}https://images.evetech.net/characters/#{agent_id}/portrait?size=1024&tenant=tranquility"
+    end
   end
 end
