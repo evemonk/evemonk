0240e2a0c60cd00a56d7a70e16e67aac100fbe2c
Igor Zubkov <igor.zubkov@gmail.com>
2020-02-19 00:02:11 +0200

    CharacterKillmailsJob and CharacterKillmailsImporter

---
+++ app/jobs/character_killmails_job.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class CharacterKillmailsJob < ActiveJob::Base
+  queue_as :important
+
+  retry_on EveOnline::Exceptions::Timeout,
+    EveOnline::Exceptions::ServiceUnavailable,
+    EveOnline::Exceptions::BadGateway,
+    EveOnline::Exceptions::InternalServerError,
+    OpenSSL::SSL::SSLError,
+    Faraday::Error::TimeoutError,
+    Faraday::ConnectionFailed
+
+  def perform(character_id, page = 1)
+    CharacterKillmailsImporter.new(character_id, page).import
+  end
+end
