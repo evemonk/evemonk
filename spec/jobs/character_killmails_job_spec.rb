213895cfef0d790e04b878adcf451145c7b28d33
Igor Zubkov <igor.zubkov@gmail.com>
2020-02-28 01:59:01 +0200

    Add spec

---
+++ spec/jobs/character_killmails_job_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe CharacterKillmailsJob do
+  it { expect(described_class.queue_name).to eq("important") }
+
+  describe "#perform" do
+    let(:character_id) { double }
+
+    let(:page) { double }
+
+    before do
+      #
+      # CharacterKillmailsImporter.new(character_id, page).import
+      #
+      expect(CharacterKillmailsImporter).to receive(:new).with(character_id, page) do
+        double.tap do |a|
+          expect(a).to receive(:import)
+        end
+      end
+    end
+
+    specify { expect { subject.perform(character_id, page) }.not_to raise_error }
+  end
+end
