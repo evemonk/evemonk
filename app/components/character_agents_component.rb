dcf7f8a8ff483d6e7e717051d224f1e8a231a32c
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-02 22:35:49 +0200

    Cleanup

--- app/components/character_agents_component.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class CharacterAgentsComponent < ActionView::Component::Base
-  attr_reader :agents_standings
-
-  def initialize(agents_standings:)
-    @agents_standings = agents_standings
-  end
-end
