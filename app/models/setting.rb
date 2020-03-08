ec6252c95f6fe152d4b390c28e09be2b43866a6f
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-08 17:23:10 +0200

    Remove gauges and google analitycs trackers

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -5,21 +5,13 @@ class Setting < RailsSettings::Base
 
   field :eve_online_sso_secret_key, default: ENV["EVE_ONLINE_SSO_SECRET_KEY"], readonly: true
 
-  field :use_image_proxy, type: :boolean, default: false
+  field :use_image_proxy, type: :boolean, default: true
 
   field :image_proxy_url, default: "https://imageproxy.evemonk.com/"
-
-  field :use_google_analytics, type: :boolean, default: false
-
-  field :google_analytics_id, default: ENV["GOOGLE_ANALYTICS_ID"], readonly: true
 
   field :use_google_search_console, type: :boolean, default: false
 
   field :google_site_verification_token
-
-  field :use_gauges, type: :boolean, default: false
-
-  field :gauges_data_site_id, default: ENV["GAUGES_DATA_SITE_ID"], readonly: true
 
   field :enabled_assets, type: :boolean, default: false
 
