9c2f9479971a723b92c28a1fe76d67052fa3d2e6
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-10 00:57:01 +0200

    Fix

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -26,7 +26,7 @@ end
 Rails.application.config.content_security_policy_nonce_generator = ->(request) { SecureRandom.base64(16) }
 
 # Set the nonce only to specific directives
-Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
+Rails.application.config.content_security_policy_nonce_directives = ["script-src"]
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
