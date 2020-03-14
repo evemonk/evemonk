38ccdd673738c131dfbaf531a6c8102c6497c123
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-14 23:50:21 +0200

    Eve::UpdateMarketPricesJob

---
+++ spec/jobs/eve/update_market_prices_job_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::UpdateMarketPricesJob do
+  it { expect(described_class.queue_name).to eq("important") }
+
+  describe "#perform" do
+    before do
+      #
+      # Eve::MarketPricesImporter.new.import
+      #
+      expect(Eve::MarketPricesImporter).to receive(:new) do
+        double.tap do |a|
+          expect(a).to receive(:import)
+        end
+      end
+    end
+
+    specify { expect { subject.perform }.not_to raise_error }
+  end
+end
