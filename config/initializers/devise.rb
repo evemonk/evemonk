060875a22a418886d6e319dd9d772100188533a4
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-15 19:33:40 +0200

    Cleanup

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,14 +3,6 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-
-  # ==> Devise Authy Authentication Extension
-  # How long should the user's device be remembered for.
-  # config.authy_remember_device = 1.month
-
-  # Should Authy OneTouch be enabled?
-  # config.authy_enable_onetouch = false
-
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -305,4 +297,11 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  # ==> Devise Authy Authentication Extension
+  # How long should the user's device be remembered for.
+  # config.authy_remember_device = 1.month
+
+  # Should Authy OneTouch be enabled?
+  # config.authy_enable_onetouch = false
 end
