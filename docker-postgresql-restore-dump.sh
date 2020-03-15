e84e832ffc0031df3ae8773454def2ff5a017ea4
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-15 22:18:09 +0200

    Update

--- docker-postgresql-restore-dump.sh
+++ docker-postgresql-restore-dump.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-gzcat backup-2019-12-27.sql.gz | docker exec -i evemonk_postgresql psql -U postgres
+gzcat backup-2020-01-21.sql.gz | docker exec -i evemonk_postgresql psql -U postgres
