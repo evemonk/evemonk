d26e6e824342d1edd06c714819c72f1577fdaa37
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-15 04:41:30 +0200

    Fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, prepend: true
 
   before_action :authenticate_user!
 
