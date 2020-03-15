bd56e7e2980706ae2fa6c86073ac9cf76f544b95
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-15 02:43:24 +0200

    AuthyController

---
+++ app/controllers/authy_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class AuthyController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def update
+    if current_user.update!(resource_params)
+      authy = Authy::API.register_user(
+        email: current_user.email,
+        cellphone: current_user.phone_number,
+        country_code: current_user.country_code
+      )
+      current_user.update!(authy_id: authy.id)
+    end
+  end
+
+  private
+
+  def resource_params
+    params.require(:user).permit(:phone_number, :country_code)
+  end
+end
