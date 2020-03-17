14c2d7fd46592fdef39f6f8ecf8b94765a415f32
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-17 03:39:52 +0200

    WalletJournalsController

--- app/controllers/wallet_journals_controller.rb
+++ app/controllers/wallet_journals_controller.rb
@@ -2,5 +2,13 @@
 
 class WalletJournalsController < ApplicationController
   def index
+    @character = current_user.characters
+      .includes(:alliance, :corporation)
+      .find_by!(character_id: params[:character_id])
+      .decorate
+
+    # @wallet_journals = @character.wallet_journals
+    #   .page(params[:page])
+    #   .per(100)
   end
 end
--- spec/controllers/wallet_journals_controller_spec.rb
+++ spec/controllers/wallet_journals_controller_spec.rb
@@ -8,5 +8,48 @@ describe WalletJournalsController do
   it { should use_before_action(:authenticate_user!) }
 
   describe "#index" do
+    context "when user signed in" do
+      let(:user) { create(:user) }
+
+      before { sign_in(user) }
+
+      before do
+        #
+        # subject.current_user.characters
+        #        .includes(:alliance, :corporation)
+        #        .find_by!(character_id: params[:character_id])
+        #        .decorate
+        #
+        expect(subject).to receive(:current_user) do
+          double.tap do |a|
+            expect(a).to receive(:characters) do
+              double.tap do |b|
+                expect(b).to receive(:includes).with(:alliance, :corporation) do
+                  double.tap do |c|
+                    expect(c).to receive(:find_by!).with(character_id: "1") do
+                      double.tap do |d|
+                        expect(d).to receive(:decorate)
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+
+      before { get :index, params: {character_id: "1"} }
+
+      it { should respond_with(:ok) }
+
+      it { should render_template(:index) }
+    end
+
+    context "when user not signed in" do
+      before { get :index, params: {character_id: "1"} }
+
+      it { should redirect_to(new_user_session_path) }
+    end
   end
 end
