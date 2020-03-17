c9f880fc46b5777851f321838a1817700374bd58
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-17 04:04:25 +0200

    Delegate :page and :per to original object

--- app/decorators/paginating_decorator.rb
+++ app/decorators/paginating_decorator.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class PaginatingDecorator < Draper::CollectionDecorator
-  delegate :current_page, :total_pages, :limit_value, :entry_name, :total_count, :offset_value, :last_page?
+  delegate :current_page, :total_pages, :limit_value, :entry_name,
+    :total_count, :offset_value, :last_page?, :page, :per
 end
