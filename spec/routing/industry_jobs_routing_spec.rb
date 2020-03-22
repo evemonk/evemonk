f7bb6bdb12d01dc3fc982c8edd9f88562bb0ab5a
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-22 23:15:20 +0200

    IndustryJobsController

---
+++ spec/routing/industry_jobs_routing_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe IndustryJobsController do
+  describe "routing" do
+    it "should route GET /characters/90729314/industry_jobs to industry_jobs#index" do
+      expect(get: "/characters/90729314/industry_jobs").to route_to(
+        controller: "industry_jobs",
+        action: "index",
+        character_id: "90729314"
+      )
+    end
+  end
+end
