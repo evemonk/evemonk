0cd5487024059d7a6ea50e00469b55c086f3e2e1
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-22 23:17:51 +0200

    Character#industry_jobs

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -51,6 +51,8 @@ describe Character do
 
   it { should have_many(:wallet_transactions).dependent(:destroy) }
 
+  it { should have_many(:industry_jobs).dependent(:destroy) }
+
   describe "#factions_standings" do
     let!(:character) { create(:character) }
 
