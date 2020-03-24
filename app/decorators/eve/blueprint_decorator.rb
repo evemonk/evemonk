816f7c76b698416aa0f988664ec7da254bb219a0
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-24 03:26:10 +0200

    Eve::BlueprintDecorator

---
+++ app/decorators/eve/blueprint_decorator.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Eve
+  class BlueprintDecorator < TypeDecorator
+    # def bpo_url
+    #   "https://images.evetech.net/types/804/bp"
+    # end
+    #
+    # def bpc_url
+    #   "https://images.evetech.net/types/804/bpc"
+    # end
+  end
+end
