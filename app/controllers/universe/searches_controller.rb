91752950569d2b0ab4b2d1d0573cd43c64ac807e
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-24 03:48:06 +0200

    Fix

--- app/controllers/universe/searches_controller.rb
+++ app/controllers/universe/searches_controller.rb
@@ -2,6 +2,8 @@
 
 module Universe
   class SearchesController < ApplicationController
+    skip_before_action :authenticate_user!
+
     def index
       # @alliances = Eve::AlliancesSearcher.new(params[:q]).search
       # @corporations = Eve::CorporationsSearcher.new(params[:q]).search
