5a3cd7447c6b3147bcb1fe565eaecb309765a3e9
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-26 03:33:21 +0200

    More specs

--- spec/decorators/character_blueprint_decorator_spec.rb
+++ spec/decorators/character_blueprint_decorator_spec.rb
@@ -6,4 +6,40 @@ describe CharacterBlueprintDecorator do
   subject { described_class.new(double) }
 
   it { should be_a(ApplicationDecorator) }
+
+  describe "#bpo?" do
+    context "when blueprint is original" do
+      let(:character_blueprint) { build(:character_blueprint, quantity: -1) }
+
+      subject { character_blueprint.decorate }
+
+      specify { expect(subject.bpo?).to eq(true) }
+    end
+
+    context "when blueprint is copy" do
+      let(:character_blueprint) { build(:character_blueprint, quantity: -2) }
+
+      subject { character_blueprint.decorate }
+
+      specify { expect(subject.bpo?).to eq(false) }
+    end
+  end
+
+  describe "#bpc?" do
+    context "when blueprint is original" do
+      let(:character_blueprint) { build(:character_blueprint, quantity: -1) }
+
+      subject { character_blueprint.decorate }
+
+      specify { expect(subject.bpc?).to eq(false) }
+    end
+
+    context "when blueprint is copy" do
+      let(:character_blueprint) { build(:character_blueprint, quantity: -2) }
+
+      subject { character_blueprint.decorate }
+
+      specify { expect(subject.bpc?).to eq(true) }
+    end
+  end
 end
