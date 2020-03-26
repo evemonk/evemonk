fbda3742d86dc3c6a211cb6d3b31e958b59339a6
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-26 03:35:08 +0200

    More specs

--- spec/decorators/character_blueprint_decorator_spec.rb
+++ spec/decorators/character_blueprint_decorator_spec.rb
@@ -42,4 +42,58 @@ describe CharacterBlueprintDecorator do
       specify { expect(subject.bpc?).to eq(true) }
     end
   end
+
+  describe "#stacked?" do
+    context "when stacked" do
+      let(:character_blueprint) { build(:character_blueprint, quantity: 10) }
+
+      subject { character_blueprint.decorate }
+
+      specify { expect(subject.stacked?).to eq(true) }
+    end
+
+    context "when not stacked" do
+      let(:character_blueprint) { build(:character_blueprint, quantity: -1) }
+
+      subject { character_blueprint.decorate }
+
+      specify { expect(subject.stacked?).to eq(false) }
+    end
+  end
+
+  # def icon_tiny
+  #   if stacked? || bpo?
+  #     "#{imageproxy_url}https://images.evetech.net/types/#{type_id}/bp?size=32"
+  #   elsif bpc?
+  #     "#{imageproxy_url}https://images.evetech.net/types/#{type_id}/bpc?size=32"
+  #   end
+  # end
+  #
+  # def icon_small
+  #   if stacked? || bpo?
+  #     "#{imageproxy_url}https://images.evetech.net/types/#{type_id}/bp?size=64"
+  #   elsif bpc?
+  #     "#{imageproxy_url}https://images.evetech.net/types/#{type_id}/bpc?size=64"
+  #   end
+  # end
+  #
+  # def material_efficiency_formatted
+  #   return if stacked?
+  #
+  #   if material_efficiency.zero?
+  #     "#{material_efficiency} %"
+  #   else
+  #     "+#{material_efficiency} %"
+  #   end
+  # end
+  #
+  # def time_efficiency_formatted
+  #   return if stacked?
+  #
+  #   if time_efficiency.zero?
+  #     "#{time_efficiency} %"
+  #   else
+  #     "+#{time_efficiency} %"
+  #   end
+  # end
 end
