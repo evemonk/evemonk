db4b50acaaee3bd93d9182d667301b29c1e6f18f
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-30 01:02:09 +0300

    Add specs

--- spec/models/eve/blueprint_spec.rb
+++ spec/models/eve/blueprint_spec.rb
@@ -4,4 +4,16 @@ require "rails_helper"
 
 describe Eve::Blueprint do
   it { should be_a(Eve::Type) }
+
+  it { should have_many(:blueprint_invention_materials).with_primary_key("type_id").dependent(:destroy) }
+
+  it { should have_many(:blueprint_invention_products).with_primary_key("type_id").dependent(:destroy) }
+
+  it { should have_many(:blueprint_invention_skills).with_primary_key("type_id").dependent(:destroy) }
+
+  it { should have_many(:blueprint_manufacturing_materials).with_primary_key("type_id").dependent(:destroy) }
+
+  it { should have_many(:blueprint_manufacturing_products).with_primary_key("type_id").dependent(:destroy) }
+
+  it { should have_many(:blueprint_manufacturing_skills).with_primary_key("type_id").dependent(:destroy) }
 end
