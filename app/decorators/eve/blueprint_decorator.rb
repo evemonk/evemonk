612177c3aa5725619605177f4e4eb4d300d84993
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-29 18:19:58 +0300

    Update decorator

--- app/decorators/eve/blueprint_decorator.rb
+++ app/decorators/eve/blueprint_decorator.rb
@@ -2,12 +2,39 @@
 
 module Eve
   class BlueprintDecorator < TypeDecorator
-    # def bpo_url
-    #   "https://images.evetech.net/types/804/bp"
-    # end
-    #
-    # def bpc_url
-    #   "https://images.evetech.net/types/804/bpc"
-    # end
+    def copying_time_formatted
+      time_to_human_time_formatted(copying_time)
+    end
+
+    def manufacturing_time_formatted
+      time_to_human_time_formatted(manufacturing_time)
+    end
+
+    def research_material_time_formatted
+      time_to_human_time_formatted(research_material_time)
+    end
+
+    def research_time_time_formatted
+      time_to_human_time_formatted(research_time_time)
+    end
+
+    def invention_time_formatted
+      time_to_human_time_formatted(invention_time)
+    end
+
+    private
+
+    def time_to_human_time_formatted(time)
+      case time
+      when 0..60
+        "#{time} seconds"
+      when 61..3600
+        "#{time / 60} minutes"
+      when 3601..86400
+        "#{time / 60 / 60} hours"
+      else
+        "#{time / 60 / 60 / 24} days"
+      end
+    end
   end
 end
