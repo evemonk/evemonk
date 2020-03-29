6d384b10736184addc8df1c166557186cc2cc576
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-29 18:26:26 +0300

    Cleanup

--- app/services/sde/blueprints_importer.rb
+++ app/services/sde/blueprints_importer.rb
@@ -14,11 +14,11 @@ module Sde
       entries.each_pair do |key, hash|
         eve_type = Eve::Type.find_or_initialize_by(type_id: key)
 
-        copying_time = hash.fetch("activities", {}).fetch("copying", {}).fetch("time", nil)
-        manufacturing_time = hash.fetch("activities", {}).fetch("manufacturing", {}).fetch("time", nil)
-        research_material_time = hash.fetch("activities", {}).fetch("research_material", {}).fetch("time", nil)
-        research_time_time = hash.fetch("activities", {}).fetch("research_time", {}).fetch("time", nil)
-        invention_time = hash.fetch("activities", {}).fetch("invention", {}).fetch("time", nil)
+        copying_time = hash.dig("activities", "copying", "time")
+        manufacturing_time = hash.dig("activities", "manufacturing", "time")
+        research_material_time = hash.dig("activities", "research_material", "time")
+        research_time_time = hash.dig("activities", "research_time", "time")
+        invention_time = hash.dig("activities", "invention", "time")
 
         eve_type.assign_attributes(copying_time: copying_time,
                                    manufacturing_time: manufacturing_time,
