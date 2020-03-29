21960240b5089c522283326d830ea2ef1b321408
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-29 19:32:12 +0300

    Sde::BlueprintsJob

---
+++ app/jobs/sde/blueprints_job.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Sde
+  class BlueprintsJob < ActiveJob::Base
+    queue_as :default
+
+    def perform(file)
+      BlueprintsImporter.new(file).import
+    end
+  end
+end
