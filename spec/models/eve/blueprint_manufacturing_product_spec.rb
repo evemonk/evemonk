3f1d53bf6b8234b0706d0673885b40288741fd34
Igor Zubkov <igor.zubkov@gmail.com>
2020-03-30 01:17:10 +0300

    Add specs

---
+++ spec/models/eve/blueprint_manufacturing_product_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::BlueprintManufacturingProduct do
+  it { should be_a(ApplicationRecord) }
+
+  it { should belong_to(:blueprint).class_name("Eve::Blueprint").with_primary_key("type_id") }
+end
