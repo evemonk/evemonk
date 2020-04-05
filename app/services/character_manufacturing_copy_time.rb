d9b223bac942720f425f32fd3e7ecad88dc347ae
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-06 00:33:00 +0300

    Update

--- app/services/character_manufacturing_copy_time.rb
+++ app/services/character_manufacturing_copy_time.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class CharacterManufacturingCopyTime
-  SCIENCE_SKILL_ID = 3402
-  ADVANCED_INDUSTRY_SKILL_ID = 3388
   DOGMA_ATTRIBUTE_COPY_SPEED_BONUS = "copySpeedBonus"
   DOGMA_ATTRIBUTE_JOB_TIME_BONUS = "advancedIndustrySkillIndustryJobTimeBonus"
 
@@ -31,36 +29,16 @@ class CharacterManufacturingCopyTime
 
   private
 
-  # Science
-  def science_skill
-    character.character_skills.find_by(skill_id: SCIENCE_SKILL_ID)
-  end
-
-  def science_level
-    # https://www.reddit.com/r/evetech/comments/bndjwi/difference_between_active_skill_level_and_trained/
-    science_skill&.active_skill_level || 0
-  end
-
   def dogma_attribute_copy_speed_bonus_id
     Eve::DogmaAttribute.find_by(name: DOGMA_ATTRIBUTE_COPY_SPEED_BONUS).attribute_id
   end
 
   def science_copy_bonus_value
-    science_skill&.skill&.type_dogma_attributes&.find_by(attribute_id: dogma_attribute_copy_speed_bonus_id)&.value || 0
+    character.science_skill&.skill&.type_dogma_attributes&.find_by(attribute_id: dogma_attribute_copy_speed_bonus_id)&.value || 0
   end
 
   def science_copy_bonus
-    science_level * science_copy_bonus_value
-  end
-
-  # Advanced Industry
-  def advanced_industry_skill
-    character.character_skills.find_by(skill_id: ADVANCED_INDUSTRY_SKILL_ID)
-  end
-
-  def advanced_industry_level
-    # https://www.reddit.com/r/evetech/comments/bndjwi/difference_between_active_skill_level_and_trained/
-    advanced_industry_skill&.active_skill_level || 0
+    character.science_level * science_copy_bonus_value
   end
 
   def dogma_attribute_job_time_bonus
@@ -68,10 +46,10 @@ class CharacterManufacturingCopyTime
   end
 
   def advanced_industry_job_time_bonus_value
-    advanced_industry_skill&.skill&.type_dogma_attributes&.find_by(attribute_id: dogma_attribute_job_time_bonus)&.value || 0
+    character.advanced_industry_skill&.skill&.type_dogma_attributes&.find_by(attribute_id: dogma_attribute_job_time_bonus)&.value || 0
   end
 
   def advanced_industry_job_time_bonus
-    advanced_industry_level * advanced_industry_job_time_bonus_value
+    character.advanced_industry_level * advanced_industry_job_time_bonus_value
   end
 end
