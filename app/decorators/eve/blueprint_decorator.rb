d286c392acb7daffdf766da3af73e036aae9fde9
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-05 23:46:01 +0300

    Eve::BlueprintDecorator#copying_time_formatted

--- app/decorators/eve/blueprint_decorator.rb
+++ app/decorators/eve/blueprint_decorator.rb
@@ -6,5 +6,13 @@ module Eve
       :blueprint_invention_products, :blueprint_invention_skills,
       :blueprint_manufacturing_materials, :blueprint_manufacturing_products,
       :blueprint_manufacturing_skills
+
+    def copying_time_formatted
+      HumanTime.new(copying_time).long_formatted
+    end
+
+    # def copying_modifier
+    #   27.11
+    # end
   end
 end
--- spec/decorators/eve/blueprint_decorator_spec.rb
+++ spec/decorators/eve/blueprint_decorator_spec.rb
@@ -6,4 +6,23 @@ describe Eve::BlueprintDecorator do
   subject { described_class.new(double) }
 
   it { should be_a(Eve::TypeDecorator) }
+
+  describe "#copying_time_formatted" do
+    let(:eve_blueprint) { Eve::Blueprint.new(attributes_for(:eve_type, copying_time: 240)) }
+
+    subject { eve_blueprint.decorate }
+
+    before do
+      #
+      # HumanTime.new(copying_time).long_formatted
+      #
+      expect(HumanTime).to receive(:new).with(240) do
+        double.tap do |a|
+          expect(a).to receive(:long_formatted)
+        end
+      end
+    end
+
+    specify { expect { subject.copying_time_formatted }.not_to raise_error }
+  end
 end
