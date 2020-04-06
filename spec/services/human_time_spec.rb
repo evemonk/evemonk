84588578692c68600f9310cb1248b6b769fb36e3
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-07 01:05:05 +0300

    Fix

--- spec/services/human_time_spec.rb
+++ spec/services/human_time_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 describe HumanTime do
-  it { expect(described_class::SECONDS_IN_DAY).to eq(86400) }
+  it { expect(described_class::SECONDS_IN_DAY).to eq(86_400) }
 
   it { expect(described_class::SECONDS_IN_HOUR).to eq(3600) }
 
