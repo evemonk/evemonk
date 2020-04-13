55a637386b942aaac63b70850094874018d8edc9
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-13 23:28:50 +0300

    Add spec

--- app/services/sde/blueprints_importer.rb
+++ app/services/sde/blueprints_importer.rb
@@ -9,44 +9,49 @@ module Sde
     end
 
     def import
-      # entries = YAML.safe_load(File.read(file))
-      #
-      # entries.each_pair do |key, hash|
-      #   eve_blueprint = Eve::Blueprint.find_or_initialize_by(type_id: key)
-      #
-      #   copying_time = hash.dig("activities", "copying", "time")
-      #   manufacturing_time = hash.dig("activities", "manufacturing", "time")
-      #   research_material_time = hash.dig("activities", "research_material", "time")
-      #   research_time_time = hash.dig("activities", "research_time", "time")
-      #   invention_time = hash.dig("activities", "invention", "time")
-      #
-      #   eve_blueprint.transaction do
-      #     import_blueprint_invention_materials(eve_blueprint, hash)
-      #
-      #     import_blueprint_invention_products(eve_blueprint, hash)
-      #
-      #     import_blueprint_invention_skills(eve_blueprint, hash)
-      #
-      #     import_blueprint_manufacturing_materials(eve_blueprint, hash)
-      #
-      #     import_blueprint_manufacturing_products(eve_blueprint, hash)
-      #
-      #     import_blueprint_manufacturing_skills(eve_blueprint, hash)
-      #
-      #     eve_blueprint.assign_attributes(copying_time: copying_time,
-      #                                     manufacturing_time: manufacturing_time,
-      #                                     research_material_time: research_material_time,
-      #                                     research_time_time: research_time_time,
-      #                                     invention_time: invention_time,
-      #                                     max_production_limit: hash["maxProductionLimit"])
-      #
-      #     eve_blueprint.save!
-      #   end
-      # end
+      entries = YAML.safe_load(File.read(file))
+
+      entries.each_pair do |key, hash|
+        eve_blueprint = Eve::Blueprint.find_or_initialize_by(type_id: key)
+
+        copying_time = hash.dig("activities", "copying", "time")
+        # manufacturing_time = hash.dig("activities", "manufacturing", "time")
+        # research_material_time = hash.dig("activities", "research_material", "time")
+        # research_time_time = hash.dig("activities", "research_time", "time")
+        # invention_time = hash.dig("activities", "invention", "time")
+
+        eve_blueprint.transaction do
+          # import_more_information(eve_blueprint, hash)
+
+          eve_blueprint.assign_attributes(copying_time: copying_time,
+                                          # manufacturing_time: manufacturing_time,
+                                          # research_material_time: research_material_time,
+                                          # research_time_time: research_time_time,
+                                          # invention_time: invention_time,
+                                          # max_production_limit: hash["maxProductionLimit"],
+                                          is_blueprint: true)
+
+          eve_blueprint.save!
+        end
+      end
     end
 
-    # private
-    #
+    private
+
+    def import_more_information(eve_blueprint, hash)
+      # import_blueprint_invention_materials(eve_blueprint, hash)
+      #
+      # import_blueprint_invention_products(eve_blueprint, hash)
+      #
+      # import_blueprint_invention_skills(eve_blueprint, hash)
+      #
+      # import_blueprint_manufacturing_materials(eve_blueprint, hash)
+      #
+      # import_blueprint_manufacturing_products(eve_blueprint, hash)
+      #
+      # import_blueprint_manufacturing_skills(eve_blueprint, hash)
+    end
+
     # def import_blueprint_invention_materials(eve_blueprint, hash)
     #   eve_blueprint.blueprint_invention_materials.destroy_all
     #
--- spec/services/sde/blueprints_importer_spec.rb
+++ spec/services/sde/blueprints_importer_spec.rb
@@ -12,6 +12,35 @@ describe Sde::BlueprintsImporter do
   end
 
   describe "#import" do
+    let(:content) { double }
+
+    before { expect(File).to receive(:read).with(file).and_return(content) }
+
+    let(:key) { double }
+
+    let(:copying_time) { double }
+
+    let(:entry) do
+      {
+        "activities" => { "copying" => { "time" => copying_time } }
+      }
+    end
+
+    let(:entries) { {key => entry} }
+
+    before { expect(YAML).to receive(:safe_load).with(content).and_return(entries) }
+
+    let(:eve_blueprint) { instance_double(Eve::Blueprint) }
+
+    before { expect(Eve::Blueprint).to receive(:find_or_initialize_by).with(type_id: key).and_return(eve_blueprint) }
+
+    before { expect(eve_blueprint).to receive(:transaction).and_yield }
+
+    before { expect(eve_blueprint).to receive(:assign_attributes).with(copying_time: copying_time,
+                                                                       is_blueprint: true) }
+
+    before { expect(eve_blueprint).to receive(:save!) }
+
     specify { expect { subject.import }.not_to raise_error }
   end
 end
