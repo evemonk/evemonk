fa2578a00b8f5929a961f17ad7e639291e37d63c
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-15 15:51:55 +0300

    Add searchers

--- app/searchers/eve/manufacturing_items_searcher.rb
+++ app/searchers/eve/manufacturing_items_searcher.rb
@@ -1,14 +1,49 @@
-ManufactutingItemsSearcher
-
 # frozen_string_literal: true
 
 module Eve
-  class BlueprintsSearcher
+  class ManufacturingItemsSearcher
     attr_reader :query, :scope
 
-    def initialize(query, scope = Eve::Blueprint.all)
+    def initialize(query, scope = Eve::Type.all)
       @query = query
       @scope = scope
+    end
+
+    def search
+      if query.present? && query.length > 2
+        ids = Eve::Type.search(query, options).map(&:id)
+
+        if ids.present?
+          scope.where(id: ids)
+        else
+          Eve::Type.none
+        end
+      else
+        scope.where(is_blueprint: false, is_manufacturing_item: true)
+      end
+    end
+
+    private
+
+    def options
+      {
+        fields: [
+          :name_en,
+          :name_de,
+          :name_fr,
+          :name_ja,
+          :name_ru,
+          :name_zh,
+          :name_ko],
+        match: :word_start,
+        where: {
+          published: true,
+          is_blueprint: false,
+          is_manufacturing_item: true
+        },
+        load: false,
+        misspellings: {below: 5}
+      }
     end
   end
 end
