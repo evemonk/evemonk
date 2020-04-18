3078052edd00755682f21654bac7e02d679c11ba
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-19 02:34:22 +0300

    CharacterOrder#type and #region

--- app/models/character_order.rb
+++ app/models/character_order.rb
@@ -4,4 +4,16 @@ class CharacterOrder < ApplicationRecord
   has_paper_trail
 
   belongs_to :character
+
+  belongs_to :type,
+    class_name: "Eve::Type",
+    primary_key: "type_id",
+    foreign_key: "type_id",
+    optional: true
+
+  belongs_to :region,
+    class_name: "Eve::Region",
+    primary_key: "region_id",
+    foreign_key: "region_id",
+    optional: true
 end
--- spec/models/character_order_spec.rb
+++ spec/models/character_order_spec.rb
@@ -8,4 +8,8 @@ describe CharacterOrder do
   it { should respond_to(:versions) }
 
   it { should belong_to(:character) }
+
+  it { should belong_to(:type).class_name("Eve::Type").with_primary_key("type_id").with_foreign_key("type_id").optional(true) }
+
+  it { should belong_to(:region).class_name("Eve::Region").with_primary_key("region_id").with_foreign_key("region_id").optional(true) }
 end
