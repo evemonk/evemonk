34a0c821898d818e0d95f8d289d2bca69bd3cfab
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-20 23:47:35 +0300

    Add indexes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_203815) do
+ActiveRecord::Schema.define(version: 2020_04_20_204632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -346,6 +346,8 @@ ActiveRecord::Schema.define(version: 2020_04_20_203815) do
     t.bigint "type_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["blueprint_id"], name: "index_eve_blueprint_manufacturing_skills_on_blueprint_id"
+    t.index ["type_id"], name: "index_eve_blueprint_manufacturing_skills_on_type_id"
   end
 
   create_table "eve_categories", force: :cascade do |t|
