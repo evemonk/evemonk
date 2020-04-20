5913eaf4f83911d965477cac4a0eb9f8f81bc1ce
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-20 15:04:27 +0300

    Add index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_115447) do
+ActiveRecord::Schema.define(version: 2020_04_20_120303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -707,6 +707,7 @@ ActiveRecord::Schema.define(version: 2020_04_20_115447) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "star_id"
     t.index ["star_id"], name: "index_eve_stars_on_star_id", unique: true
+    t.index ["type_id"], name: "index_eve_stars_on_type_id"
   end
 
   create_table "eve_stations", force: :cascade do |t|
