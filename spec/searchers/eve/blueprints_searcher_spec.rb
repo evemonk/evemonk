23f39a80c9395344b49aa71ac5ab8cb3207e87c1
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-21 14:27:58 +0300

    Fix

--- spec/searchers/eve/blueprints_searcher_spec.rb
+++ spec/searchers/eve/blueprints_searcher_spec.rb
@@ -7,25 +7,28 @@ describe Eve::BlueprintsSearcher do
     context "when query is empty" do
       let!(:blueprint1) do
         Eve::Blueprint.create!(attributes_for(:eve_type,
-                                              type_id: 1,
-                                              is_blueprint: true))
+          type_id: 1,
+          is_blueprint: true))
       end
 
       let!(:blueprint2) do
         Eve::Blueprint.create!(attributes_for(:eve_type,
-                                              type_id: 2,
-                                              is_blueprint: true))
+          type_id: 2,
+          is_blueprint: true))
       end
 
       let!(:blueprint3) do
         Eve::Blueprint.create!(attributes_for(:eve_type,
-                                              type_id: 3,
-                                              is_blueprint: true))
+          type_id: 3,
+          is_blueprint: true))
       end
 
       subject { described_class.new("") }
 
       specify { expect(subject.search.order(:type_id)).to eq([blueprint1, blueprint2, blueprint3]) }
+    end
+
+    context "when query is present" do
     end
   end
 end
