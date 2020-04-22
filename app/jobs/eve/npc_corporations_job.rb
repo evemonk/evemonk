53a1e06f95a233d391e625ba8a3dcaece1fb25bf
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-23 02:07:29 +0300

    Eve::NpcCorporationsJob

---
+++ app/jobs/eve/npc_corporations_job.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Eve
+  class NpcCorporationsJob < ActiveJob::Base
+    queue_as :default
+
+    retry_on EveOnline::Exceptions::Timeout,
+      EveOnline::Exceptions::ServiceUnavailable,
+      EveOnline::Exceptions::BadGateway,
+      EveOnline::Exceptions::InternalServerError,
+      OpenSSL::SSL::SSLError,
+      Faraday::TimeoutError,
+      Faraday::ConnectionFailed
+
+    def perform
+      Eve::NpcCorporationsImporter.new.import
+    end
+  end
+end
