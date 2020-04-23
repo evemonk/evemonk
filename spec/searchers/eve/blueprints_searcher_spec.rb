8605755fcf9622c66e68ec708b565264caa9e887
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-24 02:04:56 +0300

    More specs

--- spec/searchers/eve/blueprints_searcher_spec.rb
+++ spec/searchers/eve/blueprints_searcher_spec.rb
@@ -8,20 +8,25 @@ describe Eve::BlueprintsSearcher do
       let!(:blueprint1) do
         Eve::Blueprint.create!(attributes_for(:eve_type,
           type_id: 1,
-          is_blueprint: true))
+          is_blueprint: true,
+          published: true))
       end
 
       let!(:blueprint2) do
         Eve::Blueprint.create!(attributes_for(:eve_type,
           type_id: 2,
-          is_blueprint: true))
+          is_blueprint: true,
+          published: true))
       end
 
       let!(:blueprint3) do
         Eve::Blueprint.create!(attributes_for(:eve_type,
           type_id: 3,
-          is_blueprint: true))
+          is_blueprint: true,
+          published: true))
       end
+
+      before { Eve::Type.reindex }
 
       subject { described_class.new("") }
 
@@ -29,6 +34,29 @@ describe Eve::BlueprintsSearcher do
     end
 
     context "when query is present" do
+      context "when search return empty list" do
+        before { Eve::Type.reindex }
+
+        subject { described_class.new("drake") }
+
+        specify { expect(subject.search).to eq(Eve::Blueprint.none) }
+      end
+
+      context "when search find blueprints" do
+        let!(:blueprint1) do
+          create(:eve_type,
+            type_id: 1,
+            name_en: "Drake",
+            is_blueprint: true,
+            published: true)
+        end
+
+        before { Eve::Type.reindex }
+
+        subject { described_class.new("drake") }
+
+        specify { expect(subject.search.to_a).to eq([Eve::Blueprint.first]) }
+      end
     end
   end
 end
