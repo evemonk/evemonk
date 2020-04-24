34404322cc4d03d3f853fd2d0ff4977edfb08c63
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-24 03:31:55 +0300

    Add classes

---
+++ app/services/sde/blueprints_invention_skills_importer.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Sde
+  class BlueprintsInventionSkillsImporter
+  end
+end
---
+++ app/services/sde/blueprints_manufacturing_materials_importer.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Sde
+  class BlueprintsManufacturingMaterialsImporter
+  end
+end
