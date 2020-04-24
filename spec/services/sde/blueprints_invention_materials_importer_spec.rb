5ded1a9f5b122d4fb0c4afc6f968792fe5a46432
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-25 01:13:46 +0300

    Add spec

--- spec/services/sde/blueprints_invention_materials_importer_spec.rb
+++ spec/services/sde/blueprints_invention_materials_importer_spec.rb
@@ -12,5 +12,65 @@ describe Sde::BlueprintsInventionMaterialsImporter do
   end
 
   describe "#import" do
+    let(:content) { double }
+
+    before { expect(File).to receive(:read).with(file).and_return(content) }
+
+    let(:key) { double }
+
+    let(:quantity) { double }
+
+    let(:type_id) { double }
+
+    let(:entry) do
+      {
+        "activities" => {
+          "invention" => {
+            "materials" => [
+              {
+                "quantity" => quantity,
+                "typeID" => type_id
+              }
+            ]
+          },
+        }
+      }
+    end
+
+    let(:entries) { {key => entry} }
+
+    before { expect(YAML).to receive(:safe_load).with(content).and_return(entries) }
+
+    let(:eve_blueprint) { instance_double(Eve::Blueprint, type_id: type_id) }
+
+    before { expect(Eve::Blueprint).to receive(:find_or_initialize_by).with(type_id: key).and_return(eve_blueprint) }
+
+    before { expect(eve_blueprint).to receive(:transaction).and_yield }
+
+    before do
+      #
+      # eve_blueprint.blueprint_invention_materials.destroy_all
+      #
+      expect(eve_blueprint).to receive(:blueprint_invention_materials) do
+        double.tap do |a|
+          expect(a).to receive(:destroy_all)
+        end
+      end
+    end
+
+    before do
+      #
+      # Eve::BlueprintInventionMaterial.create!(blueprint_id: eve_blueprint.type_id,
+      #                                         quantity: invention_material["quantity"],
+      #                                         type_id: invention_material["typeID"])
+      #
+      expect(Eve::BlueprintInventionMaterial).to receive(:create!).with(blueprint_id: type_id,
+                                                                        quantity: quantity,
+                                                                        type_id: type_id)
+    end
+
+    before { expect(eve_blueprint).to receive(:save!) }
+
+    specify { expect { subject.import }.not_to raise_error }
   end
 end
