300b2a5ad47d35dc0f99aca45c6a0f8c7712e919
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-26 16:48:18 +0300

    OrdersController

---
+++ spec/controllers/orders_controller_spec.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe OrdersController do
+  it { should be_a(ApplicationController) }
+
+  it { should use_before_action(:authenticate_user!) }
+
+  describe "#index" do
+    context "when user signed in" do
+      let(:user) { create(:user) }
+
+      before { sign_in(user) }
+
+      before { expect(subject).to receive(:current_user_locale) }
+
+      let(:character) { instance_double(Character) }
+
+      before do
+        #
+        # subject.current_user
+        #        .characters
+        #        .includes(:alliance, :corporation)
+        #        .find_by!(character_id: params[:character_id])
+        #        .decorate
+        #
+        expect(subject).to receive(:current_user) do
+          double.tap do |a|
+            expect(a).to receive(:characters) do
+              double.tap do |b|
+                expect(b).to receive(:includes).with(:alliance, :corporation) do
+                  double.tap do |c|
+                    expect(c).to receive(:find_by!).with(character_id: "1") do
+                      double.tap do |d|
+                        expect(d).to receive(:decorate).and_return(character)
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+
+      before do
+        #
+        # character.character_orders
+        #          .where(is_buy_order: [nil, false], is_corporation: false)
+        #          .includes(:type)
+        #          .page(params[:page])
+        #          .decorate
+        #
+        expect(character).to receive(:character_orders) do
+          double.tap do |a|
+            expect(a).to receive(:where).with(is_buy_order: [nil, false], is_corporation: false) do
+              double.tap do |b|
+                expect(b).to receive(:includes).with(:type) do
+                  double.tap do |c|
+                    expect(c).to receive(:page).with("2") do
+                      double.tap do |d|
+                        expect(d).to receive(:decorate)
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+
+      before { get :index, params: {character_id: "1", page: "2"} }
+
+      it { should respond_with(:ok) }
+
+      it { should render_template(:index) }
+    end
+
+    context "when user not signed in" do
+      before { get :index, params: {character_id: "1"} }
+
+      it { should redirect_to(new_user_session_path) }
+    end
+  end
+end
