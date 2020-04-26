14c36e08676c0aa022fff14bd0042570490005ea
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-26 19:05:26 +0300

    Fix

--- app/services/sde/blueprints_importer.rb
+++ app/services/sde/blueprints_importer.rb
@@ -12,7 +12,7 @@ module Sde
       entries = YAML.safe_load(File.read(file))
 
       entries.each_pair do |key, hash|
-        eve_blueprint = Eve::Blueprint.find_or_initialize_by(type_id: key)
+        eve_type = Eve::Type.find_or_initialize_by(type_id: key)
 
         copying_time = hash.dig("activities", "copying", "time")
         manufacturing_time = hash.dig("activities", "manufacturing", "time")
@@ -20,16 +20,16 @@ module Sde
         research_time_time = hash.dig("activities", "research_time", "time")
         invention_time = hash.dig("activities", "invention", "time")
 
-        eve_blueprint.transaction do
-          eve_blueprint.assign_attributes(copying_time: copying_time,
-                                          manufacturing_time: manufacturing_time,
-                                          research_material_time: research_material_time,
-                                          research_time_time: research_time_time,
-                                          invention_time: invention_time,
-                                          max_production_limit: hash["maxProductionLimit"],
-                                          is_blueprint: true)
+        eve_type.transaction do
+          eve_type.assign_attributes(copying_time: copying_time,
+                                     manufacturing_time: manufacturing_time,
+                                     research_material_time: research_material_time,
+                                     research_time_time: research_time_time,
+                                     invention_time: invention_time,
+                                     max_production_limit: hash["maxProductionLimit"],
+                                     is_blueprint: true)
 
-          eve_blueprint.save!
+          eve_type.save!
         end
       end
     end
