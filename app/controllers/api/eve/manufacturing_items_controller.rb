7d8fefbf3baca2bb2504de94af5075b6a5abed62
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-26 17:09:33 +0300

    ManufacturingItemsController

--- app/controllers/api/eve/manufacturing_items_controller.rb
+++ app/controllers/api/eve/manufacturing_items_controller.rb
@@ -6,7 +6,10 @@ module Api
       skip_before_action :authenticate
 
       def index
-        @items = ::Eve::ManufacturingItemsSearcher.new(params[:q]).search.page(params[:page]).decorate
+        @items = ::Eve::ManufacturingItemsSearcher.new(params[:q])
+          .search
+          .page(params[:page])
+          .decorate
       end
     end
   end
