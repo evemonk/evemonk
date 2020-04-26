562c81357acb8335d6c0d5128c6f0b0f6c427d25
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-26 17:41:15 +0300

    More specs

--- spec/searchers/eve/manufacturing_items_searcher_spec.rb
+++ spec/searchers/eve/manufacturing_items_searcher_spec.rb
@@ -26,4 +26,61 @@ describe Eve::ManufacturingItemsSearcher do
       its(:scope) { should eq(scope) }
     end
   end
+
+  describe "#search" do
+    context "when query is empty" do
+      let!(:type1) do
+        create(:eve_type,
+          type_id: 1,
+          is_manufacturing_item: true,
+          published: true)
+      end
+
+      let!(:type2) do
+        create(:eve_type,
+          type_id: 2,
+          is_manufacturing_item: true,
+          published: true)
+      end
+
+      let!(:type3) do
+        create(:eve_type,
+          type_id: 3,
+          is_manufacturing_item: true,
+          published: true)
+      end
+
+      before { Eve::Type.reindex }
+
+      subject { described_class.new("") }
+
+      specify { expect(subject.search.order(:type_id)).to eq([type1, type2, type3]) }
+    end
+
+    context "when query is present" do
+      context "when search return empty list" do
+        before { Eve::Type.reindex }
+
+        subject { described_class.new("drake") }
+
+        specify { expect(subject.search).to eq(Eve::Type.none) }
+      end
+
+      context "when search find items" do
+        let!(:type1) do
+          create(:eve_type,
+                 type_id: 1,
+                 name_en: "Drake",
+                 is_manufacturing_item: true,
+                 published: true)
+        end
+
+        before { Eve::Type.reindex }
+
+        subject { described_class.new("drake") }
+
+        specify { expect(subject.search.to_a).to eq([Eve::Type.first]) }
+      end
+    end
+  end
 end
