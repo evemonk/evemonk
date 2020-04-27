3c2668be2a337c6d59f347008750207c7828deb6
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-27 15:58:03 +0300

    Update

--- app/importers/character_assets_importer.rb
+++ app/importers/character_assets_importer.rb
@@ -1,18 +1,45 @@
 # frozen_string_literal: true
 
 class CharacterAssetsImporter < CharacterBaseImporter
+  attr_reader :page
+
+  def initialize(character_id, page = 1)
+    super(character_id)
+
+    @page = page
+  end
+
   def update!
     refresh_character_access_token
 
     esi = EveOnline::ESI::CharacterAssets.new(character_id: character.character_id,
-                                              token: character.access_token)
+                                              token: character.access_token,
+                                              page: page)
 
     return unless character_scope_present?(esi.scope)
 
-    character.character_assets.destroy_all
+    destroy_old_character_assets(page)
 
     esi.assets.each do |asset|
       character.character_assets.create!(asset.as_json)
+    end
+
+    import_other_pages(esi.total_pages)
+  end
+
+  private
+
+  def destroy_old_character_assets(page)
+    return if page != 1
+
+    character.character_assets.destroy_all
+  end
+
+  def import_other_pages(total_pages)
+    return if page != 1 || total_pages == 1
+
+    (2..total_pages).each do |next_page|
+      CharacterAssetsJob.perform_later(character_id, next_page)
     end
   end
 end
--- spec/importers/character_assets_importer_spec.rb
+++ spec/importers/character_assets_importer_spec.rb
@@ -5,9 +5,27 @@ require "rails_helper"
 describe CharacterAssetsImporter do
   let(:character_id) { double }
 
-  subject { described_class.new(character_id) }
+  let(:page) { double }
+
+  subject { described_class.new(character_id, page) }
 
   it { should be_a(CharacterBaseImporter) }
+
+  describe "#initialize" do
+    context "when page not present" do
+      subject { described_class.new(character_id) }
+
+      its(:character_id) { should eq(character_id) }
+
+      its(:page) { should eq(1) }
+    end
+
+    context "when page is present" do
+      its(:character_id) { should eq(character_id) }
+
+      its(:page) { should eq(page) }
+    end
+  end
 
   describe "#update!" do
     context "when scope present" do
@@ -22,30 +40,35 @@ describe CharacterAssetsImporter do
           scopes: "esi-assets.read_assets.v1")
       end
 
-      before { expect(subject).to receive(:character).and_return(character).exactly(5).times }
+      before { expect(subject).to receive(:character).and_return(character).exactly(4).times }
 
       let(:json) { double }
 
       let(:asset) { instance_double(EveOnline::ESI::Models::Asset, as_json: json) }
 
+      let(:total_pages) { double }
+
       let(:esi) do
         instance_double(EveOnline::ESI::CharacterAssets,
           assets: [asset],
-          scope: "esi-assets.read_assets.v1")
+          scope: "esi-assets.read_assets.v1",
+          total_pages: total_pages)
       end
 
-      before { expect(EveOnline::ESI::CharacterAssets).to receive(:new).with(character_id: character_id, token: access_token).and_return(esi) }
+      before { expect(EveOnline::ESI::CharacterAssets).to receive(:new).with(character_id: character_id, token: access_token, page: page).and_return(esi) }
 
-      before do
-        #
-        # character.character_assets.destroy_all
-        #
-        expect(character).to receive(:character_assets) do
-          double.tap do |a|
-            expect(a).to receive(:destroy_all)
-          end
-        end
-      end
+      before { expect(subject).to receive(:destroy_old_character_assets).with(page) }
+
+      # before do
+      #   #
+      #   # character.character_assets.destroy_all
+      #   #
+      #   expect(character).to receive(:character_assets) do
+      #     double.tap do |a|
+      #       expect(a).to receive(:destroy_all)
+      #     end
+      #   end
+      # end
 
       before do
         #
@@ -57,6 +80,8 @@ describe CharacterAssetsImporter do
           end
         end
       end
+
+      before { expect(subject).to receive(:import_other_pages).with(total_pages) }
 
       specify { expect { subject.update! }.not_to raise_error }
     end
@@ -80,7 +105,7 @@ describe CharacterAssetsImporter do
           scope: "esi-assets.read_assets.v1")
       end
 
-      before { expect(EveOnline::ESI::CharacterAssets).to receive(:new).with(character_id: character_id, token: access_token).and_return(esi) }
+      before { expect(EveOnline::ESI::CharacterAssets).to receive(:new).with(character_id: character_id, token: access_token, page: page).and_return(esi) }
 
       before { expect(character).not_to receive(:character_assets) }
 
