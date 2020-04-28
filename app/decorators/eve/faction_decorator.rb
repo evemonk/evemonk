ffdc9536134654907b0a28fbdfac62201c091fe9
Igor Zubkov <igor.zubkov@gmail.com>
2020-04-28 20:58:04 +0300

    Fix eve faction icons

--- app/decorators/eve/faction_decorator.rb
+++ app/decorators/eve/faction_decorator.rb
@@ -10,15 +10,15 @@ module Eve
     end
 
     def icon_tiny
-      "#{imageproxy_url}https://images.evetech.net/alliances/#{faction_id}/logo?size=32"
+      "#{imageproxy_url}https://images.evetech.net/corporations/#{faction_id}/logo?size=32"
     end
 
     def icon_small
-      "#{imageproxy_url}https://images.evetech.net/alliances/#{faction_id}/logo?size=64"
+      "#{imageproxy_url}https://images.evetech.net/corporations/#{faction_id}/logo?size=64"
     end
 
     def icon_medium
-      "#{imageproxy_url}https://images.evetech.net/alliances/#{faction_id}/logo?size=128"
+      "#{imageproxy_url}https://images.evetech.net/corporations/#{faction_id}/logo?size=128"
     end
   end
 end
