5d1b7096296165a15378f9ebf5c236de45511ac8
Igor Zubkov <igor.zubkov@gmail.com>
2020-05-03 19:25:16 +0300

    More specs

--- spec/importers/character_wallet_transactions_importer_spec.rb
+++ spec/importers/character_wallet_transactions_importer_spec.rb
@@ -11,7 +11,54 @@ describe CharacterWalletTransactionsImporter do
 
   describe "#update!" do
     context "when scope present" do
+      before { expect(subject).to receive(:refresh_character_access_token) }
 
+      let(:access_token) { double }
+
+      let(:character) do
+        instance_double(Character,
+          character_id: character_id,
+          access_token: access_token,
+          scopes: "esi-wallet.read_character_wallet.v1")
+      end
+
+      before { expect(subject).to receive(:character).and_return(character).exactly(4).times }
+
+      let(:transaction_id) { double }
+
+      let(:json) { double }
+
+      let(:wallet_transaction) do
+        instance_double(EveOnline::ESI::Models::WalletTransaction,
+          transaction_id: transaction_id,
+          as_json: json)
+      end
+
+      let(:esi) do
+        instance_double(EveOnline::ESI::CharacterWalletTransactions,
+          wallet_transactions: [wallet_transaction],
+          scope: "esi-wallet.read_character_wallet.v1")
+      end
+
+      before { expect(EveOnline::ESI::CharacterWalletTransactions).to receive(:new).with(character_id: character_id, token: access_token).and_return(esi) }
+
+      let(:character_wallet_transaction) { instance_double(WalletTransaction) }
+
+      before do
+        #
+        # character.wallet_transactions.find_or_initialize_by(transaction_id: wallet_transaction.transaction_id)
+        #
+        expect(character).to receive(:wallet_transactions) do
+          double.tap do |a|
+            expect(a).to receive(:find_or_initialize_by).with(transaction_id: transaction_id)
+              .and_return(character_wallet_transaction)
+          end
+        end
+      end
+
+      before { expect(character_wallet_transaction).to receive(:update!).with(json) }
+
+      specify { expect { subject.update! }.not_to raise_error }
     end
 
     context "when scope not present" do
