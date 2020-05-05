49f7d117a4be6e5e5b5f24fcdf412802ad57befe
Igor Zubkov <igor.zubkov@gmail.com>
2020-05-05 21:03:42 +0300

    LocationableDecorator#location_formatted

--- app/decorators/concerns/locationable_decorator.rb
+++ app/decorators/concerns/locationable_decorator.rb
@@ -3,7 +3,10 @@
 module LocationableDecorator
   include ActiveSupport::Concern
 
-  # def location_formatted
-  #   "Aloha"
-  # end
+  def location_formatted
+    case location
+    when Eve::StationDecorator
+      "#{location.name} (#{location.system.security_status})"
+    end
+  end
 end
