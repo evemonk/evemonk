8a15d617ecdafc7b7895a43fc61daf35398fa534
Igor Zubkov <igor.zubkov@gmail.com>
2020-05-11 02:50:28 +0300

    Update

--- app/models/industry_job.rb
+++ app/models/industry_job.rb
@@ -2,4 +2,45 @@
 
 class IndustryJob < ApplicationRecord
   belongs_to :character
+
+  belongs_to :product_type,
+    class_name: "Eve::Type",
+    primary_key: "type_id",
+    foreign_key: "product_type_id",
+    optional: true
+
+  belongs_to :blueprint_type,
+    class_name: "Eve::Type",
+    primary_key: "type_id",
+    foreign_key: "blueprint_type_id",
+    optional: true
 end
+
+# create_table "industry_jobs", force: :cascade do |t|
+#   t.bigint "character_id", null: false
+#   t.bigint "activity_id"
+#   t.bigint "blueprint_id"
+#   t.bigint "blueprint_location_id"
+#   t.bigint "blueprint_type_id"
+#   t.bigint "completed_character_id"
+#   t.datetime "completed_date"
+#   t.float "cost"
+#   t.integer "duration"
+#   t.datetime "end_date"
+#   t.bigint "facility_id"
+#   t.bigint "installer_id"
+#   t.bigint "job_id"
+#   t.integer "licensed_runs"
+#   t.bigint "output_location_id"
+#   t.datetime "pause_date"
+#   t.float "probability"
+#   t.integer "product_type_id"
+#   t.integer "runs"
+#   t.datetime "start_date"
+#   t.bigint "station_id"
+#   t.string "status"
+#   t.integer "successful_runs"
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+#   t.index ["character_id"], name: "index_industry_jobs_on_character_id"
+# end
