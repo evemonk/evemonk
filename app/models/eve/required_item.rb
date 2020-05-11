bec7de919d8b5e48c2c52dafb26275aa766839be
Igor Zubkov <igor.zubkov@gmail.com>
2020-05-11 13:54:57 +0300

    Fix

--- app/models/eve/required_item.rb
+++ app/models/eve/required_item.rb
@@ -3,7 +3,7 @@
 module Eve
   class RequiredItem < ApplicationRecord
     belongs_to :loyalty_store_offer,
-      primary_key: "eve_loyalty_store_offer_id"
+      foreign_key: "eve_loyalty_store_offer_id"
 
     belongs_to :type,
       primary_key: "type_id",
--- spec/models/eve/required_item_spec.rb
+++ spec/models/eve/required_item_spec.rb
@@ -7,7 +7,7 @@ describe Eve::RequiredItem do
 
   it { expect(described_class.table_name).to eq("eve_required_items") }
 
-  it { should belong_to(:loyalty_store_offer).with_primary_key("eve_loyalty_store_offer_id") }
+  it { should belong_to(:loyalty_store_offer).with_foreign_key("eve_loyalty_store_offer_id") }
 
   it { should belong_to(:type).with_primary_key("type_id").optional(true) }
 end
