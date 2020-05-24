a5ca8a9bd47d9a6ba15975f76c58598371a1aa5e
Igor Zubkov <igor.zubkov@gmail.com>
2020-05-24 20:40:17 +0300

    Configure HttpLog

---
+++ config/initializers/httplog.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+HttpLog.configure do |config|
+  # Enable or disable all logging
+  config.enabled = !!Rails.env.development?
+
+  # Tweak which parts of the HTTP cycle to log...
+  config.log_connect   = true
+  config.log_request   = true
+  config.log_headers   = true
+  config.log_data      = true
+  config.log_status    = true
+  config.log_response  = true
+  config.log_benchmark = true
+end
