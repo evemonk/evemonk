9e068f4325bf04df735af9f3e22716e54bedefc3
Igor Zubkov <igor.zubkov@gmail.com>
2020-05-26 15:51:42 +0300

    Fix

--- config/initializers/httplog.rb
+++ config/initializers/httplog.rb
@@ -5,11 +5,11 @@ HttpLog.configure do |config|
   config.enabled = !!Rails.env.development?
 
   # Tweak which parts of the HTTP cycle to log...
-  config.log_connect   = true
-  config.log_request   = true
-  config.log_headers   = true
-  config.log_data      = true
-  config.log_status    = true
-  config.log_response  = true
+  config.log_connect = true
+  config.log_request = true
+  config.log_headers = true
+  config.log_data = true
+  config.log_status = true
+  config.log_response = true
   config.log_benchmark = true
 end
