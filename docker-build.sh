c9b69d30330e8c910bd5ea5e1b74abe487e3e302
Igor Zubkov <igor.zubkov@gmail.com>
2020-05-30 03:02:42 +0300

    Cleanup

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -t "biow0lf/evemonk:$(cat .version)" . --no-cache
-docker push "biow0lf/evemonk:$(cat .version)"
+docker build -t biow0lf/evemonk:master . --no-cache
+docker push biow0lf/evemonk:master
