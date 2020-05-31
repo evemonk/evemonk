de818b9ad2060927c64ba3fd9880b98c181eefad
Igor Zubkov <igor.zubkov@gmail.com>
2020-05-31 22:48:28 +0300

    Integrate administrate

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,5 @@
+//= link administrate/application.css
+//= link administrate/application.js
 //= link_tree ../../../node_modules/bootstrap-icons/icons
 //= link_tree ../images
 //= link_directory ../stylesheets .css
