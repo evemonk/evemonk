535a6c3581ebbc5084ab8d4f93013995434c6bd6
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-02 23:32:59 +0300

    Eve::FactionDashboard

---
+++ app/dashboards/eve/faction_dashboard.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+require "administrate/base_dashboard"
+
+module Eve
+  class FactionDashboard < Administrate::BaseDashboard
+    ATTRIBUTE_TYPES = {
+      id: Field::Number,
+      faction_id: Field::Number,
+      corporation: Field::BelongsTo.with_options(class_name: "Eve::Corporation"),
+      description_en: Field::Text,
+      description_de: Field::Text,
+      description_fr: Field::Text,
+      description_ja: Field::Text,
+      description_ru: Field::Text,
+      description_zh: Field::Text,
+      description_ko: Field::Text,
+      name_en: Field::String,
+      name_de: Field::String,
+      name_fr: Field::String,
+      name_ja: Field::String,
+      name_ru: Field::String,
+      name_zh: Field::String,
+      name_ko: Field::String,
+      is_unique: Field::Boolean,
+      militia_corporation_id: Field::Number, # TODO: militia_corporation
+      size_factor: Field::Number,
+      solar_system_id: Field::Number, # TODO: solar_system
+      station_count: Field::Number,
+      station_system_count: Field::Number,
+      created_at: Field::DateTime,
+      updated_at: Field::DateTime
+    }.freeze
+
+    COLLECTION_ATTRIBUTES = [:id, :faction_id, :name_en].freeze
+
+    SHOW_PAGE_ATTRIBUTES = [
+      :id,
+      :faction_id,
+      :corporation,
+      :description_en,
+      :description_de,
+      :description_fr,
+      :description_ja,
+      :description_ru,
+      :description_zh,
+      :description_ko,
+      :name_en,
+      :name_de,
+      :name_fr,
+      :name_ja,
+      :name_ru,
+      :name_zh,
+      :name_ko,
+      :is_unique,
+      :militia_corporation_id, # TODO: militia_corporation
+      :size_factor,
+      :solar_system_id, # TODO: solar_system
+      :station_count,
+      :station_system_count,
+      :created_at,
+      :updated_at
+    ].freeze
+
+    FORM_ATTRIBUTES = [
+      :faction_id,
+      :corporation,
+      :description_en,
+      :description_de,
+      :description_fr,
+      :description_ja,
+      :description_ru,
+      :description_zh,
+      :description_ko,
+      :name_en,
+      :name_de,
+      :name_fr,
+      :name_ja,
+      :name_ru,
+      :name_zh,
+      :name_ko,
+      :is_unique,
+      :militia_corporation_id, # TODO: militia_corporation
+      :size_factor,
+      :solar_system_id, # TODO: solar_system
+      :station_count,
+      :station_system_count,
+    ].freeze
+
+    COLLECTION_FILTERS = {}.freeze
+
+    def display_resource(faction)
+      faction.name
+    end
+  end
+end
---
+++ spec/dashboards/eve/faction_dashboard_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::FactionDashboard do
+  it { should be_an(Administrate::BaseDashboard) }
+end
