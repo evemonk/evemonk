10b97defc7deebfe4a96421c73f11affc826505c
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-02 23:53:56 +0300

    Eve::CorporationDashboard

---
+++ app/dashboards/eve/corporation_dashboard.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+require "administrate/base_dashboard"
+
+module Eve
+  class CorporationDashboard < Administrate::BaseDashboard
+    ATTRIBUTE_TYPES = {
+      id: Field::Number,
+      corporation_id: Field::Number,
+      alliance: Field::BelongsTo.with_options(class_name: "Eve::Alliance"),
+      ceo: Field::BelongsTo.with_options(class_name: "Eve::Character"),
+      creator: Field::BelongsTo.with_options(class_name: "Eve::Character"),
+      date_founded: Field::DateTime,
+      description: Field::Text,
+      faction: Field::BelongsTo.with_options(class_name: "Eve::Faction"),
+      home_station_id: Field::Number, # TODO: home_station
+      member_count: Field::Number,
+      name: Field::String,
+      shares: Field::Number,
+      tax_rate: Field::Number,
+      ticker: Field::String,
+      corporation_url: Field::String,
+      war_eligible: Field::Boolean,
+      npc: Field::Boolean,
+      created_at: Field::DateTime,
+      updated_at: Field::DateTime
+    }.freeze
+
+    COLLECTION_ATTRIBUTES = [:id, :corporation_id, :name].freeze
+
+    SHOW_PAGE_ATTRIBUTES = [
+      :id,
+      :corporation_id,
+      :alliance,
+      :ceo,
+      :creator,
+      :date_founded,
+      :description,
+      :faction,
+      :home_station_id, # TODO: home_station
+      :member_count,
+      :name,
+      :shares,
+      :tax_rate,
+      :ticker,
+      :corporation_url,
+      :war_eligible,
+      :npc,
+      :created_at,
+      :updated_at
+    ].freeze
+
+    FORM_ATTRIBUTES = [
+      :corporation_id,
+      :alliance,
+      :ceo,
+      :creator,
+      :date_founded,
+      :description,
+      :faction,
+      :home_station_id, # TODO: home_station
+      :member_count,
+      :name,
+      :shares,
+      :tax_rate,
+      :ticker,
+      :corporation_url,
+      :war_eligible,
+      :npc
+    ].freeze
+
+    COLLECTION_FILTERS = {}.freeze
+
+    def display_resource(corporation)
+      corporation.name
+    end
+  end
+end
