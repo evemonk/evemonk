8b8b156cbf4e6c78d042178663c01188a4ecf8ee
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-03 01:46:32 +0300

    Eve::CategoryDashboard

---
+++ app/dashboards/eve/category_dashboard.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require "administrate/base_dashboard"
+
+module Eve
+  class CategoryDashboard < Administrate::BaseDashboard
+    ATTRIBUTE_TYPES = {
+      id: Field::Number,
+      category_id: Field::Number,
+      name_en: Field::String,
+      name_de: Field::String,
+      name_fr: Field::String,
+      name_ja: Field::String,
+      name_ru: Field::String,
+      name_zh: Field::String,
+      name_ko: Field::String,
+      published: Field::Boolean,
+      created_at: Field::DateTime,
+      updated_at: Field::DateTime
+    }.freeze
+
+    COLLECTION_ATTRIBUTES = [:id, :category_id, :name_en].freeze
+
+    SHOW_PAGE_ATTRIBUTES = [
+      :id,
+      :category_id,
+      :name_en,
+      :name_de,
+      :name_fr,
+      :name_ja,
+      :name_ru,
+      :name_zh,
+      :name_ko,
+      :published,
+      :created_at,
+      :updated_at
+    ].freeze
+
+    FORM_ATTRIBUTES = [
+      :category_id,
+      :name_en,
+      :name_de,
+      :name_fr,
+      :name_ja,
+      :name_ru,
+      :name_zh,
+      :name_ko,
+      :published
+    ].freeze
+
+    COLLECTION_FILTERS = {}.freeze
+
+    def display_resource(category)
+      category.name
+    end
+  end
+end
