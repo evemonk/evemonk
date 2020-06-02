bca1c2dd0da4c87ccf75bbc0731abad97fa621b6
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-02 23:01:40 +0300

    Eve::ConstellationDashboard

---
+++ app/dashboards/eve/constellation_dashboard.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require "administrate/base_dashboard"
+
+module Eve
+  class ConstellationDashboard < Administrate::BaseDashboard
+    ATTRIBUTE_TYPES = {
+      id: Field::Number,
+      constellation_id: Field::Number,
+      name_en: Field::String,
+      name_de: Field::String,
+      name_fr: Field::String,
+      name_ja: Field::String,
+      name_ru: Field::String,
+      name_zh: Field::String,
+      name_ko: Field::String,
+      region: Field::BelongsTo.with_options(class_name: "Eve::Region"),
+      created_at: Field::DateTime,
+      updated_at: Field::DateTime
+    }.freeze
+
+    COLLECTION_ATTRIBUTES = [:id, :constellation_id, :name].freeze
+
+    SHOW_PAGE_ATTRIBUTES = [
+      :id,
+      :constellation_id,
+      :name_en,
+      :name_de,
+      :name_fr,
+      :name_ja,
+      :name_ru,
+      :name_zh,
+      :name_ko,
+      :region,
+      :created_at,
+      :updated_at
+    ].freeze
+
+    FORM_ATTRIBUTES = [
+      :id,
+      :constellation_id,
+      :name_en,
+      :name_de,
+      :name_fr,
+      :name_ja,
+      :name_ru,
+      :name_zh,
+      :name_ko,
+      :region
+    ].freeze
+
+    COLLECTION_FILTERS = {}.freeze
+
+    def display_resource(constellation)
+      constellation.name
+    end
+  end
+end
---
+++ spec/dashboards/eve/constellation_dashboard_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::ConstellationDashboard do
+  it { should be_an(Administrate::BaseDashboard) }
+end
