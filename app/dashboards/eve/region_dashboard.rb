7b6faf9e9959b51f1e8f2d33ad6c937ca40373bd
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-02 03:28:06 +0300

    Eve::RegionDashboard

---
+++ app/dashboards/eve/region_dashboard.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+require "administrate/base_dashboard"
+
+module Eve
+  class RegionDashboard < Administrate::BaseDashboard
+    ATTRIBUTE_TYPES = {
+      id: Field::Number,
+      region_id: Field::Number,
+      name_en: Field::String,
+      name_de: Field::String,
+      name_fr: Field::String,
+      name_ja: Field::String,
+      name_ru: Field::String,
+      name_zh: Field::String,
+      name_ko: Field::String,
+      description_en: Field::Text,
+      description_de: Field::Text,
+      description_fr: Field::Text,
+      description_ja: Field::Text,
+      description_ru: Field::Text,
+      description_zh: Field::Text,
+      description_ko: Field::Text,
+      created_at: Field::DateTime,
+      updated_at: Field::DateTime
+    }.freeze
+
+    COLLECTION_ATTRIBUTES = [:id, :region_id, :name_en].freeze
+
+    SHOW_PAGE_ATTRIBUTES = [
+      :id,
+      :region_id,
+      :name_en,
+      :name_de,
+      :name_fr,
+      :name_ja,
+      :name_ru,
+      :name_zh,
+      :name_ko,
+      :description_en,
+      :description_de,
+      :description_fr,
+      :description_ja,
+      :description_ru,
+      :description_zh,
+      :description_ko,
+      :created_at,
+      :updated_at
+    ].freeze
+
+    FORM_ATTRIBUTES = [
+      :region_id,
+      :name_en,
+      :name_de,
+      :name_fr,
+      :name_ja,
+      :name_ru,
+      :name_zh,
+      :name_ko,
+      :description_en,
+      :description_de,
+      :description_fr,
+      :description_ja,
+      :description_ru,
+      :description_zh,
+      :description_ko
+    ].freeze
+
+    COLLECTION_FILTERS = {}.freeze
+
+    def display_resource(region)
+      region.name
+    end
+  end
+end
