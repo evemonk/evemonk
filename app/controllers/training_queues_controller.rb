0d8ccf71d0a69da8a37e98a6d94d838b79a08517
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-07 22:00:03 +0300

    TrainingQueuesController

---
+++ app/controllers/training_queues_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class TrainingQueuesController < ApplicationController
+  def index
+    @character = current_user.characters
+      .includes(:alliance, :corporation)
+      .find_by!(character_id: params[:character_id])
+      .decorate
+
+    @skillqueues = @character.skillqueues
+      .order(:queue_position)
+      .where("finish_date > ?", Time.zone.now)
+  end
+end
