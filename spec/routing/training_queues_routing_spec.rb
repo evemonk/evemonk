95907949a80cf27bbd8d930838cb3d53d81320d2
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-07 21:51:20 +0300

    Add routes and route spec

---
+++ spec/routing/training_queues_routing_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe TrainingQueuesController do
+  describe "routing" do
+    it "should route GET /characters/90729314/training_queues to training_queues#index" do
+      expect(get: "/characters/90729314/training_queues").to route_to(
+        controller: "training_queues",
+        action: "index",
+        character_id: "90729314"
+      )
+    end
+  end
+end
