c839a659d8793ab914a0f2de39335c43b8e78973
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-09 17:00:21 +0300

    Enable extension pgcrypto

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_112741) do
+ActiveRecord::Schema.define(version: 2020_06_09_135403) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "character_assets", force: :cascade do |t|
