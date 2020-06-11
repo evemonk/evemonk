2fbbf1da13ced7ab9a26e4d770855be9f28621b9
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-11 14:23:26 +0300

    Add spec

--- spec/dashboards/character_dashboard_spec.rb
+++ spec/dashboards/character_dashboard_spec.rb
@@ -4,4 +4,10 @@ require "rails_helper"
 
 describe CharacterDashboard do
   it { should be_an(Administrate::BaseDashboard) }
+
+  describe "#display_resource" do
+    let(:character) { build(:character, name: "Johnn Dillinger") }
+
+    specify { expect(subject.display_resource(character)).to eq("Johnn Dillinger") }
+  end
 end
