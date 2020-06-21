d1f75338a805eae6f319874375a7e5f347c7cad5
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-21 20:37:09 +0300

    Update

--- app/importers/eve/region_contracts_importer.rb
+++ app/importers/eve/region_contracts_importer.rb
@@ -10,31 +10,28 @@ module Eve
     end
 
     def import
+      # ActiveRecord::Base.transaction do
+      #   esi = EveOnline::ESI::PublicContracts.new(region_id: region_id,
+      #                                             page: page)
+      #
+      #   etag = Eve::Etag.find_or_initialize_by(url: esi.url)
+      #
+      #   esi.etag = etag.etag
+      #
+      #   return if esi.not_modified?
+      #
+      #   remove_all_contracts
+      #
+      #   import_new_contracts(esi)
+      #
+      #   import_other_pages(esi)
+      #
+      #   etag.update!(etag: esi.etag, body: esi.response)
+      # end
     end
 
-    # def import
-    #   ActiveRecord::Base.transaction do
-    #     esi = EveOnline::ESI::PublicContracts.new(region_id: region_id,
-    #                                               page: page)
-    #
-    #     etag = Eve::Etag.find_or_initialize_by(url: esi.url)
-    #
-    #     esi.etag = etag.etag
-    #
-    #     return if esi.not_modified?
-    #
-    #     remove_all_contracts
-    #
-    #     import_new_contracts(esi)
-    #
-    #     import_other_pages
-    #
-    #     etag.update!(etag: esi.etag, body: esi.response)
-    #   end
-    # end
-    #
-    # private
-    #
+    private
+
     # def region
     #   @region ||= Eve::Region.find_by!(region_id: region_id)
     # end
@@ -48,9 +45,13 @@ module Eve
     #     region.contracts.create!(contract.as_json)
     #   end
     # end
-    #
-    # def import_other_pages
-    #
-    # end
+
+    def import_other_pages(esi)
+      return if page != 1 || esi.total_pages == 1
+
+      (2..esi.total_pages).each do |next_page|
+        Eve::RegionContractsJob.perform_later(region_id, next_page)
+      end
+    end
   end
 end
