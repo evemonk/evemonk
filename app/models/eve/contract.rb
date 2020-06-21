10ad2b5b5cfcfce88a70813dfefc300ff2c7286a
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-21 14:43:02 +0300

    Eve::Contract#issuer and #issuer_corporation

--- app/models/eve/contract.rb
+++ app/models/eve/contract.rb
@@ -5,5 +5,15 @@ module Eve
     belongs_to :region,
       primary_key: "region_id",
       optional: true
+
+    belongs_to :issuer,
+      class_name: "Eve::Character",
+      primary_key: "character_id",
+      optional: true
+
+    belongs_to :issuer_corporation,
+      class_name: "Eve::Corporation",
+      primary_key: "corporation_id",
+      optional: true
   end
 end
--- spec/models/eve/contract_spec.rb
+++ spec/models/eve/contract_spec.rb
@@ -8,4 +8,8 @@ describe Eve::Contract do
   it { expect(described_class.table_name).to eq("eve_contracts") }
 
   it { should belong_to(:region).with_primary_key("region_id").optional(true) }
+
+  it { should belong_to(:issuer).class_name("Eve::Character").with_primary_key("character_id").optional(true) }
+
+  it { should belong_to(:issuer_corporation).class_name("Eve::Corporation").with_primary_key("corporation_id").optional(true) }
 end
