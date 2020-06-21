ca9d42167bf42e58becb176a1f17079becfef340
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-21 22:31:38 +0300

    More

--- app/graphql/types/eve_blueprint_type.rb
+++ app/graphql/types/eve_blueprint_type.rb
@@ -6,22 +6,23 @@ module Types
     field :name, GraphQL::Types::JSON, null: true
     field :description, GraphQL::Types::JSON, null: true
     field :capacity, Float, null: true
-
-
-    # t.bigint "graphic_id"
-    # t.bigint "group_id"
-    # t.bigint "icon_id"
-    # t.bigint "market_group_id"
-    # t.float "mass"
-    # t.float "packaged_volume"
-    # t.integer "portion_size"
-    # t.boolean "published"
-    # t.float "radius"
-    # t.float "volume"
-
-    # t.float "base_price"
-    # t.float "adjusted_price"
-    # t.float "average_price"
+    field :graphic_id, Integer, null: true
+    # field :graphic, Types::EveGraphicType, null: true
+    field :group_id, Integer, null: true
+    # field :group, Types::EveGroupType, null: true
+    field :icon_id, Integer, null: true
+    # field :icon, Types::EveIconType, null: true
+    field :market_group_id, Integer, null: true
+    # field :market_group, Types::EveMarketGroupType, null: true
+    field :mass, Float, null: true
+    field :packaged_volume, Float, null: true
+    field :portion_size, Integer, null: true
+    field :published, Boolean, null: true
+    field :radius, Float, null: true
+    field :volume, Float, null: true
+    field :base_price, Float, null: true
+    field :adjusted_price, Float, null: true
+    field :average_price, Float, null: true
 
     # t.bigint "copying_time"
     # t.integer "max_production_limit"
