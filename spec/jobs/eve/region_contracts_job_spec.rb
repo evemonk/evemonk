547503de3578cb27b04503816abde7fb7f9b3f8d
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-21 15:17:36 +0300

    Eve::RegionContractsJob

---
+++ spec/jobs/eve/region_contracts_job_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::RegionContractsJob do
+  it { expect(described_class.queue_name).to eq("default") }
+
+  describe "#perform" do
+    let(:region_id) { double }
+
+    let(:page) { double }
+
+    before do
+      #
+      # Eve::RegionContractsImporter.new(region_id, page).import
+      #
+      expect(Eve::RegionContractsImporter).to receive(:new).with(region_id, page) do
+        double.tap do |a|
+          expect(a).to receive(:import)
+        end
+      end
+    end
+
+    specify { expect { subject.perform(region_id, page) }.not_to raise_error }
+  end
+end
