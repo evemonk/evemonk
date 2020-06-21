80b9497b4437bc9f3d9423a66530221b8db053d8
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-21 14:54:36 +0300

    Eve::AllRegionsContractsJob

---
+++ app/jobs/eve/all_regions_contracts_job.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Eve
+  class AllRegionsContractsJob < ActiveJob::Base
+    queue_as :default
+
+    retry_on EveOnline::Exceptions::Timeout,
+      EveOnline::Exceptions::ServiceUnavailable,
+      EveOnline::Exceptions::BadGateway,
+      EveOnline::Exceptions::InternalServerError,
+      OpenSSL::SSL::SSLError,
+      Faraday::TimeoutError,
+      Faraday::ConnectionFailed
+
+    def perform
+      Eve::AllRegionsContractsImporter.new.import
+    end
+  end
+end
