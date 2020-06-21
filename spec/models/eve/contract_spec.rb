6faec852f69c0215961b8c0a8bc73994c244e17a
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-21 14:28:04 +0300

    Fix typo

--- spec/models/eve/contract_spec.rb
+++ spec/models/eve/contract_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 describe Eve::Contract do
   it { should be_a(ApplicationRecord) }
 
-  it { expect(described_class.table_name).to eq("eve_characters") }
+  it { expect(described_class.table_name).to eq("eve_contracts") }
 
   it { should belong_to(:region).with_primary_key("region_id").optional(true) }
 end
