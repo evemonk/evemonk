b46cc6d1a56984cd808d744ea6ef343ff699686a
Igor Zubkov <igor.zubkov@gmail.com>
2020-06-22 04:35:02 +0300

    More

--- app/graphql/types/eve_station_type.rb
+++ app/graphql/types/eve_station_type.rb
@@ -4,21 +4,19 @@ module Types
   class EveStationType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: true
-
-    def id
-      object.type_id
-    end
-
-    # t.float "max_dockable_ship_volume"
-    # t.float "office_rental_cost"
+    field :max_dockable_ship_volume, Float, null: true
+    field :office_rental_cost, Float, null: true
     # t.bigint "owner"
-    # t.bigint "race_id"
-    # t.float "reprocessing_efficiency"
-    # t.float "reprocessing_stations_take"
-    # t.string "services", array: true
-    # t.bigint "station_id"
+    field :race_id, Integer, null: true
+    field :race, Types::EveRaceType, null: true
+    field :reprocessing_efficiency, Float, null: true
+    field :reprocessing_stations_take, Float, null: true
+    field :services, [String], null: true
     # t.bigint "system_id"
     # t.bigint "type_id"
 
+    def id
+      object.station_id
+    end
   end
 end
