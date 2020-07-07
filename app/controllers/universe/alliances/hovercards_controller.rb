c53cd6456650339f0a04743df43e87c42a61d9d9
Igor Zubkov <igor.zubkov@gmail.com>
2020-07-07 19:28:18 +0300

    Update anyway_config gem to 2.0.6

---
+++ app/controllers/universe/alliances/hovercards_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Universe
+  module Alliances
+    class HovercardsController < ApplicationController
+      skip_before_action :authenticate_user!
+
+      def show
+        @alliance = Eve::Alliance.find_by!(alliance_id: params[:alliance_id])
+          .decorate
+
+        render layout: false
+      end
+    end
+  end
+end
