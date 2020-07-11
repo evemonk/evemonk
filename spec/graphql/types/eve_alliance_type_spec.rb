cfa40476906063c6b53aac08740978469f486888
Igor Zubkov <igor.zubkov@gmail.com>
2020-07-11 04:19:51 +0300

    More specs

--- spec/graphql/types/eve_alliance_type_spec.rb
+++ spec/graphql/types/eve_alliance_type_spec.rb
@@ -4,18 +4,40 @@ require "rails_helper"
 
 describe Types::EveAllianceType do
   describe "alliances" do
+    before { travel_to Time.zone.now }
+
+    after { travel_back }
+
+    let(:date_founded1) { Time.zone.now }
+
+    let(:date_founded2) { Time.zone.now - 1.week }
+
+    let!(:create_corporation1) do
+      create(:eve_corporation,
+        corporation_id: 222)
+    end
+
+    let!(:create_corporation2) do
+      create(:eve_corporation,
+             corporation_id: 333)
+    end
+
     let!(:eve_alliance1) do
       create(:eve_alliance,
         alliance_id: 123,
         name: "Alliance 1",
-        ticker: "ALLIANCE1")
+        ticker: "ALLIANCE1",
+        date_founded: date_founded1,
+        creator_corporation: create_corporation1)
     end
 
     let!(:eve_alliance2) do
       create(:eve_alliance,
         alliance_id: 321,
         name: "Alliance 2",
-        ticker: "ALLIANCE2")
+        ticker: "ALLIANCE2",
+        date_founded: date_founded2,
+        creator_corporation: create_corporation2)
     end
 
     let(:query) do
@@ -25,6 +47,11 @@ describe Types::EveAllianceType do
             id
             name
             ticker
+            dateFounded
+            creatorCorporationId
+            creatorCorporation {
+              id
+            }
           }
         }
       )
@@ -38,12 +65,22 @@ describe Types::EveAllianceType do
           {
             "id" => "123",
             "name" => "Alliance 1",
-            "ticker" => "ALLIANCE1"
+            "ticker" => "ALLIANCE1",
+            "dateFounded" => Time.zone.now.iso8601,
+            "creatorCorporationId" => 222,
+            "creatorCorporation" => {
+              "id" => "222"
+            }
           },
           {
             "id" => "321",
             "name" => "Alliance 2",
-            "ticker" => "ALLIANCE2"
+            "ticker" => "ALLIANCE2",
+            "dateFounded" => (Time.zone.now - 1.week).iso8601,
+            "creatorCorporationId" => 333,
+            "creatorCorporation" => {
+              "id" => "333"
+            }
           }
         ]
       })
