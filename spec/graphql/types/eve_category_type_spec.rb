9c1dda54e84fb2b5f1aa2eab32f61967e32402df
Igor Zubkov <igor.zubkov@gmail.com>
2020-07-13 18:52:45 +0300

    More specs

--- spec/graphql/types/eve_category_type_spec.rb
+++ spec/graphql/types/eve_category_type_spec.rb
@@ -104,5 +104,63 @@ describe Types::EveCategoryType do
   end
 
   describe "get category by id" do
+    let!(:eve_group) do
+      create(:eve_group,
+        group_id: 1_111,
+        category: eve_category)
+    end
+
+    let!(:eve_category) do
+      create(:eve_category,
+        category_id: 123,
+        name_en: "EN: name 1",
+        name_de: "DE: name 1",
+        name_fr: "FR: name 1",
+        name_ja: "JA: name 1",
+        name_ru: "RU: name 1",
+        name_zh: "ZH: name 1",
+        name_ko: "KO: name 1",
+        published: true)
+    end
+
+    let(:query) do
+      %(
+        {
+          category(id: 123) {
+            id
+            name
+            published
+            groups {
+              id
+            }
+          }
+        }
+      )
+    end
+
+    let(:result) { EvemonkSchema.execute(query).as_json }
+
+    specify do
+      expect(result).to eq("data" => {
+        "category" => {
+          "id" => "123",
+          "name" => {
+            "en" => "EN: name 1",
+            "de" => "DE: name 1",
+            "fr" => "FR: name 1",
+            "ja" => "JA: name 1",
+            "ru" => "RU: name 1",
+            "zh" => "ZH: name 1",
+            "ko" => "KO: name 1"
+          },
+          "published" => true,
+          "groups" => [
+            {
+              "id" => "1111"
+            }
+          ]
+        }
+      })
+    end
   end
 end
