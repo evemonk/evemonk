f19152d1720136d173b59290b0d451a58da7e956
Igor Zubkov <igor.zubkov@gmail.com>
2020-07-13 03:00:06 +0300

    More specs

--- spec/graphql/types/eve_bloodline_type_spec.rb
+++ spec/graphql/types/eve_bloodline_type_spec.rb
@@ -229,5 +229,134 @@ describe Types::EveBloodlineType do
   end
 
   describe "get bloodline by id" do
+    let!(:eve_corporation) do
+      create(:eve_corporation,
+        corporation_id: 500)
+    end
+
+    let!(:eve_race) do
+      create(:eve_race,
+        race_id: 4)
+    end
+
+    let!(:eve_ship_type) do
+      create(:eve_ship,
+        type_id: 1_001)
+    end
+
+    let!(:eve_ancestry) do
+      create(:eve_ancestry,
+        ancestry_id: 10_001,
+        bloodline: eve_bloodline)
+    end
+
+    let!(:eve_bloodline) do
+      create(:eve_bloodline,
+        bloodline_id: 123,
+        name_en: "EN: name 1",
+        name_de: "DE: name 1",
+        name_fr: "FR: name 1",
+        name_ja: "JA: name 1",
+        name_ru: "RU: name 1",
+        name_zh: "ZH: name 1",
+        name_ko: "KO: name 1",
+        description_en: "EN: description 1",
+        description_de: "DE: description 1",
+        description_fr: "FR: description 1",
+        description_ja: "JA: description 1",
+        description_ru: "RU: description 1",
+        description_zh: "ZH: description 1",
+        description_ko: "KO: description 1",
+        corporation: eve_corporation,
+        race: eve_race,
+        ship_type: eve_ship_type,
+        charisma: 1,
+        intelligence: 2,
+        memory: 3,
+        perception: 4,
+        willpower: 5)
+    end
+
+    let(:query) do
+      %(
+        {
+          bloodline(id: 123) {
+            id
+            name
+            description
+            corporationId
+            corporation {
+              id
+            }
+            raceId
+            race {
+              id
+            }
+            shipTypeId
+            shipType {
+              id
+            }
+            charisma
+            intelligence
+            memory
+            perception
+            willpower
+            ancestries {
+              id
+            }
+          }
+        }
+      )
+    end
+
+    let(:result) { EvemonkSchema.execute(query).as_json }
+
+    specify do
+      expect(result).to eq("data" => {
+        "bloodline" => {
+          "id" => "123",
+          "name" => {
+            "en" => "EN: name 1",
+            "de" => "DE: name 1",
+            "fr" => "FR: name 1",
+            "ja" => "JA: name 1",
+            "ru" => "RU: name 1",
+            "zh" => "ZH: name 1",
+            "ko" => "KO: name 1"
+          },
+          "description" => {
+            "en" => "EN: description 1",
+            "de" => "DE: description 1",
+            "fr" => "FR: description 1",
+            "ja" => "JA: description 1",
+            "ru" => "RU: description 1",
+            "zh" => "ZH: description 1",
+            "ko" => "KO: description 1"
+          },
+          "corporationId" => 500,
+          "corporation" => {
+            "id" => "500"
+          },
+          "raceId" => 4,
+          "race" => {
+            "id" => "4"
+          },
+          "shipTypeId" => 1_001,
+          "shipType" => {
+            "id" => "1001"
+          },
+          "charisma" => 1,
+          "intelligence" => 2,
+          "memory" => 3,
+          "perception" => 4,
+          "willpower" => 5,
+          "ancestries" => [
+            {
+              "id" => "10001"
+            }
+          ]
+        }
+      })
+    end
   end
 end
