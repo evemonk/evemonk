8ebe3c2ab0ae131ebf85af6a491cc84506ead03a
Igor Zubkov <igor.zubkov@gmail.com>
2020-07-15 21:05:29 +0300

    More specs

--- spec/graphql/types/eve_character_type_spec.rb
+++ spec/graphql/types/eve_character_type_spec.rb
@@ -225,5 +225,138 @@ describe Types::EveCharacterType do
   end
 
   describe "get character by id" do
+    before { travel_to Time.zone.now }
+
+    after { travel_back }
+
+    let!(:birthday) { Time.zone.now }
+
+    let!(:eve_alliance) do
+      create(:eve_alliance,
+        alliance_id: 1_111)
+    end
+
+    let!(:eve_corporation) do
+      create(:eve_corporation,
+        corporation_id: 1_333,
+        alliance: eve_alliance)
+    end
+
+    let!(:eve_ancestry) do
+      create(:eve_ancestry,
+        ancestry_id: 128)
+    end
+
+    let!(:eve_bloodline) do
+      create(:eve_bloodline,
+        bloodline_id: 512)
+    end
+
+    let!(:eve_faction) do
+      create(:eve_faction,
+        faction_id: 2_048)
+    end
+
+    let!(:eve_race) do
+      create(:eve_race,
+        race_id: 5_000)
+    end
+
+    let!(:eve_character) do
+      create(:eve_character,
+        character_id: 123,
+        ancestry: eve_ancestry,
+        birthday: birthday,
+        bloodline: eve_bloodline,
+        alliance: eve_alliance,
+        corporation: eve_corporation,
+        description: "Description 1",
+        faction: eve_faction,
+        gender: "male",
+        name: "Name 1",
+        race: eve_race,
+        security_status: 3.5,
+        title: "Title 1")
+    end
+
+    let(:query) do
+      %(
+        {
+          character(id: 123) {
+            id
+            allianceId
+            alliance {
+              id
+            }
+            ancestryId
+            ancestry {
+              id
+            }
+            birthday
+            bloodlineId
+            bloodline {
+              id
+            }
+            corporationId
+            corporation {
+              id
+            }
+            description
+            factionId
+            faction {
+              id
+            }
+            gender
+            name
+            raceId
+            race {
+              id
+            }
+            securityStatus
+            title
+          }
+        }
+      )
+    end
+
+    let(:result) { EvemonkSchema.execute(query).as_json }
+
+    specify do
+      expect(result).to eq("data" => {
+        "character" => {
+          "id" => "123",
+          "allianceId" => 1_111,
+          "alliance" => {
+            "id" => "1111"
+          },
+          "ancestryId" => 128,
+          "ancestry" => {
+            "id" => "128"
+          },
+          "birthday" => birthday.iso8601,
+          "bloodlineId" => 512,
+          "bloodline" => {
+            "id" => "512"
+          },
+          "corporationId" => 1_333,
+          "corporation" => {
+            "id" => "1333"
+          },
+          "description" => "Description 1",
+          "factionId" => 2_048,
+          "faction" => {
+            "id" => "2048"
+          },
+          "gender" => "male",
+          "name" => "Name 1",
+          "raceId" => 5_000,
+          "race" => {
+            "id" => "5000"
+          },
+          "securityStatus" => 3.5,
+          "title" => "Title 1"
+        }
+      })
+    end
   end
 end
