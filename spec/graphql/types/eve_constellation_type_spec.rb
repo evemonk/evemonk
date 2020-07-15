ccd263a25b557bdd63e912cdd34db5c5be7e8a99
Igor Zubkov <igor.zubkov@gmail.com>
2020-07-16 02:59:28 +0300

    More specs

---
+++ spec/graphql/types/eve_constellation_type_spec.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Types::EveConstellationType do
+  describe "get constellations" do
+    let!(:eve_constellation1) do
+      create(:eve_constellation,
+        constellation_id: 123,
+        name_en: "EN: name 1",
+        name_de: "DE: name 1",
+        name_fr: "FR: name 1",
+        name_ja: "JA: name 1",
+        name_ru: "RU: name 1",
+        name_zh: "ZH: name 1",
+        name_ko: "KO: name 1")
+    end
+
+    let!(:eve_constellation2) do
+      create(:eve_constellation,
+        constellation_id: 321,
+        name_en: "EN: name 2",
+        name_de: "DE: name 2",
+        name_fr: "FR: name 2",
+        name_ja: "JA: name 2",
+        name_ru: "RU: name 2",
+        name_zh: "ZH: name 2",
+        name_ko: "KO: name 2")
+    end
+
+    let(:query) do
+      %(
+        {
+          constellations {
+            id
+            name
+            regionId
+            region {
+              id
+            }
+            systems {
+              id
+            }
+          }
+        }
+      )
+    end
+
+    let(:result) { EvemonkSchema.execute(query).as_json }
+
+    specify do
+      expect(result).to eq("data" => {
+        "constellations" => [
+          {
+            "id" => "123",
+            "name" => {
+              "en" => "EN: name 1",
+              "de" => "DE: name 1",
+              "fr" => "FR: name 1",
+              "ja" => "JA: name 1",
+              "ru" => "RU: name 1",
+              "zh" => "ZH: name 1",
+              "ko" => "KO: name 1"
+            }
+          },
+          {
+            "id" => "321",
+            "name" => {
+              "en" => "EN: name 2",
+              "de" => "DE: name 2",
+              "fr" => "FR: name 2",
+              "ja" => "JA: name 2",
+              "ru" => "RU: name 2",
+              "zh" => "ZH: name 2",
+              "ko" => "KO: name 2"
+            }
+          }
+        ]
+      })
+    end
+  end
+
+  describe "get constellation by id" do
+  end
+end
