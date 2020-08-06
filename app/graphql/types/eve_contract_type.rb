62ed57e825acf1852becb6f59663fb7d093a18aa
Igor Zubkov <igor.zubkov@gmail.com>
2020-08-06 22:12:09 +0300

    Update

--- app/graphql/types/eve_contract_type.rb
+++ app/graphql/types/eve_contract_type.rb
@@ -3,25 +3,25 @@
 module Types
   class EveContractType < Types::BaseObject
     field :id, ID, null: false
-    field :buyout, Float, null: true
-    field :collateral, Float, null: true
-    field :date_expired, GraphQL::Types::ISO8601DateTime, null: true
-    field :date_issued, GraphQL::Types::ISO8601DateTime, null: true
-    field :days_to_complete, Integer, null: true
-    # field :end_location_id, Integer, null: true
-    field :for_corporation, Boolean, null: true
-    field :issuer_corporation_id, Integer, null: true
-    field :issuer_corporation, Types::EveCorporationType, null: true
-    field :issuer_id, Integer, null: true
-    field :issuer, Types::EveCharacterType, null: true
-    field :price, Float, null: true
-    field :reward, Float, null: true
-    # field :start_location_id, Integer, null: true
-    field :title, String, null: true
-    field :kind, String, null: true
-    field :volume, Float, null: true
-    field :region_id, Integer, null: true
-    field :region, Types::EveRegionType, null: true
+    # field :buyout, Float, null: true
+    # field :collateral, Float, null: true
+    # field :date_expired, GraphQL::Types::ISO8601DateTime, null: true
+    # field :date_issued, GraphQL::Types::ISO8601DateTime, null: true
+    # field :days_to_complete, Integer, null: true
+    # # field :end_location_id, Integer, null: true
+    # field :for_corporation, Boolean, null: true
+    # field :issuer_corporation_id, Integer, null: true
+    # field :issuer_corporation, Types::EveCorporationType, null: true
+    # field :issuer_id, Integer, null: true
+    # field :issuer, Types::EveCharacterType, null: true
+    # field :price, Float, null: true
+    # field :reward, Float, null: true
+    # # field :start_location_id, Integer, null: true
+    # field :title, String, null: true
+    # field :kind, String, null: true
+    # field :volume, Float, null: true
+    # field :region_id, Integer, null: true
+    # field :region, Types::EveRegionType, null: true
 
     def id
       object.contract_id
