9330b0c7eca13b464eb5fbbfefa32068c7b58297
Igor Zubkov <igor.zubkov@gmail.com>
2020-08-09 00:28:49 +0300

    More specs

---
+++ spec/graphql/types/eve_blueprint_type_spec.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Types::EveBlueprintType do
+  describe "get blueprints" do
+    let!(:eve_blueprint1) do
+      create(:eve_type,
+        type_id: 4_001,
+        is_blueprint: true)
+    end
+
+    let!(:eve_blueprint2) do
+      create(:eve_type,
+        type_id: 5_001,
+        is_blueprint: true)
+    end
+
+    let(:query) do
+      %(
+        {
+          blueprints {
+            id
+          }
+        }
+      )
+    end
+
+    let(:result) { EvemonkSchema.execute(query).as_json }
+
+    specify do
+      expect(result).to eq("data" => {
+        "blueprints" => [
+          {
+            "id" => "4001"
+          },
+          {
+            "id" => "5001"
+          }
+        ]
+      })
+    end
+  end
+
+  describe "get blueprint by id" do
+    let!(:eve_blueprint) do
+      create(:eve_type,
+        type_id: 4_002,
+        is_blueprint: true)
+    end
+
+    let(:query) do
+      %(
+        {
+          blueprint(id: 4002) {
+            id
+          }
+        }
+      )
+    end
+
+    let(:result) { EvemonkSchema.execute(query).as_json }
+
+    specify do
+      expect(result).to eq("data" => {
+        "blueprint" => {
+          "id" => "4002"
+        }
+      })
+    end
+  end
+end
