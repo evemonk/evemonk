0cc5991f97687e697c5b94c816bf0bd4fb0cfd87
Igor Zubkov <igor.zubkov@gmail.com>
2020-08-09 01:27:39 +0300

    More specs

---
+++ spec/graphql/types/eve_system_type_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Types::EveSystemType do
+  describe "get systems" do
+    let!(:eve_system1) do
+      create(:eve_system,
+        system_id: 123)
+    end
+
+    let!(:eve_system2) do
+      create(:eve_system,
+        system_id: 321)
+    end
+
+    let(:query) do
+      %(
+        {
+          systems {
+            id
+          }
+        }
+      )
+    end
+
+    let(:result) { EvemonkSchema.execute(query).as_json }
+
+    specify do
+      expect(result).to eq("data" => {
+        "systems" => [
+          {
+            "id" => "123"
+          },
+          {
+            "id" => "321"
+          }
+        ]
+      })
+    end
+  end
+
+  describe "get system by id" do
+    let!(:eve_system) do
+      create(:eve_system,
+        system_id: 123)
+    end
+
+    let(:query) do
+      %(
+        {
+          system(id: 123) {
+            id
+          }
+        }
+      )
+    end
+
+    let(:result) { EvemonkSchema.execute(query).as_json }
+
+    specify do
+      expect(result).to eq("data" => {
+        "system" => {
+          "id" => "123"
+        }
+      })
+    end
+  end
+end
