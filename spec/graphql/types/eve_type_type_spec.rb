c1e85cb9bdaaf7b24e5204a2af72e85c6b8fef7c
Igor Zubkov <igor.zubkov@gmail.com>
2020-08-08 23:38:06 +0300

    More specs

---
+++ spec/graphql/types/eve_type_type_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Types::EveRegionType do
+  describe "get types" do
+    let!(:eve_type1) do
+      create(:eve_type,
+        type_id: 400)
+    end
+
+    let!(:eve_type2) do
+      create(:eve_type,
+        type_id: 500)
+    end
+
+    let(:query) do
+      %(
+        {
+          types {
+            id
+          }
+        }
+      )
+    end
+
+    let(:result) { EvemonkSchema.execute(query).as_json }
+
+    specify do
+      expect(result).to eq("data" => {
+        "types" => [
+          {
+            "id" => "400"
+          },
+          {
+            "id" => "500"
+          }
+        ]
+      })
+    end
+  end
+
+  describe "get type by id" do
+    let!(:eve_type) do
+      create(:eve_type,
+        type_id: 400)
+    end
+
+    let(:query) do
+      %(
+        {
+          type(id: 400) {
+            id
+          }
+        }
+      )
+    end
+
+    let(:result) { EvemonkSchema.execute(query).as_json }
+
+    specify do
+      expect(result).to eq("data" => {
+        "type" => {
+          "id" => "400"
+        }
+      })
+    end
+  end
+end
