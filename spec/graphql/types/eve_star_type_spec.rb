f3a889edb38fd2c06ed9ff86a423477b29b6f5ab
Igor Zubkov <igor.zubkov@gmail.com>
2020-08-09 01:20:42 +0300

    Add specs

---
+++ spec/graphql/types/eve_star_type_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Types::EveStarType do
+  describe "get stars" do
+    let!(:eve_star1) do
+      create(:eve_star,
+        star_id: 123)
+    end
+
+    let!(:eve_star2) do
+      create(:eve_star,
+        star_id: 321)
+    end
+
+    let(:query) do
+      %(
+        {
+          stars {
+            id
+          }
+        }
+      )
+    end
+
+    let(:result) { EvemonkSchema.execute(query).as_json }
+
+    specify do
+      expect(result).to eq("data" => {
+        "stars" => [
+          {
+            "id" => "123"
+          },
+          {
+            "id" => "321"
+          }
+        ]
+      })
+    end
+  end
+
+  describe "get star by id" do
+    let!(:eve_star) do
+      create(:eve_star,
+        star_id: 123)
+    end
+
+    let(:query) do
+      %(
+        {
+          star(id: 123) {
+            id
+          }
+        }
+      )
+    end
+
+    let(:result) { EvemonkSchema.execute(query).as_json }
+
+    specify do
+      expect(result).to eq("data" => {
+        "star" => {
+          "id" => "123"
+        }
+      })
+    end
+  end
+end
