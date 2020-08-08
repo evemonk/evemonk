83c9a56a0e06533758ac50bcbc59c4a0b3e36265
Igor Zubkov <igor.zubkov@gmail.com>
2020-08-09 01:11:49 +0300

    Update

--- app/graphql/types/eve_station_type.rb
+++ app/graphql/types/eve_station_type.rb
@@ -3,19 +3,19 @@
 module Types
   class EveStationType < Types::BaseObject
     field :id, ID, null: false
-    field :name, String, null: true
-    field :max_dockable_ship_volume, Float, null: true
-    field :office_rental_cost, Float, null: true
-    # t.bigint "owner"
-    field :race_id, Integer, null: true
-    field :race, Types::EveRaceType, null: true
-    field :reprocessing_efficiency, Float, null: true
-    field :reprocessing_stations_take, Float, null: true
-    field :services, [String], null: true
-    field :system_id, Integer, null: true
-    field :system, Types::EveSystemType, null: true
-    field :type_id, Integer, null: true
-    field :type, Types::EveTypeType, null: true
+    # field :name, String, null: true
+    # field :max_dockable_ship_volume, Float, null: true
+    # field :office_rental_cost, Float, null: true
+    # # t.bigint "owner"
+    # field :race_id, Integer, null: true
+    # field :race, Types::EveRaceType, null: true
+    # field :reprocessing_efficiency, Float, null: true
+    # field :reprocessing_stations_take, Float, null: true
+    # field :services, [String], null: true
+    # field :system_id, Integer, null: true
+    # field :system, Types::EveSystemType, null: true
+    # field :type_id, Integer, null: true
+    # field :type, Types::EveTypeType, null: true
 
     def id
       object.station_id
--- app/graphql/types/eve_system_type.rb
+++ app/graphql/types/eve_system_type.rb
@@ -3,28 +3,28 @@
 module Types
   class EveSystemType < Types::BaseObject
     field :id, ID, null: false
-    field :name, GraphQL::Types::JSON, null: true
-    field :constellation_id, Integer, null: true
-    field :constellation, Types::EveConstellationType, null: true
-    field :star_id, Integer, null: true
-    field :star, Types::EveStarType, null: true
-    field :security_class, String, null: true
-    field :security_status, Float, null: true
+    # field :name, GraphQL::Types::JSON, null: true
+    # field :constellation_id, Integer, null: true
+    # field :constellation, Types::EveConstellationType, null: true
+    # field :star_id, Integer, null: true
+    # field :star, Types::EveStarType, null: true
+    # field :security_class, String, null: true
+    # field :security_status, Float, null: true
 
     def id
       object.system_id
     end
 
-    def name
-      {
-        en: object.name_en,
-        de: object.name_de,
-        fr: object.name_fr,
-        ja: object.name_ja,
-        ru: object.name_ru,
-        zh: object.name_zh,
-        ko: object.name_ko
-      }
-    end
+    # def name
+    #   {
+    #     en: object.name_en,
+    #     de: object.name_de,
+    #     fr: object.name_fr,
+    #     ja: object.name_ja,
+    #     ru: object.name_ru,
+    #     zh: object.name_zh,
+    #     ko: object.name_ko
+    #   }
+    # end
   end
 end
