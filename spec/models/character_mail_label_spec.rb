ff37a0bbfa2112ed36a57cf792e48984eb7372f6
Igor Zubkov <igor.zubkov@gmail.com>
2020-08-21 00:32:41 +0300

    Fix typo

--- spec/models/character_mail_label_spec.rb
+++ spec/models/character_mail_label_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 describe CharacterMailLabel do
-  it { should be_a(ApplicationRecord) }
+  it { should be_an(ApplicationRecord) }
 
   it { should belong_to(:character) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 describe User do
-  it { should be_a(ApplicationRecord) }
+  it { should be_an(ApplicationRecord) }
 
   it {
     expect(described_class.devise_modules).to eq([:database_authenticatable,
