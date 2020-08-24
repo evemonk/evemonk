40f0d9a7cbf336c43e23d2d1bb844ab7bd5b597a
Igor Zubkov <igor.zubkov@gmail.com>
2020-08-24 13:58:06 +0300

    Drop eve_alliance_corporations

---
+++ db/migrate/20200824105401_drop_eve_alliance_corporations.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class DropEveAllianceCorporations < ActiveRecord::Migration[6.0]
+  def change
+    remove_index :eve_alliance_corporations,  column: [:alliance_id, :corporation_id], unique: true, name: :index_eve_ac_on_alliance_id_and_corporation_id
+    remove_index :eve_alliance_corporations, :corporation_id
+
+    drop_table :eve_alliance_corporations
+  end
+end
