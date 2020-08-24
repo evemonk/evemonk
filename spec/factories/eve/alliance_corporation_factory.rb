07bc8c50f7148a6e4fbebdc4e999c55bf0c3566b
Igor Zubkov <igor.zubkov@gmail.com>
2020-08-24 14:03:38 +0300

    Remove old factory

--- spec/factories/eve/alliance_corporation_factory.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :eve_alliance_corporation, class: Eve::AllianceCorporation do
-    sequence(:alliance_id)
-
-    sequence(:corporation_id)
-  end
-end
