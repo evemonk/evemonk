691aa2e03dd4a804ce795a3a7294e7bcf6f27cb8
Igor Zubkov <igor.zubkov@gmail.com>
2020-08-25 00:50:00 +0300

    TODO

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -12,8 +12,9 @@ module Admin
       redirect_to root_url unless current_user.admin?
     end
 
-    def valid_action?(name, resource = resource_class)
-      ["new", "edit", "destroy"].exclude?(name.to_s) && super
-    end
+    # TODO: later
+    # def valid_action?(name, resource = resource_class)
+    #   ["new", "edit", "destroy"].exclude?(name.to_s) && super
+    # end
   end
 end
