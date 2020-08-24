8b9bc8d1e7e9a9ffaee9ab3c1919a5e076e528ad
Igor Zubkov <igor.zubkov@gmail.com>
2020-08-24 03:21:57 +0300

    Turn off new/edit/destroy in admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -11,5 +11,9 @@ module Admin
     def admin?
       redirect_to root_url unless current_user.admin?
     end
+
+    def valid_action?(name, resource = resource_class)
+      ["new", "edit", "destroy"].exclude?(name.to_s) && super
+    end
   end
 end
