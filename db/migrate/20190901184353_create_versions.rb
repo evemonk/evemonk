33c07daaeb5268e499df253c5b3c63eed0ea761b
Igor Zubkov <igor.zubkov@gmail.com>
2020-08-25 19:28:08 +0300

    Cleanup

--- db/migrate/20190901184353_create_versions.rb
+++ db/migrate/20190901184353_create_versions.rb
@@ -11,7 +11,7 @@ class CreateVersions < ActiveRecord::Migration[6.0]
 
   def change
     create_table :versions do |t|
-      t.string :item_type, {null: false}
+      t.string :item_type, null: false
       t.integer :item_id, null: false, limit: 8
       t.string :event, null: false
       t.string :whodunnit
@@ -32,6 +32,6 @@ class CreateVersions < ActiveRecord::Migration[6.0]
       #
       t.datetime :created_at
     end
-    add_index :versions, %i[item_type item_id]
+    add_index :versions, [:item_type, :item_id]
   end
 end
