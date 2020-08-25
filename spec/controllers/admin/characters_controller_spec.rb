16cd02a9dc92ddce3aafb0974f8e95a5cbc3fec7
Igor Zubkov <igor.zubkov@gmail.com>
2020-08-25 17:16:07 +0300

    More specs

--- spec/controllers/admin/characters_controller_spec.rb
+++ spec/controllers/admin/characters_controller_spec.rb
@@ -3,6 +3,8 @@
 require "rails_helper"
 
 describe Admin::CharactersController do
+  it_behaves_like "disable modification", Character
+
   it { should be_an(Admin::ApplicationController) }
 
   it { should be_an(Admin::DisableModifications) }
