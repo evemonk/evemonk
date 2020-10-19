fe877a1a8e56a1b4ba5e9cf30c96a4e05f834424
Igor Zubkov <igor.zubkov@gmail.com>
2020-10-20 02:29:19 +0300

    Add specs

--- app/services/statistics_middleware.rb
+++ app/services/statistics_middleware.rb
@@ -10,7 +10,7 @@ class StatisticsMiddleware < Faraday::Middleware
     @app.call(request_env).on_complete do |response_env|
       method = "esi_requests_#{response_env[:status]}_counter"
 
-      Yabeda.evemonk.public_send(method).increment({}, by: 1)
+      Yabeda.evemonk.public_send(method).increment({total: "success"}, by: 1)
     end
   end
 end
---
+++ spec/services/statistics_middleware_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe StatisticsMiddleware do
+  [200, 204, 304, 400, 401, 403, 404, 420, 500, 503, 504].each do |status|
+    describe "when request is #{status}" do
+      before { stub_request(:get, "https://esi.evetech.net/v2/status/").to_return(status: status, body: {}.to_json) }
+
+      let(:esi) { EveOnline::ESI::ServerStatus.new }
+
+      before do
+        conn = Faraday.new do |f|
+          f.use StatisticsMiddleware, esi: esi
+          f.adapter Faraday.default_adapter
+        end
+
+        conn.get("https://esi.evetech.net/v2/status/")
+      end
+
+      let(:method) { "esi_requests_#{status}_counter" }
+
+      specify { expect(Yabeda.evemonk.send(method).values).to eq({ :total => "success" } => 1) }
+    end
+  end
+end
