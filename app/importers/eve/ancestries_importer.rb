e11726e2c13f536d33b49bd4edc2e9258dbca92e
Igor Zubkov <igor.zubkov@gmail.com>
2020-10-25 19:55:17 +0200

    Update

--- app/importers/eve/ancestries_importer.rb
+++ app/importers/eve/ancestries_importer.rb
@@ -1,33 +1,25 @@
 # frozen_string_literal: true
 
 module Eve
-  class AncestriesImporter
+  class AncestriesImporter < BaseImporter
     attr_reader :locale
 
     def initialize(locale = :en)
       @locale = locale
     end
 
-    def import
+    def import!
       Mobility.with_locale(locale) do
-        ActiveRecord::Base.transaction do
-          esi = EveOnline::ESI::UniverseAncestries.new(language: LanguageMapper::LANGUAGES[locale])
+        esi.ancestries.each do |ancestry|
+          eve_ancestry = Eve::Ancestry.find_or_initialize_by(ancestry_id: ancestry.ancestry_id)
 
-          etag = Eve::Etag.find_or_initialize_by(url: esi.url)
-
-          esi.etag = etag.etag
-
-          return if esi.not_modified?
-
-          esi.ancestries.each do |ancestry|
-            eve_ancestry = Eve::Ancestry.find_or_initialize_by(ancestry_id: ancestry.ancestry_id)
-
-            eve_ancestry.update!(ancestry.as_json)
-          end
-
-          etag.update!(etag: esi.etag, body: esi.response)
+          eve_ancestry.update!(ancestry.as_json)
         end
       end
+    end
+
+    def esi
+      @esi ||= EveOnline::ESI::UniverseAncestries.new(language: LanguageMapper::LANGUAGES[locale])
     end
   end
 end
