f2f96001bad477ff95cf8ea723cba3ed46816523
Igor Zubkov <igor.zubkov@gmail.com>
2020-11-02 03:11:59 +0200

    Sde::BasePricesJob

---
+++ spec/jobs/sde/base_prices_job_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Sde::BasePricesJob do
+  it { should be_an(ApplicationJob) }
+
+  it { expect(described_class.queue_name).to eq("default") }
+
+  describe "#perform" do
+    let(:file) { double }
+
+    before do
+      #
+      # Sde::BasePricesImporter.new(file).import
+      #
+      expect(Sde::BasePricesImporter).to receive(:new).with(file) do
+        double.tap do |a|
+          expect(a).to receive(:import)
+        end
+      end
+    end
+
+    specify { expect { subject.perform(file) }.not_to raise_error }
+  end
+end
