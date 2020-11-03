b87f9a2b05d5951ec1f1dafe57eea1f20e3722ad
Igor Zubkov <igor.zubkov@gmail.com>
2020-11-04 00:59:07 +0200

    Fix migration

--- db/migrate/20191216221034_add_devise_indexes.rb
+++ db/migrate/20191216221034_add_devise_indexes.rb
@@ -7,9 +7,5 @@ class AddDeviseIndexes < ActiveRecord::Migration[6.0]
     add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token, unique: true
-
-    User.find_each do |user|
-      user.update_columns(email: user.email.downcase)
-    end
   end
 end
