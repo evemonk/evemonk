330ff48026631407ae513e42d2738b40b865a784
Igor Zubkov <igor.zubkov@gmail.com>
2020-11-03 15:42:22 +0200

    DRY

--- spec/importers/eve/types_importer_spec.rb
+++ spec/importers/eve/types_importer_spec.rb
@@ -3,114 +3,62 @@
 require "rails_helper"
 
 describe Eve::TypesImporter do
+  let(:page) { double }
+
+  subject { described_class.new(page) }
+
+  it { should be_a(Eve::BaseImporter) }
+
   describe "#initialize" do
     context "with page" do
-      let(:page) { double }
-
-      let(:esi) { instance_double(EveOnline::ESI::UniverseTypes) }
-
-      before { expect(EveOnline::ESI::UniverseTypes).to receive(:new).with(page: page).and_return(esi) }
-
-      subject { described_class.new(page) }
-
       its(:page) { should eq(page) }
-
-      its(:esi) { should eq(esi) }
     end
 
     context "without page" do
-      let(:esi) { instance_double(EveOnline::ESI::UniverseTypes) }
-
-      before { expect(EveOnline::ESI::UniverseTypes).to receive(:new).with(page: 1).and_return(esi) }
+      subject { described_class.new }
 
       its(:page) { should eq(1) }
-
-      its(:esi) { should eq(esi) }
     end
   end
 
-  describe "#import" do
-    context "when fresh data available" do
-      let(:page) { double }
+  describe "#import!" do
+    before { expect(subject).to receive(:import_types) }
 
-      subject { described_class.new(page) }
+    before { expect(subject).to receive(:import_other_pages) }
 
-      let(:new_etag) { double }
+    specify { expect { subject.import! }.not_to raise_error }
+  end
 
-      let(:response) { double }
+  describe "#esi" do
+    context "when @esi is set" do
+      let(:esi) { instance_double(EveOnline::ESI::UniverseTypes) }
 
-      let(:url) { double }
+      before { subject.instance_variable_set(:@esi, esi) }
 
-      let(:esi) do
-        instance_double(EveOnline::ESI::UniverseTypes,
-          not_modified?: false,
-          etag: new_etag,
-          response: response,
-          url: url)
-      end
-
-      before { expect(EveOnline::ESI::UniverseTypes).to receive(:new).with(page: page).and_return(esi) }
-
-      let(:etag) { instance_double(Eve::Etag, etag: "97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a") }
-
-      before { expect(Eve::Etag).to receive(:find_or_initialize_by).with(url: url).and_return(etag) }
-
-      before { expect(esi).to receive(:etag=).with("97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a") }
-
-      before { expect(subject).to receive(:import_types) }
-
-      before { expect(subject).to receive(:import_other_pages) }
-
-      before { expect(etag).to receive(:update!).with(etag: new_etag, body: response) }
-
-      specify { expect { subject.import }.not_to raise_error }
+      specify { expect(subject.esi).to eq(esi) }
     end
 
-    context "when no fresh data available" do
-      let(:page) { double }
-
-      subject { described_class.new(page) }
-
-      let(:etag) { instance_double(Eve::Etag, etag: "97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a") }
-
-      let(:url) { double }
-
-      let(:esi) do
-        instance_double(EveOnline::ESI::UniverseTypes,
-          not_modified?: true,
-          url: url)
-      end
+    context "when @esi not set" do
+      let(:esi) { instance_double(EveOnline::ESI::UniverseTypes) }
 
       before { expect(EveOnline::ESI::UniverseTypes).to receive(:new).with(page: page).and_return(esi) }
 
-      before { expect(Eve::Etag).to receive(:find_or_initialize_by).with(url: url).and_return(etag) }
+      specify { expect(subject.esi).to eq(esi) }
 
-      before { expect(esi).to receive(:etag=).with("97f0c48679f2b200043cdbc3406291fc945bcd652ddc7fc11ccdc37a") }
-
-      before { expect(subject).not_to receive(:import_types) }
-
-      before { expect(subject).not_to receive(:import_other_pages) }
-
-      before { expect(etag).not_to receive(:update!) }
-
-      specify { expect { subject.import }.not_to raise_error }
+      specify { expect { subject.esi }.to change { subject.instance_variable_get(:@esi) }.from(nil).to(esi) }
     end
   end
 
   # private methods
 
   describe "#import_types" do
-    let(:page) { double }
-
-    subject { described_class.new(page) }
-
     let(:universe_type_id) { double }
 
     let(:universe_type_ids) { [universe_type_id] }
 
     let(:esi) { instance_double(EveOnline::ESI::UniverseTypes, universe_type_ids: universe_type_ids) }
 
-    before { expect(EveOnline::ESI::UniverseTypes).to receive(:new).with(page: page).and_return(esi) }
+    before { expect(subject).to receive(:esi).and_return(esi) }
 
     context "when eve type not imported" do
       before { expect(Eve::Type).to receive(:exists?).with(type_id: universe_type_id).and_return(false) }
@@ -135,10 +83,6 @@ describe Eve::TypesImporter do
 
       subject { described_class.new(page) }
 
-      let(:esi) { instance_double(EveOnline::ESI::UniverseTypes) }
-
-      before { expect(EveOnline::ESI::UniverseTypes).to receive(:new).with(page: page).and_return(esi) }
-
       before { expect(Eve::UpdateTypesJob).not_to receive(:perform_later) }
 
       specify { expect { subject.send(:import_other_pages) }.not_to raise_error }
@@ -151,7 +95,7 @@ describe Eve::TypesImporter do
 
       let(:esi) { instance_double(EveOnline::ESI::UniverseTypes, total_pages: 1) }
 
-      before { expect(EveOnline::ESI::UniverseTypes).to receive(:new).with(page: page).and_return(esi) }
+      before { expect(subject).to receive(:esi).and_return(esi) }
 
       before { expect(Eve::UpdateTypesJob).not_to receive(:perform_later) }
 
@@ -165,7 +109,7 @@ describe Eve::TypesImporter do
 
       let(:esi) { instance_double(EveOnline::ESI::UniverseTypes, total_pages: 2) }
 
-      before { expect(EveOnline::ESI::UniverseTypes).to receive(:new).with(page: page).and_return(esi) }
+      before { expect(subject).to receive(:esi).and_return(esi).twice }
 
       before { expect(Eve::UpdateTypesJob).to receive(:perform_later).with(2) }
 
