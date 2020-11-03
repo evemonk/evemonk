d828eddc26c41166a6ca50e2d6a3bf13c2df57aa
Igor Zubkov <igor.zubkov@gmail.com>
2020-11-03 22:20:18 +0200

    DRY

--- app/importers/eve/group_importer.rb
+++ app/importers/eve/group_importer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Eve
-  class GroupImporter
+  class GroupImporter < BaseImporter
     attr_reader :group_id, :locale
 
     def initialize(group_id, locale = :en)
@@ -9,26 +9,18 @@ module Eve
       @locale = locale
     end
 
-    def import
+    def import!
       Mobility.with_locale(locale) do
-        ActiveRecord::Base.transaction do
-          eve_group = Eve::Group.find_or_initialize_by(group_id: group_id)
+        eve_group = Eve::Group.find_or_initialize_by(group_id: group_id)
 
-          esi = EveOnline::ESI::UniverseGroup.new(id: group_id, language: LanguageMapper::LANGUAGES[locale])
-
-          etag = Eve::Etag.find_or_initialize_by(url: esi.url)
-
-          esi.etag = etag.etag
-
-          return if esi.not_modified?
-
-          eve_group.update!(esi.as_json)
-
-          etag.update!(etag: esi.etag, body: esi.response)
-        rescue EveOnline::Exceptions::ResourceNotFound
-          eve_group.destroy!
-        end
+        eve_group.update!(esi.as_json)
+      rescue EveOnline::Exceptions::ResourceNotFound
+        eve_group.destroy!
       end
+    end
+
+    def esi
+      @esi ||= EveOnline::ESI::UniverseGroup.new(id: group_id, language: LanguageMapper::LANGUAGES[locale])
     end
   end
 end
