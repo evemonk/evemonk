663fcd44b6df155dac637dcafccf0ae0956b1b9a
Igor Zubkov <igor.zubkov@gmail.com>
2020-11-03 03:02:28 +0200

    DRY

--- app/importers/eve/factions_importer.rb
+++ app/importers/eve/factions_importer.rb
@@ -1,33 +1,25 @@
 # frozen_string_literal: true
 
 module Eve
-  class FactionsImporter
+  class FactionsImporter < BaseImporter
     attr_reader :locale
 
     def initialize(locale = :en)
       @locale = locale
     end
 
-    def import
+    def import!
       Mobility.with_locale(locale) do
-        ActiveRecord::Base.transaction do
-          esi = EveOnline::ESI::UniverseFactions.new(language: LanguageMapper::LANGUAGES[locale])
+        esi.factions.each do |faction|
+          eve_faction = Eve::Faction.find_or_initialize_by(faction_id: faction.faction_id)
 
-          etag = Eve::Etag.find_or_initialize_by(url: esi.url)
-
-          esi.etag = etag.etag
-
-          return if esi.not_modified?
-
-          esi.factions.each do |faction|
-            eve_faction = Eve::Faction.find_or_initialize_by(faction_id: faction.faction_id)
-
-            eve_faction.update!(faction.as_json)
-          end
-
-          etag.update!(etag: esi.etag, body: esi.response)
+          eve_faction.update!(faction.as_json)
         end
       end
+    end
+
+    def esi
+      @esi ||= EveOnline::ESI::UniverseFactions.new(language: LanguageMapper::LANGUAGES[locale])
     end
   end
 end
