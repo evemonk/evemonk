65280b94c6433acc95d098fd4444966e965b99b3
Igor Zubkov <igor.zubkov@gmail.com>
2020-11-10 17:17:51 +0200

    DRY

--- app/importers/eve/graphic_importer.rb
+++ app/importers/eve/graphic_importer.rb
@@ -1,31 +1,23 @@
 # frozen_string_literal: true
 
 module Eve
-  class GraphicImporter
+  class GraphicImporter < BaseImporter
     attr_reader :graphic_id
 
     def initialize(graphic_id)
       @graphic_id = graphic_id
     end
 
-    def import
-      ActiveRecord::Base.transaction do
-        eve_graphic = Eve::Graphic.find_or_initialize_by(graphic_id: graphic_id)
+    def import!
+      eve_graphic = Eve::Graphic.find_or_initialize_by(graphic_id: graphic_id)
 
-        esi = EveOnline::ESI::UniverseGraphic.new(id: graphic_id)
+      eve_graphic.update!(esi.as_json)
+    rescue EveOnline::Exceptions::ResourceNotFound
+      eve_graphic.destroy!
+    end
 
-        etag = Eve::Etag.find_or_initialize_by(url: esi.url)
-
-        esi.etag = etag.etag
-
-        return if esi.not_modified?
-
-        eve_graphic.update!(esi.as_json)
-
-        etag.update!(etag: esi.etag, body: esi.response)
-      rescue EveOnline::Exceptions::ResourceNotFound
-        eve_graphic.destroy!
-      end
+    def esi
+      @esi ||= EveOnline::ESI::UniverseGraphic.new(id: graphic_id)
     end
   end
 end
