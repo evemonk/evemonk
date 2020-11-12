f25be70df16241fccbbe9502c872a447a05a785e
Igor Zubkov <igor.zubkov@gmail.com>
2020-11-12 17:15:54 +0200

    DRY

--- app/importers/eve/market_group_importer.rb
+++ app/importers/eve/market_group_importer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Eve
-  class MarketGroupImporter
+  class MarketGroupImporter < BaseImporter
     attr_reader :market_group_id, :locale
 
     def initialize(market_group_id, locale = :en)
@@ -9,26 +9,18 @@ module Eve
       @locale = locale
     end
 
-    def import
+    def import!
       Mobility.with_locale(locale) do
-        ActiveRecord::Base.transaction do
-          eve_market_group = Eve::MarketGroup.find_or_initialize_by(market_group_id: market_group_id)
+        eve_market_group = Eve::MarketGroup.find_or_initialize_by(market_group_id: market_group_id)
 
-          esi = EveOnline::ESI::MarketGroup.new(id: market_group_id, language: LanguageMapper::LANGUAGES[locale])
-
-          etag = Eve::Etag.find_or_initialize_by(url: esi.url)
-
-          esi.etag = etag.etag
-
-          return if esi.not_modified?
-
-          eve_market_group.update!(esi.as_json)
-
-          etag.update!(etag: esi.etag, body: esi.response)
-        rescue EveOnline::Exceptions::ResourceNotFound
-          eve_market_group.destroy!
-        end
+        eve_market_group.update!(esi.as_json)
+      rescue EveOnline::Exceptions::ResourceNotFound
+        eve_market_group.destroy!
       end
+    end
+
+    def esi
+      @esi ||= EveOnline::ESI::MarketGroup.new(id: market_group_id, language: LanguageMapper::LANGUAGES[locale])
     end
   end
 end
