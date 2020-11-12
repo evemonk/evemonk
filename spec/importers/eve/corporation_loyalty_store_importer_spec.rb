a7ebb795cc93f7b8b8a39bc82b65a577a258c46f
Igor Zubkov <igor.zubkov@gmail.com>
2020-11-12 17:35:40 +0200

    DRY

--- spec/importers/eve/corporation_loyalty_store_importer_spec.rb
+++ spec/importers/eve/corporation_loyalty_store_importer_spec.rb
@@ -7,46 +7,32 @@ describe Eve::CorporationLoyaltyStoreImporter do
 
   subject { described_class.new(corporation_id) }
 
+  it { should be_a(Eve::BaseImporter) }
+
   describe "#initialize" do
     its(:corporation_id) { should eq(corporation_id) }
   end
 
-  describe "#import" do
-    let(:eve_corporation) { instance_double(Eve::Corporation) }
+  describe "#import!" do
+    context "when eve corporation found" do
+      let(:eve_corporation) { instance_double(Eve::Corporation) }
 
-    before do
-      expect(Eve::Corporation).to receive(:find_by!)
-        .with(corporation_id: corporation_id)
-        .and_return(eve_corporation)
-    end
+      before do
+        expect(Eve::Corporation).to receive(:find_by!)
+          .with(corporation_id: corporation_id)
+          .and_return(eve_corporation)
+      end
 
-    context "when fresh data available" do
-      let(:url) { double }
+      let(:json) { double }
 
-      let(:new_etag) { double }
-
-      let(:as_json) { double }
-
-      let(:loyalty_store_offer) { instance_double(EveOnline::ESI::Models::LoyaltyStoreOffer, as_json: as_json) }
-
-      let(:response) { double }
+      let(:loyalty_store_offer) { instance_double(EveOnline::ESI::Models::LoyaltyStoreOffer, as_json: json) }
 
       let(:esi) do
         instance_double(EveOnline::ESI::CorporationLoyaltyStoreOffers,
-          not_modified?: false,
-          url: url,
-          etag: new_etag,
-          offers: [loyalty_store_offer],
-          response: response)
+          offers: [loyalty_store_offer])
       end
 
-      before { expect(EveOnline::ESI::CorporationLoyaltyStoreOffers).to receive(:new).with(corporation_id: corporation_id).and_return(esi) }
-
-      let(:etag) { instance_double(Eve::Etag, etag: "e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b") }
-
-      before { expect(Eve::Etag).to receive(:find_or_initialize_by).with(url: url).and_return(etag) }
-
-      before { expect(esi).to receive(:etag=).with("e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b") }
+      before { expect(subject).to receive(:esi).and_return(esi) }
 
       before do
         #
@@ -65,36 +51,53 @@ describe Eve::CorporationLoyaltyStoreImporter do
         #
         expect(eve_corporation).to receive(:loyalty_store_offers) do
           double.tap do |a|
-            expect(a).to receive(:create!).with(as_json)
+            expect(a).to receive(:create!).with(json)
           end
         end
       end
 
-      before { expect(etag).to receive(:update!).with(etag: new_etag, body: response) }
-
-      specify { expect { subject.import }.not_to raise_error }
+      specify { expect { subject.import! }.not_to raise_error }
     end
 
-    context "when no fresh data available" do
-      let(:url) { double }
-
-      let(:esi) do
-        instance_double(EveOnline::ESI::CorporationLoyaltyStoreOffers,
-          not_modified?: true,
-          url: url)
+    context "when eve corporation not found" do
+      before do
+        expect(Eve::Corporation).to receive(:find_by!)
+          .with(corporation_id: corporation_id)
+          .and_raise(ActiveRecord::RecordNotFound)
       end
+
+      before do
+        #
+        # Rails.logger.info("Corporation with ID #{corporation_id} not found")
+        #
+        expect(Rails).to receive(:logger) do
+          double.tap do |a|
+            expect(a).to receive(:info).with("Corporation with ID #{corporation_id} not found")
+          end
+        end
+      end
+
+      specify { expect { subject.import! }.not_to raise_error }
+    end
+  end
+
+  describe "#esi" do
+    context "when @esi is set" do
+      let(:esi) { instance_double(EveOnline::ESI::CorporationLoyaltyStoreOffers) }
+
+      before { subject.instance_variable_set(:@esi, esi) }
+
+      specify { expect(subject.esi).to eq(esi) }
+    end
+
+    context "when @esi not set" do
+      let(:esi) { instance_double(EveOnline::ESI::CorporationLoyaltyStoreOffers) }
 
       before { expect(EveOnline::ESI::CorporationLoyaltyStoreOffers).to receive(:new).with(corporation_id: corporation_id).and_return(esi) }
 
-      let(:etag) { instance_double(Eve::Etag, etag: "e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b") }
+      specify { expect(subject.esi).to eq(esi) }
 
-      before { expect(Eve::Etag).to receive(:find_or_initialize_by).with(url: url).and_return(etag) }
-
-      before { expect(esi).to receive(:etag=).with("e3f6a76b4a1287f54966c6253f8f5d6ac6460bc43d47570331b43e0b") }
-
-      before { expect(eve_corporation).not_to receive(:loyalty_store_offers) }
-
-      specify { expect { subject.import }.not_to raise_error }
+      specify { expect { subject.esi }.to change { subject.instance_variable_get(:@esi) }.from(nil).to(esi) }
     end
   end
 end
