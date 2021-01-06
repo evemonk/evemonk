47ab2e703087438b4384efaf36b652cd909960a9
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-06 13:48:58 +0200

    Sde::CertificatesJob

---
+++ spec/jobs/sde/certificates_job_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Sde::CertificatesJob do
+  it { should be_an(ApplicationJob) }
+
+  it { expect(described_class.queue_name).to eq("default") }
+
+  describe "#perform" do
+    let(:file) { double }
+
+    before do
+      #
+      # Sde::CertificatesImporter.new(file).import
+      #
+      expect(Sde::CertificatesImporter).to receive(:new).with(file) do
+        double.tap do |a|
+          expect(a).to receive(:import)
+        end
+      end
+    end
+
+    specify { expect { subject.perform(file) }.not_to raise_error }
+  end
+end
