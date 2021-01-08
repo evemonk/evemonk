7efe52a6b5ecc7281e92665b54efedb45b4bbd23
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-08 16:04:02 +0200

    Cleanup

--- app/documentation/api/docs/sessions.rb
+++ app/documentation/api/docs/sessions.rb
@@ -6,24 +6,6 @@ module Api
       # :nocov:
       include Swagger::Blocks
 
-      swagger_path "/sessions" do
-        operation :get do
-          key :summary, "Get all user sessions"
-          key :description, "Get all user sessions"
-          key :tags, ["sessions"]
-          parameter :authorization
-          parameter :page
-          response "200" do
-            key :description, "Success"
-            schema do
-              key :'$ref', :OutputSessionWithoutToken
-            end
-          end
-          extend Api::Docs::Shared::Unauthorized
-          extend Api::Docs::Shared::NotAcceptable
-        end
-      end
-
       swagger_path "/sessions/{id}" do
         operation :delete do
           key :summary, "End session"
