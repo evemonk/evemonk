f4fb1e1fdbbf090ede0456edbd8297bec8001629
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-11 00:33:04 +0200

    Update

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,6 +11,7 @@
 Devise.setup do |config|
   # ==> Devise Authy Authentication Extension
   # How long should the user's device be remembered for.
+  # config.authy_remember_device = 1.month
   config.authy_remember_device = 1.year
 
   # Should Authy OneTouch be enabled?
