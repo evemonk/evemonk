d58a29fd8e678fa1b04081d094816551b993224b
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-11 15:11:00 +0200

    Fix

---
+++ spec/support/pig-ci-rails.rb
@@ -0,0 +1,12 @@
+require "pig_ci"
+
+PigCI.start do |config|
+  # Maximum memory in megabytes
+  config.thresholds.memory = 400
+
+  # Maximum time per a HTTP request
+  # config.thresholds.request_time = 250
+
+  # Maximum database calls per a request
+  # config.thresholds.database_request = 35
+end if RSpec.configuration.files_to_run.count > 1
