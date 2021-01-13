fc666b8bf41f58dbbf23acc9db158b8d54da4637
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-13 21:10:03 +0200

    Cleanup

--- app/mailers/user_reset_password_mailer.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class UserResetPasswordMailer < ApplicationMailer
-  def email
-    @user = params[:user]
-    mail(to: @user.email, subject: "Reset password at evemonk.com")
-  end
-end
