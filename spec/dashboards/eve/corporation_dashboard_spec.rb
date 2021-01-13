325da19bca70563e91f09e37aec28f932ea43b11
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-13 22:51:33 +0200

    More specs

--- spec/dashboards/eve/corporation_dashboard_spec.rb
+++ spec/dashboards/eve/corporation_dashboard_spec.rb
@@ -4,4 +4,10 @@ require "rails_helper"
 
 describe Eve::CorporationDashboard do
   it { should be_an(Administrate::BaseDashboard) }
+
+  describe "#display_resource" do
+    let(:corporation) { build(:eve_corporation, name: "Freighting Solutions Inc.") }
+
+    specify { expect(subject.display_resource(corporation)).to eq("Freighting Solutions Inc.") }
+  end
 end
