633070efd5b81d3c55b457c5eab070db5783acbf
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-13 22:41:51 +0200

    More specs

--- spec/dashboards/eve/ancestry_dashboard_spec.rb
+++ spec/dashboards/eve/ancestry_dashboard_spec.rb
@@ -4,4 +4,10 @@ require "rails_helper"
 
 describe Eve::AncestryDashboard do
   it { should be_an(Administrate::BaseDashboard) }
+
+  describe "#display_resource" do
+    let(:ancestry) { build(:eve_ancestry, name_en: "Liberated Children") }
+
+    specify { expect(subject.display_resource(ancestry)).to eq("Liberated Children") }
+  end
 end
