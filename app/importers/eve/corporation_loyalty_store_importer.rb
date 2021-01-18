a7a81eb34ad41c67151d13619964b3bea7f7f6ca
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-19 00:22:43 +0200

    Update

--- app/importers/eve/corporation_loyalty_store_importer.rb
+++ app/importers/eve/corporation_loyalty_store_importer.rb
@@ -26,18 +26,6 @@ module Eve
       end
     end
 
-    # def import!
-    #   eve_corporation = Eve::Corporation.find_by!(corporation_id: corporation_id)
-    #
-    #   eve_corporation.loyalty_store_offers.destroy_all
-    #
-    #   esi.offers.each do |offer|
-    #     eve_corporation.loyalty_store_offers.create!(offer.as_json)
-    #   end
-    # rescue ActiveRecord::RecordNotFound
-    #   Rails.logger.info("Corporation with ID #{corporation_id} not found")
-    # end
-
     def esi
       @esi ||= EveOnline::ESI::CorporationLoyaltyStoreOffers.new(corporation_id: corporation_id)
     end
