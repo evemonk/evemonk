c605b6b8c3e8e7a30bffd7ac50f109d1d3213736
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-18 21:20:01 +0200

    Update

--- spec/importers/eve/character_corporation_history_importer_spec.rb
+++ spec/importers/eve/character_corporation_history_importer_spec.rb
@@ -21,6 +21,51 @@ describe Eve::CharacterCorporationHistoryImporter do
 
       specify { expect { subject.import }.not_to raise_error }
     end
+
+    context "when etag cache miss" do
+      context "when eve character found" do
+        let(:record_id) { double }
+
+        let(:json) { double }
+
+        let(:entry) { instance_double(EveOnline::ESI::Models::CharacterCorporationHistory, record_id: record_id, as_json: json) }
+
+        let(:entries) { [entry] }
+
+        let(:esi) do
+          instance_double(EveOnline::ESI::CharacterCorporationHistory,
+            not_modified?: false,
+            entries: entries)
+        end
+
+        before { expect(subject).to receive(:esi).and_return(esi).twice }
+
+        let(:eve_character) { instance_double(Eve::Character) }
+
+        before { expect(Eve::Character).to receive(:find_by!).with(character_id: character_id).and_return(eve_character) }
+
+        let(:character_corporation_history) { instance_double(Eve::CharacterCorporationHistory) }
+
+        before do
+          #
+          # eve_character.character_corporation_histories
+          #              .find_or_initialize_by(record_id: entry.record_id) # => character_corporation_history
+          #
+          expect(eve_character).to receive(:character_corporation_histories) do
+            double.tap do |a|
+              expect(a).to receive(:find_or_initialize_by).with(record_id: record_id)
+                .and_return(character_corporation_history)
+            end
+          end
+        end
+
+        before { expect(character_corporation_history).to receive(:update!).with(json) }
+
+        before { expect(subject).to receive(:update_etag) }
+
+        specify { expect { subject.import }.not_to raise_error }
+      end
+    end
   end
 
   # describe "#import!" do
