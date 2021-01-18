f3c9128fe90d4f8a20df8b1bb19ee7be8af7e01c
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-18 20:58:40 +0200

    Update

--- app/importers/eve/character_corporation_history_importer.rb
+++ app/importers/eve/character_corporation_history_importer.rb
@@ -2,27 +2,27 @@
 
 module Eve
   class CharacterCorporationHistoryImporter < BaseImporter
-    attr_reader :character_id
+    # attr_reader :character_id
 
-    def initialize(character_id)
-      @character_id = character_id
-    end
+    # def initialize(character_id)
+    #   @character_id = character_id
+    # end
 
-    def import!
-      eve_character = Eve::Character.find_by!(character_id: character_id)
+    # def import!
+    #   eve_character = Eve::Character.find_by!(character_id: character_id)
+    #
+    #   esi.entries.each do |entry|
+    #     history = eve_character.character_corporation_histories
+    #       .find_or_initialize_by(record_id: entry.record_id)
+    #
+    #     history.update!(entry.as_json)
+    #   end
+    # rescue ActiveRecord::RecordNotFound
+    #   Rails.logger.info("Character with ID #{character_id} not found")
+    # end
 
-      esi.entries.each do |entry|
-        history = eve_character.character_corporation_histories
-          .find_or_initialize_by(record_id: entry.record_id)
-
-        history.update!(entry.as_json)
-      end
-    rescue ActiveRecord::RecordNotFound
-      Rails.logger.info("Character with ID #{character_id} not found")
-    end
-
-    def esi
-      @esi ||= EveOnline::ESI::CharacterCorporationHistory.new(character_id: character_id)
-    end
+    # def esi
+    #   @esi ||= EveOnline::ESI::CharacterCorporationHistory.new(character_id: character_id)
+    # end
   end
 end
--- spec/importers/eve/character_corporation_history_importer_spec.rb
+++ spec/importers/eve/character_corporation_history_importer_spec.rb
@@ -3,86 +3,86 @@
 require "rails_helper"
 
 describe Eve::CharacterCorporationHistoryImporter do
-  let(:character_id) { double }
-
-  subject { described_class.new(character_id) }
-
-  it { should be_a(Eve::BaseImporter) }
-
-  describe "#import!" do
-    context "when eve character found" do
-      let(:eve_character) { instance_double(Eve::Character) }
-
-      before { expect(Eve::Character).to receive(:find_by!).with(character_id: character_id).and_return(eve_character) }
-
-      let(:record_id) { double }
-
-      let(:json) { double }
-
-      let(:entry) { instance_double(EveOnline::ESI::Models::CharacterCorporationHistory, record_id: record_id, as_json: json) }
-
-      let(:entries) { [entry] }
-
-      let(:esi) do
-        instance_double(EveOnline::ESI::CharacterCorporationHistory,
-          entries: entries)
-      end
-
-      before { expect(subject).to receive(:esi).and_return(esi) }
-
-      let(:character_corporation_history) { instance_double(Eve::CharacterCorporationHistory) }
-
-      before do
-        #
-        # eve_character.character_corporation_histories
-        #              .find_or_initialize_by(record_id: entry.record_id) # => character_corporation_history
-        #
-        expect(eve_character).to receive(:character_corporation_histories) do
-          double.tap do |a|
-            expect(a).to receive(:find_or_initialize_by).with(record_id: record_id)
-              .and_return(character_corporation_history)
-          end
-        end
-      end
-
-      before { expect(character_corporation_history).to receive(:update!).with(json) }
-
-      specify { expect { subject.import! }.not_to raise_error }
-    end
-
-    context "when eve character not found (ActiveRecord::RecordNotFound)" do
-      before { expect(Eve::Character).to receive(:find_by!).with(character_id: character_id).and_raise(ActiveRecord::RecordNotFound) }
-
-      before do
-        #
-        # Rails.logger.info("Character with ID #{ character_id } not found")
-        #
-        expect(Rails).to receive(:logger) do
-          double.tap do |a|
-            expect(a).to receive(:info).with("Character with ID #{character_id} not found")
-          end
-        end
-      end
-
-      specify { expect { subject.import! }.not_to raise_error }
-    end
-  end
-
-  describe "#esi" do
-    context "when @esi is set" do
-      let(:esi) { instance_double(EveOnline::ESI::CharacterCorporationHistory) }
-
-      before { subject.instance_variable_set(:@esi, esi) }
-
-      specify { expect(subject.esi).to eq(esi) }
-    end
-
-    context "when @esi not set" do
-      let(:esi) { instance_double(EveOnline::ESI::CharacterCorporationHistory) }
-
-      before { expect(EveOnline::ESI::CharacterCorporationHistory).to receive(:new).with(character_id: character_id).and_return(esi) }
-
-      specify { expect { subject.esi }.to change { subject.instance_variable_get(:@esi) }.from(nil).to(esi) }
-    end
-  end
+  # let(:character_id) { double }
+  #
+  # subject { described_class.new(character_id) }
+  #
+  # it { should be_a(Eve::BaseImporter) }
+  #
+  # describe "#import!" do
+  #   context "when eve character found" do
+  #     let(:eve_character) { instance_double(Eve::Character) }
+  #
+  #     before { expect(Eve::Character).to receive(:find_by!).with(character_id: character_id).and_return(eve_character) }
+  #
+  #     let(:record_id) { double }
+  #
+  #     let(:json) { double }
+  #
+  #     let(:entry) { instance_double(EveOnline::ESI::Models::CharacterCorporationHistory, record_id: record_id, as_json: json) }
+  #
+  #     let(:entries) { [entry] }
+  #
+  #     let(:esi) do
+  #       instance_double(EveOnline::ESI::CharacterCorporationHistory,
+  #         entries: entries)
+  #     end
+  #
+  #     before { expect(subject).to receive(:esi).and_return(esi) }
+  #
+  #     let(:character_corporation_history) { instance_double(Eve::CharacterCorporationHistory) }
+  #
+  #     before do
+  #       #
+  #       # eve_character.character_corporation_histories
+  #       #              .find_or_initialize_by(record_id: entry.record_id) # => character_corporation_history
+  #       #
+  #       expect(eve_character).to receive(:character_corporation_histories) do
+  #         double.tap do |a|
+  #           expect(a).to receive(:find_or_initialize_by).with(record_id: record_id)
+  #             .and_return(character_corporation_history)
+  #         end
+  #       end
+  #     end
+  #
+  #     before { expect(character_corporation_history).to receive(:update!).with(json) }
+  #
+  #     specify { expect { subject.import! }.not_to raise_error }
+  #   end
+  #
+  #   context "when eve character not found (ActiveRecord::RecordNotFound)" do
+  #     before { expect(Eve::Character).to receive(:find_by!).with(character_id: character_id).and_raise(ActiveRecord::RecordNotFound) }
+  #
+  #     before do
+  #       #
+  #       # Rails.logger.info("Character with ID #{ character_id } not found")
+  #       #
+  #       expect(Rails).to receive(:logger) do
+  #         double.tap do |a|
+  #           expect(a).to receive(:info).with("Character with ID #{character_id} not found")
+  #         end
+  #       end
+  #     end
+  #
+  #     specify { expect { subject.import! }.not_to raise_error }
+  #   end
+  # end
+  #
+  # describe "#esi" do
+  #   context "when @esi is set" do
+  #     let(:esi) { instance_double(EveOnline::ESI::CharacterCorporationHistory) }
+  #
+  #     before { subject.instance_variable_set(:@esi, esi) }
+  #
+  #     specify { expect(subject.esi).to eq(esi) }
+  #   end
+  #
+  #   context "when @esi not set" do
+  #     let(:esi) { instance_double(EveOnline::ESI::CharacterCorporationHistory) }
+  #
+  #     before { expect(EveOnline::ESI::CharacterCorporationHistory).to receive(:new).with(character_id: character_id).and_return(esi) }
+  #
+  #     specify { expect { subject.esi }.to change { subject.instance_variable_get(:@esi) }.from(nil).to(esi) }
+  #   end
+  # end
 end
