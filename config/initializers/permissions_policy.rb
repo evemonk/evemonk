6aef500e6151ad610a8545046efe13e41aa84134
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-18 21:06:10 +0200

    Add HTTP permissions policy

--- config/initializers/permissions_policy.rb
+++ config/initializers/permissions_policy.rb
@@ -2,12 +2,24 @@
 
 # Define an application-wide HTTP permissions policy. For further
 # information see https://developers.google.com/web/updates/2018/06/feature-policy
-#
-# Rails.application.config.permissions_policy do |f|
-#   f.camera      :none
-#   f.gyroscope   :none
-#   f.microphone  :none
-#   f.usb         :none
-#   f.fullscreen  :self
-#   f.payment     :self, "https://secure.example.com"
-# end
+
+Rails.application.config.permissions_policy do |f|
+  f.accelerometer :none
+  f.ambient_light_sensor :none
+  f.autoplay :none
+  f.camera :none
+  f.encrypted_media :none
+  f.fullscreen :none
+  f.geolocation :none
+  f.gyroscope :none
+  f.magnetometer :none
+  f.microphone :none
+  f.midi :none
+  f.payment :none
+  # f.payment :self, "https://secure.example.com"
+  f.picture_in_picture :none
+  f.speaker :none
+  f.usb :none
+  f.vibrate :none
+  f.vr :none
+end
