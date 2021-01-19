bff250e86d900d5a25b1fb619c964056f6ee5409
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-19 13:32:39 +0200

    Update

--- app/importers/eve/corporation_alliance_history_importer.rb
+++ app/importers/eve/corporation_alliance_history_importer.rb
@@ -27,19 +27,6 @@ module Eve
       end
     end
 
-    # def import!
-    #   eve_corporation = Eve::Corporation.find_by!(corporation_id: corporation_id)
-    #
-    #   esi.entries.each do |entry|
-    #     history = eve_corporation.corporation_alliance_histories
-    #       .find_or_initialize_by(record_id: entry.record_id)
-    #
-    #     history.update!(entry.as_json)
-    #   end
-    # rescue ActiveRecord::RecordNotFound
-    #   Rails.logger.info("Corporation with ID #{corporation_id} not found")
-    # end
-
     def esi
       @esi ||= EveOnline::ESI::CorporationAllianceHistory.new(corporation_id: corporation_id)
     end
