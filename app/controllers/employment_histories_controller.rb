c7dc3276417ef9531cdd37d0ce3dffd7d9a01259
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-25 19:12:19 +0200

    Fix sorting

--- app/controllers/employment_histories_controller.rb
+++ app/controllers/employment_histories_controller.rb
@@ -3,8 +3,13 @@
 class EmploymentHistoriesController < ApplicationController
   def index
     @character = current_user.characters
-      .includes(:alliance, :corporation, character_corporation_histories: :corporation)
+      .includes(:alliance, :corporation)
       .find_by!(character_id: params[:character_id])
+      .decorate
+
+    @character_corporation_histories = CharacterCorporationHistory.where(character_id: @character.character_id)
+      .includes(:corporation)
+      .order(start_date: :desc)
       .decorate
   end
 end
