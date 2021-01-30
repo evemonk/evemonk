5f359ab16aecf989035c24719dcf4aa73c57483f
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-30 02:48:40 +0200

    Cleanup

--- app/graphql/evemonk_schema.rb
+++ app/graphql/evemonk_schema.rb
@@ -4,13 +4,6 @@ class EvemonkSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 
-  # Opt in to the new runtime (default in future graphql-ruby versions)
-  use GraphQL::Execution::Interpreter
-  use GraphQL::Analysis::AST
-
-  # Add built-in connections for pagination
-  use GraphQL::Pagination::Connections
-
   use Yabeda::GraphQL
 
   default_max_page_size 50
