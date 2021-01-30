8bce6b227d70902d41740bf5b98f8b9fe1fccc74
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-31 00:44:47 +0200

    ApplicationRecord#imageproxy_url

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,8 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def imageproxy_url
+    Setting.image_proxy_url if Setting.use_image_proxy
+  end
 end
--- spec/models/application_record_spec.rb
+++ spec/models/application_record_spec.rb
@@ -4,4 +4,18 @@ require "rails_helper"
 
 describe ApplicationRecord do
   it { expect(described_class.abstract_class).to eq(true) }
+
+  describe "#imageproxy_url" do
+    context "when Setting.use_image_proxy is true" do
+      before { Setting.use_image_proxy = true }
+
+      specify { expect(subject.imageproxy_url).to eq("https://imageproxy.evemonk.com/") }
+    end
+
+    context "when Setting.use_image_proxy is false" do
+      before { Setting.use_image_proxy = false }
+
+      specify { expect(subject.imageproxy_url).to eq(nil) }
+    end
+  end
 end
