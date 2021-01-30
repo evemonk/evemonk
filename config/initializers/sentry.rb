453cc8fbb1d4a95d02ee63ffe35a465a65fb7d7c
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-31 01:33:32 +0200

    Add excluded_exceptions for sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,4 +5,12 @@ Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger]
   config.traces_sample_rate = 0.1
   config.async = lambda { |event, hint| SentryJob.perform_later(event, hint) }
+  config.excluded_exceptions += ["EveOnline::Exceptions::InternalServerError",
+    "EveOnline::Exceptions::BadGateway",
+    "EveOnline::Exceptions::ServiceUnavailable",
+    "EveOnline::Exceptions::Timeout",
+    "EveOnline::Exceptions::ResourceNotFound",
+    "Searchkick::ImportError",
+    "OpenSSL::SSL::SSLError",
+    "Faraday::Error::TimeoutError"]
 end
