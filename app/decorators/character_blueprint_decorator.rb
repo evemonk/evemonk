bd2bb88acf183b1a78827de86a728d107a4062ce
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-31 00:48:13 +0200

    Move code from decorator

--- app/decorators/character_blueprint_decorator.rb
+++ app/decorators/character_blueprint_decorator.rb
@@ -2,24 +2,4 @@
 
 class CharacterBlueprintDecorator < ApplicationDecorator
   decorates_associations :character, :blueprint, :location
-
-  def icon_tiny
-    if stacked? || bpo?
-      "#{imageproxy_url}https://images.evetech.net/types/#{type_id}/bp?size=32"
-    elsif bpc?
-      "#{imageproxy_url}https://images.evetech.net/types/#{type_id}/bpc?size=32"
-    end
-  end
-
-  def icon_small
-    if stacked? || bpo?
-      "#{imageproxy_url}https://images.evetech.net/types/#{type_id}/bp?size=64"
-    elsif bpc?
-      "#{imageproxy_url}https://images.evetech.net/types/#{type_id}/bpc?size=64"
-    end
-  end
-
-  # def character_copying_time_formatted
-  #   HumanTime.new(CharacterManufacturingCopyTime.new(@character, @blueprint.blueprint.copying_time).manufacturing_copy_time.round).long_formatted
-  # end
 end
--- app/models/character_blueprint.rb
+++ app/models/character_blueprint.rb
@@ -44,4 +44,24 @@ class CharacterBlueprint < ApplicationRecord
       "+#{time_efficiency} %"
     end
   end
+
+  def icon_tiny
+    if stacked? || bpo?
+      "#{imageproxy_url}https://images.evetech.net/types/#{type_id}/bp?size=32"
+    elsif bpc?
+      "#{imageproxy_url}https://images.evetech.net/types/#{type_id}/bpc?size=32"
+    end
+  end
+
+  def icon_small
+    if stacked? || bpo?
+      "#{imageproxy_url}https://images.evetech.net/types/#{type_id}/bp?size=64"
+    elsif bpc?
+      "#{imageproxy_url}https://images.evetech.net/types/#{type_id}/bpc?size=64"
+    end
+  end
+
+  # def character_copying_time_formatted
+  #   HumanTime.new(CharacterManufacturingCopyTime.new(@character, @blueprint.blueprint.copying_time).manufacturing_copy_time.round).long_formatted
+  # end
 end
