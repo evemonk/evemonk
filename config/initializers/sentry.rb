c0cab94c38a90a7f9ce7b156ed6f82afaf2fc000
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-31 01:22:22 +0200

    Add sentry config

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+Sentry.init do |config|
+  config.dsn = ENV["SENTRY_DSN"]
+  config.breadcrumbs_logger = [:active_support_logger]
+  config.traces_sample_rate = 0.1
+end
