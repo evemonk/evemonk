1b527966d91a4bbd0e7adb98ee8dea141635cf9d
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-31 01:38:57 +0200

    Move code from decorator

--- app/models/eve/agent.rb
+++ app/models/eve/agent.rb
@@ -15,5 +15,29 @@ module Eve
     # belongs_to :division_id
 
     has_many :standings, as: :standingable
+
+    def icon_tiny
+      "#{imageproxy_url}https://images.evetech.net/characters/#{agent_id}/portrait?size=32"
+    end
+
+    def icon_small
+      "#{imageproxy_url}https://images.evetech.net/characters/#{agent_id}/portrait?size=64"
+    end
+
+    def icon_medium
+      "#{imageproxy_url}https://images.evetech.net/characters/#{agent_id}/portrait?size=128"
+    end
+
+    def icon_large
+      "#{imageproxy_url}https://images.evetech.net/characters/#{agent_id}/portrait?size=256"
+    end
+
+    def icon_huge
+      "#{imageproxy_url}https://images.evetech.net/characters/#{agent_id}/portrait?size=512"
+    end
+
+    def icon_gigantic
+      "#{imageproxy_url}https://images.evetech.net/characters/#{agent_id}/portrait?size=1024"
+    end
   end
 end
