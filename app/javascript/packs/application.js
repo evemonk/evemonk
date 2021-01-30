14852eb7b7b7c6045066decebb0bf9470d66623c
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-30 02:39:10 +0200

    Remove vuejs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,10 +31,6 @@ import debounce from "lodash.debounce";
 
 window.ApexCharts = ApexCharts;
 window.$ = $;
-window.Vue = Vue;
-window.debounce = debounce;
-
-Vue.component("v-select", vSelect);
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
