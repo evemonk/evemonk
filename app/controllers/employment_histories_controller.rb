04b2511888c77d4185121e12e9620a7cabfe6874
Igor Zubkov <igor.zubkov@gmail.com>
2021-02-01 00:27:19 +0200

    Cleanup

--- app/controllers/employment_histories_controller.rb
+++ app/controllers/employment_histories_controller.rb
@@ -5,11 +5,9 @@ class EmploymentHistoriesController < ApplicationController
     @character = current_user.characters
       .includes(:alliance, :corporation)
       .find_by!(character_id: params[:character_id])
-      .decorate
 
     @character_corporation_histories = CharacterCorporationHistory.where(character_id: @character.character_id)
       .includes(:corporation)
       .order(start_date: :desc)
-      .decorate
   end
 end
