5ea4710e1a58ae7211155f2c351169c70079ec04
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-31 22:49:42 +0200

    Update specs

--- spec/models/application_record_spec.rb
+++ spec/models/application_record_spec.rb
@@ -3,19 +3,7 @@
 require "rails_helper"
 
 describe ApplicationRecord do
+  it { should be_a(ImageProxy) }
+
   it { expect(described_class.abstract_class).to eq(true) }
-
-  describe "#imageproxy_url" do
-    context "when Setting.use_image_proxy is true" do
-      before { Setting.use_image_proxy = true }
-
-      specify { expect(subject.imageproxy_url).to eq("https://imageproxy.evemonk.com/") }
-    end
-
-    context "when Setting.use_image_proxy is false" do
-      before { Setting.use_image_proxy = false }
-
-      specify { expect(subject.imageproxy_url).to eq(nil) }
-    end
-  end
 end
