c49a1ed9e769ab0a1da8eda809aa479aaf95a2bd
Igor Zubkov <igor.zubkov@gmail.com>
2021-02-01 00:19:44 +0200

    Cleanup

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -5,7 +5,6 @@ class AssetsController < ApplicationController
     @character = current_user.characters
       .includes(:alliance, :corporation)
       .find_by!(character_id: params[:character_id])
-      .decorate
 
     @assets = @character.character_assets
       .includes(:type)
