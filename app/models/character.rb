7b20618b7808bbcd6d373338e92d83f15aaef0dd
Igor Zubkov <igor.zubkov@gmail.com>
2021-02-01 01:17:39 +0200

    Move code from decorator

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Character < ApplicationRecord
+  include ImageProxy
+
   has_paper_trail
 
   belongs_to :user
@@ -172,4 +174,28 @@ class Character < ApplicationRecord
   # def omega?
   #   true
   # end
+
+  def icon_tiny
+    "#{imageproxy_url}https://images.evetech.net/characters/#{character_id}/portrait?size=32"
+  end
+
+  def icon_small
+    "#{imageproxy_url}https://images.evetech.net/characters/#{character_id}/portrait?size=64"
+  end
+
+  def icon_medium
+    "#{imageproxy_url}https://images.evetech.net/characters/#{character_id}/portrait?size=128"
+  end
+
+  def icon_large
+    "#{imageproxy_url}https://images.evetech.net/characters/#{character_id}/portrait?size=256"
+  end
+
+  def icon_huge
+    "#{imageproxy_url}https://images.evetech.net/characters/#{character_id}/portrait?size=512"
+  end
+
+  def icon_gigantic
+    "#{imageproxy_url}https://images.evetech.net/characters/#{character_id}/portrait?size=1024"
+  end
 end
--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -5,6 +5,8 @@ require "rails_helper"
 describe Character do
   it { should be_an(ApplicationRecord) }
 
+  it { should be_a(ImageProxy) }
+
   it { should respond_to(:versions) }
 
   it { should belong_to(:user) }
@@ -363,6 +365,120 @@ describe Character do
       subject { build(:character, token_expires_at: Time.zone.now + 1.hour) }
 
       specify { expect(subject.token_expired?).to eq(false) }
+    end
+  end
+
+  describe "#icon_tiny" do
+    subject do
+      build(:character,
+        character_id: 90_729_314)
+    end
+
+    context "when Setting.use_image_proxy is true" do
+      before { Setting.use_image_proxy = true }
+
+      specify { expect(subject.icon_tiny).to eq("https://imageproxy.evemonk.com/https://images.evetech.net/characters/90729314/portrait?size=32") }
+    end
+
+    context "when Setting.use_image_proxy is false" do
+      before { Setting.use_image_proxy = false }
+
+      specify { expect(subject.icon_tiny).to eq("https://images.evetech.net/characters/90729314/portrait?size=32") }
+    end
+  end
+
+  describe "#icon_small" do
+    subject do
+      build(:character,
+        character_id: 90_729_314)
+    end
+
+    context "when Setting.use_image_proxy is true" do
+      before { Setting.use_image_proxy = true }
+
+      specify { expect(subject.icon_small).to eq("https://imageproxy.evemonk.com/https://images.evetech.net/characters/90729314/portrait?size=64") }
+    end
+
+    context "when Setting.use_image_proxy is false" do
+      before { Setting.use_image_proxy = false }
+
+      specify { expect(subject.icon_small).to eq("https://images.evetech.net/characters/90729314/portrait?size=64") }
+    end
+  end
+
+  describe "#icon_medium" do
+    subject do
+      build(:character,
+        character_id: 90_729_314)
+    end
+
+    context "when Setting.use_image_proxy is true" do
+      before { Setting.use_image_proxy = true }
+
+      specify { expect(subject.icon_medium).to eq("https://imageproxy.evemonk.com/https://images.evetech.net/characters/90729314/portrait?size=128") }
+    end
+
+    context "when Setting.use_image_proxy is false" do
+      before { Setting.use_image_proxy = false }
+
+      specify { expect(subject.icon_medium).to eq("https://images.evetech.net/characters/90729314/portrait?size=128") }
+    end
+  end
+
+  describe "#icon_large" do
+    subject do
+      build(:character,
+        character_id: 90_729_314)
+    end
+
+    context "when Setting.use_image_proxy is true" do
+      before { Setting.use_image_proxy = true }
+
+      specify { expect(subject.icon_large).to eq("https://imageproxy.evemonk.com/https://images.evetech.net/characters/90729314/portrait?size=256") }
+    end
+
+    context "when Setting.use_image_proxy is false" do
+      before { Setting.use_image_proxy = false }
+
+      specify { expect(subject.icon_large).to eq("https://images.evetech.net/characters/90729314/portrait?size=256") }
+    end
+  end
+
+  describe "#icon_huge" do
+    subject do
+      build(:character,
+        character_id: 90_729_314)
+    end
+
+    context "when Setting.use_image_proxy is true" do
+      before { Setting.use_image_proxy = true }
+
+      specify { expect(subject.icon_huge).to eq("https://imageproxy.evemonk.com/https://images.evetech.net/characters/90729314/portrait?size=512") }
+    end
+
+    context "when Setting.use_image_proxy is false" do
+      before { Setting.use_image_proxy = false }
+
+      specify { expect(subject.icon_huge).to eq("https://images.evetech.net/characters/90729314/portrait?size=512") }
+    end
+  end
+
+  describe "#icon_gigantic" do
+    subject do
+      build(:character,
+        character_id: 90_729_314)
+    end
+
+    context "when Setting.use_image_proxy is true" do
+      before { Setting.use_image_proxy = true }
+
+      specify { expect(subject.icon_gigantic).to eq("https://imageproxy.evemonk.com/https://images.evetech.net/characters/90729314/portrait?size=1024") }
+    end
+
+    context "when Setting.use_image_proxy is false" do
+      before { Setting.use_image_proxy = false }
+
+      specify { expect(subject.icon_gigantic).to eq("https://images.evetech.net/characters/90729314/portrait?size=1024") }
     end
   end
 end
