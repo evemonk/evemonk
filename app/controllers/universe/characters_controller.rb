44de1b661bed827cb647a9d7e2467396f2bf4933
Igor Zubkov <igor.zubkov@gmail.com>
2021-02-01 00:38:39 +0200

    Cleanup

--- app/controllers/universe/characters_controller.rb
+++ app/controllers/universe/characters_controller.rb
@@ -9,19 +9,16 @@ module Universe
         .new(params[:q])
         .search
         .page(params[:page])
-        .decorate
     end
 
     def show
       @character = ::Eve::Character
         .includes(:alliance, :corporation)
         .find_by!(character_id: params[:id])
-        .decorate
 
       @character_corporation_histories = Eve::CharacterCorporationHistory.where(character_id: @character.character_id)
         .includes(:corporation)
         .order(start_date: :desc)
-        .decorate
     end
   end
 end
