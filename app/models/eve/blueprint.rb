89d8d1e334bb24645ed9414a4d00f621f35f6454
Igor Zubkov <igor.zubkov@gmail.com>
2021-01-31 23:05:16 +0200

    Move code from decorator

--- app/models/eve/blueprint.rb
+++ app/models/eve/blueprint.rb
@@ -27,5 +27,21 @@ module Eve
     has_many :blueprint_manufacturing_skills,
       primary_key: "type_id",
       dependent: :destroy
+
+    def copying_time_formatted
+      HumanTime.new(copying_time).long_formatted
+    end
+
+    # def time_copying_modifier
+    #   27.11
+    # end
+
+    def icon_tiny
+      "#{imageproxy_url}https://images.evetech.net/types/#{type_id}/bp?size=32"
+    end
+
+    def icon_small
+      "#{imageproxy_url}https://images.evetech.net/types/#{type_id}/bp?size=64"
+    end
   end
 end
