fa093d07b206479a941ad50af3946cde42b6cfe7
Igor Zubkov <igor.zubkov@gmail.com>
2021-02-01 00:28:31 +0200

    Cleanup

--- app/controllers/industry_jobs_controller.rb
+++ app/controllers/industry_jobs_controller.rb
@@ -5,12 +5,10 @@ class IndustryJobsController < ApplicationController
     # @character = current_user.characters
     #   .includes(:alliance, :corporation)
     #   .find_by!(character_id: params[:character_id])
-    #   .decorate
     #
     # @industry_jobs = @character.industry_jobs
     #   .includes(:blueprint_type)
     #   .page(params[:page])
     #   .per(10)
-    #   .decorate
   end
 end
