6a546bf89712ff86ddbba92a330f4bfdca5dc336
Igor Zubkov <igor.zubkov@gmail.com>
2021-02-01 00:21:03 +0200

    Cleanup

--- spec/controllers/blueprints_controller_spec.rb
+++ spec/controllers/blueprints_controller_spec.rb
@@ -22,7 +22,6 @@ describe BlueprintsController do
         # current_user.characters
         #             .includes(:alliance, :corporation)
         #             .find_by!(character_id: params[:character_id])
-        #             .decorate
         #
         expect(subject).to receive(:current_user) do
           double.tap do |a|
@@ -30,11 +29,7 @@ describe BlueprintsController do
               double.tap do |b|
                 expect(b).to receive(:includes).with(:alliance, :corporation) do
                   double.tap do |c|
-                    expect(c).to receive(:find_by!).with(character_id: "1") do
-                      double.tap do |d|
-                        expect(d).to receive(:decorate).and_return(character)
-                      end
-                    end
+                    expect(c).to receive(:find_by!).with(character_id: "1").and_return(character)
                   end
                 end
               end
@@ -84,7 +79,6 @@ describe BlueprintsController do
         # current_user.characters
         #             .includes(:alliance, :corporation)
         #             .find_by!(character_id: params[:character_id])
-        #             .decorate
         #
         expect(subject).to receive(:current_user) do
           double.tap do |a|
@@ -92,11 +86,7 @@ describe BlueprintsController do
               double.tap do |b|
                 expect(b).to receive(:includes).with(:alliance, :corporation) do
                   double.tap do |c|
-                    expect(c).to receive(:find_by!).with(character_id: "1") do
-                      double.tap do |d|
-                        expect(d).to receive(:decorate).and_return(character)
-                      end
-                    end
+                    expect(c).to receive(:find_by!).with(character_id: "1").and_return(character)
                   end
                 end
               end
