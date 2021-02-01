2d49990efd63a412fea3e1cb88ea09ccd812810f
Igor Zubkov <igor.zubkov@gmail.com>
2021-02-01 13:12:36 +0200

    Move

--- app/models/skillqueue.rb
+++ app/models/skillqueue.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Skillqueue < ApplicationRecord
+  LEVELS = {1 => "I", 2 => "II", 3 => "III", 4 => "IV", 5 => "V"}.freeze
+
   belongs_to :character
 
   belongs_to :skill,
@@ -8,4 +10,18 @@ class Skillqueue < ApplicationRecord
     primary_key: "type_id",
     foreign_key: "skill_id",
     optional: true
+
+  def finished_level
+    LEVELS.fetch(object.finished_level)
+  end
+
+  def time_left
+    seconds = if start_date.past?
+      finish_date.to_i - Time.zone.now.to_i
+    else
+      finish_date.to_i - start_date.to_i
+    end
+
+    HumanTime.new(seconds).very_short_formatted
+  end
 end
--- spec/models/skillqueue_spec.rb
+++ spec/models/skillqueue_spec.rb
@@ -5,7 +5,43 @@ require "rails_helper"
 describe Skillqueue do
   it { should be_an(ApplicationRecord) }
 
+  specify do
+    expect(described_class::LEVELS).to eq(1 => "I", 2 => "II", 3 => "III", 4 => "IV", 5 => "V")
+  end
+
   it { should belong_to(:character) }
 
   it { should belong_to(:skill).class_name("Eve::Type").with_primary_key("type_id").with_foreign_key("skill_id").optional(true) }
+
+  describe "#finished_level" do
+    subject { build(:skillqueue, finished_level: 1) }
+
+    specify { expect(subject.finished_level).to eq("I") }
+  end
+
+  describe "#time_left" do
+    before { travel_to Time.zone.now }
+
+    after { travel_back }
+
+    context "when start_date in past" do
+      subject do
+        build(:skillqueue,
+          start_date: Time.zone.now - 10.minutes,
+          finish_date: Time.zone.now + 10.minutes)
+      end
+
+      specify { expect(subject.time_left).to eq("10m") }
+    end
+
+    context "when start_date in future" do
+      subject do
+        build(:skillqueue,
+          start_date: Time.zone.now + 10.minutes,
+          finish_date: Time.zone.now + 30.minutes)
+      end
+
+      specify { expect(subject.time_left).to eq("20m") }
+    end
+  end
 end
