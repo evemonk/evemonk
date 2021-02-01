74cc7b0791d3aed8092d73ef4be870562e93c551
Igor Zubkov <igor.zubkov@gmail.com>
2021-02-01 23:14:12 +0200

    Move

--- app/models/eve/alliance.rb
+++ app/models/eve/alliance.rb
@@ -2,6 +2,9 @@
 
 module Eve
   class Alliance < ApplicationRecord
+    include ActionView::Helpers::NumberHelper
+    include ImageProxy
+
     has_paper_trail
 
     searchkick
@@ -52,6 +55,34 @@ module Eve
 
     def reset_characters_count
       update_columns(characters_count: corporations.sum(:member_count))
+    end
+
+    def icon_tiny
+      "#{imageproxy_url}https://images.evetech.net/alliances/#{alliance_id}/logo?size=32"
+    end
+
+    def icon_small
+      "#{imageproxy_url}https://images.evetech.net/alliances/#{alliance_id}/logo?size=64"
+    end
+
+    def icon_medium
+      "#{imageproxy_url}https://images.evetech.net/alliances/#{alliance_id}/logo?size=128"
+    end
+
+    def icon_large
+      "#{imageproxy_url}https://images.evetech.net/alliances/#{alliance_id}/logo?size=256"
+    end
+
+    def icon_huge
+      "#{imageproxy_url}https://images.evetech.net/alliances/#{alliance_id}/logo?size=512"
+    end
+
+    def formatted_corporations_count
+      number_with_delimiter(corporations_count, delimiter: " ")
+    end
+
+    def formatted_characters_count
+      number_with_delimiter(characters_count, delimiter: " ")
     end
   end
 end
--- spec/models/eve/alliance_spec.rb
+++ spec/models/eve/alliance_spec.rb
@@ -5,6 +5,8 @@ require "rails_helper"
 describe Eve::Alliance do
   it { should be_an(ApplicationRecord) }
 
+  it { should be_a(ImageProxy) }
+
   it { should respond_to(:versions) }
 
   it { expect(described_class).to respond_to(:search) }
@@ -70,5 +72,140 @@ describe Eve::Alliance do
     before { subject.update!(characters_count: 0) }
 
     specify { expect { subject.reset_characters_count }.to change { subject.characters_count }.from(0).to(246) }
+  end
+
+  describe "#icon_tiny" do
+    subject do
+      build(:eve_alliance,
+        alliance_id: 1_354_830_081)
+    end
+
+    context "when Setting.use_image_proxy is true" do
+      before { Setting.use_image_proxy = true }
+
+      specify { expect(subject.icon_tiny).to eq("https://imageproxy.evemonk.com/https://images.evetech.net/alliances/1354830081/logo?size=32") }
+    end
+
+    context "when Setting.use_image_proxy is false" do
+      before { Setting.use_image_proxy = false }
+
+      specify { expect(subject.icon_tiny).to eq("https://images.evetech.net/alliances/1354830081/logo?size=32") }
+    end
+  end
+
+  describe "#icon_small" do
+    subject do
+      build(:eve_alliance,
+        alliance_id: 1_354_830_081)
+    end
+
+    context "when Setting.use_image_proxy is true" do
+      before { Setting.use_image_proxy = true }
+
+      specify { expect(subject.icon_small).to eq("https://imageproxy.evemonk.com/https://images.evetech.net/alliances/1354830081/logo?size=64") }
+    end
+
+    context "when Setting.use_image_proxy is false" do
+      before { Setting.use_image_proxy = false }
+
+      specify { expect(subject.icon_small).to eq("https://images.evetech.net/alliances/1354830081/logo?size=64") }
+    end
+  end
+
+  describe "#icon_medium" do
+    subject do
+      build(:eve_alliance,
+        alliance_id: 1_354_830_081)
+    end
+
+    context "when Setting.use_image_proxy is true" do
+      before { Setting.use_image_proxy = true }
+
+      specify { expect(subject.icon_medium).to eq("https://imageproxy.evemonk.com/https://images.evetech.net/alliances/1354830081/logo?size=128") }
+    end
+
+    context "when Setting.use_image_proxy is false" do
+      before { Setting.use_image_proxy = false }
+
+      specify { expect(subject.icon_medium).to eq("https://images.evetech.net/alliances/1354830081/logo?size=128") }
+    end
+  end
+
+  describe "#icon_large" do
+    subject do
+      build(:eve_alliance,
+        alliance_id: 1_354_830_081)
+    end
+
+    context "when Setting.use_image_proxy is true" do
+      before { Setting.use_image_proxy = true }
+
+      specify { expect(subject.icon_large).to eq("https://imageproxy.evemonk.com/https://images.evetech.net/alliances/1354830081/logo?size=256") }
+    end
+
+    context "when Setting.use_image_proxy is false" do
+      before { Setting.use_image_proxy = false }
+
+      specify { expect(subject.icon_large).to eq("https://images.evetech.net/alliances/1354830081/logo?size=256") }
+    end
+  end
+
+  describe "#icon_huge" do
+    subject do
+      build(:eve_alliance,
+        alliance_id: 1_354_830_081)
+    end
+
+    context "when Setting.use_image_proxy is true" do
+      before { Setting.use_image_proxy = true }
+
+      specify { expect(subject.icon_huge).to eq("https://imageproxy.evemonk.com/https://images.evetech.net/alliances/1354830081/logo?size=512") }
+    end
+
+    context "when Setting.use_image_proxy is false" do
+      before { Setting.use_image_proxy = false }
+
+      specify { expect(subject.icon_huge).to eq("https://images.evetech.net/alliances/1354830081/logo?size=512") }
+    end
+  end
+
+  describe "#formatted_corporations_count" do
+    context "when number is 3" do
+      subject do
+        build(:eve_alliance,
+          corporations_count: 111)
+      end
+
+      specify { expect(subject.formatted_corporations_count).to eq("111") }
+    end
+
+    context "when number is 6" do
+      subject do
+        build(:eve_alliance,
+          corporations_count: 111_222)
+      end
+
+      specify { expect(subject.formatted_corporations_count).to eq("111 222") }
+    end
+  end
+
+  describe "#formatted_characters_count" do
+    context "when number is 3" do
+      subject do
+        build(:eve_alliance,
+          characters_count: 111)
+      end
+
+      specify { expect(subject.formatted_characters_count).to eq("111") }
+    end
+
+    context "when number is 6" do
+      subject do
+        build(:eve_alliance,
+          characters_count: 111_222)
+      end
+
+      specify { expect(subject.formatted_characters_count).to eq("111 222") }
+    end
   end
 end
