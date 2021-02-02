d9123c0ad42df58178e39af65a4f6e19918b210f
Igor Zubkov <igor.zubkov@gmail.com>
2021-02-03 00:05:00 +0200

    Cleanup

--- app/decorators/eve/corporation_decorator.rb
+++ app/decorators/eve/corporation_decorator.rb
@@ -2,10 +2,6 @@
 
 module Eve
   class CorporationDecorator
-    def date_founded
-      object.date_founded&.iso8601
-    end
-
     def tax_rate
       object.tax_rate.to_s
     end
--- spec/decorators/eve/corporation_decorator_spec.rb
+++ spec/decorators/eve/corporation_decorator_spec.rb
@@ -3,30 +3,6 @@
 require "rails_helper"
 
 describe Eve::CorporationDecorator do
-  xdescribe "#date_founded" do
-    context "when date_founded is empty" do
-      let(:eve_corporation) do
-        build(:eve_corporation,
-          date_founded: nil)
-      end
-
-      subject { eve_corporation.decorate }
-
-      specify { expect(subject.date_founded).to eq(nil) }
-    end
-
-    context "when date_founded is present" do
-      let(:eve_corporation) do
-        build(:eve_corporation,
-          date_founded: "Sun, 03 May 2015 19:45:17 UTC +00:00")
-      end
-
-      subject { eve_corporation.decorate }
-
-      specify { expect(subject.date_founded).to eq("2015-05-03T19:45:17Z") }
-    end
-  end
-
   xdescribe "#tax_rate" do
     let(:eve_corporation) do
       build(:eve_corporation,
