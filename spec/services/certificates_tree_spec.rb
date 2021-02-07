c93b65779f50179eec49e7499a4c2848d782a1dc
Igor Zubkov <igor.zubkov@gmail.com>
2021-02-07 16:33:17 +0200

    Add spec

--- spec/services/certificates_tree_spec.rb
+++ spec/services/certificates_tree_spec.rb
@@ -10,4 +10,16 @@ describe CertificatesTree do
   describe "#initialize" do
     its(:character) { should eq(character) }
   end
+
+  describe "#group_ids" do
+    let!(:eve_certificate1) { create(:eve_certificate, group_id: 1) }
+
+    let!(:eve_certificate2) { create(:eve_certificate, group_id: 2) }
+
+    let!(:eve_certificate3) { create(:eve_certificate, group_id: 3) }
+
+    let!(:eve_certificate4) { create(:eve_certificate, group_id: 3) }
+
+    specify { expect(subject.group_ids).to eq([1, 2, 3]) }
+  end
 end
