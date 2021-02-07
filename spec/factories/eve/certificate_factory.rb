20511214c81cb0823c1b44b182dc8011feaae69a
Igor Zubkov <igor.zubkov@gmail.com>
2021-02-07 16:31:17 +0200

    Add eve certificate factory

---
+++ spec/factories/eve/certificate_factory.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :eve_certificate, class: Eve::Certificate do
+    sequence(:certificate_id)
+
+    description { Faker::Lorem.paragraph }
+
+    sequence(:group_id)
+
+    name { Faker::Lorem.word }
+  end
+end
