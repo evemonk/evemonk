713ff120329776c6196321f3bb69100b61695074
Igor Zubkov <igor.zubkov@gmail.com>
2021-02-07 15:49:45 +0200

    Add more specs

--- spec/controllers/certificates_controller_spec.rb
+++ spec/controllers/certificates_controller_spec.rb
@@ -9,6 +9,42 @@ describe CertificatesController do
 
   describe "#index" do
     context "when user signed in" do
+      let(:user) { create(:user) }
+
+      before { sign_in(user) }
+
+      before { expect(subject).to receive(:current_user_locale) }
+
+      let(:character) { instance_double(Character) }
+
+      before do
+        #
+        # current_user.characters
+        #             .includes(:alliance, :corporation)
+        #             .find_by!(character_id: params[:character_id]) # => character
+        #
+        expect(subject).to receive(:current_user) do
+          double.tap do |a|
+            expect(a).to receive(:characters) do
+              double.tap do |b|
+                expect(b).to receive(:includes).with(:alliance, :corporation) do
+                  double.tap do |c|
+                    expect(c).to receive(:find_by!).with(character_id: "1").and_return(character)
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+
+      before { expect(CertificatesTree).to receive(:new).with(character) }
+
+      before { get :index, params: {character_id: "1", page: "2"} }
+
+      it { should respond_with(:ok) }
+
+      it { should render_template(:index) }
     end
 
     context "when user not signed in" do
