92176551ad15275069e64d390573ef50edc1ab7b
Igor Zubkov <igor.zubkov@gmail.com>
2021-02-08 23:53:28 +0200

    Update factory

--- spec/factories/eve/certificate_recommended_type_factory.rb
+++ spec/factories/eve/certificate_recommended_type_factory.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
-  factory :eve_certificate_recommended_type, class: 'Eve::CertificateRecommendedType' do
-    certificate_id { "" }
-    type_id { "" }
+  factory :eve_certificate_recommended_type, class: Eve::CertificateRecommendedType do
+    sequence(:certificate_id)
+
+    sequence(:type_id)
   end
 end
