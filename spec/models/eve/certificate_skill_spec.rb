4c6e5e5c89357c0f58aae155d9ca1c23c1adb052
Igor Zubkov <igor.zubkov@gmail.com>
2021-02-11 22:08:31 +0200

    Fix spec

--- spec/models/eve/certificate_skill_spec.rb
+++ spec/models/eve/certificate_skill_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 describe Eve::CertificateSkill do
   it { should be_an(ApplicationRecord) }
 
-  it { expect(described_class.table_name).to eq("eve_certificates") }
+  it { expect(described_class.table_name).to eq("eve_certificate_skills") }
 
   it { should belong_to(:certificate) }
 
