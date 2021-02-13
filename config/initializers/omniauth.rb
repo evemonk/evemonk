0182fde2b818f2eb21e31e7bac3134968f78ef74
Igor Zubkov <igor.zubkov@gmail.com>
2021-02-14 00:58:27 +0200

    Add esi-calendar.read_calendar_events.v1 scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -16,7 +16,8 @@ SCOPES = ["esi-wallet.read_character_wallet.v1",
   "esi-killmails.read_killmails.v1",
   "esi-industry.read_character_jobs.v1",
   "esi-markets.read_character_orders.v1",
-  "esi-corporations.read_corporation_membership.v1"].freeze
+  "esi-corporations.read_corporation_membership.v1",
+  "esi-calendar.read_calendar_events.v1"].freeze
 
 OmniAuth.config.full_host = "https://evemonk.com" if Rails.env.production?
 
