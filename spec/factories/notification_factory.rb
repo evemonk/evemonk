d367bac075cc39db5dca012d4af5cb263a37e6b3
Igor Zubkov <igor.zubkov@gmail.com>
2021-03-04 19:12:29 +0200

    Update factory

--- spec/factories/notification_factory.rb
+++ spec/factories/notification_factory.rb
@@ -2,9 +2,12 @@
 
 FactoryBot.define do
   factory :notification do
-    recipient { nil }
-    type { "" }
-    params { "" }
-    read_at { "2021-03-04 17:59:23" }
+    association :recipient, factory: :character
+
+    type { "" } # TODO: update
+
+    params { "" } # TODO: update
+
+    read_at { "2021-03-04 17:59:23" } # TODO: update
   end
 end
