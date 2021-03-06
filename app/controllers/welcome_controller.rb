2e545a9791bbf8647de13dff9d4d697d8e03b54a
Igor Zubkov <igor.zubkov@gmail.com>
2021-03-06 22:40:06 +0200

    Update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,9 +4,6 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    # @alliances = ::Eve::Alliance.order(characters_count: :desc).limit(20)
-    # @corporations = ::Eve::Corporation.not_npc.order(member_count: :desc).limit(20)
-
     action = WelcomeIndexAction.new.perform
     @alliances = action.alliances
     @corporations = action.corporations
