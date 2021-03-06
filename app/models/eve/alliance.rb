08f81d16c4a5de771bd0d98ea07360ad22423874
Igor Zubkov <igor.zubkov@gmail.com>
2021-03-06 22:34:26 +0200

    Update

--- app/models/eve/alliance.rb
+++ app/models/eve/alliance.rb
@@ -2,6 +2,9 @@
 
 module Eve
   class Alliance < Upgrow::Model
+    include ActionView::Helpers::NumberHelper
+    include ImageProxy
+
     attribute :alliance_id
     attribute :creator_corporation_id
     attribute :creator_id
@@ -12,5 +15,33 @@ module Eve
     attribute :ticker
     attribute :corporations_count
     attribute :characters_count
+
+    def icon_tiny
+      "#{imageproxy_url}https://images.evetech.net/alliances/#{alliance_id}/logo?size=32"
+    end
+
+    def icon_small
+      "#{imageproxy_url}https://images.evetech.net/alliances/#{alliance_id}/logo?size=64"
+    end
+
+    def icon_medium
+      "#{imageproxy_url}https://images.evetech.net/alliances/#{alliance_id}/logo?size=128"
+    end
+
+    def icon_large
+      "#{imageproxy_url}https://images.evetech.net/alliances/#{alliance_id}/logo?size=256"
+    end
+
+    def icon_huge
+      "#{imageproxy_url}https://images.evetech.net/alliances/#{alliance_id}/logo?size=512"
+    end
+
+    def formatted_corporations_count
+      number_with_delimiter(corporations_count, delimiter: " ")
+    end
+
+    def formatted_characters_count
+      number_with_delimiter(characters_count, delimiter: " ")
+    end
   end
 end
