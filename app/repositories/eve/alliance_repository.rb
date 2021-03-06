fcef51efc8210583c164287c9fba749766109b95
Igor Zubkov <igor.zubkov@gmail.com>
2021-03-06 22:23:57 +0200

    Update repositories

--- app/repositories/eve/alliance_repository.rb
+++ app/repositories/eve/alliance_repository.rb
@@ -3,7 +3,15 @@
 module Eve
   class AllianceRepository
     def top20
+      AllianceRecord.order(characters_count: :desc).limit(20).map do |record|
+        to_model(record.attributes)
+      end
+    end
 
+    private
+
+    def to_model(attributes)
+      Alliance.new(**attributes.symbolize_keys)
     end
   end
 end
--- app/repositories/eve/corporation_repository.rb
+++ app/repositories/eve/corporation_repository.rb
@@ -3,7 +3,15 @@
 module Eve
   class CorporationRepository
     def top20
+      CorporationRecord.where(npc: false).order(member_count: :desc).limit(20).map do |record|
+        to_model(record.attributes)
+      end
+    end
 
+    private
+
+    def to_model(attributes)
+      Corporation.new(**attributes.symbolize_keys)
     end
   end
 end
