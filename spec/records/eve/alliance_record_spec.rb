3b93f4c5b85f640c9514fff72b788bc44facd4a4
Igor Zubkov <igor.zubkov@gmail.com>
2021-03-07 13:35:45 +0200

    Update

--- spec/records/eve/alliance_record_spec.rb
+++ spec/records/eve/alliance_record_spec.rb
@@ -2,8 +2,14 @@
 
 require "rails_helper"
 
-describe Eve::AllianceRecord do
+describe Eve::AllianceRecord, type: :model do
   it { should be_an(ApplicationRecord) }
 
   it { expect(described_class.table_name).to eq("eve_alliances") }
+
+  # TODO: more
+
+  it { should have_many(:corporation_records).with_primary_key("alliance_id").with_foreign_key("alliance_id") }
+
+  # TODO: more
 end
