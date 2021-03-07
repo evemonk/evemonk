f35045be0860d12c01bfb9a009f3ab7decbca4c9
Igor Zubkov <igor.zubkov@gmail.com>
2021-03-07 12:33:32 +0200

    Add spec

---
+++ spec/records/eve/alliance_record_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::AllianceRecord do
+  it { should be_an(ApplicationRecord) }
+
+  it { expect(described_class.table_name).to eq("eve_alliances") }
+end
