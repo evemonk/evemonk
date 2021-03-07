0a991345fa67a1829846c6a17cf8dc04673a42f8
Igor Zubkov <igor.zubkov@gmail.com>
2021-03-07 14:11:08 +0200

    Update

--- app/repositories/eve/etag_repository.rb
+++ app/repositories/eve/etag_repository.rb
@@ -3,6 +3,24 @@
 module Eve
   class EtagRepository
     class << self
+      def find_by_url(url)
+        record = EtagRecord.find_or_initialize_by(url: url)
+        to_model(record.attributes)
+      end
+
+      def update_by_url(url, input)
+        record = EtagRecord.find_or_initialize_by(url: url)
+        record.update!(url: input.url,
+                       etag: input.etag,
+                       body: input.body)
+        to_model(record.attributes)
+      end
+
+      private
+
+      def to_model(attributes)
+        Etag.new(**attributes.symbolize_keys)
+      end
     end
   end
 end
