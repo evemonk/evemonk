370d89c78f6e33f255167c057c43468786ef653f
Igor Zubkov <igor.zubkov@gmail.com>
2021-03-07 15:21:57 +0200

    Turn off prosopite gem

--- app/actions/welcome_index_action.rb
+++ app/actions/welcome_index_action.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-class WelcomeIndexAction < Upgrow::Action
-  result :alliances, :corporations
+module Welcome
+  class IndexAction < Upgrow::Action
+    result :alliances, :corporations
 
-  def perform
-    result.success(alliances: Eve::AllianceRepository.new.top20,
-                   corporations: Eve::CorporationRepository.new.top20)
+    def perform
+      result.success(alliances: Eve::AllianceRepository.new.top20,
+                     corporations: Eve::CorporationRepository.new.top20)
+    end
   end
 end
