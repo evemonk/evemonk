0b939b2e3a3b1c75787d3565773188bfbabf6114
Igor Zubkov <igor.zubkov@gmail.com>
2021-03-07 14:59:34 +0200

    Update

--- app/importers/eve/alliances_corporations_importer.rb
+++ app/importers/eve/alliances_corporations_importer.rb
@@ -3,7 +3,7 @@
 module Eve
   class AlliancesCorporationsImporter
     def import
-      Eve::Alliance.pluck(:alliance_id).each do |alliance_id|
+      Eve::AllianceRepository.alliance_ids.each do |alliance_id|
         Eve::UpdateAllianceCorporationsJob.perform_later(alliance_id)
       end
     end
