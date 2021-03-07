0d481000fe4176b0a6374ed46b2e3cec2a7490da
Igor Zubkov <igor.zubkov@gmail.com>
2021-03-07 23:48:07 +0200

    Update

--- app/models/eve/faction.rb
+++ app/models/eve/faction.rb
@@ -2,6 +2,8 @@
 
 module Eve
   class Faction < Upgrow::Model
+    include ImageProxy
+
     attribute :faction_id
     attribute :corporation_id
     attribute :description
@@ -12,5 +14,17 @@ module Eve
     attribute :solar_system_id
     attribute :station_count
     attribute :station_system_count
+
+    def icon_tiny
+      "#{imageproxy_url}https://images.evetech.net/corporations/#{faction_id}/logo?size=32"
+    end
+
+    def icon_small
+      "#{imageproxy_url}https://images.evetech.net/corporations/#{faction_id}/logo?size=64"
+    end
+
+    def icon_medium
+      "#{imageproxy_url}https://images.evetech.net/corporations/#{faction_id}/logo?size=128"
+    end
   end
 end
