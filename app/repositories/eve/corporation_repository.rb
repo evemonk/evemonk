152411e5035cbc72282b62bf8d9ef5dcf406be87
Igor Zubkov <igor.zubkov@gmail.com>
2021-03-07 14:57:45 +0200

    Update

--- app/repositories/eve/corporation_repository.rb
+++ app/repositories/eve/corporation_repository.rb
@@ -3,6 +3,32 @@
 module Eve
   class CorporationRepository
     class << self
+      def update(corporation_id, input)
+        record = CorporationRecord.find_or_initialize_by(corporation_id: corporation_id)
+        record.update!(corporation_id: input.corporation_id,
+                       alliance_id: input.alliance_id,
+                       ceo_id: input.ceo_id,
+                       creator_id: input.creator_id,
+                       date_founded: input.date_founded,
+                       description: input.description,
+                       faction_id: input.faction_id,
+                       home_station_id: input.home_station_id,
+                       member_count: input.member_count,
+                       name: input.name,
+                       shares: input.shares,
+                       tax_rate: input.tax_rate,
+                       ticker: input.ticker,
+                       corporation_url: input.corporation_url,
+                       war_eligible: input.war_eligible,
+                       npc: input.npc)
+        to_model(record.attributes)
+      end
+
+      def destroy(corporation_id)
+        record = CorporationRecord.find_or_initialize_by(corporation_id: corporation_id)
+        record.destroy!
+      end
+
       def top20
         CorporationRecord.where(npc: false).order(member_count: :desc).limit(20).map do |record|
           to_model(record.attributes)
