483245aee0523589b758e562248d9c8a86acb080
Igor Zubkov <igor.zubkov@gmail.com>
2021-03-08 14:00:35 +0200

    Update

---
+++ app/inputs/eve/character_input.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Eve
+  class CharacterInput < Upgrow::Input
+    attribute :character_id
+    attribute :alliance_id
+    attribute :ancestry_id
+    attribute :birthday
+    attribute :bloodline_id
+    attribute :corporation_id
+    attribute :description
+    attribute :faction_id
+    attribute :gender
+    attribute :name
+    attribute :race_id
+    attribute :security_status
+    attribute :title
+  end
+end
