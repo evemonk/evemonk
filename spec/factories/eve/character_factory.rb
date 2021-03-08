4d0f202b76ab4faedbc1d09bf290a2f813616149
Igor Zubkov <igor.zubkov@gmail.com>
2021-03-08 21:01:25 +0200

    Update

--- spec/factories/eve/character_factory.rb
+++ spec/factories/eve/character_factory.rb
@@ -1,31 +1,31 @@
 # frozen_string_literal: true
 
-# FactoryBot.define do
-#   factory :eve_character, class: Eve::Character do
-#     sequence(:character_id)
-#
-#     sequence(:alliance_id)
-#
-#     sequence(:ancestry_id)
-#
-#     birthday { Time.zone.now }
-#
-#     sequence(:bloodline_id)
-#
-#     sequence(:corporation_id)
-#
-#     description { Faker::Lorem.paragraph }
-#
-#     sequence(:faction_id)
-#
-#     gender { ["male", "female"].sample }
-#
-#     name { Faker::Lorem.word }
-#
-#     sequence(:race_id)
-#
-#     security_status { 0.0 }
-#
-#     title { "Test title" }
-#   end
-# end
+FactoryBot.define do
+  factory :eve_character, class: Eve::CharacterRecord do
+    sequence(:character_id)
+
+    sequence(:alliance_id)
+
+    sequence(:ancestry_id)
+
+    birthday { Time.zone.now }
+
+    sequence(:bloodline_id)
+
+    sequence(:corporation_id)
+
+    description { Faker::Lorem.paragraph }
+
+    sequence(:faction_id)
+
+    gender { ["male", "female"].sample }
+
+    name { Faker::Lorem.word }
+
+    sequence(:race_id)
+
+    security_status { 0.0 }
+
+    title { "Test title" }
+  end
+end
