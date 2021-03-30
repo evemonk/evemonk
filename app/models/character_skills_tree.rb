1297e2f52772d02e38ceb99f228f52e997ca096f
Igor Zubkov <igor.zubkov@gmail.com>
2021-03-30 14:48:03 +0300

    Update

--- app/models/character_skills_tree.rb
+++ app/models/character_skills_tree.rb
@@ -16,6 +16,8 @@ class CharacterSkillsTree
 
     skills_types
 
+    character_skills
+
     self
   end
 
@@ -27,6 +29,12 @@ class CharacterSkillsTree
     skills_types.select { |type| type.group_id == group_id }.size
   end
 
+  def levels_trained_in_group(group_id)
+    skill_ids = skills_types.select { |type| type.group_id == group_id }.map(&:type_id)
+
+    character_skills.select { |character_skill| skill_ids.include?(character_skill.skill_id) }.map(&:trained_skill_level).sum
+  end
+
   private
 
   def skill_category
@@ -35,5 +43,9 @@ class CharacterSkillsTree
 
   def skills_types
     @skills_types ||= Eve::Type.published.where(group_id: skills_groups.map(&:group_id)).to_a
+  end
+
+  def character_skills
+    @character_skills ||= character.character_skills.to_a
   end
 end
