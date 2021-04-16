ed59f05e9b79f489af670715d4fff545c0d824ea
Igor Zubkov <igor.zubkov@gmail.com>
2021-04-16 17:42:43 +0300

    TODO: later

--- app/models/character_certificates_tree.rb
+++ app/models/character_certificates_tree.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class CharacterCertificatesTree
+  # LEVELS = { 0 => "79_64_1.png",
+  #            1 => "79_64_2.png",
+  #            2 => "79_64_3.png",
+  #            3 => "79_64_4.png",
+  #            4 => "79_64_5.png",
+  #            5 => "79_64_6.png"}
+
   attr_reader :character
 
   def initialize(character)
@@ -23,6 +30,16 @@ class CharacterCertificatesTree
     certificates.select { |certificate| certificate.group_id == group_id }.sort_by(&:name)
   end
 
+  # def certificate_level(certificate_id)
+  #   (5..1).each do |level|
+  #     certificate = certificates.find { |certificate| certificate.certificate_id == certificate_id }
+  #
+  #     certificate.certificate_skills
+  #   end
+  #
+  #   0
+  # end
+
   private
 
   def group_ids
@@ -30,6 +47,11 @@ class CharacterCertificatesTree
   end
 
   def certificates
+    # @certificates ||= Eve::Certificate.includes(:certificate_skills).all.to_a
     @certificates ||= Eve::Certificate.all.to_a
   end
+
+  # def character_skills
+  #   @character_skills ||= character.character_skills.to_a
+  # end
 end
