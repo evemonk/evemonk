d86ae4f6c85b41292f52287be8300a83cc4d4b34
Igor Zubkov <igor.zubkov@gmail.com>
2021-05-19 13:51:37 +0300

    Fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,7 +29,6 @@ import "../stylesheets/application";
 
 // window.ApexCharts = ApexCharts;
 window.$ = $;
-window.Turbo = Turbo;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
