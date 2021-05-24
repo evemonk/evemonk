e6b383825b11dc89b13bc56e57b06a2729b6fe35
Igor Zubkov <igor.zubkov@gmail.com>
2021-05-24 18:44:56 +0300

    One more test

--- spec/models/countries_mapper_spec.rb
+++ spec/models/countries_mapper_spec.rb
@@ -4,6 +4,8 @@ require "rails_helper"
 
 describe CountriesMapper do
   describe ".countries" do
+    specify { expect(described_class.countries.size).to eq(249) }
+
     specify { expect(described_class.countries[230]).to eq(name: "Ukraine", code: "+380") }
   end
 end
