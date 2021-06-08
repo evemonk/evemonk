ceeef9e15a0d324863a6aa2107ae0bb6ddd9aaf1
Igor Zubkov <igor.zubkov@gmail.com>
2021-06-09 00:50:39 +0300

    Reformat

--- app/graphql/types/eve_bloodline_type.rb
+++ app/graphql/types/eve_bloodline_type.rb
@@ -4,21 +4,50 @@ module Types
   class EveBloodlineType < Types::BaseObject
     description "Eve Bloodline object"
 
-    field :id, ID, null: false
-    field :name, GraphQL::Types::JSON, null: true
-    field :description, GraphQL::Types::JSON, null: true
-    field :corporation_id, Integer, null: true
-    field :corporation, Types::EveCorporationType, null: true
-    field :race_id, Integer, null: true
-    field :race, Types::EveRaceType, null: true
-    field :ship_type_id, Integer, null: true
-    field :ship_type, Types::EveShipType, null: true
-    field :charisma, Integer, null: true
-    field :intelligence, Integer, null: true
-    field :memory, Integer, null: true
-    field :perception, Integer, null: true
-    field :willpower, Integer, null: true
-    field :ancestries, Types::EveAncestryType.connection_type, null: true
+    field :id, ID,
+      null: false
+
+    field :name, GraphQL::Types::JSON,
+      null: true
+
+    field :description, GraphQL::Types::JSON,
+      null: true
+
+    field :corporation_id, Integer,
+      null: true
+
+    field :corporation, Types::EveCorporationType,
+      null: true
+
+    field :race_id, Integer,
+      null: true
+
+    field :race, Types::EveRaceType,
+      null: true
+
+    field :ship_type_id, Integer,
+      null: true
+
+    field :ship_type, Types::EveShipType,
+      null: true
+
+    field :charisma, Integer,
+      null: true
+
+    field :intelligence, Integer,
+      null: true
+
+    field :memory, Integer,
+      null: true
+
+    field :perception, Integer,
+      null: true
+
+    field :willpower, Integer,
+      null: true
+
+    field :ancestries, Types::EveAncestryType.connection_type,
+      null: true
 
     def id
       object.bloodline_id
