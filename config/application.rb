a3407751e03da5f8f2055d89accb2405172dad35
Igor Zubkov <igor.zubkov@gmail.com>
2021-06-08 12:17:28 +0300

    ENV -> ENV.fetch

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,6 @@ module Evemonk
     # Don't generate system test files.
     config.generators.system_tests = nil
 
-    config.secret_key_base = ENV["SECRET_KEY_BASE"]
+    config.secret_key_base = ENV.fetch("SECRET_KEY_BASE")
   end
 end
