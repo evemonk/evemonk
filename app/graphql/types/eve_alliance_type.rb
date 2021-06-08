6045c5c1d75025a6cea152a8d5472394207e6c2c
Igor Zubkov <igor.zubkov@gmail.com>
2021-06-08 12:43:30 +0300

    Fix rubocop warnings

--- app/graphql/types/eve_alliance_type.rb
+++ app/graphql/types/eve_alliance_type.rb
@@ -4,9 +4,9 @@ module Types
   class EveAllianceType < Types::BaseObject
     description "Eve Alliance object"
 
-    field :id, ID, null: false
-    field :name, String, null: true
-    field :ticker, String, null: true
+    field :id, ID, null: false, description: "Eve Alliance ID"
+    field :name, String, null: true, description: "Eve Alliance name"
+    field :ticker, String, null: true, description: "Eve Alliance ticker"
     field :date_founded, GraphQL::Types::ISO8601DateTime, null: true
     field :creator_corporation_id, Integer, null: true
     field :creator_corporation, Types::EveCorporationType, null: true
@@ -16,10 +16,10 @@ module Types
     field :executor_corporation, Types::EveCorporationType, null: true
     field :faction_id, Integer, null: true
     field :faction, Types::EveFactionType, null: true
-    field :corporations_count, Integer, null: true
-    field :characters_count, Integer, null: true
-    field :corporations, Types::EveCorporationType.connection_type, null: true
-    field :characters, Types::EveCharacterType.connection_type, null: true
+    field :corporations_count, Integer, null: true, description: "Eve Alliance corporations count"
+    field :characters_count, Integer, null: true, description: "Eve Alliance characters count"
+    field :corporations, Types::EveCorporationType.connection_type, null: true, description: "Eve Alliance corporations collection"
+    field :characters, Types::EveCharacterType.connection_type, null: true, description: "Eve Alliance characters collection"
     # TODO: corporation_alliance_histories
     # field :history, [Types::AllianceHistoryType], null: true
 
