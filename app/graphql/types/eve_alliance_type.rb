f4598ba5f31e824e8b66563f4482f85a902ad5bb
Igor Zubkov <igor.zubkov@gmail.com>
2021-06-08 12:57:57 +0300

    Reformat

--- app/graphql/types/eve_alliance_type.rb
+++ app/graphql/types/eve_alliance_type.rb
@@ -4,22 +4,61 @@ module Types
   class EveAllianceType < Types::BaseObject
     description "Eve Alliance object"
 
-    field :id, ID, null: false, description: "Eve Alliance ID"
-    field :name, String, null: true, description: "Eve Alliance name"
-    field :ticker, String, null: true, description: "Eve Alliance ticker"
-    field :date_founded, GraphQL::Types::ISO8601DateTime, null: true
-    field :creator_corporation_id, Integer, null: true
-    field :creator_corporation, Types::EveCorporationType, null: true
-    field :creator_id, Integer, null: true
-    field :creator, Types::EveCharacterType, null: true
-    field :executor_corporation_id, Integer, null: true
-    field :executor_corporation, Types::EveCorporationType, null: true
-    field :faction_id, Integer, null: true
-    field :faction, Types::EveFactionType, null: true
-    field :corporations_count, Integer, null: true, description: "Eve Alliance corporations count"
-    field :characters_count, Integer, null: true, description: "Eve Alliance characters count"
-    field :corporations, Types::EveCorporationType.connection_type, null: true, description: "Eve Alliance corporations collection"
-    field :characters, Types::EveCharacterType.connection_type, null: true, description: "Eve Alliance characters collection"
+    field :id, ID,
+      description: "Eve Alliance ID",
+      null: false
+
+    field :name, String,
+      description: "Eve Alliance name",
+      null: true
+
+    field :ticker, String,
+      description: "Eve Alliance ticker",
+      null: true
+
+    field :date_founded, GraphQL::Types::ISO8601DateTime,
+      null: true
+
+    field :creator_corporation_id, Integer,
+      null: true
+
+    field :creator_corporation, Types::EveCorporationType,
+      null: true
+
+    field :creator_id, Integer,
+      null: true
+
+    field :creator, Types::EveCharacterType,
+      null: true
+
+    field :executor_corporation_id, Integer,
+      null: true
+
+    field :executor_corporation, Types::EveCorporationType,
+      null: true
+
+    field :faction_id, Integer,
+      null: true
+
+    field :faction, Types::EveFactionType,
+      null: true
+
+    field :corporations_count, Integer,
+      description: "Eve Alliance corporations count",
+      null: true
+
+    field :characters_count, Integer,
+      description: "Eve Alliance characters count",
+      null: true
+
+    field :corporations, Types::EveCorporationType.connection_type,
+      description: "Eve Alliance corporations collection",
+      null: true
+
+    field :characters, Types::EveCharacterType.connection_type,
+      description: "Eve Alliance characters collection",
+      null: true
+
     # TODO: corporation_alliance_histories
     # field :history, [Types::AllianceHistoryType], null: true
 
