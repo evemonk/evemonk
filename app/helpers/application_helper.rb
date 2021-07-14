10a02dadd266a5b521f96c5defb79d02d93516e0
Igor Zubkov <igor.zubkov@gmail.com>
2021-07-14 20:09:38 +0300

    Fix sign_in_via_eve_online

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,11 @@ module ApplicationHelper
   def sign_in_via_eve_online
     image_uri = "https://web.ccpgamescdn.com/eveonlineassets/developers/eve-sso-login-black-large.png"
 
-    link_to(image_tag("#{Setting.image_proxy_url if Setting.use_image_proxy}#{image_uri}",
-      alt: "Sign in via EVE Online"),
-      "/auth/eve_online_sso",
-      method: :post)
+    form_with(url: "/auth/eve_online_sso", method: :post, data: { turbo: false }) do
+      button_tag do
+        image_tag("#{Setting.image_proxy_url if Setting.use_image_proxy}#{image_uri}",
+                  alt: "Sign in via EVE Online")
+      end
+    end
   end
 end
