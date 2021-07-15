7dc96c00f4b32cfeaedad6883efd6b38a37941ad
Igor Zubkov <igor.zubkov@gmail.com>
2021-07-15 17:21:25 +0300

    Cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,10 @@ module ApplicationHelper
   def sign_in_via_eve_online
     image_uri = "https://web.ccpgamescdn.com/eveonlineassets/developers/eve-sso-login-black-large.png"
 
-    form_with(url: "/auth/eve_online_sso", method: :post, data: { turbo: false }) do
+    form_with(url: "/auth/eve_online_sso", method: :post, data: {turbo: false}) do
       button_tag do
         image_tag("#{Setting.image_proxy_url if Setting.use_image_proxy}#{image_uri}",
-                  alt: "Sign in via EVE Online")
+          alt: "Sign in via EVE Online")
       end
     end
   end
