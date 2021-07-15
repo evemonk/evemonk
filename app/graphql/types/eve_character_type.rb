6ed8f246a47d649e5e856b23c8e58c1a89a9ceba
Igor Zubkov <igor.zubkov@gmail.com>
2021-07-15 18:09:12 +0300

    Cleanup

--- app/graphql/types/eve_character_type.rb
+++ app/graphql/types/eve_character_type.rb
@@ -4,32 +4,63 @@ module Types
   class EveCharacterType < Types::BaseObject
     description "Eve Character object"
 
-    field :id, ID, null: false
-    field :alliance_id, Integer, null: true
-    field :alliance, Types::EveAllianceType, null: true
-    field :ancestry_id, Integer, null: true
-    field :ancestry, Types::EveAncestryType, null: true
-    field :birthday, GraphQL::Types::ISO8601DateTime, null: true
-    field :bloodline_id, Integer, null: true
-    field :bloodline, Types::EveBloodlineType, null: true
-    field :corporation_id, Integer, null: true
-    field :corporation, Types::EveCorporationType, null: true
-    field :description, String, null: true
-    field :faction_id, Integer, null: true
-    field :faction, Types::EveFactionType, null: true
-    field :gender, String, null: true
-    field :name, String, null: true
-    field :race_id, Integer, null: true
-    field :race, Types::EveRaceType, null: true
-    field :security_status, Float, null: true
-    field :title, String, null: true
+    field :id, ID,
+      method: :character_id,
+      null: false
 
-    def id
-      object.character_id
-    end
+    field :alliance_id, Integer,
+      null: true
 
-    def description
-      object.sanitized_description
-    end
+    field :alliance, Types::EveAllianceType,
+      null: true
+
+    field :ancestry_id, Integer,
+      null: true
+
+    field :ancestry, Types::EveAncestryType,
+      null: true
+
+    field :birthday, GraphQL::Types::ISO8601DateTime,
+      null: true
+
+    field :bloodline_id, Integer,
+      null: true
+
+    field :bloodline, Types::EveBloodlineType,
+      null: true
+
+    field :corporation_id, Integer,
+      null: true
+
+    field :corporation, Types::EveCorporationType,
+      null: true
+
+    field :description, String,
+      method: :sanitized_description,
+      null: true
+
+    field :faction_id, Integer,
+      null: true
+
+    field :faction, Types::EveFactionType,
+      null: true
+
+    field :gender, String,
+      null: true
+
+    field :name, String,
+      null: true
+
+    field :race_id, Integer,
+      null: true
+
+    field :race, Types::EveRaceType,
+      null: true
+
+    field :security_status, Float,
+      null: true
+
+    field :title, String,
+      null: true
   end
 end
