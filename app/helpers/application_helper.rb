f34348eab41853f3ce2e7e58c1c3e5f6488b3a29
Igor Zubkov <igor.zubkov@gmail.com>
2021-07-21 14:43:38 +0300

    Refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,4 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def sign_in_via_eve_online
-    image_uri = "https://web.ccpgamescdn.com/eveonlineassets/developers/eve-sso-login-black-large.png"
-
-    form_with(url: "/auth/eve_online_sso", method: :post, data: {turbo: false}) do
-      button_tag do
-        image_tag("#{Setting.image_proxy_url if Setting.use_image_proxy}#{image_uri}",
-          alt: "Sign in via EVE Online")
-      end
-    end
-  end
 end
