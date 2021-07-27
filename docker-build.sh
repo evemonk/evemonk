8e99b744e3f1d4396f512423e941817d7f5285f7
Igor Zubkov <igor.zubkov@gmail.com>
2021-07-27 13:22:54 +0300

    Update

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -t biow0lf/evemonk:dev . --no-cache --progress plain
+docker build -t biow0lf/evemonk:dev . --no-cache --progress plain --build-arg COMMIT="$(git rev-parse HEAD)"
 docker push biow0lf/evemonk:dev
