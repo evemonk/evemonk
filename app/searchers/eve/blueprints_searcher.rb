6b708ee7267dbb7128bbab16a27be33b748e6788
Igor Zubkov <igor.zubkov@gmail.com>
2021-08-04 17:47:50 +0300

    Update

--- app/searchers/eve/blueprints_searcher.rb
+++ app/searchers/eve/blueprints_searcher.rb
@@ -2,47 +2,47 @@
 
 module Eve
   class BlueprintsSearcher
-    attr_reader :query, :scope
-
-    def initialize(query, scope = Eve::Blueprint.all)
-      @query = query
-      @scope = scope
-    end
-
-    def search
-      if query.present? && query.length > 2
-        ids = Eve::Type.search(query, **options).map(&:id)
-
-        if ids.present?
-          scope.where(id: ids)
-        else
-          Eve::Blueprint.none
-        end
-      else
-        scope
-      end
-    end
-
-    private
-
-    def options
-      {
-        fields: [
-          :name_en,
-          :name_de,
-          :name_fr,
-          :name_ja,
-          :name_ru,
-          :name_ko
-        ],
-        match: :word_start,
-        where: {
-          published: true,
-          is_blueprint: true
-        },
-        load: false,
-        misspellings: {below: 5}
-      }
-    end
+    # attr_reader :query, :scope
+    #
+    # def initialize(query, scope = Eve::Blueprint.all)
+    #   @query = query
+    #   @scope = scope
+    # end
+    #
+    # def search
+    #   if query.present? && query.length > 2
+    #     ids = Eve::Type.search(query, **options).map(&:id)
+    #
+    #     if ids.present?
+    #       scope.where(id: ids)
+    #     else
+    #       Eve::Blueprint.none
+    #     end
+    #   else
+    #     scope
+    #   end
+    # end
+    #
+    # private
+    #
+    # def options
+    #   {
+    #     fields: [
+    #       :name_en,
+    #       :name_de,
+    #       :name_fr,
+    #       :name_ja,
+    #       :name_ru,
+    #       :name_ko
+    #     ],
+    #     match: :word_start,
+    #     where: {
+    #       published: true,
+    #       is_blueprint: true
+    #     },
+    #     load: false,
+    #     misspellings: {below: 5}
+    #   }
+    # end
   end
 end
