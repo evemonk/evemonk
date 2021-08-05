2a8adb1524bf0671b66992b44e299cfa0837ea2a
Igor Zubkov <igor.zubkov@gmail.com>
2021-08-05 17:13:51 +0300

    BaseQuery

---
+++ spec/queries/base_query_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe BaseQuery do
+  describe "#scope" do
+    specify { expect { subject.scope }.to raise_error(NotImplementedError) }
+  end
+
+  describe "#query" do
+    specify { expect { subject.query }.to raise_error(NotImplementedError) }
+  end
+end
