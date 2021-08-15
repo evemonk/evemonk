efe71a562df4269e8b388fd602f51af7bff32078
Igor Zubkov <igor.zubkov@gmail.com>
2021-08-15 17:47:10 +0300

    Update

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,5 +2,8 @@
 
 class SettingsController < ApplicationController
   def index
+    @character = current_user.characters
+      .includes(:alliance, :corporation, :character_scopes)
+      .find_by!(character_id: params[:character_id])
   end
 end
--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -13,6 +13,30 @@ describe SettingsController do
 
       before { sign_in(user) }
 
+      before { expect(subject).to receive(:current_user_locale) }
+
+      before do
+        #
+        # subject.current_user
+        #        .characters
+        #        .includes(:alliance, :corporation, :character_scopes)
+        #        .find_by!(character_id: params[:character_id])
+        #
+        expect(subject).to receive(:current_user) do
+          double.tap do |a|
+            expect(a).to receive(:characters) do
+              double.tap do |b|
+                expect(b).to receive(:includes).with(:alliance, :corporation, :character_scopes) do
+                  double.tap do |c|
+                    expect(c).to receive(:find_by!).with(character_id: "1")
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+
       before { get :index, params: {character_id: "1"} }
 
       it { should respond_with(:ok) }
