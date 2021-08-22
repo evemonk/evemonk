2852173fafd69551310a358a1d9b6bc68ba12c10
Igor Zubkov <igor.zubkov@gmail.com>
2021-08-22 15:57:25 +0300

    Update

--- app/importers/import_missing_for_development.rb
+++ app/importers/import_missing_for_development.rb
@@ -2,22 +2,5 @@
 
 class ImportMissingForDevelopment
   def import
-    Rails.logger.info "Import eve systems"
-    Character.where.not(current_solar_system_id: nil)
-      .pluck(:current_solar_system_id)
-      .sort
-      .uniq
-      .each do |system_id|
-      Eve::UpdateSystemJob.perform_later(system_id)
-    end
-
-    Rails.logger.info "Import eve stations"
-    Character.where.not(current_station_id: nil)
-      .pluck(:current_station_id)
-      .sort
-      .uniq
-      .each do |station_id|
-      Eve::UpdateStationJob.perform_later(station_id)
-    end
   end
 end
