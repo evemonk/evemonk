c32908cb81896b6ac1549ddbf29b401611b1395d
Igor Zubkov <igor.zubkov@gmail.com>
2021-08-22 17:09:47 +0300

    Turn off pidfile feature for puma

--- config/puma.rb
+++ config/puma.rb
@@ -27,7 +27,7 @@ activate_control_app
 plugin :yabeda
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
+# pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
