7c5cc84d01b3dbd941573b826c23ec6fd0399911
Igor Zubkov <igor.zubkov@gmail.com>
2021-08-27 01:09:48 +0300

    Remove old migrations

--- db/migrate/20190924213954_drop_active_storage.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class DropActiveStorage < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :active_storage_attachments
-    drop_table :active_storage_blobs
-  end
-end
