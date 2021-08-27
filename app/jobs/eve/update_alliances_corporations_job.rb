ae2c860efa107552c62786de3e1ce9d0e45c7594
Igor Zubkov <igor.zubkov@gmail.com>
2021-08-27 19:54:42 +0300

    Cleanup

--- app/jobs/eve/update_alliances_corporations_job.rb
+++ app/jobs/eve/update_alliances_corporations_job.rb
@@ -4,14 +4,6 @@ module Eve
   class UpdateAlliancesCorporationsJob < ApplicationJob
     queue_as :default
 
-    retry_on EveOnline::Exceptions::Timeout,
-      EveOnline::Exceptions::ServiceUnavailable,
-      EveOnline::Exceptions::BadGateway,
-      EveOnline::Exceptions::InternalServerError,
-      OpenSSL::SSL::SSLError,
-      Faraday::TimeoutError,
-      Faraday::ConnectionFailed
-
     def perform
       Eve::AlliancesCorporationsImporter.new.import
     end
