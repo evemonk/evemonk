149ceca592fc0ade64159ed3700488e055795b0d
Igor Zubkov <igor.zubkov@gmail.com>
2021-08-27 16:12:36 +0300

    Rename minio_public to minio

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,8 +33,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  # config.active_storage.service = :minio
+  # Store uploaded files in S3 (Minio) (see config/storage.yml for options).
+  config.active_storage.service = :minio
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  # config.active_storage.service = :minio
+  # Store uploaded files in S3 (Minio) (see config/storage.yml for options).
+  config.active_storage.service = :minio
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
