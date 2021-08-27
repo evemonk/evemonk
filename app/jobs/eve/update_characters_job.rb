08169659540a47eec3f7ff5d45af8dd446666c72
Igor Zubkov <igor.zubkov@gmail.com>
2021-08-27 20:06:06 +0300

    Cleanup

--- app/jobs/eve/update_characters_job.rb
+++ app/jobs/eve/update_characters_job.rb
@@ -4,14 +4,6 @@ module Eve
   class UpdateCharactersJob < ApplicationJob
     queue_as :default
 
-    retry_on EveOnline::Exceptions::Timeout,
-      EveOnline::Exceptions::ServiceUnavailable,
-      EveOnline::Exceptions::BadGateway,
-      EveOnline::Exceptions::InternalServerError,
-      OpenSSL::SSL::SSLError,
-      Faraday::TimeoutError,
-      Faraday::ConnectionFailed
-
     def perform
       Eve::CharactersImporter.new.import
     end
