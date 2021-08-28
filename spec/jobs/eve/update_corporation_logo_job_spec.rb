222b35ca06f4110c8d5e1957ed49c6f0abd5de3a
Igor Zubkov <igor.zubkov@gmail.com>
2021-08-28 23:08:38 +0300

    Eve::UpdateCorporationLogoJob

---
+++ spec/jobs/eve/update_corporation_logo_job_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::UpdateCorporationLogoJob do
+  it { should be_an(ApplicationJob) }
+
+  it { expect(described_class.queue_name).to eq("default") }
+
+  describe "#perform" do
+    let(:corporation_id) { double }
+
+    before do
+      #
+      # Eve::CorporationLogoImporter.new(corporation_id).import
+      #
+      expect(Eve::CorporationLogoImporter).to receive(:new).with(corporation_id) do
+        double.tap do |a|
+          expect(a).to receive(:import)
+        end
+      end
+    end
+
+    specify { expect { subject.perform(corporation_id) }.not_to raise_error }
+  end
+end
