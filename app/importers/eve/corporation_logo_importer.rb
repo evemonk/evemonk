6da6c0930f658167100e8b1dca7738c71ad95edf
Igor Zubkov <igor.zubkov@gmail.com>
2021-08-28 05:42:27 +0300

    Eve::CorporationLogoImporter

---
+++ app/importers/eve/corporation_logo_importer.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "down"
+
+module Eve
+  class CorporationLogoImporter < BaseImporter
+    attr_reader :corporation_id
+
+    CORPORATIONS_LOGO_URL = "https://images.evetech.net/corporations/%s/logo"
+
+    def initialize(corporation_id)
+      @corporation_id = corporation_id
+    end
+
+    def import
+      eve_corporation = Eve::Corporation.find_or_initialize_by(corporation_id: corporation_id)
+
+      tempfile = Down.download(CORPORATIONS_LOGO_URL % corporation_id)
+
+      eve_corporation.logo.attach(io: tempfile, filename: "#{corporation_id}.png")
+
+      eve_corporation.save!
+    end
+  end
+end
---
+++ spec/importers/eve/corporation_logo_importer_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::CorporationLogoImporter do
+  let(:corporation_id) { double }
+
+  subject { described_class.new(corporation_id) }
+
+  it { should be_a(Eve::BaseImporter) }
+
+  specify { expect(described_class::CORPORATIONS_LOGO_URL).to eq("https://images.evetech.net/corporations/%s/logo") }
+
+  describe "#import" do
+    let(:corporation_id) { 109_299_958 }
+
+    let(:logo) { instance_double(ActiveStorage::Attached::One) }
+
+    let(:eve_corporation) { instance_double(Eve::Corporation, logo: logo) }
+
+    before do
+      expect(Eve::Corporation).to receive(:find_or_initialize_by)
+        .with(corporation_id: corporation_id)
+        .and_return(eve_corporation)
+    end
+
+    let(:tempfile) { instance_double(Tempfile) }
+
+    before do
+      expect(Down).to receive(:download)
+        .with("https://images.evetech.net/corporations/109299958/logo")
+        .and_return(tempfile)
+    end
+
+    before do
+      expect(logo).to receive(:attach)
+        .with(io: tempfile, filename: "109299958.png")
+    end
+
+    before { expect(eve_corporation).to receive(:save!) }
+
+    specify { expect { subject.import }.not_to raise_error }
+  end
+end
