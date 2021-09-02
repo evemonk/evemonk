110ac56caebc0aa6fb15b5d1d78f493cb322a0f4
Igor Zubkov <igor.zubkov@gmail.com>
2021-09-02 21:31:18 +0300

    Update

--- spec/models/application_record_spec.rb
+++ spec/models/application_record_spec.rb
@@ -4,4 +4,6 @@ require "rails_helper"
 
 describe ApplicationRecord do
   it { expect(described_class.abstract_class).to eq(true) }
+
+  it { expect(ActiveRecord.application_record_class).to eq(described_class) }
 end
