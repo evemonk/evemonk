ed663c07fd4a4d4c477fb5d3684671e98e9f9b04
Igor Zubkov <igor.zubkov@gmail.com>
2021-09-03 12:03:49 +0300

    Update

---
+++ app/javascript/application.js
@@ -0,0 +1,7 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "@hotwired/stimulus-importmap-autoloader"
+import "@github/hotkey"
+import "jquery"
+import "popper.js"
+import "bootstrap"
--- config/importmap.rb
+++ config/importmap.rb
@@ -12,3 +12,8 @@ pin "@hotwired/turbo-rails", to: "turbo.js"
 pin "@hotwired/stimulus", to: "stimulus.js"
 pin "@hotwired/stimulus-importmap-autoloader", to: "stimulus-importmap-autoloader.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "@github/hotkey", to: "https://ga.jspm.io/npm:@github/hotkey@1.4.4/dist/index.js"
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@4.6.0/dist/js/bootstrap.js"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
+pin "popper.js", to: "https://ga.jspm.io/npm:popper.js@1.16.1/dist/umd/popper.js"
