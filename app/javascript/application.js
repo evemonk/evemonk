e0523ef5451c30a1a88a1e1435012d78d6e80750
Igor Zubkov <igor.zubkov@gmail.com>
2021-09-03 12:07:57 +0300

    Update

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "@hotwired/stimulus-importmap-autoloader"
-import "@github/hotkey"
+import { install } from "@github/hotkey"
 import "jquery"
 import "popper.js"
 import "bootstrap"
+
+document.addEventListener("turbo:load", () => {
+  // $('[data-toggle="tooltip"]').tooltip();
+
+  // Install all the hotkeys on the page
+  for (const el of document.querySelectorAll("[data-hotkey]")) {
+    install(el);
+  }
+});
