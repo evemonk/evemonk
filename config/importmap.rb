f2956b6251638e53ff64b2c9174113a6e62089ea
Igor Zubkov <igor.zubkov@gmail.com>
2021-09-05 19:30:06 +0300

    Fix

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-# Use Action Cable channels (remember to import "@rails/actionable" in your application.js)
-# pin "@rails/actioncable", to: "actioncable.esm.js"
-# pin_all_from "app/javascript/channels", under: "channels"
-
 # Use direct uploads for Active Storage (remember to import "@rails/activestorage" in your application.js)
 # pin "@rails/activestorage", to: "activestorage.esm.js"
 
@@ -14,6 +10,8 @@ pin "@hotwired/turbo-rails", to: "turbo.js"
 pin "@hotwired/stimulus", to: "stimulus.js"
 pin "@hotwired/stimulus-importmap-autoloader", to: "stimulus-importmap-autoloader.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@6.1.4-1/lib/assets/compiled/rails-ujs.js"
 
 pin "@github/hotkey", to: "https://ga.jspm.io/npm:@github/hotkey@1.4.4/dist/index.js"
 
