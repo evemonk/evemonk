f81b1e8bcc6e57c2cf5939cc9a522e0ff4302c96
Igor Zubkov <igor.zubkov@gmail.com>
2021-09-10 13:08:47 +0300

    Update filter

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -5,5 +5,5 @@
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [
   :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn,
-  :password, :country_code, :cellphone
+  :country_code, :cellphone
 ]
