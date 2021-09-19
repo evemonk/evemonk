29cc25172527dcb5bd59362b1a674f2cf3a9eeae
Igor Zubkov <igor.zubkov@gmail.com>
2021-09-19 18:40:30 +0300

    Relax CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -11,14 +11,14 @@ Rails.application.config.content_security_policy do |policy|
   policy.default_src :none
   policy.font_src "https://fonts.gstatic.com"
   policy.img_src :self, :data, "https://imageproxy.evemonk.com", "https://static.evemonk.com"
-  policy.script_src :self, :unsafe_inline, :unsafe_eval, "https://plausible.evemonk.com"
+  policy.script_src :self, :unsafe_inline, :unsafe_eval, "https://ga.jspm.io/", "https://plausible.evemonk.com"
   policy.style_src :self, :unsafe_inline, "https://fonts.googleapis.com"
   # TODO: https://bugs.webkit.org/show_bug.cgi?id=201591
   # policy.connect_src :self, "https://sentry.io" if Rails.env.production?
   policy.connect_src :self, :wss, "https://sentry.io", "https://plausible.evemonk.com", "https://login.eveonline.com" if Rails.env.production?
 
   # If you are using webpack-dev-server then specify webpack-dev-server host
-  policy.connect_src :self, :https, "https://sentry.io", "https://plausible.evemonk.com", "https://login.eveonline.com", "http://localhost:3035", "ws://localhost:3035", "ws://localhost:3000" if Rails.env.development?
+  policy.connect_src :self, :https, "https://sentry.io", "https://plausible.evemonk.com", "https://login.eveonline.com", "ws://localhost:3000" if Rails.env.development?
 
   # Specify URI for violation reports
   # policy.report_uri "/csp-violation-report-endpoint"
@@ -33,4 +33,4 @@ Rails.application.config.content_security_policy_nonce_directives = ["script-src
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-# Rails.application.config.content_security_policy_report_only = true
+Rails.application.config.content_security_policy_report_only = true
