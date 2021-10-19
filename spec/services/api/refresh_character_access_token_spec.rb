f2cadd5aa558744d3b08fa5159263e15b34d8b2e
Igor Zubkov <igor.zubkov@gmail.com>
2021-10-19 23:01:34 +0300

    Update spec

--- spec/services/api/refresh_character_access_token_spec.rb
+++ spec/services/api/refresh_character_access_token_spec.rb
@@ -16,29 +16,28 @@ describe Api::RefreshCharacterAccessToken do
     specify { expect { subject.refresh }.not_to raise_error }
   end
 
-  # context "when token needed to refresh" do
-  #   before { VCR.insert_cassette "api/refresh_character_access_token/success" }
-  #
-  #   after { VCR.eject_cassette }
-  #
-  #   before { expect(Setting).to receive(:eve_online_sso_client_id).and_return("eve-online-sso-client-id") }
-  #
-  #   before { expect(Setting).to receive(:eve_online_sso_secret_key).and_return("eve-online-sso-secret-key") }
-  #
-  #   let(:character) do
-  #     create(:character,
-  #       character_id: 1_337_512_245,
-  #       access_token: "expired-access-token123",
-  #       refresh_token: "fresh-token-1232132132132131231312312312312312321321321321312312",
-  #       token_expires_at: Time.zone.now)
-  #   end
-  #
-  #   subject { described_class.new(character) }
-  #
-  #   before { expect(character).to receive(:update!) }
-  #
-  #   specify { expect { subject.refresh }.not_to raise_error }
-  # end
+  context "when token needed to refresh" do
+    before { VCR.insert_cassette "api/refresh_character_access_token/success" }
+
+    after { VCR.eject_cassette }
+
+    before { expect(Setting).to receive(:eve_online_sso_client_id).and_return("eve-online-sso-client-id") }
+
+    before { expect(Setting).to receive(:eve_online_sso_secret_key).and_return("eve-online-sso-secret-key") }
+
+    let(:character) do
+      create(:character,
+        character_id: 1_337_512_245,
+        refresh_token: "fresh-token-1232132132132131231312312312312312321321321321312312",
+        token_expires_at: Time.zone.now)
+    end
+
+    subject { described_class.new(character) }
+
+    before { expect(character).to receive(:update!) }
+
+    specify { expect { subject.refresh }.not_to raise_error }
+  end
 
   # context "when oauth2 error" do
   #   context "when token invalid" do
