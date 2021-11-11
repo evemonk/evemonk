ff89dbd4da905bc1cbdbe7e6d22671d7e5f001b1
Igor Zubkov <igor.zubkov@gmail.com>
2021-11-11 23:48:17 +0200

    Preload stimulus controllers

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,7 @@
 pin "application", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from "app/javascript/controllers", under: "controllers", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.0-alpha2/lib/assets/compiled/rails-ujs.js"
