668bb9f79414648d100728be56279cafad6a203b
Igor Zubkov <igor.zubkov@gmail.com>
2021-11-25 02:31:11 +0200

    Fix specs

--- spec/requests/api/eve/blueprints_controller_spec.rb
+++ spec/requests/api/eve/blueprints_controller_spec.rb
@@ -6,14 +6,25 @@ describe Api::Eve::BlueprintsController do
   before { Setting.use_image_proxy = true }
 
   describe "#index" do
-    it "returns list of blueprints" do
+    it "without param q" do
+      get "/api/eve/blueprints"
+
+      expect(response).to have_http_status(:ok)
+
+      expect(JSON.parse(response.body)).to eq("total_count" => 0,
+        "total_pages" => 0,
+        "current_page" => 1,
+        "blueprints" => [])
+    end
+
+    it "with param q" do
       create(:eve_type,
         type_id: 24_699,
         name: "Drake Blueprint",
         published: true,
         is_blueprint: true)
 
-      get "/api/eve/blueprints"
+      get "/api/eve/blueprints", params: { q: "drake" }
 
       expect(response).to have_http_status(:ok)
 
