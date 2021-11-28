6e08cebe595109cffcc62019a78089f557cd296e
Igor Zubkov <igor.zubkov@gmail.com>
2021-11-28 22:14:01 +0200

    Enable pg_buffercache postgres extension

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_151002) do
+ActiveRecord::Schema.define(version: 2021_11_28_201247) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_buffercache"
   enable_extension "pg_trgm"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
