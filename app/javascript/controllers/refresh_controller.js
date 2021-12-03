b202723ce3244a1bf25ff007dc785b0d19d6da9b
Igor Zubkov <igor.zubkov@gmail.com>
2021-12-03 22:55:26 +0200

    Cleanup

--- app/javascript/controllers/refresh_controller.js
+++ app/javascript/controllers/refresh_controller.js
@@ -3,28 +3,28 @@ import { patch } from "@rails/request.js";
 
 export default class extends Controller {
   static values = {
-    id: Number
-  }
+    id: Number,
+  };
 
   connect() {
-    console.log("connected")
+    console.log("connected");
   }
 
   disconnect() {
-    console.log("disconnect")
+    console.log("disconnect");
   }
 
   async update() {
-    console.log("update")
-    console.log(this.idValue)
+    console.log("update");
+    console.log(this.idValue);
 
-    const response = await patch(`/characters/${this.idValue}`)
+    const response = await patch(`/characters/${this.idValue}`);
 
     if (response.ok) {
-      console.log("OK")
-      document.getElementById("#alerts").append("hello")
+      console.log("OK");
+      document.getElementById("#alerts").append("hello");
     } else {
-      console.log("NOT OK")
+      console.log("NOT OK");
     }
   }
 }
