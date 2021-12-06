cfe28babed20d78eba9e2475a989e1dbe36a146d
Igor Zubkov <igor.zubkov@gmail.com>
2021-12-06 12:56:37 +0200

    Add Pagy::Backend to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Pagy::Backend
+
   protect_from_forgery with: :exception, prepend: true
 
   before_action :authenticate_user!
