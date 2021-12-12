0858866e6d2a589b27a55f6fd62b98bb2e3050ab
Igor Zubkov <igor.zubkov@gmail.com>
2021-12-12 03:47:09 +0200

    Update

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -2,6 +2,10 @@
 
 class WalletsController < ApplicationController
   def update
+    character = current_user.characters.find_by!(character_id: params[:character_id])
 
+    CharacterWalletJob.perform_later(character.character_id)
+
+    head :ok
   end
 end
