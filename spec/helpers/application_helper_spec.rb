3be8b6c952eadf9bd3269e1bd610ed3ca01ecbeb
Igor Zubkov <igor.zubkov@gmail.com>
2021-12-19 19:33:41 +0200

    Add spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,4 +3,11 @@
 require "rails_helper"
 
 describe ApplicationHelper do
+  subject do
+    class TestClass
+      include ApplicationHelper
+    end.new
+  end
+
+  it { expect(subject.respond_to?(:pagy_nav)).to eq(true) }
 end
