35acc3dc10b38cd25b29bb111e6bad3254da91a1
Igor Zubkov <igor.zubkov@gmail.com>
2022-02-02 01:47:11 +0200

    More specs

--- spec/controllers/blueprints_controller_spec.rb
+++ spec/controllers/blueprints_controller_spec.rb
@@ -68,6 +68,79 @@ describe BlueprintsController do
 
   describe "#show" do
     context "when user signed in" do
+      let(:user) { create(:user) }
+
+      before { sign_in(user) }
+
+      before { expect(subject).to receive(:current_user_locale) }
+
+      let(:character) { instance_double(Character) }
+
+      before do
+        #
+        # subject.current_user
+        #        .characters
+        #        .includes(:alliance, :corporation)
+        #        .find_by!(character_id: params[:character_id]) # => character
+        #
+        expect(subject).to receive(:current_user) do
+          double.tap do |a|
+            expect(a).to receive(:characters) do
+              double.tap do |b|
+                expect(b).to receive(:includes).with(:alliance, :corporation) do
+                  double.tap do |c|
+                    expect(c).to receive(:find_by!).with(character_id: "1")
+                      .and_return(character)
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+
+      let(:character_blueprint) { instance_double(CharacterBlueprint) }
+
+      before do
+        #
+        # character.character_blueprints
+        #          .includes(:blueprint) # => character_blueprint
+        #
+        expect(character).to receive(:character_blueprints) do
+          double.tap do |a|
+            expect(a).to receive(:includes).with(:blueprint)
+              .and_return(character_blueprint)
+          end
+        end
+      end
+
+      before do
+        #
+        # character.character_blueprints
+        #          .includes(:blueprint)
+        #          .where(item_id: params[:id])
+        #          .first
+        #
+        expect(character).to receive(:character_blueprints) do
+          double.tap do |a|
+            expect(a).to receive(:includes).with(:blueprint) do
+              double.tap do |b|
+                expect(b).to receive(:where).with(item_id: "2") do
+                  double.tap do |c|
+                    expect(c).to receive(:first)
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+
+      before { get :show, params: {character_id: "1", id: "2"} }
+
+      it { should respond_with(:ok) }
+
+      it { should render_template(:show) }
     end
 
     context "when user not signed in" do
