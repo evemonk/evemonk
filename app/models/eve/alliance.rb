65a867b0898dd42d1dbd1678ec95be77e2effb69
Igor Zubkov <igor.zubkov@gmail.com>
2022-02-16 18:17:38 +0200

    Eve::Alliance#alliance_id -> #id

--- app/models/eve/alliance.rb
+++ app/models/eve/alliance.rb
@@ -54,23 +54,23 @@ module Eve
     end
 
     def icon_tiny
-      "#{imageproxy_url}https://images.evetech.net/alliances/#{alliance_id}/logo?size=32"
+      "#{imageproxy_url}https://images.evetech.net/alliances/#{id}/logo?size=32"
     end
 
     def icon_small
-      "#{imageproxy_url}https://images.evetech.net/alliances/#{alliance_id}/logo?size=64"
+      "#{imageproxy_url}https://images.evetech.net/alliances/#{id}/logo?size=64"
     end
 
     def icon_medium
-      "#{imageproxy_url}https://images.evetech.net/alliances/#{alliance_id}/logo?size=128"
+      "#{imageproxy_url}https://images.evetech.net/alliances/#{id}/logo?size=128"
     end
 
     def icon_large
-      "#{imageproxy_url}https://images.evetech.net/alliances/#{alliance_id}/logo?size=256"
+      "#{imageproxy_url}https://images.evetech.net/alliances/#{id}/logo?size=256"
     end
 
     def icon_huge
-      "#{imageproxy_url}https://images.evetech.net/alliances/#{alliance_id}/logo?size=512"
+      "#{imageproxy_url}https://images.evetech.net/alliances/#{id}/logo?size=512"
     end
 
     def formatted_corporations_count
