b5f71d9fa91f18237b1066daffdc12354007d5c4
Igor Zubkov <igor.zubkov@gmail.com>
2022-02-18 18:38:44 +0200

    Update @rails/ujs to 7.0.2-2

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,7 +8,7 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.js", preload: true
 
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.2/lib/assets/compiled/rails-ujs.js"
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.2-2/lib/assets/compiled/rails-ujs.js"
 
 pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.6/src/index.js"
 
