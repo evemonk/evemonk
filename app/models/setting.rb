9e0b788bb6153930fcd5888154876452c1512b0d
Igor Zubkov <igor.zubkov@gmail.com>
2022-05-10 11:31:58 +0100

    Fix rubocop warnings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Setting < RailsSettings::Base
-  field :eve_online_sso_client_id, default: ENV["EVE_ONLINE_SSO_CLIENT_ID"], readonly: true
+  field :eve_online_sso_client_id, default: ENV.fetch("EVE_ONLINE_SSO_CLIENT_ID", nil), readonly: true
 
-  field :eve_online_sso_secret_key, default: ENV["EVE_ONLINE_SSO_SECRET_KEY"], readonly: true
+  field :eve_online_sso_secret_key, default: ENV.fetch("EVE_ONLINE_SSO_SECRET_KEY", nil), readonly: true
 
   field :use_image_proxy, type: :boolean, default: true
 
