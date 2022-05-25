98b83134217887d89d59e556023af88101280f49
Igor Zubkov <igor.zubkov@gmail.com>
2022-05-25 19:17:55 +0100

    Cleanup

--- app/graphql/evemonk_schema.rb
+++ app/graphql/evemonk_schema.rb
@@ -12,16 +12,16 @@ class EvemonkSchema < GraphQL::Schema
   default_max_page_size 50
 
   # GraphQL-Ruby calls this when something goes wrong while running a query:
-  def self.type_error(err, context)
-    # if err.is_a?(GraphQL::InvalidNullError)
-    #   # report to your bug tracker here
-    #   return nil
-    # end
-    super
-  end
+  # def self.type_error(_err, _context)
+  #   # if err.is_a?(GraphQL::InvalidNullError)
+  #   #   # report to your bug tracker here
+  #   #   return nil
+  #   # end
+  #   super
+  # end
 
   # Union and Interface Resolution
-  def self.resolve_type(abstract_type, obj, ctx)
+  def self.resolve_type(_abstract_type, _obj, _ctx)
     # TODO: Implement this method
     # to return the correct GraphQL object type for `obj`
     raise(GraphQL::RequiredImplementationMissingError)
@@ -30,13 +30,13 @@ class EvemonkSchema < GraphQL::Schema
   # Relay-style Object Identification:
 
   # Return a string UUID for `object`
-  def self.id_from_object(object, type_definition, query_ctx)
+  def self.id_from_object(object, _type_definition, _query_ctx)
     # For example, use Rails' GlobalID library (https://github.com/rails/globalid):
     object.to_gid_param
   end
 
   # Given a string UUID, find the object
-  def self.object_from_id(global_id, query_ctx)
+  def self.object_from_id(global_id, _query_ctx)
     # For example, use Rails' GlobalID library (https://github.com/rails/globalid):
     GlobalID.find(global_id)
   end
