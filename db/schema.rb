528c35fb7dbe865a6a206337480946e4d8dc2973
Igor Zubkov <igor.zubkov@gmail.com>
2022-06-12 14:23:43 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_10_002904) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_12_125211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_buffercache"
   enable_extension "pg_trgm"
@@ -955,6 +955,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_10_002904) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["war_id"], name: "index_eve_wars_on_war_id", unique: true
+  end
+
+  create_table "flipper_features", force: :cascade do |t|
+    t.string "key", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["key"], name: "index_flipper_features_on_key", unique: true
+  end
+
+  create_table "flipper_gates", force: :cascade do |t|
+    t.string "feature_key", null: false
+    t.string "key", null: false
+    t.string "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true
   end
 
   create_table "industry_jobs", force: :cascade do |t|
