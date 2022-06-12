f092400a394e4223c5eb347fd3708d623f320504
Igor Zubkov <igor.zubkov@gmail.com>
2022-06-12 19:47:02 +0100

    Works

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,8 @@
 import { Application } from "@hotwired/stimulus";
+import PasswordVisibility from "stimulus-password-visibility";
 
 const application = Application.start();
+application.register('password-visibility', PasswordVisibility);
 
 // Configure Stimulus development experience
 application.debug = false;
--- config/importmap.rb
+++ config/importmap.rb
@@ -19,3 +19,4 @@ pin "@github/hotkey", to: "https://ga.jspm.io/npm:@github/hotkey@2.0.0/dist/inde
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@4.6.1/dist/js/bootstrap.js"
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
 pin "popper.js", to: "https://ga.jspm.io/npm:popper.js@1.16.1/dist/umd/popper.js"
+pin "stimulus-password-visibility", to: "https://ga.jspm.io/npm:stimulus-password-visibility@2.0.0/dist/stimulus-password-visibility.es.js"
