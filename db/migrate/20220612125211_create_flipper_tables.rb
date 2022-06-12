a588d2a72a415bb755568894b357cad633f35639
Igor Zubkov <igor.zubkov@gmail.com>
2022-06-12 14:23:55 +0100

    Cleanup

--- db/migrate/20220612125211_create_flipper_tables.rb
+++ db/migrate/20220612125211_create_flipper_tables.rb
@@ -1,5 +1,5 @@
 class CreateFlipperTables < ActiveRecord::Migration[7.0]
-  def self.up
+  def up
     create_table :flipper_features do |t|
       t.string :key, null: false
       t.timestamps null: false
@@ -15,7 +15,10 @@ class CreateFlipperTables < ActiveRecord::Migration[7.0]
     add_index :flipper_gates, [:feature_key, :key, :value], unique: true
   end
 
-  def self.down
+  def down
+    remove_index :flipper_features, :key, unique: true
+    remove_index :flipper_gates, [:feature_key, :key, :value], unique: true
+
     drop_table :flipper_gates
     drop_table :flipper_features
   end
