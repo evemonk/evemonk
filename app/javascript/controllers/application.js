1c37ef903c4ee4930b94485a976ddb26d61fe197
Igor Zubkov <igor.zubkov@gmail.com>
2022-06-12 21:28:56 +0100

    Prettify

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,7 +2,7 @@ import { Application } from "@hotwired/stimulus";
 import PasswordVisibility from "stimulus-password-visibility";
 
 const application = Application.start();
-application.register('password-visibility', PasswordVisibility);
+application.register("password-visibility", PasswordVisibility);
 
 // Configure Stimulus development experience
 application.debug = false;
