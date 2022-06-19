742903de284616db023921a30bee99016aefd263
Igor Zubkov <igor.zubkov@gmail.com>
2022-06-20 00:46:46 +0100

    Cleanup

--- app/graphql/types/eve_region_type.rb
+++ app/graphql/types/eve_region_type.rb
@@ -26,27 +26,5 @@ module Types
     field :contracts, Types::EveContractType.connection_type,
       description: "Contracts collection",
       null: true
-
-    def name
-      {
-        en: object.name_en,
-        de: object.name_de,
-        fr: object.name_fr,
-        ja: object.name_ja,
-        ru: object.name_ru,
-        ko: object.name_ko
-      }
-    end
-
-    def description
-      {
-        en: object.description_en,
-        de: object.description_de,
-        fr: object.description_fr,
-        ja: object.description_ja,
-        ru: object.description_ru,
-        ko: object.description_ko
-      }
-    end
   end
 end
