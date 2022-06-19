dab7bdf753a558a3bd92b8dde7a55c321d0daa79
Igor Zubkov <igor.zubkov@gmail.com>
2022-06-19 21:09:05 +0100

    Cleanup

--- app/graphql/types/eve_name_type.rb
+++ app/graphql/types/eve_name_type.rb
@@ -6,50 +6,32 @@ module Types
 
     field :en, String,
       description: "English",
+      method: :name_en,
       null: true
 
     field :de, String,
       description: "Deutsch",
+      method: :name_de,
       null: true
 
     field :fr, String,
       description: "French",
+      method: :name_fr,
       null: true
 
     field :ja, String,
       description: "Japanese",
+      method: :name_ja,
       null: true
 
     field :ru, String,
       description: "Russian",
+      method: :name_ru,
       null: true
 
     field :ko, String,
       description: "Korean",
+      method: :name_ko,
       null: true
-
-    def en
-      object.name_en
-    end
-
-    def de
-      object.name_de
-    end
-
-    def fr
-      object.name_fr
-    end
-
-    def ja
-      object.name_ja
-    end
-
-    def ru
-      object.name_ru
-    end
-
-    def ko
-      object.name_ko
-    end
   end
 end
