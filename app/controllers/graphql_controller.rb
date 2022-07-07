fe18b5a789349112fb655d3199a575c070adefda
Igor Zubkov <igor.zubkov@gmail.com>
2022-07-07 22:36:36 +0100

    Fix rubocop warning

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -50,6 +50,6 @@ class GraphqlController < ApplicationController
     logger.error e.message
     logger.error e.backtrace.join("\n")
 
-    render json: {errors: [{message: e.message, backtrace: e.backtrace}], data: {}}, status: 500
+    render json: {errors: [{message: e.message, backtrace: e.backtrace}], data: {}}, status: :internal_server_error
   end
 end
