0cc90c8e5c0a2f0810f023427bc23cf14cda02c5
Igor Zubkov <igor.zubkov@gmail.com>
2022-07-15 14:36:40 +0100

    Update

--- app/services/cool_down_middleware.rb
+++ app/services/cool_down_middleware.rb
@@ -28,13 +28,14 @@ class CoolDownMiddleware < Faraday::Middleware
   def on_complete(env)
     esi_error_limit_remain = env.response_headers["x-esi-error-limit-remain"]
     esi_error_limit_reset = env.response_headers["x-esi-error-limit-reset"]
+    redis = Redis.new
 
     if esi_error_limit_remain && esi_error_limit_reset
-      Redis.current.set("esi_error_limit_remain",
+      redis.set("esi_error_limit_remain",
         esi_error_limit_remain.to_i,
         ex: esi_error_limit_reset.to_i)
 
-      Redis.current.set("esi_error_limit_remain_till",
+      redis.set("esi_error_limit_remain_till",
         esi_error_limit_reset.to_i.seconds.from_now,
         ex: esi_error_limit_reset.to_i)
     end
