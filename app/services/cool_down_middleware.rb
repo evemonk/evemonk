2200991c616d2c6d1d462ee9a249a97607c1970f
Igor Zubkov <igor.zubkov@gmail.com>
2022-07-15 14:43:54 +0100

    Update

--- app/services/cool_down_middleware.rb
+++ app/services/cool_down_middleware.rb
@@ -16,19 +16,22 @@ class CoolDownMiddleware < Faraday::Middleware
   #   # @logger = options.fetch(:logger)
   # end
 
-  def on_request(env)
+  def on_request(_)
+    error_limit_remain = redis.get("esi_error_limit_remain")
+    esi_error_limit_remain_till = redis.get("esi_error_limit_remain_till")
 
-
-    # binding.pry
-    #
-    # puts "hi"
-
+    if error_limit_remain
+      remain = error_limit_remain.to_i
+      if remain < 50
+        seconds = ActiveSupport::TimeZone["UTC"].parse(esi_error_limit_remain_till)
+        sleep(seconds)
+      end
+    end
   end
 
   def on_complete(env)
     esi_error_limit_remain = env[:response_headers]["x-esi-error-limit-remain"]
     esi_error_limit_reset = env[:response_headers]["x-esi-error-limit-reset"]
-    redis = Redis.new
 
     if esi_error_limit_remain && esi_error_limit_reset
       redis.set("esi_error_limit_remain",
@@ -36,9 +39,13 @@ class CoolDownMiddleware < Faraday::Middleware
         ex: esi_error_limit_reset.to_i)
 
       redis.set("esi_error_limit_remain_till",
-        esi_error_limit_reset.to_i.seconds.from_now,
+        esi_error_limit_reset.to_i.seconds.from_now.iso8601,
         ex: esi_error_limit_reset.to_i)
     end
+  end
+
+  def redis
+    redis = Redis.new
   end
 
   # def call(request_env)
