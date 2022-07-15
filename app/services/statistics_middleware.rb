5f5f5045c5d337a2fcc1f42954db0b47cc975584
Igor Zubkov <igor.zubkov@gmail.com>
2022-07-16 00:46:47 +0100

    Cleanup

--- app/services/statistics_middleware.rb
+++ app/services/statistics_middleware.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 class StatisticsMiddleware < Faraday::Middleware
-  def call(request_env)
-    @app.call(request_env).on_complete do |response_env|
-      method = "esi_requests_#{response_env[:status]}_counter"
+  def on_complete(env)
+    method = "esi_requests_#{env[:status]}_counter"
 
-      Yabeda.evemonk.public_send(method).increment({total: "success"}, by: 1)
-    end
+    Yabeda.evemonk.public_send(method).increment({total: "success"}, by: 1)
   end
 end
