d5888a3fe70909c3f652a2aa61909befea191c50
Igor Zubkov <igor.zubkov@gmail.com>
2022-07-15 23:45:32 +0100

    Update

--- spec/services/cool_down_middleware_spec.rb
+++ spec/services/cool_down_middleware_spec.rb
@@ -3,20 +3,49 @@
 require "rails_helper"
 
 describe CoolDownMiddleware do
-  describe "when request is 200" do
-    before { stub_request(:get, "https://esi.evetech.net/v2/status/").to_return(status: 200, body: {}.to_json) }
+  context "when esi_error_limit_remain and esi_error_limit_remain_till are not set yet" do
+    before { travel_to Time.zone.now }
+
+    before { VCR.insert_cassette "services/cool_down_middleware/on_complete" }
+
+    after { VCR.eject_cassette }
+
+    let(:redis) { instance_double(Redis) }
+
+    before { expect(Redis).to receive(:new).and_return(redis).exactly(4).times }
+
+    before { expect(redis).to receive(:get).with("esi_error_limit_remain").and_return(nil) }
+
+    before { expect(redis).to receive(:get).with("esi_error_limit_remain_till").and_return(nil) }
+
+    before do
+      #
+      # redis.set("esi_error_limit_remain",
+      #   esi_error_limit_remain.to_i,
+      #   ex: esi_error_limit_reset.to_i)
+      #
+      expect(redis).to receive(:set).with("esi_error_limit_remain", 100, ex: 19)
+    end
+
+    before do
+      #
+      # redis.set("esi_error_limit_remain_till",
+      #   esi_error_limit_reset.to_i.seconds.from_now.iso8601,
+      #   ex: esi_error_limit_reset.to_i)
+      expect(redis).to receive(:set).with("esi_error_limit_remain_till", 19.seconds.from_now.iso8601, ex: 19)
+    end
 
     let(:esi) { EveOnline::ESI::ServerStatus.new }
 
     let(:conn) do
       conn = Faraday.new { |f|
-        f.use CoolDownMiddleware, esi: esi
+        f.use CoolDownMiddleware
         f.adapter Faraday.default_adapter
       }
 
       conn.get("https://esi.evetech.net/v2/status/")
     end
 
-    specify { expect(conn.status).to eq(200) }
+    specify { expect { conn.status }.not_to raise_error }
   end
 end
