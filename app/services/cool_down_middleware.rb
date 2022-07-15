cd74e59478af4ce948278568e442d37c4eb6fd97
Igor Zubkov <igor.zubkov@gmail.com>
2022-07-15 14:35:37 +0100

    Update

--- app/services/cool_down_middleware.rb
+++ app/services/cool_down_middleware.rb
@@ -1,22 +1,23 @@
 # frozen_string_literal: true
 
 class CoolDownMiddleware < Faraday::Middleware
-  def initialize(app = nil, options = {})
-    super
-
-    # binding.pry
-
-    @esi = options.fetch(:esi)
-    # @logger = options.fetch(:logger)
-
-    # binding.pry
-
-    # @app = app
-    # @esi = options.fetch(:esi)
-    # @logger = options.fetch(:logger)
-  end
+  # def initialize(app = nil, options = {})
+  #   super
+  #
+  #   # binding.pry
+  #
+  #   @esi = options.fetch(:esi)
+  #   # @logger = options.fetch(:logger)
+  #
+  #   # binding.pry
+  #
+  #   # @app = app
+  #   # @esi = options.fetch(:esi)
+  #   # @logger = options.fetch(:logger)
+  # end
 
   def on_request(env)
+
 
     # binding.pry
     #
@@ -25,16 +26,18 @@ class CoolDownMiddleware < Faraday::Middleware
   end
 
   def on_complete(env)
-    esi_error_limit_remain = env.response_headers["x-esi-error-limit-remain"]&.to_i
-    esi_error_limit_reset = env.response_headers["x-esi-error-limit-reset"]&.to_i
+    esi_error_limit_remain = env.response_headers["x-esi-error-limit-remain"]
+    esi_error_limit_reset = env.response_headers["x-esi-error-limit-reset"]
 
-    Redis.current.set("esi_error_limit_remain",
-      esi_error_limit_remain,
-      ex: esi_error_limit_reset)
+    if esi_error_limit_remain && esi_error_limit_reset
+      Redis.current.set("esi_error_limit_remain",
+        esi_error_limit_remain.to_i,
+        ex: esi_error_limit_reset.to_i)
 
-    Redis.current.set("esi_error_limit_remain_till",
-      esi_error_limit_reset.seconds.from_now,
-      ex: esi_error_limit_reset)
+      Redis.current.set("esi_error_limit_remain_till",
+        esi_error_limit_reset.to_i.seconds.from_now,
+        ex: esi_error_limit_reset.to_i)
+    end
   end
 
   # def call(request_env)
