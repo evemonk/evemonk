681e66534f0bb08916a3c32e6ba5f665c44c5a00
Igor Zubkov <igor.zubkov@gmail.com>
2022-07-16 00:11:13 +0100

    Cleanup

--- app/services/statistics_middleware.rb
+++ app/services/statistics_middleware.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 class StatisticsMiddleware < Faraday::Middleware
-  def initialize(app = nil, options = {})
-    @app = app
-    @esi = options.fetch(:esi)
-  end
-
   def call(request_env)
     @app.call(request_env).on_complete do |response_env|
       method = "esi_requests_#{response_env[:status]}_counter"
