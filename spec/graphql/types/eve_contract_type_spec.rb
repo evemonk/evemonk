04eec46bb8e0fe899536b7f7199a22906fc04e81
Igor Zubkov <igor.zubkov@gmail.com>
2022-07-16 14:08:09 +0100

    Extract test

--- spec/graphql/types/eve_contract_type_spec.rb
+++ spec/graphql/types/eve_contract_type_spec.rb
@@ -3,30 +3,4 @@
 require "rails_helper"
 
 describe Types::EveContractType do
-  describe "get contract by id" do
-    let!(:eve_contract) do
-      create(:eve_contract,
-        contract_id: 123)
-    end
-
-    let(:query) do
-      %(
-        {
-          contract(id: 123) {
-            id
-          }
-        }
-      )
-    end
-
-    let(:result) { EvemonkSchema.execute(query).as_json }
-
-    specify do
-      expect(result).to eq("data" => {
-        "contract" => {
-          "id" => "123"
-        }
-      })
-    end
-  end
 end
