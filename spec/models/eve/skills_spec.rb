9eae4a390f1793d9e83ce8dc8c7bb6f6363279b8
Igor Zubkov <igor.zubkov@gmail.com>
2022-07-29 00:15:04 +0100

    Eve::Skills and Eve::Groups

---
+++ spec/models/eve/skills_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::Skills do
+  it { expect(described_class::SCIENCE_ID).to eq(3402) }
+
+  it { expect(described_class::ADVANCED_INDUSTRY_ID).to eq(3388) }
+
+  describe ".science" do
+    let!(:type) { create(:eve_type, type_id: described_class::SCIENCE_ID) }
+
+    specify { expect(described_class.science).to eq(type) }
+  end
+
+  describe ".advanced_industry" do
+    let!(:type) { create(:eve_type, type_id: described_class::ADVANCED_INDUSTRY_ID) }
+
+    specify { expect(described_class.advanced_industry).to eq(type) }
+  end
+end
