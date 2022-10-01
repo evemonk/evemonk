69b5ecf7567ce0fd3789bb23fa1a1264aaeb689d
Igor Zubkov <igor.zubkov@gmail.com>
2022-10-01 16:10:15 +0100

    Add Eve::IconDashboard

---
+++ app/dashboards/eve/icon_dashboard.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "administrate/base_dashboard"
+
+module Eve
+  class IconDashboard < Administrate::BaseDashboard
+    ATTRIBUTE_TYPES = {
+      id: Field::Number,
+      icon_id: Field::Number,
+      description: Field::String,
+      icon_file: Field::String,
+      obsolete: Field::Boolean,
+      created_at: Field::DateTime,
+      updated_at: Field::DateTime
+    }.freeze
+
+    COLLECTION_ATTRIBUTES = [:id, :icon_id, :description].freeze
+
+    SHOW_PAGE_ATTRIBUTES = [
+      :id,
+      :icon_id,
+      :description,
+      :icon_file,
+      :obsolete,
+      :created_at,
+      :updated_at
+    ].freeze
+
+    COLLECTION_FILTERS = {}.freeze
+
+    def display_resource(icon)
+      icon.description
+    end
+  end
+end
