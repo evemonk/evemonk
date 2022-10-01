0c3e88ae7138a2ff540dcff5d0612ded272f4a2f
Igor Zubkov <igor.zubkov@gmail.com>
2022-10-01 23:32:58 +0100

    Add tests

--- spec/models/eve/alliance_spec.rb
+++ spec/models/eve/alliance_spec.rb
@@ -68,98 +68,33 @@ describe Eve::Alliance do
   end
 
   describe "#icon_tiny" do
-    subject do
-      build(:eve_alliance,
-        alliance_id: 1_354_830_081)
-    end
+    before { expect(subject).to receive(:alliance_logo_url).with(32) }
 
-    context "when Setting.use_image_proxy is true" do
-      before { Setting.use_image_proxy = true }
-
-      specify { expect(subject.icon_tiny).to eq("https://imageproxy.evemonk.com/https://images.evetech.net/alliances/1354830081/logo?size=32") }
-    end
-
-    context "when Setting.use_image_proxy is false" do
-      before { Setting.use_image_proxy = false }
-
-      specify { expect(subject.icon_tiny).to eq("https://images.evetech.net/alliances/1354830081/logo?size=32") }
-    end
+    specify { expect { subject.icon_tiny }.not_to raise_error }
   end
 
   describe "#icon_small" do
-    subject do
-      build(:eve_alliance,
-        alliance_id: 1_354_830_081)
-    end
+    before { expect(subject).to receive(:alliance_logo_url).with(64) }
 
-    context "when Setting.use_image_proxy is true" do
-      before { Setting.use_image_proxy = true }
-
-      specify { expect(subject.icon_small).to eq("https://imageproxy.evemonk.com/https://images.evetech.net/alliances/1354830081/logo?size=64") }
-    end
-
-    context "when Setting.use_image_proxy is false" do
-      before { Setting.use_image_proxy = false }
-
-      specify { expect(subject.icon_small).to eq("https://images.evetech.net/alliances/1354830081/logo?size=64") }
-    end
+    specify { expect { subject.icon_small }.not_to raise_error }
   end
 
   describe "#icon_medium" do
-    subject do
-      build(:eve_alliance,
-        alliance_id: 1_354_830_081)
-    end
+    before { expect(subject).to receive(:alliance_logo_url).with(128) }
 
-    context "when Setting.use_image_proxy is true" do
-      before { Setting.use_image_proxy = true }
-
-      specify { expect(subject.icon_medium).to eq("https://imageproxy.evemonk.com/https://images.evetech.net/alliances/1354830081/logo?size=128") }
-    end
-
-    context "when Setting.use_image_proxy is false" do
-      before { Setting.use_image_proxy = false }
-
-      specify { expect(subject.icon_medium).to eq("https://images.evetech.net/alliances/1354830081/logo?size=128") }
-    end
+    specify { expect { subject.icon_medium }.not_to raise_error }
   end
 
   describe "#icon_large" do
-    subject do
-      build(:eve_alliance,
-        alliance_id: 1_354_830_081)
-    end
+    before { expect(subject).to receive(:alliance_logo_url).with(256) }
 
-    context "when Setting.use_image_proxy is true" do
-      before { Setting.use_image_proxy = true }
-
-      specify { expect(subject.icon_large).to eq("https://imageproxy.evemonk.com/https://images.evetech.net/alliances/1354830081/logo?size=256") }
-    end
-
-    context "when Setting.use_image_proxy is false" do
-      before { Setting.use_image_proxy = false }
-
-      specify { expect(subject.icon_large).to eq("https://images.evetech.net/alliances/1354830081/logo?size=256") }
-    end
+    specify { expect { subject.icon_large }.not_to raise_error }
   end
 
   describe "#icon_huge" do
-    subject do
-      build(:eve_alliance,
-        alliance_id: 1_354_830_081)
-    end
+    before { expect(subject).to receive(:alliance_logo_url).with(512) }
 
-    context "when Setting.use_image_proxy is true" do
-      before { Setting.use_image_proxy = true }
-
-      specify { expect(subject.icon_huge).to eq("https://imageproxy.evemonk.com/https://images.evetech.net/alliances/1354830081/logo?size=512") }
-    end
-
-    context "when Setting.use_image_proxy is false" do
-      before { Setting.use_image_proxy = false }
-
-      specify { expect(subject.icon_huge).to eq("https://images.evetech.net/alliances/1354830081/logo?size=512") }
-    end
+    specify { expect { subject.icon_huge }.not_to raise_error }
   end
 
   describe "#formatted_corporations_count" do
