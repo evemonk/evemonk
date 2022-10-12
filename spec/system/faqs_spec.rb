f7b7f17ac567fd169caccc2d7f19cdcbe8717204
Igor Zubkov <igor.zubkov@gmail.com>
2022-10-12 19:49:24 +0100

    More tests

--- spec/system/faqs_spec.rb
+++ spec/system/faqs_spec.rb
@@ -3,7 +3,17 @@
 require "rails_helper"
 
 describe "FAQ features" do
-  it "should render show action" do
-    visit faq_path
+  describe "should render page" do
+    it "when user not logged in" do
+      visit faq_path
+    end
+
+    it "when user is logged in" do
+      user = create(:user)
+
+      sign_in user
+
+      visit faq_path
+    end
   end
 end
