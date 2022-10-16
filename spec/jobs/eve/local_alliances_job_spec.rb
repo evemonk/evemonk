3495a99c082ef2572d98c95b905f0b249b0f0d01
Igor Zubkov <igor.zubkov@gmail.com>
2022-10-16 16:55:30 +0100

    Update

--- spec/jobs/eve/local_alliances_job_spec.rb
+++ spec/jobs/eve/local_alliances_job_spec.rb
@@ -8,17 +8,29 @@ describe Eve::LocalAlliancesJob do
   it { expect(described_class.queue_name).to eq("default") }
 
   describe "#perform" do
-    before do
-      #
-      # Eve::LocalAlliancesImporter.new.import
-      #
-      expect(Eve::LocalAlliancesImporter).to receive(:new) do
-        double.tap do |a|
-          expect(a).to receive(:import)
+    context "when eve_local_alliances_job enabled" do
+      before { Flipper.enable(:eve_local_alliances_job) }
+
+      before do
+        #
+        # Eve::LocalAlliancesImporter.new.import
+        #
+        expect(Eve::LocalAlliancesImporter).to receive(:new) do
+          double.tap do |a|
+            expect(a).to receive(:import)
+          end
         end
       end
+
+      specify { expect { subject.perform }.not_to raise_error }
     end
 
-    specify { expect { subject.perform }.not_to raise_error }
+    context "when eve_local_alliances_job disabled" do
+      before { Flipper.disable(:eve_local_alliances_job) }
+
+      before { expect(Eve::LocalAlliancesImporter).not_to receive(:new) }
+
+      specify { expect { subject.perform }.not_to raise_error }
+    end
   end
 end
