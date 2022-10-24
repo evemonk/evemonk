652eeab4353dc16f47c9795ab8f06e98d22351a6
Igor Zubkov <igor.zubkov@gmail.com>
2022-10-24 21:52:06 +0100

    Types::EveAgentIconType

---
+++ app/graphql/types/eve_agent_icon_type.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Types
+  class EveAgentIconType < Types::BaseObject
+    description "Agent Icon object"
+
+    field :tiny, String,
+      description: "URL to tiny icon",
+      method: :icon_tiny,
+      null: true
+
+    field :small, String,
+      description: "URL to small icon",
+      method: :icon_small,
+      null: true
+
+    field :medium, String,
+      description: "URL to medium icon",
+      method: :icon_medium,
+      null: true
+
+    field :large, String,
+      description: "URL to large icon",
+      method: :icon_large,
+      null: true
+
+    field :huge, String,
+      description: "URL to huge icon",
+      method: :icon_huge,
+      null: true
+
+    field :gigantic, String,
+      description: "URL to gigantic icon",
+      method: :icon_gigantic,
+      null: true
+  end
+end
