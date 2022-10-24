2b517d4a732ee658dcdc6e46659871ccb45e4a03
Igor Zubkov <igor.zubkov@gmail.com>
2022-10-24 21:53:53 +0100

    Types::EveAgentType

---
+++ app/graphql/types/eve_agent_type.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Types
+  class EveAgentType < Types::BaseObject
+    description "Agent object"
+
+    field :id, ID,
+      description: "Agent ID",
+      null: false
+
+    field :name, String,
+      description: "Agent name",
+      null: true
+
+    field :icon, EveAgentIconType,
+      description: "Eve Agent Icons",
+      method: :itself,
+      null: true
+
+    field :corporation_id, Integer,
+      description: "Corporation ID",
+      null: true
+
+    field :corporation, Types::EveCorporationType,
+      description: "Agent Corporation",
+      null: true
+
+    # TODO: location -> Eve::Station
+
+    # t.bigint "agent_type_id"
+    # t.bigint "division_id"
+    # t.boolean "is_locator"
+    # t.integer "level"
+    # t.bigint "location_id"
+    # t.integer "quality"
+  end
+end
