82c0dd6499a3dac8b62a5f69ab1e84abf1547e2b
Igor Zubkov <igor.zubkov@gmail.com>
2022-10-24 21:48:09 +0100

    More tests

---
+++ spec/integration/graphql/get_agent_by_id_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Get Agent by ID" do
+  let!(:eve_corporation) do
+    create(:eve_corporation,
+      corporation_id: 222)
+  end
+
+  let!(:eve_agent) do
+    create(:eve_agent,
+      id: 123,
+      name: "Agent 1",
+      corporation_id: 222)
+  end
+
+  let(:query) do
+    %(
+      query getAgentByID {
+        agent(id: 123) {
+          id
+          name
+          icon {
+            tiny
+            small
+            medium
+            large
+            huge
+            gigantic
+          }
+          corporationId
+          corporation {
+            id
+          }
+        }
+      }
+    )
+  end
+
+  let(:result) { EvemonkSchema.execute(query).as_json }
+
+  specify do
+    expect(result).to eq("data" => {
+      "agent" => {
+        "id" => "123",
+        "name" => "Agent 1",
+        "icon" => {
+          "tiny" => "https://imageproxy.evemonk.com/https://images.evetech.net/characters/123/portrait?size=32",
+          "small" => "https://imageproxy.evemonk.com/https://images.evetech.net/characters/123/portrait?size=64",
+          "medium" => "https://imageproxy.evemonk.com/https://images.evetech.net/characters/123/portrait?size=128",
+          "large" => "https://imageproxy.evemonk.com/https://images.evetech.net/characters/123/portrait?size=256",
+          "huge" => "https://imageproxy.evemonk.com/https://images.evetech.net/characters/123/portrait?size=512",
+          "gigantic" => "https://imageproxy.evemonk.com/https://images.evetech.net/characters/123/portrait?size=1024"
+        },
+        "corporationId" => 222,
+        "corporation" => {
+          "id" => "222"
+        }
+      }
+    })
+  end
+end
