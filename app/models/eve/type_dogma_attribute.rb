0332db51ae9b7bce1f89214464982d121404d255
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-17 13:21:54 +0100

    Update

--- app/models/eve/type_dogma_attribute.rb
+++ app/models/eve/type_dogma_attribute.rb
@@ -13,5 +13,20 @@ module Eve
       primary_key: "attribute_id",
       foreign_key: "attribute_id",
       optional: true
+
+    def value_with_unit
+      case dogma_attribute.unit.unit_name
+      when "attributePoints"
+        "#{value.to_i} points"
+      when "Slot"
+        "Slot #{value.to_i}"
+      when "Level"
+        "Level #{value.to_i}"
+      when "Volume"
+        "#{value.to_i} &#13221;".html_safe
+      else
+        "#{value.to_i} #{dogma_attribute.unit.unit_name}"
+      end
+    end
   end
 end
