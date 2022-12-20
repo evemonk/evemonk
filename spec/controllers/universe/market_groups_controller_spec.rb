5a0cc4cff333113f12eb974de05969ecbd886196
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-20 19:58:12 +0100

    Backport changes

--- spec/controllers/universe/market_groups_controller_spec.rb
+++ spec/controllers/universe/market_groups_controller_spec.rb
@@ -8,6 +8,23 @@ describe Universe::MarketGroupsController do
   it { should_not use_before_action(:authenticate_user!) }
 
   describe "#index" do
+    before do
+      #
+      # Eve::MarketGroup.main_groups
+      #   .includes(:icon)
+      #   .order(:name_en)
+      #
+      expect(Eve::MarketGroup).to receive(:main_groups) do
+        double.tap do |a|
+          expect(a).to receive(:includes).with(:icon) do
+            double.tap do |b|
+              expect(b).to receive(:order).with(:name_en)
+            end
+          end
+        end
+      end
+    end
+
     before { get :index }
 
     it { should respond_with(:ok) }
@@ -16,6 +33,31 @@ describe Universe::MarketGroupsController do
   end
 
   describe "#show" do
+    let(:eve_market_group) { instance_double(Eve::MarketGroup) }
+
+    before do
+      #
+      # Eve::MarketGroup.find_by!(market_group_id: params[:id]) # => eve_market_group
+      #
+      expect(Eve::MarketGroup).to receive(:find_by!).with(market_group_id: "1")
+        .and_return(eve_market_group)
+    end
+
+    before do
+      #
+      # eve_market_group.subgroups.includes(:icon).order(:name_en)
+      #
+      expect(eve_market_group).to receive(:subgroups) do
+        double.tap do |a|
+          expect(a).to receive(:includes).with(:icon) do
+            double.tap do |b|
+              expect(b).to receive(:order).with(:name_en)
+            end
+          end
+        end
+      end
+    end
+
     before { get :show, params: {id: "1"} }
 
     it { should respond_with(:ok) }
