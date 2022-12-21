0421acffe3bc70e03159654723723342be4e7bbf
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-21 13:40:39 +0100

    More tests

--- spec/factories/eve/graphic_factory.rb
+++ spec/factories/eve/graphic_factory.rb
@@ -2,12 +2,20 @@
 
 FactoryBot.define do
   factory :eve_graphic, class: "Eve::Graphic" do
+    sequence(:graphic_id)
+
     collision_file { "" }
+
     graphic_file { "" }
+
     icon_folder { "" }
+
     sof_dna { "" }
+
     sof_fation_name { "" }
+
     sof_hull_name { "" }
+
     sof_race_name { "" }
   end
 end
---
+++ spec/system/admin/eve/graphics_spec.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Admin::Eve::Graphic's features" do
+  describe "#index" do
+    context "when user is logged in" do
+      context "when user is admin" do
+        it "should render page" do
+          user = create(:user, locale: :english, admin: true)
+
+          sign_in user
+
+          create(:eve_graphic)
+
+          visit admin_eve_graphics_path
+
+          assert current_path == admin_eve_graphics_path
+        end
+      end
+
+      context "when user is not admin" do
+        it "should render page" do
+          user = create(:user, locale: :english, admin: false)
+
+          sign_in user
+
+          create(:eve_graphic)
+
+          visit admin_eve_graphics_path
+
+          assert current_path == root_path
+        end
+      end
+    end
+
+    context "when user is not logged in" do
+      it "should render page" do
+        create(:eve_graphic)
+
+        visit admin_eve_graphics_path
+
+        assert current_path == new_user_session_path
+      end
+    end
+  end
+
+  describe "#show" do
+    context "when user is logged in" do
+      context "when user is admin" do
+        it "should render page" do
+          user = create(:user, locale: :english, admin: true)
+
+          sign_in user
+
+          eve_graphic = create(:eve_graphic, id: 1)
+
+          visit admin_eve_graphic_path(eve_graphic)
+
+          assert current_path == admin_eve_graphic_path(eve_graphic)
+        end
+      end
+
+      context "when user is not admin" do
+        it "should render page" do
+          user = create(:user, locale: :english, admin: false)
+
+          sign_in user
+
+          eve_graphic = create(:eve_graphic, id: 1)
+
+          visit admin_eve_graphic_path(eve_graphic)
+
+          assert current_path == root_path
+        end
+      end
+    end
+
+    context "when user is not logged in" do
+      it "should render page" do
+        eve_graphic = create(:eve_graphic, id: 1)
+
+        visit admin_eve_graphic_path(eve_graphic)
+
+        assert current_path == new_user_session_path
+      end
+    end
+  end
+end
