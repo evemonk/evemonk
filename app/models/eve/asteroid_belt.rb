8642d4c062a69aa1132048a71bd379d85c4967ac
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-29 00:22:21 +0100

    Cleanup

--- app/models/eve/asteroid_belt.rb
+++ app/models/eve/asteroid_belt.rb
@@ -4,9 +4,7 @@ module Eve
   class AsteroidBelt < ApplicationRecord
     has_paper_trail
 
-    belongs_to :system,
-      primary_key: "system_id",
-      optional: true
+    belongs_to :system, optional: true
 
     belongs_to :planet,
       primary_key: "planet_id",
--- spec/models/eve/asteroid_belt_spec.rb
+++ spec/models/eve/asteroid_belt_spec.rb
@@ -7,7 +7,7 @@ describe Eve::AsteroidBelt do
 
   it { should respond_to(:versions) }
 
-  it { should belong_to(:system).with_primary_key("system_id").optional(true) }
+  it { should belong_to(:system).optional(true) }
 
   it { should belong_to(:planet).with_primary_key("planet_id").optional(true) }
 
