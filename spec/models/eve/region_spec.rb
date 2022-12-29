b33cba85c169acfbf5ed80230e5fba83050fea90
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-29 11:04:50 +0100

    Cleanup

--- spec/models/eve/region_spec.rb
+++ spec/models/eve/region_spec.rb
@@ -5,6 +5,8 @@ require "rails_helper"
 describe Eve::Region do
   it { should be_an(ApplicationRecord) }
 
+  it { expect(described_class.primary_key).to eq("region_id") }
+
   it { should respond_to(:versions) }
 
   it { expect(described_class).to respond_to(:translates) }
@@ -13,7 +15,7 @@ describe Eve::Region do
 
   it { expect(described_class.table_name).to eq("eve_regions") }
 
-  it { should have_many(:constellations).with_primary_key("region_id") }
+  it { should have_many(:constellations) }
 
-  it { should have_many(:contracts).with_primary_key("region_id") }
+  it { should have_many(:contracts) }
 end
