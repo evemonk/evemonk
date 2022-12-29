537c28c0674b3b9a0db31a264fadda8a6c689f9b
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-29 13:53:57 +0100

    Cleanup

--- app/models/character.rb
+++ app/models/character.rb
@@ -8,10 +8,7 @@ class Character < ApplicationRecord
 
   belongs_to :user
 
-  belongs_to :race,
-    class_name: "Eve::Race",
-    primary_key: "race_id",
-    optional: true
+  belongs_to :race, class_name: "Eve::Race", optional: true
 
   belongs_to :bloodline,
     class_name: "Eve::Bloodline",
