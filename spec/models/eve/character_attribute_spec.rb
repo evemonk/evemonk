a54e9ee200cf7f206404d126632ace2b0777ad93
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-29 23:27:20 +0100

    Cleanup

--- spec/models/eve/character_attribute_spec.rb
+++ spec/models/eve/character_attribute_spec.rb
@@ -13,7 +13,7 @@ describe Eve::CharacterAttribute do
 
   it { expect(described_class.table_name).to eq("eve_character_attributes") }
 
-  it { should belong_to(:icon).class_name("Eve::Icon").with_primary_key("icon_id").optional(true) }
+  it { should belong_to(:icon).optional(true) }
 
   describe "#sanitized_description" do
     subject do
