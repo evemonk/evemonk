83caacb06d7cfb7801e4340551e237faef483959
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-30 14:30:22 +0100

    Cleanup

--- app/models/eve/group.rb
+++ app/models/eve/group.rb
@@ -15,9 +15,7 @@ module Eve
       foreign_key: "category_id",
       optional: true
 
-    has_many :types, # rubocop:disable Rails/HasManyOrHasOneDependent
-      primary_key: "group_id",
-      foreign_key: "group_id"
+    has_many :types # rubocop:disable Rails/HasManyOrHasOneDependent
 
     has_many :certificates, # rubocop:disable Rails/HasManyOrHasOneDependent
       primary_key: "group_id",
