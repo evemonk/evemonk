d94e032efb2c748f3480890078aca43269131d2c
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-30 20:23:38 +0100

    Cleanup

--- app/models/character_implant.rb
+++ app/models/character_implant.rb
@@ -5,7 +5,6 @@ class CharacterImplant < ApplicationRecord
 
   belongs_to :implant,
     class_name: "Eve::Implant",
-    primary_key: "type_id",
     foreign_key: "type_id",
     optional: true
 end
