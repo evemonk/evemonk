9e351787a710c245f4c619054529deb0a51a63b7
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-31 01:42:19 +0100

    Fix rubocop warning

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -3,6 +3,6 @@
 class WelcomeMailer < ApplicationMailer
   def email
     @user = params[:user]
-    mail(to: @user.email, subject: "Welcome to evemonk.com")
+    mail(to: @user.email, subject: default_i18n_subject)
   end
 end
