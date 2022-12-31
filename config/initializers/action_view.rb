c41290acef99be75247cfc2c9a25a14a9a35e0ae
Igor Zubkov <igor.zubkov@gmail.com>
2022-12-31 02:09:06 +0100

    Turn off rubocop warning

--- config/initializers/action_view.rb
+++ config/initializers/action_view.rb
@@ -2,5 +2,5 @@
 
 # https://coderwall.com/p/s-zwrg/remove-rails-field_with_errors-wrapper
 ActionView::Base.field_error_proc = proc do |html_tag, _|
-  html_tag.html_safe
+  html_tag.html_safe # rubocop:disable Rails/OutputSafety
 end
