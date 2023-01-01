d606dc6e8407bd56e7b89186a4e5ecec1cd72bc5
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-01 19:29:18 +0100

    Fix rubocop warnings

--- db/migrate/20201025183221_drop_zh_language.rb
+++ db/migrate/20201025183221_drop_zh_language.rb
@@ -2,24 +2,24 @@
 
 class DropZhLanguage < ActiveRecord::Migration[6.0]
   def change
-    remove_column :eve_ancestries, :name_zh
-    remove_column :eve_ancestries, :description_zh
-    remove_column :eve_bloodlines, :name_zh
-    remove_column :eve_bloodlines, :description_zh
-    remove_column :eve_categories, :name_zh
-    remove_column :eve_character_attributes, :name_zh
-    remove_column :eve_constellations, :name_zh
-    remove_column :eve_factions, :name_zh
-    remove_column :eve_factions, :description_zh
-    remove_column :eve_groups, :name_zh
-    remove_column :eve_market_groups, :description_zh
-    remove_column :eve_market_groups, :name_zh
-    remove_column :eve_races, :description_zh
-    remove_column :eve_races, :name_zh
-    remove_column :eve_regions, :description_zh
-    remove_column :eve_regions, :name_zh
-    remove_column :eve_systems, :name_zh
-    remove_column :eve_types, :description_zh
-    remove_column :eve_types, :name_zh
+    remove_column :eve_ancestries, :name_zh, :string
+    remove_column :eve_ancestries, :description_zh, :text
+    remove_column :eve_bloodlines, :name_zh, :string
+    remove_column :eve_bloodlines, :description_zh, :text
+    remove_column :eve_categories, :name_zh, :string
+    remove_column :eve_character_attributes, :name_zh, :string
+    remove_column :eve_constellations, :name_zh, :string
+    remove_column :eve_factions, :name_zh, :string
+    remove_column :eve_factions, :description_zh, :text
+    remove_column :eve_groups, :name_zh, :string
+    remove_column :eve_market_groups, :description_zh, :text
+    remove_column :eve_market_groups, :name_zh, :string
+    remove_column :eve_races, :description_zh, :text
+    remove_column :eve_races, :name_zh, :string
+    remove_column :eve_regions, :description_zh, :text
+    remove_column :eve_regions, :name_zh, :string
+    remove_column :eve_systems, :name_zh, :string
+    remove_column :eve_types, :description_zh, :text
+    remove_column :eve_types, :name_zh, :string
   end
 end
