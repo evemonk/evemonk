001df4f478e904eafebc36d5c13b601495facdd3
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-02 18:26:07 +0100

    Remove ancestry

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -10,7 +10,7 @@ class CharactersController < ApplicationController
 
   def show
     @character = current_user.characters
-      .includes(:race, :bloodline, :ancestry, :faction, :alliance, :corporation, :current_ship_type)
+      .includes(:race, :bloodline, :faction, :alliance, :corporation, :current_ship_type)
       .find_by!(character_id: params[:id])
   end
 
