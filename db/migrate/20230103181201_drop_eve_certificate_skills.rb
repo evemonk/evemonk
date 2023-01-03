3887bdb7221e8544e9120fbf8268dd7f98ad325f
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-03 19:15:09 +0100

    Cleanup

--- db/migrate/20230103181201_drop_eve_certificate_skills.rb
+++ db/migrate/20230103181201_drop_eve_certificate_skills.rb
@@ -5,10 +5,10 @@ class DropEveCertificateSkills < ActiveRecord::Migration[7.0]
     remove_index :eve_certificate_skills, :certificate_id
 
     drop_table :eve_certificate_skills, id: :uuid do |t|
-      t.bigint "certificate_id"
-      t.bigint "skill_id"
-      t.integer "level"
-      t.integer "skill_level"
+      t.bigint :certificate_id
+      t.bigint :skill_id
+      t.integer :level
+      t.integer :skill_level
 
       t.timestamps
     end
