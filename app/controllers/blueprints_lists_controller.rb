32f42acf9d6b0fc6689a6cdac3390d888e72754f
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-06 23:07:14 +0100

    BlueprintsListsController

---
+++ app/controllers/blueprints_lists_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class BlueprintsListsController < ApplicationController
+  def index
+    @character = current_user.characters
+      .find_by!(character_id: params[:character_id])
+
+    @character_blueprints = @character.character_blueprints
+      .includes(blueprint: :group)
+  end
+end
