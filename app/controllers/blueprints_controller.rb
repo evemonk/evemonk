f9c17b9d19b05e25c9bbe1451885abe2347de3f7
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-06 23:15:30 +0100

    Cleanup

--- app/controllers/blueprints_controller.rb
+++ app/controllers/blueprints_controller.rb
@@ -5,9 +5,6 @@ class BlueprintsController < ApplicationController
     @character = current_user.characters
       .includes(:alliance, :corporation)
       .find_by!(character_id: params[:character_id])
-
-    @character_blueprints = @character.character_blueprints
-      .includes(blueprint: :group)
   end
 
   def show
