68c882f9b44fb1bfecf9f5b27bebc4037c6adbcf
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-07 02:49:43 +0100

    Eve::NpcCorporationDivision

---
+++ app/models/eve/npc_corporation_division.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Eve
+  class NpcCorporationDivision < ApplicationRecord
+  end
+end
---
+++ spec/models/eve/npc_corporation_division_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Eve::NpcCorporationDivision do
+  it { should be_an(ApplicationRecord) }
+end
