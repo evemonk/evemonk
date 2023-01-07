b4c8c4fe81729737d549ddd6bf5146f95c39ab07
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-07 03:44:57 +0100

    Update

--- app/models/eve/npc_corporation_division.rb
+++ app/models/eve/npc_corporation_division.rb
@@ -5,5 +5,7 @@ module Eve
     extend Mobility
 
     translates :name, :description_full, :leader_type_name
+
+    has_many :agents, foreign_key: "division_id"
   end
 end
--- spec/models/eve/npc_corporation_division_spec.rb
+++ spec/models/eve/npc_corporation_division_spec.rb
@@ -8,4 +8,6 @@ describe Eve::NpcCorporationDivision do
   it { expect(described_class).to respond_to(:translates) }
 
   it { expect(described_class.mobility_attributes).to eq(["name", "description_full", "leader_type_name"]) }
+
+  it { should have_many(:agents).with_foreign_key("division_id") }
 end
