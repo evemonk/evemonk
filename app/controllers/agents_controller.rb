9c7ec2d2ff775ac8fe420390d63f628260d8becf
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-08 19:59:23 +0100

    Fix N+1

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -3,7 +3,7 @@
 class AgentsController < ApplicationController
   def index
     @character = current_user.characters
-      .includes(:alliance, :corporation, :agents_standings)
+      .includes(:alliance, :corporation, agents_standings: {standingable: :division})
       .find_by!(character_id: params[:character_id])
   end
 end
