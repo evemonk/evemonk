95cd85554738bf09583159c456a7be2775a679f5
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-08 14:45:53 +0100

    Update

--- config/initializers/prosopite.rb
+++ config/initializers/prosopite.rb
@@ -4,4 +4,5 @@ if defined? Prosopite
   Prosopite.raise = true
   Prosopite.rails_logger = true
   Prosopite.prosopite_logger = true
+  Prosopite.ignore_queries = [/regex_match/, "SELECT var, value FROM \"settings\""]
 end
