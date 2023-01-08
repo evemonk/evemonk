cb707546fba6ef7b26b628697e4e4dd8b681aebd
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-08 20:25:46 +0100

    Turn off prosopite for now

--- config/initializers/prosopite.rb
+++ config/initializers/prosopite.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-if defined? Prosopite
-  Prosopite.raise = true
-  Prosopite.rails_logger = true
-  Prosopite.prosopite_logger = true
-  Prosopite.ignore_queries = [/regex_match/, "SELECT var, value FROM \"settings\""]
-end
+# if defined? Prosopite
+#   Prosopite.raise = true
+#   Prosopite.rails_logger = true
+#   Prosopite.prosopite_logger = true
+#   Prosopite.ignore_queries = [/regex_match/, "SELECT var, value FROM \"settings\""]
+# end
