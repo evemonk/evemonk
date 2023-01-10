f1296e3277493cc7537eb0365ff704c6932a7710
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-10 20:45:32 +0100

    Update

--- app/controllers/blueprints_lists_controller.rb
+++ app/controllers/blueprints_lists_controller.rb
@@ -5,6 +5,8 @@ class BlueprintsListsController < ApplicationController
     @character = current_user.characters
       .find_by!(character_id: params[:character_id])
 
+    @form = CharacterStandingsFilterForm.new(params[:filter])
+
     @character_blueprints = @character.character_blueprints
       .includes(blueprint: :group)
       .order("eve_types.name_en": :asc)
