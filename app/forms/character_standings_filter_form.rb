8f5c751f2b3db023c36fcd460155c8e55ede0014
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-10 20:17:38 +0100

    Update form

--- app/forms/character_standings_filter_form.rb
+++ app/forms/character_standings_filter_form.rb
@@ -3,17 +3,29 @@
 class CharacterStandingsFilterForm
   include ActiveModel::Model
 
-  def filter
+  attr_accessor :filter
+
+  def initialize(filter)
+    @filter = filter
+  end
+
+  def select_data
+    raw_values.map(&:values)
+  end
+
+  def selected
+    filter
+  end
+
+  private
+
+  def raw_values
     [
       {name: "All", id: 0},
       {name: "BPO", id: 1},
       {name: "BPC", id: 2},
       {name: "Ancient Relics", id: 3},
       {name: "Stacked", id: 4}
-    ].map(&:values)
-  end
-
-  def filter_selected
-
+    ]
   end
 end
