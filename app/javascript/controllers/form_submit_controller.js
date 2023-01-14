5f62a6952cf3133f02543cf086f63a912c6400af
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-14 21:11:39 +0100

    Update

--- app/javascript/controllers/form_submit_controller.js
+++ app/javascript/controllers/form_submit_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   /**
-   * Submit form with form.requestSubmit().
+   * Submit form with form.requestSubmit(). So, Turbo can intercept.
    *
    * https://discuss.hotwired.dev/t/form-submit-with-turbo-streams-response-without-redirect/3290
    */
