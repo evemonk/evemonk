8d479ae4fe7b9432f7f8f96b54a5d98ab48e4d0d
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-14 20:32:11 +0100

    Fix form submit for Turbo

--- app/javascript/controllers/form_submit_controller.js
+++ app/javascript/controllers/form_submit_controller.js
@@ -2,6 +2,6 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   submit_form() {
-    this.element.submit();
+    this.element.requestSubmit();
   }
 }
