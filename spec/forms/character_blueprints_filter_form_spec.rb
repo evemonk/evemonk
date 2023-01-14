561dfa1d0e1d69d360477cb7a6b5857f892026f2
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-14 22:22:27 +0100

    Add tests

--- spec/forms/character_blueprints_filter_form_spec.rb
+++ spec/forms/character_blueprints_filter_form_spec.rb
@@ -3,4 +3,47 @@
 require "rails_helper"
 
 describe CharacterBlueprintsFilterForm do
+  it { should be_an(ActiveModel::Model) }
+
+  let(:filter) { double }
+
+  subject { described_class.new(filter) }
+
+  describe "#initialize" do
+    its(:filter) { should eq(filter) }
+  end
+
+  describe "#select_data" do
+    specify do
+      expect(subject.select_data).to eq(
+        [
+          ["All", 0],
+          ["BPO", 1],
+          ["BPC", 2],
+          ["Ancient Relics", 3],
+          ["Stacked", 4]
+        ]
+      )
+    end
+  end
+
+  describe "#selected" do
+    specify { expect(subject.selected).to eq(filter) }
+  end
+
+  # private methods
+
+  describe "#raw_values" do
+    specify do
+      expect(subject.send(:raw_values)).to eq(
+        [
+          {name: "All", id: 0},
+          {name: "BPO", id: 1},
+          {name: "BPC", id: 2},
+          {name: "Ancient Relics", id: 3},
+          {name: "Stacked", id: 4}
+        ]
+      )
+    end
+  end
 end
