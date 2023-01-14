0b22c942b05725ebcb9831409cfcce32a2a95396
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-14 21:06:55 +0100

    Add documentation

--- app/javascript/controllers/form_submit_controller.js
+++ app/javascript/controllers/form_submit_controller.js
@@ -1,6 +1,11 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  /**
+   * Submit form with form.requestSubmit().
+   *
+   * https://discuss.hotwired.dev/t/form-submit-with-turbo-streams-response-without-redirect/3290
+   */
   submit_form() {
     this.element.requestSubmit();
   }
