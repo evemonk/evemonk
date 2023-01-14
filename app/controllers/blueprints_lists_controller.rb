ec35e1f3b316a0c99f8896121a646990405b8ee7
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-14 20:57:50 +0100

    Update

--- app/controllers/blueprints_lists_controller.rb
+++ app/controllers/blueprints_lists_controller.rb
@@ -8,8 +8,28 @@ class BlueprintsListsController < ApplicationController
     @form = CharacterStandingsFilterForm.new(params[:filter])
 
     @character_blueprints = @character.character_blueprints
-      .includes(blueprint: :group)
+      .joins(blueprint: :group)
       .order("eve_types.name_en": :asc)
+
+    @character_blueprints =
+      if params[:filter].present?
+        case params[:filter]
+        when "0"
+          @character_blueprints
+        when "1"
+          @character_blueprints.where(quantity: -1)
+        when "2"
+          @character_blueprints.where(quantity: -2)
+        when "3"
+          @character_blueprints.where(eve_types: {eve_groups: {category_id: Eve::Categories::ANCIENT_RELICS_ID}})
+        when "4"
+          @character_blueprints.where(quantity: [0..])
+        else
+          @character_blueprints
+        end
+      else
+        @character_blueprints
+      end
 
     render layout: false
   end
