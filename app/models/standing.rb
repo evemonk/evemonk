d9e9fd957ebcc792baad77564c7e1a4b25d97a6f
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-16 11:54:47 +0100

    Later

--- app/models/standing.rb
+++ app/models/standing.rb
@@ -9,26 +9,27 @@ class Standing < ApplicationRecord
     polymorphic: true,
     optional: true
 
-  scope :factions, -> do
-    joins("LEFT JOIN eve_factions ON standings.standingable_id = eve_factions.id")
-      .where(standingable_type: Eve::Faction.name)
-      .order("eve_factions.name_en": :asc)
-      .includes(:standingable)
-  end
-
-  scope :corporations, -> do
-    joins("LEFT JOIN eve_corporations ON standings.standingable_id = eve_corporations.id")
-      .where(standingable_type: Eve::Corporation.name)
-      .order("eve_corporations.name": :asc)
-      .includes(:standingable)
-  end
-
-  scope :agents, -> do
-    joins("LEFT JOIN eve_agents ON standings.standingable_id = eve_agents.id")
-      .where(standingable_type: Eve::Agent.name)
-      .order("eve_agents.name": :asc)
-      .includes(:standingable)
-  end
+  # TODO: later
+  # scope :factions, -> do
+  #   joins("LEFT JOIN eve_factions ON standings.standingable_id = eve_factions.id")
+  #     .where(standingable_type: Eve::Faction.name)
+  #     .order("eve_factions.name_en": :asc)
+  #     .includes(:standingable)
+  # end
+  #
+  # scope :corporations, -> do
+  #   joins("LEFT JOIN eve_corporations ON standings.standingable_id = eve_corporations.id")
+  #     .where(standingable_type: Eve::Corporation.name)
+  #     .order("eve_corporations.name": :asc)
+  #     .includes(:standingable)
+  # end
+  #
+  # scope :agents, -> do
+  #   joins("LEFT JOIN eve_agents ON standings.standingable_id = eve_agents.id")
+  #     .where(standingable_type: Eve::Agent.name)
+  #     .order("eve_agents.name": :asc)
+  #     .includes(:standingable)
+  # end
 
   def rounded_standing
     standing.round(1).to_s
--- spec/models/standing_spec.rb
+++ spec/models/standing_spec.rb
@@ -11,62 +11,47 @@ describe Standing do
 
   it { should belong_to(:standingable).optional(true) }
 
-  describe ".factions" do
-    let!(:eve_faction_1) { create(:eve_faction, name_en: "A") }
-
-    let!(:eve_faction_2) { create(:eve_faction, name_en: "B") }
-
-    let!(:character) { create(:character) }
-
-    let!(:standing_1) { create(:standing, :for_faction, character: character, standingable: eve_faction_1) }
-
-    let!(:standing_2) { create(:standing, :for_faction, character: character, standingable: eve_faction_2) }
-
-    specify { expect(described_class.factions).to eq([standing_1, standing_2]) }
-  end
-
-  describe ".corporations" do
-    let!(:eve_corporation_1) { create(:eve_corporation, name: "A") }
-
-    let!(:eve_corporation_2) { create(:eve_corporation, name: "B") }
-
-    let!(:character) { create(:character) }
-
-    let!(:standing_1) { create(:standing, :for_corporation, character: character, standingable: eve_corporation_1) }
-
-    let!(:standing_2) { create(:standing, :for_corporation, character: character, standingable: eve_corporation_2) }
-
-    specify { expect(described_class.corporations).to eq([standing_1, standing_2]) }
-  end
-
-  # describe ".agents" do
-  #   let!(:eve_faction_1) { create(:eve_faction, name_en: "Faction A") }
+  # TODO: later
+  # describe ".factions" do
+  #   let!(:eve_faction_1) { create(:eve_faction, name_en: "A") }
   #
-  #   let!(:eve_faction_2) { create(:eve_faction, name_en: "Faction B") }
-  #
-  #   let!(:eve_corporation_1) { create(:eve_corporation, name: "Corporation A") }
-  #
-  #   let!(:eve_corporation_2) { create(:eve_corporation, name: "Corporation B") }
-  #
-  #   let!(:eve_agent_1) { create(:eve_agent, name: "Agent A") }
-  #
-  #   let!(:eve_agent_2) { create(:eve_agent, name: "Agent B") }
+  #   let!(:eve_faction_2) { create(:eve_faction, name_en: "B") }
   #
   #   let!(:character) { create(:character) }
   #
-  #   let!(:standing_1) { create(:standing, character: character, standingable: eve_faction_1) }
+  #   let!(:standing_1) { create(:standing, :for_faction, character: character, standingable: eve_faction_1) }
   #
-  #   let!(:standing_2) { create(:standing, character: character, standingable: eve_faction_2) }
+  #   let!(:standing_2) { create(:standing, :for_faction, character: character, standingable: eve_faction_2) }
   #
-  #   let!(:standing_3) { create(:standing, character: character, standingable: eve_corporation_1) }
+  #   specify { expect(described_class.factions).to eq([standing_1, standing_2]) }
+  # end
   #
-  #   let!(:standing_4) { create(:standing, character: character, standingable: eve_corporation_2) }
+  # describe ".corporations" do
+  #   let!(:eve_corporation_1) { create(:eve_corporation, name: "A") }
   #
-  #   let!(:standing_5) { create(:standing, character: character, standingable: eve_agent_1) }
+  #   let!(:eve_corporation_2) { create(:eve_corporation, name: "B") }
   #
-  #   let!(:standing_6) { create(:standing, character: character, standingable: eve_agent_2) }
+  #   let!(:character) { create(:character) }
   #
-  #   specify { expect(described_class.agents).to eq([standing_5, standing_6]) }
+  #   let!(:standing_1) { create(:standing, :for_corporation, character: character, standingable: eve_corporation_1) }
+  #
+  #   let!(:standing_2) { create(:standing, :for_corporation, character: character, standingable: eve_corporation_2) }
+  #
+  #   specify { expect(described_class.corporations).to eq([standing_1, standing_2]) }
+  # end
+  #
+  # describe ".agents" do
+  #   let!(:eve_agent_1) { create(:eve_agent, name: "A") }
+  #
+  #   let!(:eve_agent_2) { create(:eve_agent, name: "B") }
+  #
+  #   let!(:character) { create(:character) }
+  #
+  #   let!(:standing_1) { create(:standing, character: character, standingable: eve_agent_1) }
+  #
+  #   let!(:standing_2) { create(:standing, character: character, standingable: eve_agent_2) }
+  #
+  #   specify { expect(described_class.agents).to eq([standing_2, standing_1]) }
   # end
 
   describe "#rounded_standing" do
