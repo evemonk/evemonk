c944b90aa4fdf078ae6026de6d3091414177e382
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-16 20:14:21 +0100

    Update

--- app/queries/character_agents.rb
+++ app/queries/character_agents.rb
@@ -9,6 +9,20 @@ class CharacterAgents
   end
 
   def query
-    character.agents_standings.includes(standingable: :division)
+    return @character_agents if @character_agents
+
+    @character_agents = character.agents_standings
+      .includes(standingable: :division)
+
+    return @character_agents if filter.blank?
+
+    @character_agents =
+      case filter
+      when "-1" # All divisions
+        @character_agents
+      else
+        @character_agents.joins("LEFT JOIN eve_divisions ON eve_divisions.id = eve_agents.division_id")
+                         .where("eve_agents.division_id = :id", id: filter)
+      end
   end
 end
