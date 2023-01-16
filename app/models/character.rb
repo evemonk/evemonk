778042848cf35bf0fd8bda119a0fd99f0b84844a
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-16 21:16:38 +0100

    Update

--- app/models/character.rb
+++ app/models/character.rb
@@ -70,26 +70,26 @@ class Character < ApplicationRecord
 
   has_many :notifications, as: :recipient, dependent: :destroy
 
-  has_many :factions_standings, -> { # rubocop:disable Rails/HasManyOrHasOneDependent
-    joins("LEFT JOIN eve_factions ON standings.standingable_id = eve_factions.id")
-      .where(standingable_type: Eve::Faction.name)
-      .order("eve_factions.name_en": :asc)
-      .includes(:standingable)
-  }, class_name: "Standing", inverse_of: :character
-
-  has_many :corporations_standings, -> { # rubocop:disable Rails/HasManyOrHasOneDependent
-    joins("LEFT JOIN eve_corporations ON standings.standingable_id = eve_corporations.id")
-      .where(standingable_type: Eve::Corporation.name)
-      .order("eve_corporations.name": :asc)
-      .includes(:standingable)
-  }, class_name: "Standing", inverse_of: :character
-
-  has_many :agents_standings, -> { # rubocop:disable Rails/HasManyOrHasOneDependent
-    joins("LEFT JOIN eve_agents ON standings.standingable_id = eve_agents.id")
-      .where(standingable_type: Eve::Agent.name)
-      .order("eve_agents.name": :asc)
-      .includes(:standingable)
-  }, class_name: "Standing", inverse_of: :character
+  # has_many :factions_standings, -> { # rubocop:disable Rails/HasManyOrHasOneDependent
+  #   joins("LEFT JOIN eve_factions ON standings.standingable_id = eve_factions.id")
+  #     .where(standingable_type: Eve::Faction.name)
+  #     .order("eve_factions.name_en": :asc)
+  #     .includes(:standingable)
+  # }, class_name: "Standing", inverse_of: :character
+  #
+  # has_many :corporations_standings, -> { # rubocop:disable Rails/HasManyOrHasOneDependent
+  #   joins("LEFT JOIN eve_corporations ON standings.standingable_id = eve_corporations.id")
+  #     .where(standingable_type: Eve::Corporation.name)
+  #     .order("eve_corporations.name": :asc)
+  #     .includes(:standingable)
+  # }, class_name: "Standing", inverse_of: :character
+  #
+  # has_many :agents_standings, -> { # rubocop:disable Rails/HasManyOrHasOneDependent
+  #   joins("LEFT JOIN eve_agents ON standings.standingable_id = eve_agents.id")
+  #     .where(standingable_type: Eve::Agent.name)
+  #     .order("eve_agents.name": :asc)
+  #     .includes(:standingable)
+  # }, class_name: "Standing", inverse_of: :character
 
   scope :with_valid_tokens, -> { where(esi_token_valid: true) }
 
