88c550a83b04df0f091b4c1364818fa395641b2c
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-16 18:51:01 +0100

    CharacterAgentDivisionsFilterForm

---
+++ app/forms/character_agent_divisions_filter_form.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class CharacterAgentDivisionsFilterForm
+  include ActiveModel::Model
+
+  attr_accessor :filter
+
+  def initialize(filter)
+    @filter = filter
+  end
+
+  def select_data
+    raw_values.map(&:values)
+  end
+
+  def selected
+    filter
+  end
+
+  private
+
+  def raw_values
+    divisions = [{name: "Division: All", id: -1}]
+
+    Eve::Division.find_each do |division|
+      divisions.append({ name: "Division: #{division.name}", id: division.id })
+    end
+
+    divisions
+  end
+end
