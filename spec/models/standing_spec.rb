9d15faa41ea4d4d84c4d5e2926d679b3e3c3cc6f
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-17 01:11:08 +0100

    More tests

--- spec/models/standing_spec.rb
+++ spec/models/standing_spec.rb
@@ -52,43 +52,33 @@ describe Standing do
     specify { expect(described_class.factions.includes(:faction)).to eq([standing_2, standing_1]) }
   end
 
-  # scope :corporations, -> do
-  #   where(standings: { standingable_type: Eve::Corporation.name })
-  #     .order("eve_corporations.name ASC")
-  # end
-  #
-  # scope :agents, -> do
-  #   where(standings: { standingable_type: Eve::Agent.name })
-  #     .order("eve_agents.name ASC")
-  # end
+  describe ".corporations" do
+    let!(:eve_corporation_1) { create(:eve_corporation, name: "B") }
 
-  # describe ".corporations" do
-  #   let!(:eve_corporation_1) { create(:eve_corporation, name: "A") }
-  #
-  #   let!(:eve_corporation_2) { create(:eve_corporation, name: "B") }
-  #
-  #   let!(:character) { create(:character) }
-  #
-  #   let!(:standing_1) { create(:standing, :for_corporation, character: character, standingable: eve_corporation_1) }
-  #
-  #   let!(:standing_2) { create(:standing, :for_corporation, character: character, standingable: eve_corporation_2) }
-  #
-  #   specify { expect(described_class.corporations).to eq([standing_1, standing_2]) }
-  # end
-  #
-  # describe ".agents" do
-  #   let!(:eve_agent_1) { create(:eve_agent, name: "A") }
-  #
-  #   let!(:eve_agent_2) { create(:eve_agent, name: "B") }
-  #
-  #   let!(:character) { create(:character) }
-  #
-  #   let!(:standing_1) { create(:standing, character: character, standingable: eve_agent_1) }
-  #
-  #   let!(:standing_2) { create(:standing, character: character, standingable: eve_agent_2) }
-  #
-  #   specify { expect(described_class.agents).to eq([standing_2, standing_1]) }
-  # end
+    let!(:eve_corporation_2) { create(:eve_corporation, name: "A") }
+
+    let!(:character) { create(:character) }
+
+    let!(:standing_1) { create(:standing, :for_corporation, character: character, standingable: eve_corporation_1) }
+
+    let!(:standing_2) { create(:standing, :for_corporation, character: character, standingable: eve_corporation_2) }
+
+    specify { expect(described_class.corporations.includes(:corporation)).to eq([standing_2, standing_1]) }
+  end
+
+  describe ".agents" do
+    let!(:eve_agent_1) { create(:eve_agent, name: "B") }
+
+    let!(:eve_agent_2) { create(:eve_agent, name: "A") }
+
+    let!(:character) { create(:character) }
+
+    let!(:standing_1) { create(:standing, character: character, standingable: eve_agent_1) }
+
+    let!(:standing_2) { create(:standing, character: character, standingable: eve_agent_2) }
+
+    specify { expect(described_class.agents.includes(:agent)).to eq([standing_2, standing_1]) }
+  end
 
   describe "#rounded_standing" do
     subject { build(:standing, standing: 1.56789) }
