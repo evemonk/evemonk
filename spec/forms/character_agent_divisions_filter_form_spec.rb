431761d4d7cea3e588e92a1c6ab3f4823c49f8a6
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-19 19:00:36 +0100

    More tests

--- spec/forms/character_agent_divisions_filter_form_spec.rb
+++ spec/forms/character_agent_divisions_filter_form_spec.rb
@@ -12,4 +12,21 @@ describe CharacterAgentDivisionsFilterForm do
   describe "#initialize" do
     its(:division_id) { should eq(division_id) }
   end
+
+  describe "#select_data" do
+    let!(:eve_division) { create(:eve_division, name: "Security") }
+
+    specify do
+      expect(subject.select_data).to eq(
+        [
+          ["Division: All", -1],
+          ["Division: Security", eve_division.id]
+        ]
+      )
+    end
+  end
+
+  describe "#selected" do
+    specify { expect(subject.selected).to eq(division_id) }
+  end
 end
