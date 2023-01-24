fdfbb446e8c3d946bc90d0c9e7707a486caa3309
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-24 20:51:17 +0100

    Cleanup

--- app/importers/eve/station_importer.rb
+++ app/importers/eve/station_importer.rb
@@ -12,19 +12,13 @@ module Eve
       import! do
         eve_station = Eve::Station.find_or_initialize_by(station_id: station_id)
 
-        return if esi.not_modified?
-
         eve_station.update!(esi.as_json)
 
         eve_station.position&.destroy
 
         eve_station.create_position!(esi.position.as_json)
-
-        update_etag
       rescue EveOnline::Exceptions::ResourceNotFound
         Rails.logger.info("EveOnline::Exceptions::ResourceNotFound: Eve Station ID #{station_id}")
-
-        etag.destroy!
 
         eve_station.destroy!
       end
