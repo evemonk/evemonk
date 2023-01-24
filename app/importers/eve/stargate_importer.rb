1e195039aa22ed017ca991f451991dd38201e036
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-24 20:47:59 +0100

    Cleanup

--- app/importers/eve/stargate_importer.rb
+++ app/importers/eve/stargate_importer.rb
@@ -12,19 +12,13 @@ module Eve
       import! do
         eve_stargate = Eve::Stargate.find_or_initialize_by(stargate_id: stargate_id)
 
-        return if esi.not_modified?
-
         eve_stargate.update!(esi.as_json)
 
         eve_stargate.position&.destroy
 
         eve_stargate.create_position!(esi.position.as_json)
-
-        update_etag
       rescue EveOnline::Exceptions::ResourceNotFound
         Rails.logger.info("EveOnline::Exceptions::ResourceNotFound: Eve Stargate ID #{stargate_id}")
-
-        etag.destroy!
 
         eve_stargate.destroy!
       end
