334cbc9f4849f2b27f00bc1d3f8e3a369f10da93
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-24 20:58:50 +0100

    Cleanup

--- spec/importers/eve/systems_importer_spec.rb
+++ spec/importers/eve/systems_importer_spec.rb
@@ -8,29 +8,11 @@ describe Eve::SystemsImporter do
   describe "#import" do
     before { expect(subject).to receive(:configure_middlewares) }
 
-    before { expect(subject).to receive(:configure_etag) }
+    before { expect(subject).to receive(:import_new_systems) }
 
-    context "when etag cache hit" do
-      let(:esi) { instance_double(EveOnline::ESI::UniverseSystems, not_modified?: true) }
+    before { expect(subject).to receive(:remove_old_systems) }
 
-      before { expect(EveOnline::ESI::UniverseSystems).to receive(:new).and_return(esi) }
-
-      specify { expect { subject.import }.not_to raise_error }
-    end
-
-    context "when etag cache miss" do
-      let(:esi) { instance_double(EveOnline::ESI::UniverseSystems, not_modified?: false) }
-
-      before { expect(EveOnline::ESI::UniverseSystems).to receive(:new).and_return(esi) }
-
-      before { expect(subject).to receive(:import_new_systems) }
-
-      before { expect(subject).to receive(:remove_old_systems) }
-
-      before { expect(subject).to receive(:update_etag) }
-
-      specify { expect { subject.import }.not_to raise_error }
-    end
+    specify { expect { subject.import }.not_to raise_error }
   end
 
   describe "#esi" do
