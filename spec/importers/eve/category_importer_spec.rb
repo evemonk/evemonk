c33399fc7ff12a25d851f7c20715d060b4f25d71
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-24 21:15:36 +0100

    Cleanup

--- spec/importers/eve/category_importer_spec.rb
+++ spec/importers/eve/category_importer_spec.rb
@@ -30,59 +30,39 @@ describe Eve::CategoryImporter do
   describe "#import" do
     before { expect(subject).to receive(:configure_middlewares) }
 
-    before { expect(subject).to receive(:configure_etag) }
-
     let(:eve_category) { instance_double(Eve::Category) }
 
     before { expect(Eve::Category).to receive(:find_or_initialize_by).with(category_id: category_id).and_return(eve_category) }
 
-    context "when etag cache hit" do
-      let(:esi) { instance_double(EveOnline::ESI::UniverseCategory, not_modified?: true) }
+    context "when eve category found" do
+      let(:json) { double }
+
+      let(:esi) { instance_double(EveOnline::ESI::UniverseCategory, as_json: json) }
 
       before { expect(subject).to receive(:esi).and_return(esi) }
+
+      before { expect(eve_category).to receive(:update!).with(json) }
 
       specify { expect { subject.import }.not_to raise_error }
     end
 
-    context "when etag cache miss" do
-      context "when eve category found" do
-        let(:json) { double }
+    context "when eve category not found" do
+      before { expect(subject).to receive(:esi).and_raise(EveOnline::Exceptions::ResourceNotFound) }
 
-        let(:esi) { instance_double(EveOnline::ESI::UniverseCategory, not_modified?: false, as_json: json) }
+      before do
+        #
+        # Rails.logger.info("EveOnline::Exceptions::ResourceNotFound: Eve Category ID #{category_id}")
 
-        before { expect(subject).to receive(:esi).and_return(esi).twice }
-
-        before { expect(eve_category).to receive(:update!).with(json) }
-
-        before { expect(subject).to receive(:update_etag) }
-
-        specify { expect { subject.import }.not_to raise_error }
-      end
-
-      context "when eve category not found" do
-        before { expect(subject).to receive(:esi).and_raise(EveOnline::Exceptions::ResourceNotFound) }
-
-        let(:eve_etag) { instance_double(Eve::Etag) }
-
-        before { expect(subject).to receive(:etag).and_return(eve_etag) }
-
-        before do
-          #
-          # Rails.logger.info("EveOnline::Exceptions::ResourceNotFound: Eve Category ID #{category_id}")
-
-          expect(Rails).to receive(:logger) do
-            double.tap do |a|
-              expect(a).to receive(:info).with("EveOnline::Exceptions::ResourceNotFound: Eve Category ID #{category_id}")
-            end
+        expect(Rails).to receive(:logger) do
+          double.tap do |a|
+            expect(a).to receive(:info).with("EveOnline::Exceptions::ResourceNotFound: Eve Category ID #{category_id}")
           end
         end
-
-        before { expect(eve_etag).to receive(:destroy!) }
-
-        before { expect(eve_category).to receive(:destroy!) }
-
-        specify { expect { subject.import }.not_to raise_error }
       end
+
+      before { expect(eve_category).to receive(:destroy!) }
+
+      specify { expect { subject.import }.not_to raise_error }
     end
   end
 
