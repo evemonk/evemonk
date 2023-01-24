7856a801f30eeaecab3efad14dbfce6805069e6c
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-25 00:31:12 +0100

    Cleanup

--- app/importers/eve/alliance_importer.rb
+++ app/importers/eve/alliance_importer.rb
@@ -12,15 +12,9 @@ module Eve
       import! do
         eve_alliance = Eve::Alliance.find_or_initialize_by(id: id)
 
-        return if esi.not_modified?
-
         eve_alliance.update!(esi.as_json)
-
-        update_etag
       rescue EveOnline::Exceptions::ResourceNotFound
         Rails.logger.info("EveOnline::Exceptions::ResourceNotFound: Eve Alliance ID #{id}")
-
-        etag.destroy!
 
         eve_alliance.destroy!
       end
