de6082e4f1870fba3935e73c9d1f97a637cc4ce2
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-25 14:52:06 +0100

    Cleanup

--- spec/importers/eve/corporation_alliance_history_importer_spec.rb
+++ spec/importers/eve/corporation_alliance_history_importer_spec.rb
@@ -12,83 +12,61 @@ describe Eve::CorporationAllianceHistoryImporter do
   describe "#import" do
     before { expect(subject).to receive(:configure_middlewares) }
 
-    before { expect(subject).to receive(:configure_etag) }
+    context "when eve corporation found" do
+      let(:record_id) { double }
 
-    context "when etag cache hit" do
-      let(:esi) { instance_double(EveOnline::ESI::CorporationAllianceHistory, not_modified?: true) }
+      let(:json) { double }
+
+      let(:entry) { instance_double(EveOnline::ESI::Models::CorporationAllianceHistory, record_id: record_id, as_json: json) }
+
+      let(:entries) { [entry] }
+
+      let(:esi) do
+        instance_double(EveOnline::ESI::CorporationAllianceHistory,
+          entries: entries)
+      end
 
       before { expect(subject).to receive(:esi).and_return(esi) }
+
+      let(:eve_corporation) { instance_double(Eve::Corporation) }
+
+      before { expect(Eve::Corporation).to receive(:find_by!).with(corporation_id: corporation_id).and_return(eve_corporation) }
+
+      let(:corporation_alliance_history) { instance_double(Eve::CorporationAllianceHistory) }
+
+      before do
+        #
+        # eve_corporation.corporation_alliance_histories
+        #                .find_or_initialize_by(record_id: entry.record_id) # => corporation_alliance_history
+        #
+        expect(eve_corporation).to receive(:corporation_alliance_histories) do
+          double.tap do |a|
+            expect(a).to receive(:find_or_initialize_by).with(record_id: record_id)
+              .and_return(corporation_alliance_history)
+          end
+        end
+      end
+
+      before { expect(corporation_alliance_history).to receive(:update!).with(json) }
 
       specify { expect { subject.import }.not_to raise_error }
     end
 
-    context "when etag cache miss" do
-      context "when eve corporation found" do
-        let(:record_id) { double }
+    context "when eve corporation not found (ActiveRecord::RecordNotFound)" do
+      before { expect(Eve::Corporation).to receive(:find_by!).with(corporation_id: corporation_id).and_raise(ActiveRecord::RecordNotFound) }
 
-        let(:json) { double }
-
-        let(:entry) { instance_double(EveOnline::ESI::Models::CorporationAllianceHistory, record_id: record_id, as_json: json) }
-
-        let(:entries) { [entry] }
-
-        let(:esi) do
-          instance_double(EveOnline::ESI::CorporationAllianceHistory,
-            not_modified?: false,
-            entries: entries)
-        end
-
-        before { expect(subject).to receive(:esi).and_return(esi).twice }
-
-        let(:eve_corporation) { instance_double(Eve::Corporation) }
-
-        before { expect(Eve::Corporation).to receive(:find_by!).with(corporation_id: corporation_id).and_return(eve_corporation) }
-
-        let(:corporation_alliance_history) { instance_double(Eve::CorporationAllianceHistory) }
-
-        before do
-          #
-          # eve_corporation.corporation_alliance_histories
-          #                .find_or_initialize_by(record_id: entry.record_id) # => corporation_alliance_history
-          #
-          expect(eve_corporation).to receive(:corporation_alliance_histories) do
-            double.tap do |a|
-              expect(a).to receive(:find_or_initialize_by).with(record_id: record_id)
-                .and_return(corporation_alliance_history)
-            end
+      before do
+        #
+        # Rails.logger.info("Corporation with ID #{corporation_id} not found")
+        #
+        expect(Rails).to receive(:logger) do
+          double.tap do |a|
+            expect(a).to receive(:info).with("Corporation with ID #{corporation_id} not found")
           end
         end
-
-        before { expect(corporation_alliance_history).to receive(:update!).with(json) }
-
-        before { expect(subject).to receive(:update_etag) }
-
-        specify { expect { subject.import }.not_to raise_error }
       end
 
-      context "when eve corporation not found (ActiveRecord::RecordNotFound)" do
-        let(:esi) do
-          instance_double(EveOnline::ESI::CorporationAllianceHistory,
-            not_modified?: false)
-        end
-
-        before { expect(subject).to receive(:esi).and_return(esi) }
-
-        before { expect(Eve::Corporation).to receive(:find_by!).with(corporation_id: corporation_id).and_raise(ActiveRecord::RecordNotFound) }
-
-        before do
-          #
-          # Rails.logger.info("Corporation with ID #{corporation_id} not found")
-          #
-          expect(Rails).to receive(:logger) do
-            double.tap do |a|
-              expect(a).to receive(:info).with("Corporation with ID #{corporation_id} not found")
-            end
-          end
-        end
-
-        specify { expect { subject.import }.not_to raise_error }
-      end
+      specify { expect { subject.import }.not_to raise_error }
     end
   end
 
