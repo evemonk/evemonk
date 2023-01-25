3060852a5309dd2240621b3d873535ea0233b5d8
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-25 14:03:32 +0100

    Cleanup

--- spec/importers/eve/group_importer_spec.rb
+++ spec/importers/eve/group_importer_spec.rb
@@ -30,59 +30,39 @@ describe Eve::GroupImporter do
   describe "#import" do
     before { expect(subject).to receive(:configure_middlewares) }
 
-    before { expect(subject).to receive(:configure_etag) }
-
     let(:eve_group) { instance_double(Eve::Group) }
 
     before { expect(Eve::Group).to receive(:find_or_initialize_by).with(group_id: group_id).and_return(eve_group) }
 
-    context "when etag cache hit" do
-      let(:esi) { instance_double(EveOnline::ESI::UniverseGroup, not_modified?: true) }
+    context "when eve group found" do
+      let(:json) { double }
+
+      let(:esi) { instance_double(EveOnline::ESI::UniverseGroup, as_json: json) }
 
       before { expect(subject).to receive(:esi).and_return(esi) }
+
+      before { expect(eve_group).to receive(:update!).with(json) }
 
       specify { expect { subject.import }.not_to raise_error }
     end
 
-    context "when etag cache miss" do
-      context "when eve group found" do
-        let(:json) { double }
+    context "when eve group not found" do
+      before { expect(subject).to receive(:esi).and_raise(EveOnline::Exceptions::ResourceNotFound) }
 
-        let(:esi) { instance_double(EveOnline::ESI::UniverseGroup, not_modified?: false, as_json: json) }
-
-        before { expect(subject).to receive(:esi).and_return(esi).twice }
-
-        before { expect(eve_group).to receive(:update!).with(json) }
-
-        before { expect(subject).to receive(:update_etag) }
-
-        specify { expect { subject.import }.not_to raise_error }
-      end
-
-      context "when eve group not found" do
-        before { expect(subject).to receive(:esi).and_raise(EveOnline::Exceptions::ResourceNotFound) }
-
-        let(:eve_etag) { instance_double(Eve::Etag) }
-
-        before { expect(subject).to receive(:etag).and_return(eve_etag) }
-
-        before do
-          #
-          # Rails.logger.info("EveOnline::Exceptions::ResourceNotFound: Eve Group ID #{group_id}")
-          #
-          expect(Rails).to receive(:logger) do
-            double.tap do |a|
-              expect(a).to receive(:info).with("EveOnline::Exceptions::ResourceNotFound: Eve Group ID #{group_id}")
-            end
+      before do
+        #
+        # Rails.logger.info("EveOnline::Exceptions::ResourceNotFound: Eve Group ID #{group_id}")
+        #
+        expect(Rails).to receive(:logger) do
+          double.tap do |a|
+            expect(a).to receive(:info).with("EveOnline::Exceptions::ResourceNotFound: Eve Group ID #{group_id}")
           end
         end
-
-        before { expect(eve_etag).to receive(:destroy!) }
-
-        before { expect(eve_group).to receive(:destroy!) }
-
-        specify { expect { subject.import }.not_to raise_error }
       end
+
+      before { expect(eve_group).to receive(:destroy!) }
+
+      specify { expect { subject.import }.not_to raise_error }
     end
   end
 
