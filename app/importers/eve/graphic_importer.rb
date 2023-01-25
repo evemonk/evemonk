00b8e45f9ef3bde00afee7742d5a9babc9a45162
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-25 14:15:16 +0100

    Cleanup

--- app/importers/eve/graphic_importer.rb
+++ app/importers/eve/graphic_importer.rb
@@ -12,15 +12,9 @@ module Eve
       import! do
         eve_graphic = Eve::Graphic.find_or_initialize_by(graphic_id: graphic_id)
 
-        return if esi.not_modified?
-
         eve_graphic.update!(esi.as_json)
-
-        update_etag
       rescue EveOnline::Exceptions::ResourceNotFound
         Rails.logger.info("EveOnline::Exceptions::ResourceNotFound: Eve Graphic ID #{graphic_id}")
-
-        etag.destroy!
 
         eve_graphic.destroy!
       end
--- spec/importers/eve/graphic_importer_spec.rb
+++ spec/importers/eve/graphic_importer_spec.rb
@@ -12,59 +12,39 @@ describe Eve::GraphicImporter do
   describe "#import" do
     before { expect(subject).to receive(:configure_middlewares) }
 
-    before { expect(subject).to receive(:configure_etag) }
-
     let(:eve_graphic) { instance_double(Eve::Graphic) }
 
     before { expect(Eve::Graphic).to receive(:find_or_initialize_by).with(graphic_id: graphic_id).and_return(eve_graphic) }
 
-    context "when etag cache hit" do
-      let(:esi) { instance_double(EveOnline::ESI::UniverseGraphic, not_modified?: true) }
+    context "when eve graphic found" do
+      let(:json) { double }
+
+      let(:esi) { instance_double(EveOnline::ESI::UniverseGraphic, as_json: json) }
 
       before { expect(subject).to receive(:esi).and_return(esi) }
+
+      before { expect(eve_graphic).to receive(:update!).with(json) }
 
       specify { expect { subject.import }.not_to raise_error }
     end
 
-    context "when etag cache miss" do
-      context "when eve graphic found" do
-        let(:json) { double }
+    context "when eve graphic not found" do
+      before { expect(subject).to receive(:esi).and_raise(EveOnline::Exceptions::ResourceNotFound) }
 
-        let(:esi) { instance_double(EveOnline::ESI::UniverseGraphic, not_modified?: false, as_json: json) }
-
-        before { expect(subject).to receive(:esi).and_return(esi).twice }
-
-        before { expect(eve_graphic).to receive(:update!).with(json) }
-
-        before { expect(subject).to receive(:update_etag) }
-
-        specify { expect { subject.import }.not_to raise_error }
-      end
-
-      context "when eve graphic not found" do
-        before { expect(subject).to receive(:esi).and_raise(EveOnline::Exceptions::ResourceNotFound) }
-
-        let(:eve_etag) { instance_double(Eve::Etag) }
-
-        before { expect(subject).to receive(:etag).and_return(eve_etag) }
-
-        before do
-          #
-          # Rails.logger.info("EveOnline::Exceptions::ResourceNotFound: Eve Graphic ID #{graphic_id}")
-          #
-          expect(Rails).to receive(:logger) do
-            double.tap do |a|
-              expect(a).to receive(:info).with("EveOnline::Exceptions::ResourceNotFound: Eve Graphic ID #{graphic_id}")
-            end
+      before do
+        #
+        # Rails.logger.info("EveOnline::Exceptions::ResourceNotFound: Eve Graphic ID #{graphic_id}")
+        #
+        expect(Rails).to receive(:logger) do
+          double.tap do |a|
+            expect(a).to receive(:info).with("EveOnline::Exceptions::ResourceNotFound: Eve Graphic ID #{graphic_id}")
           end
         end
-
-        before { expect(eve_etag).to receive(:destroy!) }
-
-        before { expect(eve_graphic).to receive(:destroy!) }
-
-        specify { expect { subject.import }.not_to raise_error }
       end
+
+      before { expect(eve_graphic).to receive(:destroy!) }
+
+      specify { expect { subject.import }.not_to raise_error }
     end
   end
 
