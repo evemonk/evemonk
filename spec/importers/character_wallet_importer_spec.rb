dd24d7a7f597f60af4ca068798764a13f669fda7
Igor Zubkov <igor.zubkov@gmail.com>
2023-01-26 14:21:30 +0100

    Cleanup

--- spec/importers/character_wallet_importer_spec.rb
+++ spec/importers/character_wallet_importer_spec.rb
@@ -44,7 +44,7 @@ describe CharacterWalletImporter do
 
       before { expect(subject).to receive(:character).and_return(character) }
 
-      before { expect(EveOnline::ESI::CharacterWallet).to receive(:new).with({character_id: character_id}).and_return(esi) }
+      before { expect(EveOnline::ESI::CharacterWallet).to receive(:new).with(character_id: character_id).and_return(esi) }
 
       specify { expect { subject.esi }.to change { subject.instance_variable_get(:@esi) }.from(nil).to(esi) }
     end
