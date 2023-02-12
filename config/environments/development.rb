f947106de2f772a62301a0a94e2703496f3afa0e
Igor Zubkov <igor.zubkov@gmail.com>
2023-02-12 11:14:57 +0100

    Configure prosopite gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,9 @@ Rails.application.configure do
 
   # Devise
   config.action_mailer.default_url_options = {host: "localhost", port: 3000}
+
+  # Prosopite
+  config.after_initialize do
+    Prosopite.rails_logger = true
+  end
 end
