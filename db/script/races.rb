55247766fdae6f642dd024f62e957783b4791b9e
Igor Zubkov <igor.zubkov@gmail.com>
2023-02-15 12:23:36 +0100

    Add migrate script

--- db/script/races.rb
+++ db/script/races.rb
@@ -2,8 +2,17 @@
 
 Rails.logger.info "Migrate Eve::Race#race_id to #id"
 
-Eve::Race.find_each do |race|
-  race.update!(id: race.race_id)
+ActiveRecord::Base.transaction do
+  start = 100
+
+  Eve::Race.find_each do |race|
+    race.update_column(:id, start)
+    start += 1
+  end
+
+  Eve::Race.find_each do |race|
+    race.update_column(:id, race.race_id)
+  end
 end
 
 Rails.logger.info "Done"
