2a9e66b3b004e141224cd43fe46fb67f1f60c347
Igor Zubkov <igor.zubkov@gmail.com>
2023-02-16 14:57:43 +0100

    Update migration script

--- db/script/groups.rb
+++ db/script/groups.rb
@@ -4,4 +4,13 @@ Rails.logger.info "Migrate Eve::Group#group_id to #id"
 
 ActiveRecord::Base.transaction do
   start = 1_000
+
+  Eve::Group.find_each do |group|
+    group.update_column(:id, start)
+    start += 1
+  end
+
+  Eve::Group.find_each do |group|
+    group.update_column(:id, group.group_id)
+  end
 end
