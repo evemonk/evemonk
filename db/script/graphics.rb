2c53cce372a4b403a679ce146b672dc5298c14ba
Igor Zubkov <igor.zubkov@gmail.com>
2023-02-18 22:22:32 +0100

    Update

--- db/script/graphics.rb
+++ db/script/graphics.rb
@@ -3,6 +3,13 @@
 Rails.logger.info "Migrate Eve::Graphic#graphic_id to #id"
 
 ActiveRecord::Base.transaction do
+  start = 1_000_000
+
+  Eve::Graphic.find_each do |graphic|
+    graphic.update_column(:id, start)
+    start += 1
+  end
+
   Eve::Graphic.find_each do |graphic|
     graphic.update_column(:id, graphic.graphic_id)
   end
--- db/script/icons.rb
+++ db/script/icons.rb
@@ -3,6 +3,13 @@
 Rails.logger.info "Migrate Eve::Icon#icon_id to #id"
 
 ActiveRecord::Base.transaction do
+  start = 1_000_000
+
+  Eve::Icon.find_each do |icon|
+    icon.update_column(:id, start)
+    start += 1
+  end
+
   Eve::Icon.find_each do |icon|
     icon.update_column(:id, icon.icon_id)
   end
