39f40e2123e263f94120ad2dea62075113a6fbcd
Igor Zubkov <igor.zubkov@gmail.com>
2023-02-18 22:24:02 +0100

    Update

--- app/importers/eve/local_graphics_importer.rb
+++ app/importers/eve/local_graphics_importer.rb
@@ -3,8 +3,8 @@
 module Eve
   class LocalGraphicsImporter
     def import
-      Eve::Graphic.pluck(:graphic_id).each do |graphic_id|
-        Eve::UpdateGraphicJob.perform_later(graphic_id)
+      Eve::Graphic.ids.each do |id|
+        Eve::UpdateGraphicJob.perform_later(id)
       end
     end
   end
