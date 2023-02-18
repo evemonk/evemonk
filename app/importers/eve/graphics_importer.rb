2a8c3196aec9e30ea76140f7e6c91391247d973e
Igor Zubkov <igor.zubkov@gmail.com>
2023-02-18 22:34:19 +0100

    Update

--- app/importers/eve/graphics_importer.rb
+++ app/importers/eve/graphics_importer.rb
@@ -18,19 +18,19 @@ module Eve
 
     def import_new_graphics
       esi.graphic_ids.each do |graphic_id|
-        unless Eve::Graphic.exists?(graphic_id: graphic_id)
+        unless Eve::Graphic.exists?(id: graphic_id)
           Eve::UpdateGraphicJob.perform_later(graphic_id)
         end
       end
     end
 
     def remove_old_graphics
-      eve_graphic_ids = Eve::Graphic.pluck(:graphic_id)
+      eve_graphic_ids = Eve::Graphic.ids
 
       graphic_ids_to_remove = eve_graphic_ids - esi.graphic_ids
 
       graphic_ids_to_remove.each do |graphic_id|
-        eve_graphic = Eve::Graphic.find_or_initialize_by(graphic_id: graphic_id)
+        eve_graphic = Eve::Graphic.find_or_initialize_by(id: graphic_id)
 
         eve_graphic.destroy!
       end
