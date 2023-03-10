0114c92af1bfea669c8ff82fa413897a3ca21da0
Igor Zubkov <igor.zubkov@gmail.com>
2023-03-10 15:32:21 +0100

    Turn off yabeda

--- app/services/statistics_middleware.rb
+++ app/services/statistics_middleware.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class StatisticsMiddleware < Faraday::Middleware
-  def on_complete(env)
-    method = "esi_requests_#{env[:status]}_counter"
-
-    Yabeda.evemonk.public_send(method).increment({total: "success"}, by: 1)
-  end
+  # def on_complete(env)
+  #   method = "esi_requests_#{env[:status]}_counter"
+  #
+  #   Yabeda.evemonk.public_send(method).increment({total: "success"}, by: 1)
+  # end
 end
--- spec/services/statistics_middleware_spec.rb
+++ spec/services/statistics_middleware_spec.rb
@@ -3,24 +3,24 @@
 require "rails_helper"
 
 describe StatisticsMiddleware do
-  [200, 201, 204, 304, 400, 401, 403, 404, 420, 500, 502, 503, 504].each do |status|
-    describe "when request is #{status}" do
-      before { stub_request(:get, "https://esi.evetech.net/v2/status/").to_return(status: status, body: {}.to_json) }
-
-      let(:esi) { EveOnline::ESI::ServerStatus.new }
-
-      before do
-        conn = Faraday.new { |f|
-          f.use StatisticsMiddleware
-          f.adapter Faraday.default_adapter
-        }
-
-        conn.get("https://esi.evetech.net/v2/status/")
-      end
-
-      let(:method) { "esi_requests_#{status}_counter" }
-
-      specify { expect(Yabeda.evemonk.public_send(method).values).to eq({total: "success"} => 1) }
-    end
-  end
+  # [200, 201, 204, 304, 400, 401, 403, 404, 420, 500, 502, 503, 504].each do |status|
+  #   describe "when request is #{status}" do
+  #     before { stub_request(:get, "https://esi.evetech.net/v2/status/").to_return(status: status, body: {}.to_json) }
+  #
+  #     let(:esi) { EveOnline::ESI::ServerStatus.new }
+  #
+  #     before do
+  #       conn = Faraday.new { |f|
+  #         f.use StatisticsMiddleware
+  #         f.adapter Faraday.default_adapter
+  #       }
+  #
+  #       conn.get("https://esi.evetech.net/v2/status/")
+  #     end
+  #
+  #     let(:method) { "esi_requests_#{status}_counter" }
+  #
+  #     specify { expect(Yabeda.evemonk.public_send(method).values).to eq({total: "success"} => 1) }
+  #   end
+  # end
 end
