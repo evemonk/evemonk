07de392c88216fd2d2cae5592b2b198640ec7654
Igor Zubkov <igor.zubkov@gmail.com>
2023-03-10 15:54:48 +0100

    Cleanup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,9 +70,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  # Sidekiq
-  config.active_job.queue_adapter = :sidekiq
-
   # Devise
   config.action_mailer.default_url_options = {host: "localhost", port: 3000}
 
