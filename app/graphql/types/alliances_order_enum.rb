4481150533c92dd1bd78c629fd13c4650c419281
Igor Zubkov <igor.zubkov@gmail.com>
2023-03-22 17:00:47 +0100

    Update AlliancesOrderEnum descriptions

--- app/graphql/types/alliances_order_enum.rb
+++ app/graphql/types/alliances_order_enum.rb
@@ -4,10 +4,10 @@ module Types
   class AlliancesOrderEnum < BaseEnum
     description "Alliances order by enum"
 
-    value "CORPORATIONS_COUNT_DESC", "corporations_count by descending order"
-    value "CORPORATIONS_COUNT_ASC", "corporations_count by ascending order"
-    value "CHARACTERS_COUNT_DESC", "characters_count by ascending order"
-    value "CHARACTERS_COUNT_ASC", "characters_count by ascending order"
-    value "NONE", "None: order by default (by ID)"
+    value "CORPORATIONS_COUNT_DESC", "Order by corporations_count desc"
+    value "CORPORATIONS_COUNT_ASC", "Order by corporations_count asc"
+    value "CHARACTERS_COUNT_DESC", "Order by characters_count desc"
+    value "CHARACTERS_COUNT_ASC", "Order by characters_count asc"
+    value "NONE", "Order by default (by ID)"
   end
 end
