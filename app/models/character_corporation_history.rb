5c83213eff2871300327a21b3ecb5a8b56a6668b
Igor Zubkov <igor.zubkov@gmail.com>
2023-03-22 13:11:06 +0100

    Cleanup

--- app/models/character_corporation_history.rb
+++ app/models/character_corporation_history.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class CharacterCorporationHistory < ApplicationRecord
-  # TODO: has_paper_trail
-
   belongs_to :character, primary_key: "character_id"
 
   belongs_to :corporation, class_name: "Eve::Corporation", optional: true
--- app/models/character_order.rb
+++ app/models/character_order.rb
@@ -3,8 +3,6 @@
 class CharacterOrder < ApplicationRecord
   include Locationable
 
-  has_paper_trail
-
   belongs_to :character
 
   belongs_to :type, class_name: "Eve::Type", optional: true
