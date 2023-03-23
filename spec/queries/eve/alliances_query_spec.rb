21a18f51db7751be2e457e71105d494faf4453c7
Igor Zubkov <igor.zubkov@gmail.com>
2023-03-23 22:51:37 +0100

    More tests

--- spec/queries/eve/alliances_query_spec.rb
+++ spec/queries/eve/alliances_query_spec.rb
@@ -8,4 +8,28 @@ describe Eve::AlliancesQuery do
   subject { described_class.new(order_by) }
 
   it { should be_a(BaseQuery) }
+
+  describe "#initialize" do
+    context "without order_by and scope" do
+      let(:scope) { double }
+
+      before { expect(Eve::Alliance).to receive(:all).and_return(scope) }
+
+      subject { described_class.new }
+
+      its(:order_by) { should eq(nil) }
+
+      its(:scope) { should eq(scope) }
+    end
+
+    context "with order_by and scope" do
+      let(:scope) { double }
+
+      subject { described_class.new(order_by, scope) }
+
+      its(:order_by) { should eq(order_by) }
+
+      its(:scope) { should eq(scope) }
+    end
+  end
 end
