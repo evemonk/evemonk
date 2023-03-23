ec7000046762f99ff976bb9e83f8e92e1eb58d6c
Igor Zubkov <igor.zubkov@gmail.com>
2023-03-23 21:34:02 +0100

    Cleanup

--- app/models/character.rb
+++ app/models/character.rb
@@ -68,33 +68,15 @@ class Character < ApplicationRecord
 
   scope :with_valid_tokens, -> { where(esi_token_valid: true) }
 
-  delegate :perception_without_bonuses, to: :character_attributes
+  delegate :perception_without_bonuses, :perception_bonus,
+    :memory_without_bonuses, :memory_bonus,
+    :willpower_without_bonuses, :willpower_bonus,
+    :intelligence_without_bonuses, :intelligence_bonus,
+    :charisma_without_bonuses, :charisma_bonus, to: :character_attributes
 
-  delegate :perception_bonus, to: :character_attributes
+  delegate :science_skill, :science_level, to: :character_skills_levels
 
-  delegate :memory_without_bonuses, to: :character_attributes
-
-  delegate :memory_bonus, to: :character_attributes
-
-  delegate :willpower_without_bonuses, to: :character_attributes
-
-  delegate :willpower_bonus, to: :character_attributes
-
-  delegate :intelligence_without_bonuses, to: :character_attributes
-
-  delegate :intelligence_bonus, to: :character_attributes
-
-  delegate :charisma_without_bonuses, to: :character_attributes
-
-  delegate :charisma_bonus, to: :character_attributes
-
-  delegate :science_skill, to: :character_skills_levels
-
-  delegate :science_level, to: :character_skills_levels
-
-  delegate :advanced_industry_skill, to: :character_skills_levels
-
-  delegate :advanced_industry_level, to: :character_skills_levels
+  delegate :advanced_industry_skill, :advanced_industry_level, to: :character_skills_levels
 
   def perception_attribute
     @perception_attribute ||= Eve::CharacterAttribute.find_by(name_en: "Perception")
