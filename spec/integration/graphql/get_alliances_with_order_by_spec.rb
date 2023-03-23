7fb3d9daaac580a850b6c4698dc04c5850591086
Igor Zubkov <igor.zubkov@gmail.com>
2023-03-23 23:08:21 +0100

    More tests

---
+++ spec/integration/graphql/get_alliances_with_order_by_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Get Alliances with orderBy" do
+  context "when orderBy is CORPORATIONS_COUNT_DESC" do
+  end
+
+  context "when orderBy is CORPORATIONS_COUNT_ASC" do
+  end
+
+  context "when orderBy is CHARACTERS_COUNT_DESC" do
+  end
+
+  context "when orderBy is CHARACTERS_COUNT_ASC" do
+  end
+
+  context "when orderBy is NONE" do
+  end
+end
