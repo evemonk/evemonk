a084fd2f5fa64c8b6b6c1125b5ba89014dedecb1
Igor Zubkov <igor.zubkov@gmail.com>
2023-03-30 21:09:21 +0200

    Remove authy fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_22_120536) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_30_185853) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_buffercache"
   enable_extension "pg_trgm"
@@ -1107,9 +1107,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_22_120536) do
     t.datetime "confirmed_at", precision: nil
     t.datetime "confirmation_sent_at", precision: nil
     t.string "unconfirmed_email"
-    t.string "authy_id"
-    t.datetime "last_sign_in_with_authy", precision: nil
-    t.boolean "authy_enabled", default: false
     t.integer "locale", default: 0
     t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
