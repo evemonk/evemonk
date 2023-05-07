c0d98d17192cd53a3e52d6891c4e1ddef051c045
Igor Zubkov <igor.zubkov@gmail.com>
2023-05-07 23:07:26 +0200

    TopAlliancesComponent

---
+++ spec/components/previews/top_alliances_component_preview.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class TopAlliancesComponentPreview < Lookbook::Preview
+  def loading
+    render TopAlliancesComponent.new(alliances: nil)
+  end
+
+  def loaded
+    render TopAlliancesComponent.new(
+      alliances: FactoryBot.build_list(
+        :eve_alliance,
+        20,
+        id: 99_005_338,
+        name: "Pandemic Horde",
+        corporations_count: 12_345,
+        characters_count: 12_345
+      )
+    )
+  end
+end
---
+++ spec/components/top_alliances_component_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe TopAlliancesComponent, type: :component do
+  it { should be_an(ApplicationComponent) }
+end
