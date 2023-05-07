860d2881f2316b549e66744124a85a2ed6018290
Igor Zubkov <igor.zubkov@gmail.com>
2023-05-07 20:23:54 +0200

    Update

--- config/initializers/rails_performance.rb
+++ config/initializers/rails_performance.rb
@@ -1,37 +1,10 @@
 # frozen_string_literal: true
 
 RailsPerformance.setup do |config|
-  config.redis = Redis::Namespace.new("#{Rails.env}-rails-performance", redis: Redis.new)
-  config.duration = 4.hours
-
-  config.debug = false # currently not used>
+  config.redis = Redis.new(url: ENV.fetch("REDIS_PERFORMANCE_URL", "redis://localhost:6381/1"))
+  config.duration = 24.hours
   config.enabled = true
-
-  # default path where to mount gem
-  config.mount_at = '/rails/performance'
-
-  # protect your Performance Dashboard with HTTP BASIC password
-  config.http_basic_authentication_enabled = false
-  config.http_basic_authentication_user_name = 'rails_performance'
-  config.http_basic_authentication_password = 'password12'
-
-  # if you need an additional rules to check user permissions
-  config.verify_access_proc = proc { |controller| true }
-  # for example when you have `current_user`
-  # config.verify_access_proc = proc { |controller| controller.current_user && controller.current_user.admin? }
-
-  # You can ignore endpoints with Rails standard notation controller#action
-  # config.ignored_endpoints = ['HomeController#contact']
-
-  # store custom data for the request
-  # config.custom_data_proc = proc do |env|
-  #   request = Rack::Request.new(env)
-  #   {
-  #     email: request.env['warden'].user&.email, # if you are using Devise for example
-  #     user_agent: request.env['HTTP_USER_AGENT']
-  #   }
-  # end
-
-  # config home button link
-  config.home_link = '/'
-end if defined?(RailsPerformance)
+  config.mount_at = "/rails/performance"
+  config.verify_access_proc = proc { |controller| controller.current_user && controller.current_user.admin? }
+  config.home_link = "/"
+end
