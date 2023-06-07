15ab9b26805e35f7d932440b4d228571533877f8
Igor Zubkov <igor.zubkov@gmail.com>
2023-06-07 19:06:16 +0200

    Migrate Eve::Constellation#constellation_id -> #id via data migrate

---
+++ db/data/20230607170433_eve_constellation_constellation_id.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class EveConstellationConstellationId < ActiveRecord::Migration[7.0]
+  def up
+    Eve::Constellation.find_each do |constellation|
+      constellation.update_column(:id, constellation.constellation_id)
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
