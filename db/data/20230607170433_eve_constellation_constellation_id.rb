a0a4caf89cf092986d4d6ceb0434e3a02df07d3c
Igor Zubkov <igor.zubkov@gmail.com>
2023-06-07 20:50:52 +0200

    Update

--- db/data/20230607170433_eve_constellation_constellation_id.rb
+++ db/data/20230607170433_eve_constellation_constellation_id.rb
@@ -2,9 +2,14 @@
 
 class EveConstellationConstellationId < ActiveRecord::Migration[7.0]
   def up
-    # Eve::Constellation.find_each do |constellation|
-    #   constellation.update_column(:id, constellation.constellation_id)
-    # end
+    Eve::Constellation.find_each do |constellation|
+      position = Eve::Position
+        .where(positionable_type: "Eve::Constellation", positionable_id: constellation.id)
+        .first
+
+      constellation.update_column(:id, constellation.constellation_id)
+      position&.update_column(:positionable_id, constellation.constellation_id)
+    end
   end
 
   def down
