8430d789beb1f5bd64ab7f53ba3077209cb4280f
Igor Zubkov <igor.zubkov@gmail.com>
2023-06-30 10:28:02 +0200

    Use minified versions of stimulus js and turbo js

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,10 +3,10 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/stimulus", to: "stimulus.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.js", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 
 pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.8/src/index.js"
 
