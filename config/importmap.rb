665bc055acdcffe3f10f19758f6c85f2ab21cb8e
Igor Zubkov <igor.zubkov@gmail.com>
2023-08-31 12:26:31 +0200

    Update js deps

--- config/importmap.rb
+++ config/importmap.rb
@@ -21,8 +21,8 @@ pin "stimulus-password-visibility", to: "https://ga.jspm.io/npm:stimulus-passwor
 
 # ninja-keys and deps
 pin "ninja-keys", to: "https://ga.jspm.io/npm:ninja-keys@1.2.2/dist/ninja-keys.js"
-pin "@lit/reactive-element", to: "https://ga.jspm.io/npm:@lit/reactive-element@1.6.2/reactive-element.js"
-pin "@lit/reactive-element/decorators/", to: "https://ga.jspm.io/npm:@lit/reactive-element@1.6.2/decorators/"
+pin "@lit/reactive-element", to: "https://ga.jspm.io/npm:@lit/reactive-element@1.6.3/reactive-element.js"
+pin "@lit/reactive-element/decorators/", to: "https://ga.jspm.io/npm:@lit/reactive-element@1.6.3/decorators/"
 pin "@material/mwc-icon", to: "https://ga.jspm.io/npm:@material/mwc-icon@0.25.3/mwc-icon.js"
 pin "hotkeys-js", to: "https://ga.jspm.io/npm:hotkeys-js@3.8.7/dist/hotkeys.esm.js"
 pin "lit", to: "https://ga.jspm.io/npm:lit@2.2.6/index.js"
@@ -30,4 +30,4 @@ pin "lit-element/lit-element.js", to: "https://ga.jspm.io/npm:lit-element@3.3.3/
 pin "lit-html", to: "https://ga.jspm.io/npm:lit-html@2.8.0/lit-html.js"
 pin "lit-html/directives/", to: "https://ga.jspm.io/npm:lit-html@2.8.0/directives/"
 pin "lit/", to: "https://ga.jspm.io/npm:lit@2.2.6/"
-pin "tslib", to: "https://ga.jspm.io/npm:tslib@2.6.1/tslib.es6.mjs"
+pin "tslib", to: "https://ga.jspm.io/npm:tslib@2.6.2/tslib.es6.mjs"
