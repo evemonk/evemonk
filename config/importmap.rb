9ebffdbd954960c71ce6752307ae646d09e55c78
Igor Zubkov <igor.zubkov@gmail.com>
2024-01-23 23:47:08 +0100

    Don't preload all js

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,7 +8,7 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 
-pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.9/src/index.js"
+pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.9/src/index.js", preload: false
 
 pin "local-time", to: "https://ga.jspm.io/npm:local-time@3.0.2/app/assets/javascripts/local-time.es2017-esm.js"
 
@@ -20,14 +20,14 @@ pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.e
 pin "stimulus-password-visibility", to: "https://ga.jspm.io/npm:stimulus-password-visibility@2.1.1/dist/stimulus-password-visibility.mjs"
 
 # ninja-keys and deps
-pin "ninja-keys", to: "https://ga.jspm.io/npm:ninja-keys@1.2.2/dist/ninja-keys.js"
-pin "@lit/reactive-element", to: "https://ga.jspm.io/npm:@lit/reactive-element@1.6.3/reactive-element.js"
-pin "@lit/reactive-element/decorators/", to: "https://ga.jspm.io/npm:@lit/reactive-element@1.6.3/decorators/"
-pin "@material/mwc-icon", to: "https://ga.jspm.io/npm:@material/mwc-icon@0.25.3/mwc-icon.js"
-pin "hotkeys-js", to: "https://ga.jspm.io/npm:hotkeys-js@3.8.7/dist/hotkeys.esm.js"
-pin "lit", to: "https://ga.jspm.io/npm:lit@2.2.6/index.js"
-pin "lit-element/lit-element.js", to: "https://ga.jspm.io/npm:lit-element@3.3.3/lit-element.js"
-pin "lit-html", to: "https://ga.jspm.io/npm:lit-html@2.8.0/lit-html.js"
-pin "lit-html/directives/", to: "https://ga.jspm.io/npm:lit-html@2.8.0/directives/"
-pin "lit/", to: "https://ga.jspm.io/npm:lit@2.2.6/"
-pin "tslib", to: "https://ga.jspm.io/npm:tslib@2.6.2/tslib.es6.mjs"
+pin "ninja-keys", to: "https://ga.jspm.io/npm:ninja-keys@1.2.2/dist/ninja-keys.js", preload: false
+pin "@lit/reactive-element", to: "https://ga.jspm.io/npm:@lit/reactive-element@1.6.3/reactive-element.js", preload: false
+pin "@lit/reactive-element/decorators/", to: "https://ga.jspm.io/npm:@lit/reactive-element@1.6.3/decorators/", preload: false
+pin "@material/mwc-icon", to: "https://ga.jspm.io/npm:@material/mwc-icon@0.25.3/mwc-icon.js", preload: false
+pin "hotkeys-js", to: "https://ga.jspm.io/npm:hotkeys-js@3.8.7/dist/hotkeys.esm.js", preload: false
+pin "lit", to: "https://ga.jspm.io/npm:lit@2.2.6/index.js", preload: false
+pin "lit-element/lit-element.js", to: "https://ga.jspm.io/npm:lit-element@3.3.3/lit-element.js", preload: false
+pin "lit-html", to: "https://ga.jspm.io/npm:lit-html@2.8.0/lit-html.js", preload: false
+pin "lit-html/directives/", to: "https://ga.jspm.io/npm:lit-html@2.8.0/directives/", preload: false
+pin "lit/", to: "https://ga.jspm.io/npm:lit@2.2.6/", preload: false
+pin "tslib", to: "https://ga.jspm.io/npm:tslib@2.6.2/tslib.es6.mjs", preload: false
