c43dff42c5444db266fd7cedac8746cac4d09fe6
Igor Zubkov <igor.zubkov@gmail.com>
2024-01-23 01:17:15 +0100

    Update @rails/request.js to 0.0.9

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,7 +8,7 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 
-pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.8/src/index.js"
+pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.9/src/index.js"
 
 pin "local-time", to: "https://ga.jspm.io/npm:local-time@3.0.2/app/assets/javascripts/local-time.es2017-esm.js"
 
