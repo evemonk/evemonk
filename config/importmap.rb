62b312527faaf7605ca46d4ab3dd386231057d30
Igor Zubkov <igor.zubkov@gmail.com>
2024-02-13 14:53:49 +0100

    Don't preload

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,14 +10,14 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 
 pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.9/src/index.js", preload: false
 
-pin "local-time", to: "https://ga.jspm.io/npm:local-time@3.0.2/app/assets/javascripts/local-time.es2017-esm.js"
+pin "local-time", to: "https://ga.jspm.io/npm:local-time@3.0.2/app/assets/javascripts/local-time.es2017-esm.js", preload: false
 
-pin "@github/hotkey", to: "https://ga.jspm.io/npm:@github/hotkey@3.1.0/dist/index.js"
+pin "@github/hotkey", to: "https://ga.jspm.io/npm:@github/hotkey@3.1.0/dist/index.js", preload: false
 
-pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js", preload: false
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js", preload: false
 
-pin "stimulus-password-visibility", to: "https://ga.jspm.io/npm:stimulus-password-visibility@2.1.1/dist/stimulus-password-visibility.mjs"
+pin "stimulus-password-visibility", to: "https://ga.jspm.io/npm:stimulus-password-visibility@2.1.1/dist/stimulus-password-visibility.mjs", preload: false
 
 # ninja-keys and deps
 pin "ninja-keys", to: "https://ga.jspm.io/npm:ninja-keys@1.2.2/dist/ninja-keys.js", preload: false
