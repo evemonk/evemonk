6ebfba7e02267be55867041ba744301c502c1df5
Igor Zubkov <igor.zubkov@gmail.com>
2024-02-24 14:37:31 +0100

    Sync with rails 7.1

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ require "action_mailer/railtie"
 # require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
-require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -24,7 +23,12 @@ Bundler.require(*Rails.groups)
 module Evemonk
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: ["assets", "tasks"])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -40,7 +44,7 @@ module Evemonk
     # Compiles the ERB template with the frozen_string_literal true magic comment
     config.action_view.frozen_string_literal = true
 
-    # Sidekiq
+    # sidekiq
     config.active_job.queue_adapter = :sidekiq
   end
 end
