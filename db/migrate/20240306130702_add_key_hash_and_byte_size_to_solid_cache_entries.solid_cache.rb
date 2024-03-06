adcc4af5a3db4919805b0f8a858e9cb893dcfd1e
Igor Zubkov <igor.zubkov@gmail.com>
2024-03-06 14:08:41 +0100

    Add migration

---
+++ db/migrate/20240306130702_add_key_hash_and_byte_size_to_solid_cache_entries.solid_cache.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class AddKeyHashAndByteSizeToSolidCacheEntries < ActiveRecord::Migration[7.1]
+  def change
+    change_table :solid_cache_entries do |t|
+      t.column :key_hash, :integer, null: true, limit: 8
+      t.column :byte_size, :integer, null: true, limit: 4
+    end
+  end
+end
