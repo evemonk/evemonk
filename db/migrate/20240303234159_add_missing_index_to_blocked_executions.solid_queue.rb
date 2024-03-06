5aa3c9e6ec97790d74a5c37c85500a4caf0b1d89
Igor Zubkov <igor.zubkov@gmail.com>
2024-03-06 13:56:31 +0100

    Cleanup

--- db/migrate/20240303234159_add_missing_index_to_blocked_executions.solid_queue.rb
+++ db/migrate/20240303234159_add_missing_index_to_blocked_executions.solid_queue.rb
@@ -1,4 +1,5 @@
-# This migration comes from solid_queue (originally 20240110143450)
+# frozen_string_literal: true
+
 class AddMissingIndexToBlockedExecutions < ActiveRecord::Migration[7.1]
   def change
     add_index :solid_queue_blocked_executions, [:concurrency_key, :priority, :job_id], name: "index_solid_queue_blocked_executions_for_release"
