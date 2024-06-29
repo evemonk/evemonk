895a1bb59b3fb6501042694de78b37a9c419ceda
Igor Zubkov <igor.zubkov@gmail.com>
2024-06-29 20:59:27 +0200

    Update

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,14 +8,14 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.9/src/index.js", preload: false
+pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.9
 
-pin "local-time", to: "https://ga.jspm.io/npm:local-time@3.0.2/app/assets/javascripts/local-time.es2017-esm.js", preload: false
+pin "local-time" # @3.0.2
 
-pin "@github/hotkey", to: "https://ga.jspm.io/npm:@github/hotkey@3.1.0/dist/index.js", preload: false
+pin "@github/hotkey", to: "@github--hotkey.js" # @3.1.1
 
-pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js", preload: false
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js", preload: false
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "bootstrap" # @5.2.3
 
 pin "stimulus-password-visibility", to: "https://ga.jspm.io/npm:stimulus-password-visibility@2.1.1/dist/stimulus-password-visibility.mjs", preload: false
 
