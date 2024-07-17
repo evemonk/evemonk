73e857de299487b7ab0279c43e5a5c0b012e7402
Igor Zubkov <igor.zubkov@gmail.com>
2024-07-17 21:07:38 +0200

    Update

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,7 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.9/src/index.js", preload: false
