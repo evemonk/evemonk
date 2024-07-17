e0223ae4ede175dc456a4b9b3cb78b3965a102e7
Igor Zubkov <igor.zubkov@gmail.com>
2024-07-17 21:16:45 +0200

    Update

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-# require "action_mailbox/engine"
-# require "action_text/engine"
+require "action_mailbox/engine"
+require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "rails/test_unit/railtie"
@@ -25,7 +25,7 @@ module Evemonk
     config.evemonk = config_for(:evemonk)
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -45,8 +45,5 @@ module Evemonk
 
     # Compiles the ERB template with the frozen_string_literal true magic comment
     config.action_view.frozen_string_literal = true
-
-    # sidekiq
-    config.active_job.queue_adapter = :sidekiq
   end
 end
