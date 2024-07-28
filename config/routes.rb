998ae4cd2e839087cbe7a74091db7ce7468c30d3
Igor Zubkov <igor.zubkov@gmail.com>
2024-07-28 12:58:57 +0200

    Remove sidekiq/web

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-if Rails.env.local?
-  require "sidekiq/web"
-end
-
 Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", :as => :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/*
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   if Rails.env.local?
     mount Lookbook::Engine, at: "/lookbook"
@@ -21,15 +21,9 @@ Rails.application.routes.draw do
 
   get ".well-known/change-password", to: "well_known#change_password"
 
-  if Rails.env.local?
-    namespace :backoffice do
-      mount Sidekiq::Web, at: "sidekiq"
-    end
-  end
-
-  authenticate :user, ->(user) { user.admin? } do
-    mount Flipper::UI.app(Flipper), at: "/flipper"
-  end
+  # authenticate :user, ->(user) { user.admin? } do
+  #   mount Flipper::UI.app(Flipper), at: "/flipper"
+  # end
 
   namespace :universe do
     resources :alliances, only: [:index, :show] do
