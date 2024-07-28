196812a7a86826599aebbdc500a3ffe0932f5b23
Igor Zubkov <igor.zubkov@gmail.com>
2024-07-28 22:01:47 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  get ".well-known/change-password", to: "well_known#change_password"
+  get ".well-known/change-password", to: "well_known#change_password", as: "well_known_change_password"
 
   # authenticate :user, ->(user) { user.admin? } do
   #   mount Flipper::UI.app(Flipper), at: "/flipper"
