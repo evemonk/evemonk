62af7cbbf2e9dce6fa019e892ae2ac56cb315682
Igor Zubkov <igor.zubkov@gmail.com>
2024-07-28 14:20:31 +0200

    Cleanup

--- spec/requests/abouts_controller_spec.rb
+++ spec/requests/abouts_controller_spec.rb
@@ -8,12 +8,28 @@ RSpec.describe AboutsController do
   describe "#show" do
     before { get about_url }
 
-    it "is expected to render template show with status ok" do
-      expect(response).to render_template(:show)
+    context "when user not logged in" do
+      it "is expected to render template show with status ok" do
+        expect(response).to render_template(:show)
 
-      expect(response).to have_http_status(:ok)
+        expect(response).to have_http_status(:ok)
 
-      expect(response.body).to include("About EveMonk")
+        expect(response.body).to include("About EveMonk")
+      end
+    end
+
+    context "when user is logged in" do
+      let(:user) { create(:user) }
+
+      before { sign_in(user) }
+
+      it "is expected to render template show with status ok" do
+        expect(response).to render_template(:show)
+
+        expect(response).to have_http_status(:ok)
+
+        expect(response.body).to include("About EveMonk")
+      end
     end
   end
 end
