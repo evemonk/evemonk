86f34170761141db468ac6e179831277718dc2c7
Igor Zubkov <igor.zubkov@gmail.com>
2024-09-01 21:21:02 +0200

    Cleanup

--- spec/models/loyalty_point_spec.rb
+++ spec/models/loyalty_point_spec.rb
@@ -3,9 +3,9 @@
 require "rails_helper"
 
 RSpec.describe LoyaltyPoint do
-  it { should be_an(ApplicationRecord) }
+  it { is_expected.to be_an(ApplicationRecord) }
 
-  it { should belong_to(:character) }
+  it { is_expected.to belong_to(:character) }
 
-  it { should belong_to(:corporation).class_name("Eve::Corporation").optional(true) }
+  it { is_expected.to belong_to(:corporation).class_name("Eve::Corporation").optional }
 end
