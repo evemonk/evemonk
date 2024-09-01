346a806cb5939a6331cceab83599dbb907108c06
Igor Zubkov <igor.zubkov@gmail.com>
2024-09-01 18:35:55 +0200

    Remove ru locale

--- app/models/language_mapper.rb
+++ app/models/language_mapper.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class LanguageMapper
-  LANGUAGES = {en: "en-us",
-               de: "de",
-               fr: "fr",
-               ja: "ja",
-               ru: "ru",
-               ko: "ko"}.freeze
+  LANGUAGES = {
+    en: "en-us",
+    de: "de",
+    fr: "fr",
+    ja: "ja",
+    ko: "ko"
+  }.freeze
 end
