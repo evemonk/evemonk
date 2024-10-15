ce778eb4f0b3801b7ce4f32855b0af1905bdbf84
Igor Zubkov <igor.zubkov@gmail.com>
2024-10-15 17:23:43 +0200

    TopAlliancesComponent

---
+++ app/components/top_alliances_component.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class TopAlliancesComponent < ApplicationComponent
+  include Turbo::FramesHelper
+
+  # @param alliances [ActiveRecord::Relation<Eve::Alliance>, Array<Eve::Alliance>]
+  def initialize(alliances: Eve::Alliance.none)
+    @alliances = alliances
+  end
+end
