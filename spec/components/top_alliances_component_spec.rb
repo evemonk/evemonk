45e15210002f1f22ba6486032c4bd18a6f4f9051
Igor Zubkov <igor.zubkov@gmail.com>
2024-10-15 17:39:22 +0200

    Add tests

--- spec/components/top_alliances_component_spec.rb
+++ spec/components/top_alliances_component_spec.rb
@@ -12,4 +12,24 @@ RSpec.describe TopAlliancesComponent, type: :component do
 
     specify { expect(page).to have_text("Loading...") }
   end
+
+  context "when alliances is present" do
+    let(:alliances) do
+      [
+        build(:eve_alliance,
+              id: 99_005_338,
+              name: "Pandemic Horde",
+              corporations_count: 12_345,
+              characters_count: 12_345)
+      ]
+    end
+
+    subject { described_class.new(alliances) }
+
+    before { render_inline(subject) }
+
+    specify { expect(page).not_to have_text("Loading...") }
+
+    specify { expect(page).to have_link("Pandemic Horde", href: "/universe/alliances/99005338") }
+  end
 end
