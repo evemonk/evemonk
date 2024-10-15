787a346e8162666c11e26a4f9747103842ee7f4b
Igor Zubkov <igor.zubkov@gmail.com>
2024-10-15 17:34:32 +0200

    Add tests

--- spec/components/top_alliances_component_spec.rb
+++ spec/components/top_alliances_component_spec.rb
@@ -4,4 +4,12 @@ require "rails_helper"
 
 RSpec.describe TopAlliancesComponent, type: :component do
   it { expect(subject).to be_an(ApplicationComponent) }
+
+  context "when alliances is blank" do
+    subject { described_class.new }
+
+    before { render_inline(subject) }
+
+    specify { expect(page).to have_text("Loading...") }
+  end
 end
