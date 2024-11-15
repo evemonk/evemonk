d694180243372e7d1197dd1890f87304bde2102a
Igor Zubkov <igor.zubkov@gmail.com>
2024-11-15 19:31:34 +0100

    Update

---
+++ app/components/character_nav_bar_component.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class CharacterNavBarComponent < ApplicationComponent
+  # @param character [Character]
+  # @param current_page [Symbol]
+  def initialize(character, current_page)
+    @character = character
+    @current_page = current_page
+  end
+end
