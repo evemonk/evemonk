3634825a069fae99c4d2ac1f95a9e68f1e3eee3c
Igor Zubkov <igor.zubkov@gmail.com>
2024-11-17 15:56:17 +0100

    Update

--- app/components/character_nav_bar_component.rb
+++ app/components/character_nav_bar_component.rb
@@ -2,7 +2,7 @@
 
 class CharacterNavBarComponent < ApplicationComponent
   # @param character [Character]
-  # @param current_page [String]
+  # @param current_page [String] Enum: "sheet", "employment_history", "agents", "assets", "loyalty_points", "skills", "mail", "standings", "wallet_journal", "wallet_transactions", "industry_jobs", "blueprints", "manufacturing_jobs", "orders", "training_queue", "certificates", "settings".
   def initialize(character, current_page)
     @character = character
     @current_page = current_page
