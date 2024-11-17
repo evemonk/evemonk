401d45fdde60203762c110b66ee7be87e0b3302c
Igor Zubkov <igor.zubkov@gmail.com>
2024-11-17 18:23:50 +0100

    Update

--- spec/components/character_card_component_spec.rb
+++ spec/components/character_card_component_spec.rb
@@ -3,9 +3,38 @@
 require "rails_helper"
 
 RSpec.describe CharacterCardComponent, type: :component do
-  let(:character) { build_stubbed(:character) }
+  let(:eve_race) { create(:eve_race, name_en: "Minmatar") }
+
+  let(:eve_bloodline) { create(:eve_bloodline, name_en: "Brutor") }
+
+  let(:character) { create(:character, race: eve_race, bloodline: eve_bloodline) }
 
   subject { described_class.new(character) }
 
   it { expect(subject).to be_an(ApplicationComponent) }
+
+  context "when full card" do
+    subject { described_class.new(character, full: true) }
+
+    before { render_inline(subject) }
+
+    # TODO: Corporation
+    # TODO: Alliance
+
+    specify { expect(page).to have_content("Minmatar / Brutor") }
+
+    # specify { expect(page).to have_text("Loading...") }
+    # specify { expect(page).to have_text("Loading...") }
+    # specify { expect(page).to have_text("Loading...") }
+    # specify { expect(page).to have_text("Loading...") }
+    # specify { expect(page).to have_text("Loading...") }
+  end
+
+  context "when not full card" do
+    subject { described_class.new(character, full: false) }
+
+    before { render_inline(subject) }
+
+    specify { expect(page).not_to have_content("Minmatar / Brutor") }
+  end
 end
