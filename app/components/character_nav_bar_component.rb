660e1d162900b7b02b54691ed721045a531d898a
Igor Zubkov <igor.zubkov@gmail.com>
2024-11-17 15:07:43 +0100

    Update

--- app/components/character_nav_bar_component.rb
+++ app/components/character_nav_bar_component.rb
@@ -2,9 +2,9 @@
 
 class CharacterNavBarComponent < ApplicationComponent
   # @param character [Character]
-  # @param current_page [String, Symbol]
-  def initialize(character:, current_page:)
+  # @param current_page [String]
+  def initialize(character, current_page)
     @character = character
-    @current_page = current_page.to_sym
+    @current_page = current_page
   end
 end
