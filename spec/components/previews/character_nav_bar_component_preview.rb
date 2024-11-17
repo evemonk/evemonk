d524462e77a351326e1eab7fbf5cc667188e3fdd
Igor Zubkov <igor.zubkov@gmail.com>
2024-11-17 15:20:50 +0100

    Update

--- spec/components/previews/character_nav_bar_component_preview.rb
+++ spec/components/previews/character_nav_bar_component_preview.rb
@@ -4,7 +4,7 @@ class CharacterNavBarComponentPreview < Lookbook::Preview
   def default
     character = FactoryBot.build_stubbed(:character)
 
-    render CharacterNavBarComponent.new(character: character, current_page: :sheet)
+    render CharacterNavBarComponent.new(character, "sheet")
   end
 
   # @param id text
@@ -12,13 +12,13 @@ class CharacterNavBarComponentPreview < Lookbook::Preview
   def with_real_character(id: "1", current_page: "sheet")
     character = Character.find(id)
 
-    render CharacterNavBarComponent.new(character: character, current_page: current_page)
+    render CharacterNavBarComponent.new(character, current_page)
   end
 
   # @param current_page text
   def with_current_page(current_page: "sheet")
     character = FactoryBot.build_stubbed(:character)
 
-    render CharacterNavBarComponent.new(character: character, current_page: current_page)
+    render CharacterNavBarComponent.new(character, current_page)
   end
 end
