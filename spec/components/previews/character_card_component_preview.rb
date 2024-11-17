0a54e569b1aeefcdbc5bb10f6729eb9f20a8ea75
Igor Zubkov <igor.zubkov@gmail.com>
2024-11-17 18:06:38 +0100

    Update

---
+++ spec/components/previews/character_card_component_preview.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class CharacterCardComponentPreview < Lookbook::Preview
+  # @param id text
+  def default(id: "1")
+    character = Character.find(id)
+
+    render CharacterCardComponent.new(character)
+  end
+
+  # @param id text
+  def full(id: "1")
+    character = Character.find(id)
+
+    render CharacterCardComponent.new(character)
+  end
+
+  # @param id text
+  def small(id: "1")
+    character = Character.find(id)
+
+    render CharacterCardComponent.new(character)
+  end
+end
