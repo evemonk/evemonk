7dc667eba7afc66fbee9f01c66f62698fb1566d6
Igor Zubkov <igor.zubkov@gmail.com>
2024-11-17 16:51:00 +0100

    Update

--- spec/components/character_nav_bar_component_spec.rb
+++ spec/components/character_nav_bar_component_spec.rb
@@ -22,15 +22,26 @@ RSpec.describe CharacterNavBarComponent, type: :component do
   specify { expect(page).to have_link("Loyalty points", href: Rails.application.routes.url_helpers.character_loyalty_points_path(character)) }
 
   specify { expect(page).to have_link("Skills", href: Rails.application.routes.url_helpers.character_skills_path(character)) }
-# <%= link_to("Mail", character_mails_path(@character), class: "nav-link active") %>
-# <%= link_to("Standings", character_standings_path(@character), class: "nav-link active") %>
-# <%= link_to("Wallet journal", character_wallet_journals_path(@character), class: "nav-link active") %>
-# <%= link_to("Wallet transactions", nil, class: "nav-link active") %>
-# <%= link_to("Industry Jobs", character_industry_jobs_path(@character), class: "nav-link active") %>
-# <%= link_to("Blueprints", character_blueprints_path(@character), class: "nav-link active") %>
-# <%= link_to("Manufacturing Jobs", character_manufacturing_jobs_path(@character), class: "nav-link active") %>
-# <%= link_to("Orders", character_orders_path(@character), class: "nav-link active") %>
-# <%= link_to("Training Queue", character_training_queues_path(@character), class: "nav-link active") %>
-# <%= link_to("Certificates", character_certificates_path(@character), class: "nav-link active") %>
-# <%= link_to("Settings", character_settings_path(@character), class: "nav-link active") %>
+
+  xspecify { expect(page).to have_link("Mail", href: Rails.application.routes.url_helpers.character_mails_path(character)) }
+
+  xspecify { expect(page).to have_link("Standings", href: Rails.application.routes.url_helpers.character_standings_path(character)) }
+
+  xspecify { expect(page).to have_link("Wallet journal", href: Rails.application.routes.url_helpers.character_wallet_journals_path(character)) }
+
+  xspecify { expect(page).to have_link("Wallet transactions", href: "") }
+
+  xspecify { expect(page).to have_link("Industry Jobs", href: Rails.application.routes.url_helpers.character_industry_jobs_path(character)) }
+
+  specify { expect(page).to have_link("Blueprints", href: Rails.application.routes.url_helpers.character_blueprints_path(character)) }
+
+  xspecify { expect(page).to have_link("Manufacturing Jobs", href: Rails.application.routes.url_helpers.character_manufacturing_jobs_path(character)) }
+
+  xspecify { expect(page).to have_link("Orders", href: Rails.application.routes.url_helpers.character_orders_path(character)) }
+
+  specify { expect(page).to have_link("Training Queue", href: Rails.application.routes.url_helpers.character_training_queues_path(character)) }
+
+  specify { expect(page).to have_link("Certificates", href: Rails.application.routes.url_helpers.character_certificates_path(character)) }
+
+  specify { expect(page).to have_link("Settings", href: Rails.application.routes.url_helpers.character_settings_path(character)) }
 end
