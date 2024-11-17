8ce5a80c6ebe0a3d5df1a696e05ee7d34d5cf0a8
Igor Zubkov <igor.zubkov@gmail.com>
2024-11-17 16:54:38 +0100

    Update

--- spec/components/character_nav_bar_component_spec.rb
+++ spec/components/character_nav_bar_component_spec.rb
@@ -17,27 +17,59 @@ RSpec.describe CharacterNavBarComponent, type: :component do
 
   specify { expect(page).to have_link("Agents", href: Rails.application.routes.url_helpers.character_agents_path(character)) }
 
-  xspecify { expect(page).to have_link("Assets", href: Rails.application.routes.url_helpers.character_assets_path(character)) }
+  specify do
+    skip "Not implemented"
+
+    expect(page).to have_link("Assets", href: Rails.application.routes.url_helpers.character_assets_path(character))
+  end
 
   specify { expect(page).to have_link("Loyalty points", href: Rails.application.routes.url_helpers.character_loyalty_points_path(character)) }
 
   specify { expect(page).to have_link("Skills", href: Rails.application.routes.url_helpers.character_skills_path(character)) }
 
-  xspecify { expect(page).to have_link("Mail", href: Rails.application.routes.url_helpers.character_mails_path(character)) }
+  specify do
+    skip "Not implemented"
 
-  xspecify { expect(page).to have_link("Standings", href: Rails.application.routes.url_helpers.character_standings_path(character)) }
+    expect(page).to have_link("Mail", href: Rails.application.routes.url_helpers.character_mails_path(character))
+  end
 
-  xspecify { expect(page).to have_link("Wallet journal", href: Rails.application.routes.url_helpers.character_wallet_journals_path(character)) }
+  specify do
+    skip "Not implemented"
 
-  xspecify { expect(page).to have_link("Wallet transactions", href: "") }
+    expect(page).to have_link("Standings", href: Rails.application.routes.url_helpers.character_standings_path(character))
+  end
 
-  xspecify { expect(page).to have_link("Industry Jobs", href: Rails.application.routes.url_helpers.character_industry_jobs_path(character)) }
+  specify do
+    skip "Not implemented"
+
+    expect(page).to have_link("Wallet journal", href: Rails.application.routes.url_helpers.character_wallet_journals_path(character))
+  end
+
+  specify do
+    skip "Not implemented"
+
+    expect(page).to have_link("Wallet transactions", href: "")
+  end
+
+  specify do
+    skip "Not implemented"
+
+    expect(page).to have_link("Industry Jobs", href: Rails.application.routes.url_helpers.character_industry_jobs_path(character))
+  end
 
   specify { expect(page).to have_link("Blueprints", href: Rails.application.routes.url_helpers.character_blueprints_path(character)) }
 
-  xspecify { expect(page).to have_link("Manufacturing Jobs", href: Rails.application.routes.url_helpers.character_manufacturing_jobs_path(character)) }
+  specify do
+    skip "Not implemented"
 
-  xspecify { expect(page).to have_link("Orders", href: Rails.application.routes.url_helpers.character_orders_path(character)) }
+    expect(page).to have_link("Manufacturing Jobs", href: Rails.application.routes.url_helpers.character_manufacturing_jobs_path(character))
+  end
+
+  specify do
+    skip "Not implemented"
+
+    expect(page).to have_link("Orders", href: Rails.application.routes.url_helpers.character_orders_path(character))
+  end
 
   specify { expect(page).to have_link("Training Queue", href: Rails.application.routes.url_helpers.character_training_queues_path(character)) }
 
