023dc1cf814d3feeae1f56a26e589c124f753e93
Igor Zubkov <igor.zubkov@gmail.com>
2024-11-17 18:02:54 +0100

    Update

--- spec/components/previews/character_nav_bar_component_preview.rb
+++ spec/components/previews/character_nav_bar_component_preview.rb
@@ -1,23 +1,17 @@
 # frozen_string_literal: true
 
 class CharacterNavBarComponentPreview < Lookbook::Preview
-  def default
-    character = FactoryBot.build_stubbed(:character)
+  # @param id text
+  def default(id: "1")
+    character = Character.find(id)
 
     render CharacterNavBarComponent.new(character, "sheet")
   end
 
   # @param id text
   # @param current_page text
-  def with_real_character(id: "1", current_page: "sheet")
+  def with_current_page(id: "1", current_page: "sheet")
     character = Character.find(id)
-
-    render CharacterNavBarComponent.new(character, current_page)
-  end
-
-  # @param current_page text
-  def with_current_page(current_page: "sheet")
-    character = FactoryBot.build_stubbed(:character)
 
     render CharacterNavBarComponent.new(character, current_page)
   end
