0956b2faeeceec089c3c84b6a55cb934042fc8bf
Igor Zubkov <me@biow0lf.pp.ua>
2024-11-25 02:35:03 +0100

    Backports changes (part 7) (#5919)

--- app/controllers/corporations/top_controller.rb
+++ app/controllers/corporations/top_controller.rb
@@ -5,6 +5,8 @@ module Corporations
     skip_before_action :authenticate_user!
 
     def index
+      skip_policy_scope
+
       corporations = ::Eve::Corporation.not_npc.order(member_count: :desc).limit(20)
 
       render TopCorporationsComponent.new(corporations: corporations, lazy: false), layout: false
--- app/controllers/privacy_policies_controller.rb
+++ app/controllers/privacy_policies_controller.rb
@@ -4,5 +4,6 @@ class PrivacyPoliciesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def show
+    skip_authorization
   end
 end
