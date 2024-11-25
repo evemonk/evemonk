ebf5e503a42a58feb8aeb0e1fd845d6bacccb114
Igor Zubkov <igor.zubkov@gmail.com>
2024-11-25 01:03:49 +0100

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
   protect_from_forgery with: :exception, prepend: true
 
   before_action :authenticate_user!
