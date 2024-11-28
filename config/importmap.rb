e2c1b789e0dcbbfbdd133a47f29de989574deaef
Igor Zubkov <igor.zubkov@gmail.com>
2024-11-28 19:30:09 +0100

    Update

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,18 +4,18 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "@rails/request.js", to: "@rails--request.js.js", preload: false # @0.0.11
+pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.11
 
-pin "local-time", preload: false # @3.0.2
+pin "local-time" # @3.0.2
 
-pin "@github/hotkey", to: "@github--hotkey.js", preload: false # @3.1.1
+pin "@github/hotkey", to: "@github--hotkey.js" # @3.1.1
 
-pin "@popperjs/core", to: "popper.js", preload: false # @2.11.8
-pin "bootstrap", to: "bootstrap.esm.min.js", preload: false # @5.2.3
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "bootstrap" # @5.2.3
 
 pin "@stimulus-components/password-visibility", to: "@stimulus-components--password-visibility.js", preload: false # @3.0.0
