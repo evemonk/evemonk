da69020be8e9e9717ace698cfad2ef8aadeeee48
Igor Zubkov <me@biow0lf.pp.ua>
2024-12-05 19:43:00 +0100

    allow_browser versions: :modern (#5974)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
   include Pundit::Authorization
 
   protect_from_forgery with: :exception, prepend: true
