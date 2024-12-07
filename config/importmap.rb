d828d366c88d93f6e01d8e04098fb3178342924b
Igor Zubkov <igor.zubkov@gmail.com>
2024-12-07 18:45:41 +0100

    Vendor bootstrap js and popperjs

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,7 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.11/src/index.js", preload: false
@@ -14,7 +15,7 @@ pin "local-time", to: "https://ga.jspm.io/npm:local-time@3.0.2/app/assets/javasc
 
 pin "@github/hotkey", to: "https://ga.jspm.io/npm:@github/hotkey@3.1.1/dist/index.js", preload: false
 
-pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js", preload: false
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js", preload: false
+pin "@popperjs/core", to: "popper.js", preload: false # @2.11.8
+pin "bootstrap", to: "bootstrap.esm.min.js", preload: false # @5.2.3
 
 pin "stimulus-password-visibility", to: "https://ga.jspm.io/npm:stimulus-password-visibility@2.1.1/dist/stimulus-password-visibility.mjs", preload: false
