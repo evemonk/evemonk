395cb594bd2bc0e0d62f2113230d3cf00cc28916
Igor Zubkov <igor.zubkov@gmail.com>
2024-12-08 22:17:15 +0100

    Update

--- spec/requests/universe/corporations/characters_controller_spec.rb
+++ spec/requests/universe/corporations/characters_controller_spec.rb
@@ -5,35 +5,31 @@ require "rails_helper"
 RSpec.describe Universe::Corporations::CharactersController do
   it { expect(subject).to be_a(ApplicationController) }
 
-  # describe "#index" do
-  #   let(:eve_corporation) { instance_double(Eve::Corporation) }
-  #
-  #   before do
-  #     #
-  #     # Eve::Corporation.find(params[:corporation_id]) # => eve_corporation
-  #     #
-  #     expect(Eve::Corporation).to receive(:find).with("1000001").and_return(eve_corporation)
-  #   end
-  #
-  #   before do
-  #     #
-  #     # Eve::Character
-  #     #   .where(corporation: @corporation)
-  #     #   .order(:name)
-  #     #
-  #     expect(Eve::Character).to receive(:where).with(corporation: eve_corporation) do
-  #       double.tap do |a|
-  #         expect(a).to receive(:order).with(:name)
-  #       end
-  #     end
-  #   end
-  #
-  #   before { get :index, params: {corporation_id: "1000001"} }
-  #
-  #   it { expect(subject).to respond_with(:ok) }
-  #
-  #   it { expect(subject).not_to render_with_layout }
-  #
-  #   it { expect(subject).to render_template(:index) }
-  # end
+  describe "#index" do
+    let!(:corporation) { create(:eve_corporation) }
+
+    let!(:character) { create(:eve_character, corporation: corporation) }
+
+    before { get universe_corporation_characters_path(corporation) }
+
+    context "when user not logged in" do
+      it "is expected to have http status ok" do
+        expect(response).to have_http_status(:ok)
+
+        expect(response.body).to include("Members (1)")
+      end
+    end
+
+    context "when user is logged in" do
+      let(:user) { create(:user) }
+
+      before { sign_in(user) }
+
+      it "is expected to have http status ok" do
+        expect(response).to have_http_status(:ok)
+
+        expect(response.body).to include("Members (1)")
+      end
+    end
+  end
 end
