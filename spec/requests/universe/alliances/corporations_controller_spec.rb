f3e54b7e97ce5980f90c102530d83dd30a4280d4
Igor Zubkov <igor.zubkov@gmail.com>
2024-12-08 21:52:03 +0100

    Update

---
+++ spec/requests/universe/alliances/corporations_controller_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Universe::Alliances::CorporationsController do
+  it { expect(subject).to be_an(ApplicationController) }
+
+  describe "#index" do
+  end
+
+  # describe "#index" do
+  #   let(:eve_alliance) { instance_double(Eve::Alliance) }
+  #
+  #   before do
+  #     #
+  #     # Eve::Alliance.find(params[:alliance_id])
+  #     #
+  #     expect(Eve::Alliance).to receive(:find).with("1354830081").and_return(eve_alliance)
+  #   end
+  #
+  #   before do
+  #     #
+  #     # Eve::Corporation.where(alliance: @alliance)
+  #     #                 .order(:name)
+  #     #
+  #     expect(Eve::Corporation).to receive(:where).with(alliance: eve_alliance) do
+  #       double.tap do |a|
+  #         expect(a).to receive(:order).with(:name)
+  #       end
+  #     end
+  #   end
+  #
+  #   before { get :index, params: {alliance_id: "1354830081"} }
+  #
+  #   it { expect(subject).to respond_with(:ok) }
+  #
+  #   it { expect(subject).not_to render_with_layout }
+  #
+  #   it { expect(subject).to render_template(:index) }
+  # end
+end
