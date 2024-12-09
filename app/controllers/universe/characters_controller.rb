96582515019afbd713f1c81822fee287c8295304
Igor Zubkov <igor.zubkov@gmail.com>
2024-12-09 18:15:50 +0100

    Update

--- app/controllers/universe/characters_controller.rb
+++ app/controllers/universe/characters_controller.rb
@@ -11,9 +11,18 @@ module Universe
     end
 
     def show
-      @character = ::Eve::Character.includes(:alliance, :corporation).find(params[:id])
+      # @character = ::Eve::Character.includes(:alliance, :corporation).find(params[:id])
+      #
+      # @character_corporation_histories = Eve::CharacterCorporationHistory.where(character: @character).includes(:corporation).order(start_date: :desc)
 
-      @character_corporation_histories = Eve::CharacterCorporationHistory.where(character: @character).includes(:corporation).order(start_date: :desc)
+      @character = Eve::Character.includes(:alliance, :corporation).find(params[:id])
+
+      authorize @character
+
+      @character_corporation_histories = Eve::CharacterCorporationHistory
+        .where(character: @character)
+        .includes(:corporation)
+        .order(start_date: :desc)
     end
   end
 end
