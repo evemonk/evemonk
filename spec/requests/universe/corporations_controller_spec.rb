0392adf36b5d0bd351ebbecec93bbc52959be434
Igor Zubkov <igor.zubkov@gmail.com>
2024-12-09 23:25:47 +0100

    Fix typo

--- spec/requests/universe/corporations_controller_spec.rb
+++ spec/requests/universe/corporations_controller_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 RSpec.describe Universe::CorporationsController do
-  it { expect(subject).to be_a(ApplicationController) }
+  it { expect(subject).to be_an(ApplicationController) }
 
   describe "#index" do
     let!(:corporation) { create(:eve_corporation, name: "Freighting Solutions Inc.") }
