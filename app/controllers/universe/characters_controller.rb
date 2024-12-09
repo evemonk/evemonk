4ef3dcd9e5ec563df045ea932e07692cb3315416
Igor Zubkov <igor.zubkov@gmail.com>
2024-12-09 18:13:51 +0100

    Update

--- app/controllers/universe/characters_controller.rb
+++ app/controllers/universe/characters_controller.rb
@@ -5,20 +5,15 @@ module Universe
     skip_before_action :authenticate_user!
 
     def index
-      @characters = Eve::SearchCharactersQuery
-        .new(params[:q])
-        .query
-        .page(params[:page])
+      # @characters = Eve::SearchCharactersQuery.new(params[:q]).query.page(params[:page])
+
+      @characters = policy_scope(Eve::Character).order(:name).page(params[:page])
     end
 
     def show
-      @character = ::Eve::Character
-        .includes(:alliance, :corporation)
-        .find(params[:id])
+      @character = ::Eve::Character.includes(:alliance, :corporation).find(params[:id])
 
-      @character_corporation_histories = Eve::CharacterCorporationHistory.where(character: @character)
-        .includes(:corporation)
-        .order(start_date: :desc)
+      @character_corporation_histories = Eve::CharacterCorporationHistory.where(character: @character).includes(:corporation).order(start_date: :desc)
     end
   end
 end
