4c5ccc95caf7d856bb2d06dc39c667d6f8984602
Igor Zubkov <igor.zubkov@gmail.com>
2024-12-10 00:04:53 +0100

    Update

--- spec/requests/universe/categories_controller_spec.rb
+++ spec/requests/universe/categories_controller_spec.rb
@@ -31,33 +31,29 @@ RSpec.describe Universe::CategoriesController do
     end
   end
 
-  # describe "#show" do
-  #   let!(:alliance) { create(:eve_alliance) }
-  #
-  #   let!(:ceo) { create(:eve_character) }
-  #
-  #   let!(:corporation) { create(:eve_corporation, alliance: alliance, ceo: ceo, name: "Freighting Solutions Inc.") }
-  #
-  #   before { get universe_corporation_path(corporation) }
-  #
-  #   context "when user not logged in" do
-  #     it "is expected to have http status ok" do
-  #       expect(response).to have_http_status(:ok)
-  #
-  #       expect(response.body).to include("Corporation: Freighting Solutions Inc.")
-  #     end
-  #   end
-  #
-  #   context "when user is logged in" do
-  #     let(:user) { create(:user) }
-  #
-  #     before { sign_in(user) }
-  #
-  #     it "is expected to have http status ok" do
-  #       expect(response).to have_http_status(:ok)
-  #
-  #       expect(response.body).to include("Corporation: Freighting Solutions Inc.")
-  #     end
-  #   end
-  # end
+  describe "#show" do
+    let!(:category) { create(:eve_category, name_en: "Test Category") }
+
+    before { get universe_category_path(category) }
+
+    context "when user not logged in" do
+      it "is expected to have http status ok" do
+        expect(response).to have_http_status(:ok)
+
+        expect(response.body).to include("Test Category")
+      end
+    end
+
+    context "when user is logged in" do
+      let(:user) { create(:user) }
+
+      before { sign_in(user) }
+
+      it "is expected to have http status ok" do
+        expect(response).to have_http_status(:ok)
+
+        expect(response.body).to include("Test Category")
+      end
+    end
+  end
 end
