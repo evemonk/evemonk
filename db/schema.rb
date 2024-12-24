f488d2435ac6a6354938ef9acb837ae5a5691fda
Igor Zubkov <igor.zubkov@gmail.com>
2024-12-24 18:54:59 +0100

    Drop table solid_cache_entries

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_06_29_173432) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_24_175408) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_buffercache"
+  enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
   enable_extension "pgcrypto"
-  enable_extension "plpgsql"
   enable_extension "sslinfo"
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -1072,17 +1072,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_06_29_173432) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["character_id"], name: "index_skillqueues_on_character_id"
-  end
-
-  create_table "solid_cache_entries", force: :cascade do |t|
-    t.binary "key", null: false
-    t.binary "value", null: false
-    t.datetime "created_at", null: false
-    t.bigint "key_hash", null: false
-    t.integer "byte_size", null: false
-    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
-    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
-    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
 
   create_table "standings", force: :cascade do |t|
