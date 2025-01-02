214ea1b8d78fbf1af0aa0a746134e23374de3163
Igor Zubkov <igor.zubkov@gmail.com>
2025-01-02 19:11:01 +0100

    Update

--- app/importers/eve/races_importer.rb
+++ app/importers/eve/races_importer.rb
@@ -12,13 +12,15 @@ module Eve
       import! do
         Mobility.with_locale(locale) do
           esi.races.each do |race|
-            eve_race = Eve::Race.find_or_initialize_by(id: race.race_id)
+            eve_race = Eve::Race.find_or_initialize_by(id: race.id)
 
-            eve_race.update!(race.as_json.transform_keys(race_id: :id))
+            eve_race.update!(race.as_json)
           end
         end
       end
     end
+
+    private
 
     def esi
       @esi ||= EveOnline::ESI::UniverseRaces.new(language: LanguageMapper::LANGUAGES[locale])
