97a3df67144c5406969a203a2b26e5f50c73c5ce
Igor Zubkov <igor.zubkov@gmail.com>
2025-01-03 10:05:14 +0100

    More tests

--- spec/importers/eve/factions_importer_spec.rb
+++ spec/importers/eve/factions_importer_spec.rb
@@ -41,65 +41,21 @@ RSpec.describe Eve::FactionsImporter do
         expect(faction.id).to eq(500_001)
       end
     end
-  end
 
-  # describe "#initialize" do
-  #   context "without locale" do
-  #     its(:locale) { is_expected.to eq(:en) }
-  #   end
-  #
-  #   context "with locale" do
-  #     let(:locale) { :ru }
-  #
-  #     subject { described_class.new(locale) }
-  #
-  #     its(:locale) { is_expected.to eq(:ru) }
-  #   end
-  # end
-  #
-  # describe "#import" do
-  #   before { expect(subject).to receive(:configure_middlewares) }
-  #
-  #   let(:faction_id) { double }
-  #
-  #   let(:json) { double }
-  #
-  #   let(:faction) { instance_double(EveOnline::ESI::Models::Faction, faction_id: faction_id, as_json: json) }
-  #
-  #   let(:esi) { instance_double(EveOnline::ESI::UniverseFactions, factions: [faction]) }
-  #
-  #   before { expect(subject).to receive(:esi).and_return(esi) }
-  #
-  #   let(:eve_faction) { instance_double(Eve::Faction) }
-  #
-  #   before { expect(Eve::Faction).to receive(:find_or_initialize_by).with(id: faction_id).and_return(eve_faction) }
-  #
-  #   let(:transformed_json) { double }
-  #
-  #   before { expect(json).to receive(:transform_keys).with(faction_id: :id).and_return(transformed_json) }
-  #
-  #   before { expect(eve_faction).to receive(:update!).with(transformed_json) }
-  #
-  #   specify { expect { subject.import }.not_to raise_error }
-  # end
-  #
-  # describe "#esi" do
-  #   context "when @esi is set" do
-  #     let(:esi) { instance_double(EveOnline::ESI::UniverseFactions) }
-  #
-  #     before { subject.instance_variable_set(:@esi, esi) }
-  #
-  #     specify { expect(subject.esi).to eq(esi) }
-  #   end
-  #
-  #   context "when @esi not set" do
-  #     let(:esi) { instance_double(EveOnline::ESI::UniverseFactions) }
-  #
-  #     before { expect(EveOnline::ESI::UniverseFactions).to receive(:new).with(language: "en-us").and_return(esi) }
-  #
-  #     specify { expect(subject.esi).to eq(esi) }
-  #
-  #     specify { expect { subject.esi }.to change { subject.instance_variable_get(:@esi) }.from(nil).to(esi) }
-  #   end
-  # end
+    context "with de locale" do
+      before { VCR.insert_cassette "esi/universe/factions_de" }
+
+      after { VCR.eject_cassette }
+
+      subject { described_class.new(:de) }
+
+      specify do
+        subject.import
+
+        faction = Eve::Faction.first
+
+        expect(faction.description_de).to start_with("Der Staat der Caldari wird von mehreren Mega-Corporations beherrscht.")
+      end
+    end
+  end
 end
