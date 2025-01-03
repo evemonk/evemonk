d17f918c300b012e7dac8f7c9d0d31e08f5c07ad
Igor Zubkov <igor.zubkov@gmail.com>
2025-01-03 10:01:55 +0100

    Add tests

--- spec/importers/eve/factions_importer_spec.rb
+++ spec/importers/eve/factions_importer_spec.rb
@@ -5,6 +5,44 @@ require "rails_helper"
 RSpec.describe Eve::FactionsImporter do
   it { expect(subject).to be_a(Eve::BaseImporter) }
 
+  describe "#import" do
+    context "with default locale" do
+      before { VCR.insert_cassette "esi/universe/factions" }
+
+      after { VCR.eject_cassette }
+
+      specify { expect { subject.import }.to change(Eve::Faction, :count).by(27) }
+
+      specify do
+        subject.import
+
+        faction = Eve::Faction.first
+
+        expect(faction.id).to eq(500_001)
+
+        expect(faction.corporation_id).to eq(1_000_035)
+
+        expect(faction.description_en).to start_with("The Caldari State is ruled by several mega-corporations.")
+
+        expect(faction.is_unique).to eq(true)
+
+        expect(faction.militia_corporation_id).to eq(1_000_180)
+
+        expect(faction.name_en).to eq("Caldari State")
+
+        expect(faction.size_factor).to eq(5.0)
+
+        expect(faction.solar_system_id).to eq(30_000_145)
+
+        expect(faction.station_count).to eq(1_527)
+
+        expect(faction.station_system_count).to eq(528)
+
+        expect(faction.id).to eq(500_001)
+      end
+    end
+  end
+
   # describe "#initialize" do
   #   context "without locale" do
   #     its(:locale) { is_expected.to eq(:en) }
