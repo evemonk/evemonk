a690446d0be54e0d460fb9b28646705f8223b2ca
Igor Zubkov <me@biow0lf.pp.ua>
2025-01-06 18:50:14 +0100

    Updates (#6105)
    * Enable caching
    
    * Update
    
    * Update
    
    * Update
    
    * Enable caching
    
    * Update

--- app/importers/eve/bloodlines_importer.rb
+++ app/importers/eve/bloodlines_importer.rb
@@ -4,6 +4,7 @@ module Eve
   class BloodlinesImporter < BaseImporter
     attr_reader :locale
 
+    # @param locale [Symbol] Default: :en
     def initialize(locale = :en)
       @locale = locale
     end
