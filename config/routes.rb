710e1b24d44233929a1e883071e776c5e54ddf2b
Igor Zubkov <igor.zubkov@gmail.com>
2025-01-07 18:24:07 +0100

    Cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-if Rails.env.local?
-  require "sidekiq/web"
-end
-
 Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
