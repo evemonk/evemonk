468c9289c79b7b47569c47e95c245392fef35d3f
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-24 13:39:38 +0100

    Fix migration

--- db/migrate/20251224123506_add_index_eve_chars_name.rb
+++ db/migrate/20251224123506_add_index_eve_chars_name.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AddIndexEveCharsName < ActiveRecord::Migration[8.1]
+  disable_ddl_transaction!
+
   def change
     add_index :eve_characters, :name, algorithm: :concurrently
   end
