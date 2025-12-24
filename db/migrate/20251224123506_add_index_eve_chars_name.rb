68e0277fbf300dd1a314c1d067f3a17487cf5375
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-24 13:47:20 +0100

    Fix migration

--- db/migrate/20251224123506_add_index_eve_chars_name.rb
+++ db/migrate/20251224123506_add_index_eve_chars_name.rb
@@ -4,6 +4,6 @@ class AddIndexEveCharsName < ActiveRecord::Migration[8.1]
   disable_ddl_transaction!
 
   def change
-    add_index :eve_characters, :name, algorithm: :concurrently
+    add_index :eve_characters, :name, algorithm: :concurrently, name: :index_eve_characters_on_name_2
   end
 end
