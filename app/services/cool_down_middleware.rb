0b2accabbb2dfeb2f4d48f47c7711c5e75c8b288
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-01 17:12:46 +0100

    Update

--- app/services/cool_down_middleware.rb
+++ app/services/cool_down_middleware.rb
@@ -2,35 +2,35 @@
 
 class CoolDownMiddleware < Faraday::Middleware
   def on_request(_)
-    error_limit_remain = redis.get("esi_error_limit_remain")
-    esi_error_limit_remain_till = redis.get("esi_error_limit_remain_till")
-
-    if error_limit_remain
-      remain = error_limit_remain.to_i
-      if remain < 50
-        seconds = ActiveSupport::TimeZone["UTC"].parse(esi_error_limit_remain_till) - Time.zone.now
-
-        Kernel.sleep(seconds.to_i)
-      end
-    end
+    # error_limit_remain = redis.get("esi_error_limit_remain")
+    # esi_error_limit_remain_till = redis.get("esi_error_limit_remain_till")
+    #
+    # if error_limit_remain
+    #   remain = error_limit_remain.to_i
+    #   if remain < 50
+    #     seconds = ActiveSupport::TimeZone["UTC"].parse(esi_error_limit_remain_till) - Time.zone.now
+    #
+    #     Kernel.sleep(seconds.to_i)
+    #   end
+    # end
   end
 
   def on_complete(env)
-    esi_error_limit_remain = env[:response_headers]["x-esi-error-limit-remain"]
-    esi_error_limit_reset = env[:response_headers]["x-esi-error-limit-reset"]
-
-    if esi_error_limit_remain && esi_error_limit_reset
-      redis.set("esi_error_limit_remain",
-        esi_error_limit_remain.to_i,
-        ex: esi_error_limit_reset.to_i)
-
-      redis.set("esi_error_limit_remain_till",
-        esi_error_limit_reset.to_i.seconds.from_now.iso8601,
-        ex: esi_error_limit_reset.to_i)
-    end
+    # esi_error_limit_remain = env[:response_headers]["x-esi-error-limit-remain"]
+    # esi_error_limit_reset = env[:response_headers]["x-esi-error-limit-reset"]
+    #
+    # if esi_error_limit_remain && esi_error_limit_reset
+    #   redis.set("esi_error_limit_remain",
+    #     esi_error_limit_remain.to_i,
+    #     ex: esi_error_limit_reset.to_i)
+    #
+    #   redis.set("esi_error_limit_remain_till",
+    #     esi_error_limit_reset.to_i.seconds.from_now.iso8601,
+    #     ex: esi_error_limit_reset.to_i)
+    # end
   end
 
-  def redis
-    Redis.new
-  end
+  # def redis
+  #   Redis.new
+  # end
 end
