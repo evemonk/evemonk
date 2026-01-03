d77e35cc7874e475a60cecb81a573d130e622780
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-03 17:22:22 +0100

    Disable everything

---
+++ db/migrate/20260103161820_disable_features.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class DisableFeatures < ActiveRecord::Migration[8.1]
+  def change
+    # Disable external calls to ESI server status
+    Flipper.disable :eve_server_status
+
+    # Disable external calls to ESI races
+    Flipper.disable :eve_races
+
+    # Disable external calls to ESI bloodlines
+    Flipper.disable :eve_bloodlines
+
+    # Disable external calls to ESI ancestries
+    Flipper.disable :eve_ancestries
+
+    # Disable external calls to ESI factions
+    Flipper.disable :eve_factions
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_03_160721) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_03_161820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_buffercache"
   enable_extension "pg_catalog.plpgsql"
