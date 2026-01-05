dac09044f84c31d4f8acbe5cb607c21dbd4d1742
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-05 17:15:42 +0100

    Update

--- app/controllers/helps_controller.rb
+++ app/controllers/helps_controller.rb
@@ -2,6 +2,8 @@
 
 class HelpsController < ApplicationController
   def index
-
+    @character = policy_scope(Character)
+      .includes(:alliance, :corporation)
+      .find_by!(character_id: params[:character_id])
   end
 end
