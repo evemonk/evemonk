c1eaf0c37003a586a3fcbad16d220c9368f2c0c0
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-05 18:23:21 +0100

    Update

--- app/services/api/refresh_character_access_token.rb
+++ app/services/api/refresh_character_access_token.rb
@@ -2,14 +2,15 @@
 
 module Api
   class RefreshCharacterAccessToken
-    attr_reader :character
+    attr_reader :character_scope
 
-    def initialize(character)
-      @character = character
+    # @param character_scope [CharacterScope] Character scope with token to refresh
+    def initialize(character_scope)
+      @character_scope = character_scope
     end
 
     def refresh
-      return if !character.token_expired?
+      return if !character_scope.token_expired?
 
       oauth_client_options = OmniAuth::Strategies::EveOnlineSso.default_options["client_options"].to_hash.symbolize_keys
 
@@ -18,19 +19,23 @@ module Api
         oauth_client_options)
 
       response = OAuth2::AccessToken.from_hash(oauth_client,
-        refresh_token: character.refresh_token)
+        refresh_token: character_scope.refresh_token)
         .refresh!
 
-      character.update!(access_token: response.token,
+      character_scope.update!(
+        access_token: response.token,
         refresh_token: response.refresh_token,
         token_expires_at: Time.zone.at(response.expires_at),
-        token_expires: response.expires?)
+        token_expires: response.expires?
+      )
     rescue OAuth2::Error => e
-      character.update!(esi_token_valid: false,
+      character_scope.update!(
+        esi_token_valid: false,
         esi_token_invalid_at: Time.zone.now,
-        esi_last_error: e.description)
+        esi_last_error: e.description
+      )
 
-      Rails.logger.info("OAuth2::Error: Character ID: #{character.character_id} / code: #{e.code} / description: #{e.description}")
+      Rails.logger.info("OAuth2::Error: Character ID: #{character_scope.character.character_id} / code: #{e.code} / description: #{e.description}")
 
       raise CharacterInvalidToken
     end
