1969cb175a0f2e8412e057b30f86a6a1b850841c
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-05 18:26:34 +0100

    Update

--- spec/services/api/refresh_character_access_token_spec.rb
+++ spec/services/api/refresh_character_access_token_spec.rb
@@ -72,9 +72,11 @@ RSpec.describe Api::RefreshCharacterAccessToken do
       #                         esi_token_invalid_at: Time.zone.now,
       #                         esi_last_error: e.description)
       #
-      expect(character_scope).to receive(:update!).with(esi_token_valid: false,
+      expect(character_scope).to receive(:update!).with(
+        esi_token_valid: false,
         esi_token_invalid_at: Time.zone.now,
-        esi_last_error: "Invalid refresh token. Unable to migrate grant.")
+        esi_last_error: "Invalid refresh token. Unable to migrate grant."
+      )
     end
 
     before do
