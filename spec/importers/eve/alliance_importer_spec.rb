0b799a87853b894a72503c73ca8bea4797102d0b
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-07 21:45:52 +0100

    Update

--- spec/importers/eve/alliance_importer_spec.rb
+++ spec/importers/eve/alliance_importer_spec.rb
@@ -3,72 +3,56 @@
 require "rails_helper"
 
 RSpec.describe Eve::AllianceImporter do
-  let(:id) { double }
-
-  subject { described_class.new(id) }
-
-  it { expect(subject).to be_a(Eve::BaseImporter) }
-
   describe "#import" do
-    before { expect(subject).to receive(:configure_middlewares) }
+    before { VCR.insert_cassette "esi/alliances/1354830081" }
 
-    let(:eve_alliance) { instance_double(Eve::Alliance) }
+    after { VCR.eject_cassette }
 
-    before { expect(Eve::Alliance).to receive(:find_or_initialize_by).with(id: id).and_return(eve_alliance) }
+    subject { described_class.new(1_354_830_081) }
 
-    context "when eve alliance resource found" do
-      let(:json) { double }
-
-      let(:esi) { instance_double(EveOnline::ESI::Alliance, as_json: json) }
-
-      before { expect(subject).to receive(:esi).and_return(esi) }
-
-      before { expect(eve_alliance).to receive(:update!).with(json) }
-
-      before { expect(eve_alliance).to receive(:reset_corporations_count) }
-
-      before { expect(eve_alliance).to receive(:reset_characters_count) }
-
-      specify { expect { subject.import }.not_to raise_error }
-    end
-
-    context "when eve alliance resource not found" do
-      before { expect(subject).to receive(:esi).and_raise(EveOnline::Exceptions::ResourceNotFound) }
-
-      before do
-        #
-        # Rails.logger.info("EveOnline::Exceptions::ResourceNotFound: Eve Alliance ID #{id}")
-
-        expect(Rails).to receive(:logger) do
-          double.tap do |a|
-            expect(a).to receive(:info).with("EveOnline::Exceptions::ResourceNotFound: Eve Alliance ID #{id}")
-          end
-        end
-      end
-
-      before { expect(eve_alliance).to receive(:destroy!) }
-
-      specify { expect { subject.import }.not_to raise_error }
-    end
+    specify { expect { subject.import }.to change(Eve::Alliance, :count).by(1) }
   end
 
-  describe "#esi" do
-    context "when @esi is set" do
-      let(:esi) { instance_double(EveOnline::ESI::Alliance) }
-
-      before { subject.instance_variable_set(:@esi, esi) }
-
-      specify { expect(subject.esi).to eq(esi) }
-    end
-
-    context "when @esi not set" do
-      let(:esi) { instance_double(EveOnline::ESI::Alliance) }
-
-      before { expect(EveOnline::ESI::Alliance).to receive(:new).with(alliance_id: id).and_return(esi) }
-
-      specify { expect(subject.esi).to eq(esi) }
-
-      specify { expect { subject.esi }.to change { subject.instance_variable_get(:@esi) }.from(nil).to(esi) }
-    end
-  end
+  # describe "#import" do
+  #   before { expect(subject).to receive(:configure_middlewares) }
+  #
+  #   let(:eve_alliance) { instance_double(Eve::Alliance) }
+  #
+  #   before { expect(Eve::Alliance).to receive(:find_or_initialize_by).with(id: id).and_return(eve_alliance) }
+  #
+  #   context "when eve alliance resource found" do
+  #     let(:json) { double }
+  #
+  #     let(:esi) { instance_double(EveOnline::ESI::Alliance, as_json: json) }
+  #
+  #     before { expect(subject).to receive(:esi).and_return(esi) }
+  #
+  #     before { expect(eve_alliance).to receive(:update!).with(json) }
+  #
+  #     before { expect(eve_alliance).to receive(:reset_corporations_count) }
+  #
+  #     before { expect(eve_alliance).to receive(:reset_characters_count) }
+  #
+  #     specify { expect { subject.import }.not_to raise_error }
+  #   end
+  #
+  #   context "when eve alliance resource not found" do
+  #     before { expect(subject).to receive(:esi).and_raise(EveOnline::Exceptions::ResourceNotFound) }
+  #
+  #     before do
+  #       #
+  #       # Rails.logger.info("EveOnline::Exceptions::ResourceNotFound: Eve Alliance ID #{id}")
+  #       #
+  #       expect(Rails).to receive(:logger) do
+  #         double.tap do |a|
+  #           expect(a).to receive(:info).with("EveOnline::Exceptions::ResourceNotFound: Eve Alliance ID #{id}")
+  #         end
+  #       end
+  #     end
+  #
+  #     before { expect(eve_alliance).to receive(:destroy!) }
+  #
+  #     specify { expect { subject.import }.not_to raise_error }
+  #   end
+  # end
 end
