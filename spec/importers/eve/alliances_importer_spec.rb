8a97df1744fbb4f3bb18793c444c57b65c74aa8c
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-07 21:10:15 +0100

    Update

--- spec/importers/eve/alliances_importer_spec.rb
+++ spec/importers/eve/alliances_importer_spec.rb
@@ -18,17 +18,17 @@ RSpec.describe Eve::AlliancesImporter do
 
     context "when database is not empty" do
       context "when alliance is not exists anymore" do
-        let!(:alliance) { create(:eve_alliance, id: 123_456) }
-
-        let!(:corporation_1) { create(:eve_corporation, id: 123_444, alliance_id: alliance) }
-
-        let!(:corporation_2) { create(:eve_corporation, id: 222_444, alliance_id: alliance) }
-
         before { VCR.insert_cassette "esi/alliances/list" }
 
         after { VCR.eject_cassette }
 
-        before { expect(Eve::UpdateAllianceJob).to receive(:perform_later).at_least(1).time }
+        let!(:alliance) { create(:eve_alliance, id: 123_456) }
+
+        let!(:corporation_1) { create(:eve_corporation, id: 123_444, alliance: alliance) }
+
+        let!(:corporation_2) { create(:eve_corporation, id: 222_444, alliance: alliance) }
+
+        before { expect(Eve::UpdateAllianceJob).to receive(:perform_later).exactly(3541).times }
 
         before { expect(Eve::UpdateCorporationJob).to receive(:perform_later).with(123_444) }
 
