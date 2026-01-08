c480b80413a6fa55152d3356ba3ae08aee3101c4
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-08 02:16:55 +0100

    Update

--- app/importers/eve/corporation_importer.rb
+++ app/importers/eve/corporation_importer.rb
@@ -2,26 +2,33 @@
 
 module Eve
   class CorporationImporter < BaseImporter
-    # attr_reader :corporation_id
-    #
-    # def initialize(corporation_id)
-    #   @corporation_id = corporation_id
-    # end
-    #
-    # def import
-    #   import! do
-    #     eve_corporation = Eve::Corporation.find_or_initialize_by(id: corporation_id)
-    #
-    #     eve_corporation.update!(esi.as_json)
-    #   rescue EveOnline::Exceptions::ResourceNotFound
-    #     Rails.logger.info("EveOnline::Exceptions::ResourceNotFound: Eve Corporation ID #{corporation_id}")
-    #
-    #     eve_corporation.destroy!
-    #   end
-    # end
-    #
-    # def esi
-    #   @esi ||= EveOnline::ESI::Corporation.new(corporation_id: corporation_id)
-    # end
+    attr_reader :id
+
+    # @param id [Integer] Eve Corporation ID
+    def initialize(id)
+      @id = id
+    end
+
+    def import
+      import! do
+        eve_corporation = Eve::Corporation.find_or_initialize_by(id: id)
+
+        eve_corporation.update!(corporation.as_json)
+      rescue EveOnline::Exceptions::ResourceNotFound
+        Rails.logger.info("EveOnline::Exceptions::ResourceNotFound: Eve Corporation ID #{id}")
+
+        eve_corporation&.destroy!
+      end
+    end
+
+    private
+
+    def client
+      @client ||= EveOnline::ESI::Client.new(cache: true, cache_store: Rails.cache)
+    end
+
+    def corporation
+      @corporation ||= client.corporations.retrieve(id: id)
+    end
   end
 end
--- spec/jobs/eve/update_corporation_job_spec.rb
+++ spec/jobs/eve/update_corporation_job_spec.rb
@@ -8,19 +8,31 @@ RSpec.describe Eve::UpdateCorporationJob do
   it { expect(described_class.queue_name).to eq("default") }
 
   describe "#perform" do
-    # let(:id) { double }
-    #
-    # before do
-    #   #
-    #   # Eve::CorporationImporter.new(id).import
-    #   #
-    #   expect(Eve::CorporationImporter).to receive(:new).with(id) do
-    #     double.tap do |a|
-    #       expect(a).to receive(:import)
-    #     end
-    #   end
-    # end
-    #
-    # specify { expect { subject.perform(id) }.not_to raise_error }
+    let(:id) { 111_222 }
+
+    context "when eve corporation enabled" do
+      before { Flipper.enable(:eve_corporation) }
+
+      after { Flipper.disable(:eve_corporation) }
+
+      before do
+        #
+        # Eve::CorporationImporter.new(id).import
+        #
+        expect(Eve::CorporationImporter).to receive(:new).with(id) do
+          double.tap do |a|
+            expect(a).to receive(:import)
+          end
+        end
+      end
+
+      specify { expect { subject.perform(id) }.not_to raise_error }
+    end
+
+    context "when eve corporation disabled" do
+      before { expect(Eve::CorporationImporter).not_to receive(:new) }
+
+      specify { expect { subject.perform(id) }.not_to raise_error }
+    end
   end
 end
