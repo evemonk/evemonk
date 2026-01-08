4fdaa97e670e2708182e144c8fa495220f7414ff
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-08 02:26:06 +0100

    Update

--- spec/importers/eve/alliance_importer_spec.rb
+++ spec/importers/eve/alliance_importer_spec.rb
@@ -4,13 +4,27 @@ require "rails_helper"
 
 RSpec.describe Eve::AllianceImporter do
   describe "#import" do
-    before { VCR.insert_cassette "esi/alliances/1354830081" }
+    context "when alliance is exist" do
+      before { VCR.insert_cassette "esi/alliances/1354830081" }
 
-    after { VCR.eject_cassette }
+      after { VCR.eject_cassette }
 
-    subject { described_class.new(1_354_830_081) }
+      subject { described_class.new(1_354_830_081) }
 
-    specify { expect { subject.import }.to change(Eve::Alliance, :count).by(1) }
+      specify { expect { subject.import }.to change(Eve::Alliance, :count).by(1) }
+    end
+
+    context "when alliance is not exist anymore" do
+      before { VCR.insert_cassette "esi/alliances/111222" }
+
+      after { VCR.eject_cassette }
+
+      let!(:alliance) { create(:eve_alliance, id: 111_222) }
+
+      subject { described_class.new(111_222) }
+
+      specify { expect { subject.import }.to change(Eve::Alliance, :count).by(-1) }
+    end
   end
 
   # describe "#import" do
