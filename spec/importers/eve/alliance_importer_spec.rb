5255346dd03c6fb7c25d9aa7a40488b588aad041
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-08 02:26:42 +0100

    Update

--- spec/importers/eve/alliance_importer_spec.rb
+++ spec/importers/eve/alliance_importer_spec.rb
@@ -26,47 +26,4 @@ RSpec.describe Eve::AllianceImporter do
       specify { expect { subject.import }.to change(Eve::Alliance, :count).by(-1) }
     end
   end
-
-  # describe "#import" do
-  #   before { expect(subject).to receive(:configure_middlewares) }
-  #
-  #   let(:eve_alliance) { instance_double(Eve::Alliance) }
-  #
-  #   before { expect(Eve::Alliance).to receive(:find_or_initialize_by).with(id: id).and_return(eve_alliance) }
-  #
-  #   context "when eve alliance resource found" do
-  #     let(:json) { double }
-  #
-  #     let(:esi) { instance_double(EveOnline::ESI::Alliance, as_json: json) }
-  #
-  #     before { expect(subject).to receive(:esi).and_return(esi) }
-  #
-  #     before { expect(eve_alliance).to receive(:update!).with(json) }
-  #
-  #     before { expect(eve_alliance).to receive(:reset_corporations_count) }
-  #
-  #     before { expect(eve_alliance).to receive(:reset_characters_count) }
-  #
-  #     specify { expect { subject.import }.not_to raise_error }
-  #   end
-  #
-  #   context "when eve alliance resource not found" do
-  #     before { expect(subject).to receive(:esi).and_raise(EveOnline::Exceptions::ResourceNotFound) }
-  #
-  #     before do
-  #       #
-  #       # Rails.logger.info("EveOnline::Exceptions::ResourceNotFound: Eve Alliance ID #{id}")
-  #       #
-  #       expect(Rails).to receive(:logger) do
-  #         double.tap do |a|
-  #           expect(a).to receive(:info).with("EveOnline::Exceptions::ResourceNotFound: Eve Alliance ID #{id}")
-  #         end
-  #       end
-  #     end
-  #
-  #     before { expect(eve_alliance).to receive(:destroy!) }
-  #
-  #     specify { expect { subject.import }.not_to raise_error }
-  #   end
-  # end
 end
