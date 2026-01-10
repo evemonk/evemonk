9b2bc4ed85fdf1a1d6bc5dbd4eb223e6f6c300cf
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-10 17:35:06 +0100

    Update

--- spec/requests/characters_controller_spec.rb
+++ spec/requests/characters_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe CharactersController, type: :request do
 
       let!(:character) { create(:character, user: user) }
 
-      before { sign_in(user) }
+      before { sign_in_as(user) }
 
       before { get characters_path }
 
@@ -25,7 +25,7 @@ RSpec.describe CharactersController, type: :request do
 
       it { expect(response).to have_http_status(:found) }
 
-      it { expect(subject).to redirect_to(new_user_session_path) }
+      it { expect(subject).to redirect_to(new_session_path) }
     end
   end
 
@@ -36,7 +36,7 @@ RSpec.describe CharactersController, type: :request do
 
         let(:character) { create(:character, user: user, name: "Johnn Dillinger") }
 
-        before { sign_in(user) }
+        before { sign_in_as(user) }
 
         before { get character_path(character) }
 
@@ -50,7 +50,7 @@ RSpec.describe CharactersController, type: :request do
 
         let(:character) { create(:character) }
 
-        before { sign_in(user) }
+        before { sign_in_as(user) }
 
         before { get character_path(character) }
 
@@ -67,7 +67,7 @@ RSpec.describe CharactersController, type: :request do
 
       it { expect(response).to have_http_status(:found) }
 
-      it { expect(subject).to redirect_to(new_user_session_path) }
+      it { expect(subject).to redirect_to(new_session_path) }
     end
   end
 
@@ -78,7 +78,7 @@ RSpec.describe CharactersController, type: :request do
 
         let(:character) { create(:character, user: user, name: "Johnn Dillinger") }
 
-        before { sign_in(user) }
+        before { sign_in_as(user) }
 
         before do
           #
@@ -113,7 +113,7 @@ RSpec.describe CharactersController, type: :request do
 
         let(:character) { create(:character) }
 
-        before { sign_in(user) }
+        before { sign_in_as(user) }
 
         context "when format html" do
           before { patch character_path(character), params: {format: :html} }
@@ -140,7 +140,7 @@ RSpec.describe CharactersController, type: :request do
 
       it { expect(response).to have_http_status(:found) }
 
-      it { expect(subject).to redirect_to(new_user_session_path) }
+      it { expect(subject).to redirect_to(new_session_path) }
     end
   end
 
@@ -151,7 +151,7 @@ RSpec.describe CharactersController, type: :request do
 
         let!(:character) { create(:character, user: user, name: "Johnn Dillinger") }
 
-        before { sign_in(user) }
+        before { sign_in_as(user) }
 
         context "when format html" do
           before do
@@ -185,7 +185,7 @@ RSpec.describe CharactersController, type: :request do
 
         let(:character) { create(:character) }
 
-        before { sign_in(user) }
+        before { sign_in_as(user) }
 
         context "when format html" do
           before { delete character_path(character), params: {format: :html} }
@@ -216,7 +216,7 @@ RSpec.describe CharactersController, type: :request do
 
       it { expect(response).to have_http_status(:found) }
 
-      it { expect(subject).to redirect_to(new_user_session_path) }
+      it { expect(subject).to redirect_to(new_session_path) }
     end
   end
 end
