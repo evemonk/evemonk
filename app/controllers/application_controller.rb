3197ddb8a1009b0e0cb6504876477f66de68684f
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-10 15:48:27 +0100

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Authentication
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Changes to the importmap will invalidate the etag for HTML responses
+  stale_when_importmap_changes
 
   include Pundit::Authorization
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", :as => :rails_health_check
 
+  resource :session
+
+  resources :passwords, param: :token
+
   if Rails.env.local?
     mount Lookbook::Engine, at: "/lookbook"
   end
