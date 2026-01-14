6d4d219b777eb91393290843219f7bfe1ece080b
Igor Zubkov <igor.zubkov@gmail.com>
2026-01-15 00:24:58 +0100

    Update

--- spec/requests/registrations_controller_spec.rb
+++ spec/requests/registrations_controller_spec.rb
@@ -3,4 +3,11 @@
 require "rails_helper"
 
 RSpec.describe RegistrationsController, type: :request do
+  describe "#new" do
+    before { get new_session_path }
+
+    it { expect(response).to have_http_status(:ok) }
+  end
+
+  # TODO: more
 end
