074ab3e5d02c748c59eb7bad818837bd642e78df
Igor Zubkov <igor.zubkov@gmail.com>
2026-03-22 20:23:02 +0100

    Update

--- spec/requests/blueprints_controller_spec.rb
+++ spec/requests/blueprints_controller_spec.rb
@@ -43,7 +43,41 @@ RSpec.describe BlueprintsController, type: :request do
 
   describe "#show" do
     context "when user signed in" do
+      context "when user is own character" do
+        let(:user) { create(:user) }
 
+        let(:character) { create(:character, user: user) }
+
+        let(:type) { create(:eve_type) }
+
+        let(:blueprint) { create(:character_blueprint, character: character, item_id: type.id) }
+
+        before { sign_in(user) }
+
+        before { get character_blueprint_path(character, id: blueprint.item_id) }
+
+        it { expect(response).to have_http_status(:ok) }
+
+        it { expect(response.body).to include("Blueprints") }
+      end
+
+      context "when user is not own character" do
+        let(:user) { create(:user) }
+
+        let(:character) { create(:character, user: user) }
+
+        let(:type) { create(:eve_type) }
+
+        let(:character) { create(:character) }
+
+        let(:blueprint) { create(:character_blueprint, character: character, item_id: type.id) }
+
+        before { sign_in(user) }
+
+        before { get character_blueprint_path(character, blueprint) }
+
+        it { expect(response).to have_http_status(:not_found) }
+      end
     end
 
     context "when user not signed in" do
