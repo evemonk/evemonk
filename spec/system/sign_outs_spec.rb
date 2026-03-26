45ca3a4897a6ced644d34ada225bc9f67463e102
Igor Zubkov <me@biow0lf.pp.ua>
2026-03-26 19:14:28 +0100

    Updates (#7687)

--- spec/system/sign_outs_spec.rb
+++ spec/system/sign_outs_spec.rb
@@ -3,8 +3,6 @@
 require "rails_helper"
 
 RSpec.describe "Sign out features", type: :system do
-  before { driven_by(:selenium_chrome_headless) }
-
   it "when user successfully sign in and sign out" do
     create(:user, email: "me@example.com", password: "eidii7EeooVe8ahk", locale: "english")
 
