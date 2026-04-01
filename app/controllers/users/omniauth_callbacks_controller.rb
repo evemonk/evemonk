ae0e61f0414a807e21f22d6c59221ac1d6ec864a
Igor Zubkov <igor.zubkov@gmail.com>
2026-04-01 23:07:49 +0200

    Update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,13 +3,41 @@
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def eve_online_sso
-      @user = User.from_omniauth(request.env["omniauth.auth"])
+      # binding.pry
+
+      if user_signed_in?
+
+      else
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+
+        sign_in_and_redirect @user # sign_in_and_redirect is an OAuth method
+      end
+
+      # @user = User.from_omniauth(request.env["omniauth.auth"])
 
       # service = EveOnlineCallbackService.new(request: request, user: current_user)
       #
       # service.save!
 
-      sign_in_and_redirect @user # sign_in_and_redirect is an OAuth method
+      # sign_in_and_redirect @user # sign_in_and_redirect is an OAuth method
+    end
+
+    private
+
+    def service_unavailable_handle
+      redirect_to characters_path, alert: t(".service_unavailable")
+    end
+
+    def internal_server_error_handle
+      redirect_to characters_path, alert: t(".internal_server_error")
+    end
+
+    def bad_gateway_handle
+      redirect_to characters_path, alert: t(".bad_gateway")
+    end
+
+    def timeout_handle
+      redirect_to characters_path, alert: t(".timeout")
     end
   end
 end
