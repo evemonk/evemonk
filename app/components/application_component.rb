bfccbf5cba88593586919eb94c5d60db50236cc5
Igor Zubkov <igor.zubkov@gmail.com>
2026-04-20 23:40:11 +0200

    Disable rubocop warning for base

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-class ApplicationComponent < ViewComponent::Base
+class ApplicationComponent < ViewComponent::Base # rubocop:disable ViewComponent/MissingPreview
 end
