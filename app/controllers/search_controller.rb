088c07f67e25bcc01846622bdc762ec19c634d30
Igor Zubkov <igor.zubkov@gmail.com>
2026-05-04 15:12:47 +0200

    Update

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,17 +4,15 @@ class SearchController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    # @alliances = policy_scope(Eve::SearchAlliancesQuery.new(params[:q]).query)
-    #   .select(:id, :name, :ticker, :corporations_count, :characters_count)
-    #   .page(1)
-    #
+    @alliances = Eve::SearchAlliancesQuery.new(params[:q], policy_scope(Eve::Alliance.all))
+      .query
+      .page(1)
+
     # @corporations = policy_scope(Eve::SearchCorporationsQuery.new(params[:q]).query)
-    #   .select(:id, :name, :member_count)
     #   .page(1)
-    #
+
     # @characters = policy_scope(Eve::SearchCharactersQuery.new(params[:q]).query).select(:id, :name).page(1)
 
-    @alliances = Eve::Alliance.search(params[:q]).page(1)
     @corporations = Eve::Corporation.search(params[:q]).page(1)
     @characters = Eve::Character.search(params[:q]).page(1)
 
