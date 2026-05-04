90cc59a5224e9884b2e21b773bbf4125c08dc5cd
Igor Zubkov <igor.zubkov@gmail.com>
2026-05-04 15:39:09 +0200

    Update

--- app/queries/eve/search_manufacturing_items_query.rb
+++ app/queries/eve/search_manufacturing_items_query.rb
@@ -2,16 +2,16 @@
 
 module Eve
   class SearchManufacturingItemsQuery < BaseQuery
-    attr_reader :search, :scope
+    attr_reader :q, :scope
 
-    def initialize(search = nil, scope = Eve::Type.published.manufacturing_items)
-      @search = search
+    def initialize(q = nil, scope = Eve::Type.published.manufacturing_items)
+      @q = q
       @scope = scope
     end
 
     def query
-      if search.present?
-        scope.search_by_name(search)
+      if q.present?
+        scope.search(q)
       else
         scope.none
       end
