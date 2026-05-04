40df602bbab1bf9827cdb9e4a6f9b24039d4c3fb
Igor Zubkov <igor.zubkov@gmail.com>
2026-05-04 20:32:51 +0200

    Fix

--- db/migrate/20260504151915_drop_index_eve_characters_on_name.rb
+++ db/migrate/20260504151915_drop_index_eve_characters_on_name.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class DropIndexEveCharactersOnName < ActiveRecord::Migration[8.1]
+  disable_ddl_transaction!
+
   def change
-    remove_index :eve_characters, :name, name: :index_eve_characters_on_name, opclass: :gin_trgm_ops, using: :gin
+    remove_index :eve_characters, :name, name: :index_eve_characters_on_name, opclass: :gin_trgm_ops, using: :gin, algorithm: :concurrently
   end
 end
--- db/migrate/20260504162913_drop_index_eve_corporations_on_name_and_ticker.rb
+++ db/migrate/20260504162913_drop_index_eve_corporations_on_name_and_ticker.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class DropIndexEveCorporationsOnNameAndTicker < ActiveRecord::Migration[8.1]
+  disable_ddl_transaction!
+
   def change
-    remove_index :eve_corporations, [:name, :ticker], name: "index_eve_corporations_on_name_and_ticker", opclass: :gin_trgm_ops, using: :gin
+    remove_index :eve_corporations, [:name, :ticker], name: "index_eve_corporations_on_name_and_ticker", opclass: :gin_trgm_ops, using: :gin, algorithm: :concurrently
   end
 end
