a4cae5a214d6141617306f3e320595e3b33fbe9e
Igor Zubkov <igor.zubkov@gmail.com>
2026-05-04 19:29:49 +0200

    Drop index

---
+++ db/migrate/20260504172806_drop_index_eve_types_on_name_en.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class DropIndexEveTypesOnNameEn < ActiveRecord::Migration[8.1]
+  def change
+    remove_index :eve_types, :name_en, name: "index_eve_types_on_name_en", opclass: :gin_trgm_ops, using: :gin
+  end
+end
