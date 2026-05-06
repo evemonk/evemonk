ba7541821012f0e2cd3ceb39b5f3a2b59ae9ac08
Igor Zubkov <igor.zubkov@gmail.com>
2026-05-06 16:26:41 +0200

    Update

--- spec/tasks/maintenance/meilisearch_reindex_alliances_task_spec.rb
+++ spec/tasks/maintenance/meilisearch_reindex_alliances_task_spec.rb
@@ -6,14 +6,8 @@ RSpec.describe Maintenance::MeilisearchReindexAlliancesTask do
   describe "#process" do
     let!(:alliance) { create(:eve_alliance) }
 
-    before { expect(Eve::Alliance).to receive(:clear_index!).and_call_original }
+    before { expect(Meilisearch::Eve::ReindexAllianceJob).to receive(:perform_later).with(alliance.id, false) }
 
-    before { expect(Meilisearch::Rails::MSJob).to receive(:perform_later).with(alliance, :index!).and_call_original }
-
-    # Meilisearch::Rails::MSJob.perform_later(alliance, :index!)
-    #
-    # before { expect(Eve::Alliance).to receive(:reindex!).with(100).and_call_original }
-
-    specify { expect { subject.process }.not_to raise_error }
+    specify { expect { subject.process(alliance) }.not_to raise_error }
   end
 end
