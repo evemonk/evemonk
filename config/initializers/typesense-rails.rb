a4a91b60f3641a00d06f40482fc2f9c36ea5c3d4
Igor Zubkov <igor.zubkov@gmail.com>
2026-05-25 19:29:01 +0200

    Remove meilisearch

--- config/initializers/typesense-rails.rb
+++ config/initializers/typesense-rails.rb
@@ -3,12 +3,12 @@
 Typesense.configuration = {
   nodes: [
     {
-      host: ENV.fetch("TYPESEARCH_HOST", "localhost"),
-      port: ENV.fetch("TYPESEARCH_PORT", "8108"),
-      protocol: ENV.fetch("TYPESEARCH_PROTOCOL", "http")
+      host: ENV.fetch("TYPESENSE_HOST", "localhost"),
+      port: ENV.fetch("TYPESENSE_PORT", "8108"),
+      protocol: ENV.fetch("TYPESENSE_PROTOCOL", "http")
     }
   ],
-  api_key: ENV.fetch("TYPESEARCH_API_KEY", "API_KEY"),
+  api_key: ENV.fetch("TYPESENSE_API_KEY", "API_KEY"),
   connection_timeout_seconds: 2,
   log_level: :info, # Messages below this level will be silenced. One of (:debug, :info, :warn, :error, :fatal),
   pagination_backend: :kaminari
