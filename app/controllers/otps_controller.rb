5e3aaceef717ef68ed5f673aefe526a54fe7c2a0
Igor Zubkov <igor.zubkov@gmail.com>
2026-08-09 23:10:17 +0200

    Update

--- app/controllers/otps_controller.rb
+++ app/controllers/otps_controller.rb
@@ -2,6 +2,8 @@
 
 class OtpsController < ApplicationController
   def create
+    @codes = current_user.generate_otp_backup_codes!
+
     current_user.otp_secret = User.generate_otp_secret
     current_user.otp_required_for_login = true
     current_user.save!
--- app/controllers/two_factors_controller.rb
+++ app/controllers/two_factors_controller.rb
@@ -4,11 +4,11 @@ class TwoFactorsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @codes = current_user.generate_otp_backup_codes!
+
     current_user.otp_secret = User.generate_otp_secret
     current_user.otp_required_for_login = true
     current_user.save!
-
-    @codes = current_user.generate_otp_backup_codes!
   end
 
   def destroy
