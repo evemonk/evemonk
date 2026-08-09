6ffdf3a2ed8f10361d269e7e34393e3be420cfac
Igor Zubkov <igor.zubkov@gmail.com>
2026-08-09 20:06:20 +0200

    Fix spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User, type: :model do
   it { expect(subject).to be_an(ApplicationRecord) }
 
   it {
-    expect(described_class.devise_modules).to eq([:database_authenticatable,
+    expect(described_class.devise_modules).to eq([:two_factor_authenticatable,
       :rememberable,
       :recoverable,
       :registerable,
