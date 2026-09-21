ea98a04ffcabf4fb5d8321dccfe82da1c215b52a
Igor Zubkov <igor.zubkov@gmail.com>
2026-09-21 19:50:32 +0200

    Cleanup

--- spec/system/maintenance_tasks_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "rails_helper"
-
-RSpec.describe "Maintenance tasks", type: :system do
-  it "should render page" do
-    visit maintenance_tasks_path
-  end
-end
